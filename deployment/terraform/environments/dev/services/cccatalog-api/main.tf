0ce869c40ee84b41c352d5e087dca53ecc601ee5
Alden Page <aldenstpage@gmail.com>
2018-06-01 17:09:05 -0400

    Automate load balancer URL refresh

--- deployment/terraform/environments/dev/services/cccatalog-api/main.tf
+++ deployment/terraform/environments/dev/services/cccatalog-api/main.tf
@@ -18,7 +18,7 @@ module "cccatalog-api" {
   max_size                  = 5
   instance_type             = "t2.micro"
   enable_monitoring         = false
-  git_revision              = "853ed6ac069ec4b9f3a0d0d1062838434bef3e7d"
+  git_revision              = "b13b70f3295fdef604d68dd46bea00368ee34bc9"
 
   # Environment-specific variables
   database_host             = "openledger-db-dev3-nvirginia.ctypbfibkuqv.us-east-1.rds.amazonaws.com"
--- deployment/terraform/modules/services/cccatalog-api/init.tpl
+++ deployment/terraform/modules/services/cccatalog-api/init.tpl
@@ -8,6 +8,7 @@ export DJANGO_DATABASE_PASSWORD="${database_password}"
 export DJANGO_DATABASE_HOST="${database_host}"
 export DJANGO_DEBUG_ENABLED="${django_debug_enabled}"
 export DJANGO_SECRET_KEY="${django_secret_key}"
+export LOAD_BALANCER_URL="${load_balancer_url}"
 
 # Install dependencies
 yum -y install git python3-3.7.0-0.16.b3.amzn2.0.1 gcc python3-setuptools python3-devel postgresql-devel
