d98473695e9942dba1a573a5e8b3ad420cb456d1
Alden Page <aldenstpage@gmail.com>
2018-06-01 20:18:58 -0400

    Fixing bad security group egress rule

--- deployment/terraform/modules/services/cccatalog-api/main.tf
+++ deployment/terraform/modules/services/cccatalog-api/main.tf
@@ -160,6 +160,13 @@ resource "aws_security_group" "cccatalog-alb-sg" {
   vpc_id = "vpc-d6b1bfb4"
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
