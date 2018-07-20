c69c0bda745e2dcafd40160ce5828998deecb344
Alden Page <aldenstpage@gmail.com>
2018-07-20 11:30:33 -0400

    [#58] Allow cross-origin requests from anywhere and everywhere

--- cccatalog-api/cccatalog/settings.py
+++ cccatalog-api/cccatalog/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'social_django',
     'rest_framework',
     'rest_framework_social_oauth2',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
@@ -155,6 +157,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Allow anybody to access the API from any domain
+CORS_ORIGIN_ALLOW_ALL = True
 
 # API specific configuration
 SOCIAL_AUTH_POSTGRES_JSONFIELD = True
