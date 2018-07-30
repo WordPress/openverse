7f8bb02cb6b9eb103fae578d840713ff0730a354
Alden Page <aldenstpage@gmail.com>
2018-07-30 14:29:06 -0400

    [#56] Remember recent IPs to prevent people from gaming our page view statistics.

--- cccatalog-api/cccatalog/api/utils/view_count.py
+++ cccatalog-api/cccatalog/api/utils/view_count.py
@@ -24,34 +24,34 @@ def track_model_views(model):
     """
     def func_wrap(django_view):
         def decorated(*args, **kwargs):
-            request = kwargs.get('request')
+            request = args[1]
             model_id = kwargs.get('id')
-            view_count = _increment_viewcount(model, model_id)
+            view_count = _increment_viewcount(model, model_id, request)
             return django_view(*args, **kwargs, view_count=view_count)
         return decorated
     return func_wrap
 
 
-def _increment_viewcount(model, model_id: int):
+def _increment_viewcount(model, model_id: int, request):
     """
     Increment the viewcount. Cache the result for at least six hours. When
     the key expires, it is evicted from the cache and stored in Postgres by a
     cache persistence worker.
 
     This strategy minimizes write load on the database while providing
-    accurate view statistics. The cache is also kept small as infrequently used
-    data is evicted, reducing the need for memory.
+    accurate real-time view statistics. The cache is also kept small as
+    infrequently used data is evicted, reducing the need for memory.
 
     :return: The view count AFTER incrementing.
     """
     object_key = model.__name__ + ':' + str(model_id)
 
     redis = get_redis_connection('traffic_stats')
-    view_count = redis.get(object_key)
+    view_count = int(redis.get(object_key))
     if not view_count:
-        # Cache miss. Get the view count from the database.
+        # Cache miss. Get the view count from the database and cache it.
         try:
-            view_count = model.objects.get(id=model_id).view_count
+            view_count = int(model.objects.get(id=model_id).view_count)
         except ObjectDoesNotExist:
             # If the object doesn't even exist in the database, don't track it.
             return
@@ -59,11 +59,16 @@ def _increment_viewcount(model, model_id: int):
             log.error('Cannot track model ' + model.__name__ +
                       'because it has no view_count field. Views for this ' +
                       'model will be lost.')
-            return
-        redis.set(object_key, view_count + 1)
-    else:
-        # Cache hit.
+            return -1
+        redis.set(object_key, view_count)
+
+    # Only increment the view count if the user has not visited the resource in
+    # the last few minutes. Prevents metrics gaming shenanigans.
+    ip = _get_user_ip(request)
+    if not _is_recent_visitor(ip, object_key):
         redis.incr(object_key)
+        view_count += 1
+    _mark_recent_visitor(ip, object_key)
 
     # Update the last access time of the model.
     # Store in a sorted set so we can easily find the oldest keys.
@@ -74,17 +79,57 @@ def _increment_viewcount(model, model_id: int):
     return view_count
 
 
-def _is_recent_visitor(ip, recent_seconds):
+def _get_user_ip(request):
     """
-    Return True if a given `ip` was seen in the last `recent_seconds`.
+    Read request headers to find the correct IP address.
+
+    It is assumed that X-Forwarded-For has been sanitized by the load balancer
+    and thus cannot be rewritten by malicious users.
+
+    :param request: A Django request object.
+    :return: An IP address.
+    """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def _is_recent_visitor(ip, object_key):
+    """
+    Return True if a given `ip` was seen accessing an `object_key` in the last
+    10 hours.
+
+    Cleans out old IPs.
     :return: bool
     """
-    pass
+    redis = get_redis_connection('traffic_stats')
+    recent_ips_key = object_key + '-recent-ips'
+    # Delete all IPs that haven't visited the resource in the last 10 minutes.
+    ten_hours_ago = time.time() - (60 * 60 * 10)
+    redis.zremrangebyscore(recent_ips_key, '-inf', ten_hours_ago)
+    is_recent = bool(redis.zscore(recent_ips_key, ip))
+    return is_recent
 
 
-def _mark_recent_visitor(ip, recent_seconds):
+def _mark_recent_visitor(ip, object_key):
     """
-    Mark a given IP as a recent visitor. If it has already been marked, reset
-    its expiration time.
+    Mark a given IP as a recent visitor to a specific resource (such as an
+    Image).
+
+    A recent visitor is an IP address that has hit a resource in the last 10
+    hours.
     """
-    pass
+    redis = get_redis_connection('traffic_stats')
+    recent_ips_key = object_key + '-recent-ips'
+    current_timestamp = time.time()
+    redis.execute_command(
+        'ZADD {} {} {}'.format(recent_ips_key, current_timestamp, ip)
+    )
+
+    # The visitor set will be automatically deleted if nobody has visited the
+    # resource in the last 24 hours.
+    one_day_seconds = 60 * 60 * 24
+    redis.expire(recent_ips_key, one_day_seconds)
