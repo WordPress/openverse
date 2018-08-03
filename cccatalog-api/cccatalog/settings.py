17d844d2580b5efa333eed448b3be1cbe3cf56b1
Alden Page <aldenstpage@gmail.com>
2018-08-03 13:26:51 -0400

    [#44] Remove protocol from whitelisted app

--- cccatalog-api/cccatalog/settings.py
+++ cccatalog-api/cccatalog/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('LOAD_BALANCER_URL'),
 SHORT_URL_WHITELIST = {
     'api-dev.creativecommons.engineering',
     'api.creativecommons.engineering',
-    'http://ccccatalog.herokuapp.com'
+    'ccccatalog.herokuapp.com'
 }
 SHORT_URL_PATH_WHITELIST = ['/list', '/image/']
 
