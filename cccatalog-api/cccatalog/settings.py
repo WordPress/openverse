82d22307ef6e178bf9271707d0643857a2277fc0
Alden Page <aldenstpage@gmail.com>
2018-08-15 11:11:30 -0400

    [#108] Enable global rate limiting

--- cccatalog-api/cccatalog/settings.py
+++ cccatalog-api/cccatalog/settings.py
@@ -87,7 +87,15 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '200/minute',
+        'user': '200/minute'
+    },
 }
 
 CACHES = {
