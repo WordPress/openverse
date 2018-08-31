00eb0cbdb474a86a6f9bd53ec8733d420b40738a
Alden Page <aldenstpage@gmail.com>
2018-08-31 14:37:15 -0400

    [#116] [#115] Allow anonymous updates and deletions of lists. Pass around an auth token.

--- cccatalog-api/cccatalog/api/serializers/list_serializers.py
+++ cccatalog-api/cccatalog/api/serializers/list_serializers.py
@@ -1,18 +1,12 @@
 from rest_framework import serializers
 from cccatalog.api.models import ImageList
 from cccatalog.api.serializers.image_serializers import ImageDetailSerializer
+import secrets
 
 
-class ImageListCreateSerializer(serializers.ModelSerializer):
-    """
-    Responsible for parsing POST JSON body and persisting to the database.
-    """
-    lookup_field = 'id'
-    id = serializers.ReadOnlyField()
-
+class ImageListBaseSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ImageList
-        fields = ('id', 'title', 'images')
+        fields = ('images',)
 
     def validate_images(self, image_keys):
         if len(image_keys) > 500:
@@ -21,15 +15,28 @@ class ImageListCreateSerializer(serializers.ModelSerializer):
             )
         return image_keys
 
+
+class ImageListCreateSerializer(ImageListBaseSerializer):
+    """
+    Responsible for parsing POST JSON body and persisting to the database.
+    """
+    lookup_field = 'id'
+    id = serializers.ReadOnlyField()
+    auth = serializers.ReadOnlyField()
+
+    class Meta:
+        model = ImageList
+        fields = ('id', 'title', 'images', 'auth')
+
     def save(self):
         title = self.validated_data['title']
         images = self.validated_data['images']
-        image_list = ImageList(title=title)
+        auth = secrets.token_urlsafe(48)
+        image_list = ImageList(title=title, auth=auth)
         image_list.save()
-        _id = image_list.slug
         image_list.images.add(*images)
 
-        return _id
+        return image_list
 
 
 class ImageListResponseSerializer(serializers.Serializer):
@@ -39,3 +46,11 @@ class ImageListResponseSerializer(serializers.Serializer):
     lookup_field = 'slug'
     id = serializers.ReadOnlyField()
     images = ImageDetailSerializer(many=True)
+
+
+class ImageListUpdateSerializer(ImageListBaseSerializer):
+    lookup_field = 'id'
+
+    class Meta:
+        model = ImageList
+        fields = ('images',)
--- cccatalog-api/cccatalog/api/views/list_views.py
+++ cccatalog-api/cccatalog/api/views/list_views.py
@@ -1,5 +1,6 @@
-from cccatalog.api.serializers.list_serializers import\
-    ImageListCreateSerializer, ImageListResponseSerializer
+from cccatalog.api.serializers.list_serializers import \
+    ImageListCreateSerializer, ImageListResponseSerializer, \
+    ImageListUpdateSerializer
 from django.forms.models import model_to_dict
 from cccatalog.api.models import ImageList
 from cccatalog.api.utils.throttle import PostRequestThrottler
@@ -23,6 +24,11 @@ class _CreateResponse(serializers.Serializer):
         view_name='list-detail',
         read_only=True,
         help_text="The URL of the new list."
+    )
+    auth = serializers.CharField(
+        help_text="A 64 character authorization code used to prove ownership of"
+                  " a list. Add this to the authorization header when updating "
+                  "or deleting lists."
     )
 
 
@@ -51,12 +57,13 @@ class CreateList(_List):
                 data=serialized.errors
             )
 
-        list_id = serialized.save()
-        url = request.build_absolute_uri(reverse('list-detail', [list_id]))
+        _list = serialized.save()
+        url = request.build_absolute_uri(reverse('list-detail', [_list.slug]))
         return Response(
             status=201,
             data={
-                'url': url
+                'url': url,
+                'auth': _list.auth
             }
         )
 
@@ -77,5 +84,55 @@ class ListDetail(_List, RetrieveModelMixin):
             'id': slug,
             'images': [model_to_dict(x) for x in _list.images.all()]
         }
-        serialized = ImageListResponseSerializer(data=resolved)
-        return Response(status=200, data=serialized.initial_data)
+        return Response(status=200, data=resolved)
+
+    @staticmethod
+    def _authenticated(list_model, request):
+        return list_model.auth == \
+               request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+
+    @swagger_auto_schema(operation_id="list_delete",
+                         security=[
+                             {
+                                 "anonymous_list": {
+                                     "type": "basic"
+                                 }
+                             }
+                         ],
+                         responses={
+                             204: '',
+                             403: 'Forbidden',
+                             404: 'Not Found'
+                         })
+    def delete(self, request, slug, format=None):
+        try:
+            _list = ImageList.objects.get(slug=slug)
+        except ImageList.DoesNotExist:
+            return Response(status=404)
+        if self._authenticated(_list, request):
+            _list.delete()
+            return Response(status=204)
+        else:
+            return Response(status=403)
+
+    def put(self, request, slug, format=None):
+        """ Update the contents of a list. """
+        serialized = ImageListUpdateSerializer(data=request.data)
+        if not serialized.is_valid():
+            return Response(
+                status=400,
+                data=serialized.errors
+            )
+
+        try:
+            _list = ImageList.objects.get(slug=slug)
+        except ImageList.DoesNotExist:
+            return Response(status=404)
+        supplied_auth = request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+        if self._authenticated(_list, request):
+            _list.images.clear()
+            _list.images.add(*serialized.validated_data['images'])
+            return Response(status=204)
+        else:
+            return Response(status=403)
+
