77ce097af03e84fae01c037adb98fdefc33e37f3
sebworks <sebworks@yahoo.com>
2018-09-05 09:23:32 -0400

    Modifying image list data service

--- src/api/ShareListService.js
+++ src/api/ShareListService.js
@@ -9,6 +9,28 @@ const ShareListService = {
     return ApiService.post('/list', { title: params.listTitle, images: params.images });
   },
   /**
+   * Implements an endpoint to create a shortened list url.
+   */
+  createShortenedListURL(params) {
+    return ApiService.post('/link_create', { url: params.url });
+  },
+  /**
+   * Implements an endpoint to edit a list.
+   */
+  editList(params) {
+    ApiService.setHeader({ auth: params.auth });
+
+    return ApiService.update('/list_update', params.id, params.ids);
+  },
+  /**
+   * Implements an endpoint to delete a list.
+   */
+  deleteList(params) {
+    ApiService.setHeader({ auth: params.auth });
+
+    return ApiService.delete('/list_delete', params.id);
+  },
+  /**
    * Implements an endpoint to get a list based on ID.
   */
   getList(params) {
