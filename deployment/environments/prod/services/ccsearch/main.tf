82b56c3d4c398efe569ef43c273e7329070ea06c
Alden Page <aldenstpage@gmail.com>
2018-10-03 09:56:39 -0400

    Release ccsearch 1.4

--- deployment/environments/prod/services/ccsearch/main.tf
+++ deployment/environments/prod/services/ccsearch/main.tf
@@ -7,7 +7,7 @@ module "ccsearch" {
 
   vpc_id                    = "vpc-b741b4cc"
   environment               = "-prod"
-  git_revision              = "247cedd5b68b15ce4c2b660840dc3d62063c44ba"
+  git_revision              = "c889f9e68ea0823e3b22aaa9a7bc4f24e4402833"
   instance_type             = "t2.small"
   api_url                   = "https://api.creativecommons.engineering"
 }
