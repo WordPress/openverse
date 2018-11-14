0627513a029c7e7babcb1c30023e90e62dd4f9ff
Alden Page <aldenstpage@gmail.com>
2018-11-14 11:43:00 -0500

    Add test data; add docker-external kafka listener

--- ccbot/crawl_planner/settings.py
+++ ccbot/crawl_planner/settings.py
@@ -6,7 +6,8 @@ DATABASE_PORT = int(os.getenv('CCCATALOG_DATABASE_PORT', 5432))
 DATABASE_PASSWORD = os.getenv('CCCATALOG_DATABASE_PASSWORD', 'deploy')
 CCCATALOG_API_URL = os.getenv('CCCATALOG_API_URL', 'http://localhost:8000')
 
-# Request-per-second limits for each rate limit strategy.
+# Request-per-second limits for each rate limit strategy. If you change these
+# numbers, you must regenerate the crawl plan.
 VERY_LIGHT_RPS = 1
 LIGHT_RPS = 3
 MODERATE_RPS = 4
@@ -19,5 +20,5 @@ VERY_HEAVY_RPS = 20
 # Crawl executor settings
 CLUSTER_REST_URL = os.getenv('CLUSTER_REST_HOST', 'http://localhost:5343')
 # Kafka brokers
-CLUSTER_BROKER_HOSTS = os.getenv('CLUSTER_KAFKA_HOST', 'http://localhost:9092')
+CLUSTER_BROKER_HOSTS = os.getenv('CLUSTER_KAFKA_HOST', 'localhost:9092')
 CLUSTER_INCOMING_TOPIC = os.getenv('CLUSTER_INCOMING_TOPIC', 'ccbot.incoming')
---
+++ ccbot/crawl_planner/test/README.md
@@ -0,0 +1,3 @@
+Requires Twisted for Python 2.7 and a running instance of Scrapy Cluster.
+
+Ensures that rate limits are obeyed by the crawler.
