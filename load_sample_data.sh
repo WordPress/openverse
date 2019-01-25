02505dc6444d2ccb2717df54049bfbc585747e9c
Alden Page <aldenstpage@gmail.com>
2019-01-25 16:49:22 -0500

    Set container name in load_sample_data.sh via environment variable to work around container naming inconsistency that only shows up in the Travis build environment

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+CCCAPI_CONTAINER_NAME="${CCCAPI_CONTAINER_NAME:-cccatalogapi_web_1}"
 # Set up databases
-docker exec -ti cccatalog_api /bin/bash -c 'python3 manage.py makemigrations'
-docker exec -ti cccatalog_api /bin/bash -c 'python3 manage.py migrate'
+docker exec -ti $CCCAPI_CONTAINER_NAME /bin/bash -c 'python3 manage.py makemigrations'
+docker exec -ti $CCCAPI_CONTAINER_NAME /bin/bash -c 'python3 manage.py migrate'
 PGPASSWORD=deploy pg_dump -s -t image -U deploy -d openledger -h localhost -p 5432 | PGPASSWORD=deploy psql -U deploy -d openledger -p 5433 -h localhost
 # Load sample data
 PGPASSWORD=deploy psql -U deploy -d openledger -h localhost -p 5432 -c "INSERT INTO content_provider (created_on, provider_identifier, provider_name, domain_name, filter_content) VALUES (now(), 'flickr', 'Flickr', 'https://www.flickr.com', false);"
