ff46a09a7d069a030357867baabfec7787897fdd
Alden Page <aldenstpage@gmail.com>
2019-01-25 15:28:29 -0500

    Fix mock content_provider entry being inserted into upstream database instead of API database

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -4,7 +4,7 @@ docker exec -ti cccatalog-api_web_1 /bin/bash -c 'python3 manage.py makemigratio
 docker exec -ti cccatalog-api_web_1 /bin/bash -c 'python3 manage.py migrate'
 PGPASSWORD=deploy pg_dump -s -t image -U deploy -d openledger -h localhost -p 5432 | PGPASSWORD=deploy psql -U deploy -d openledger -p 5433 -h localhost
 # Load sample data
-PGPASSWORD=deploy psql -U deploy -d openledger -h localhost -p 5433 -c "INSERT INTO content_provider (created_on, provider_identifier, provider_name, domain_name, filter_content) VALUES (now(), 'flickr', 'Flickr', 'https://www.flickr.com', false);"
+PGPASSWORD=deploy psql -U deploy -d openledger -h localhost -p 5432 -c "INSERT INTO content_provider (created_on, provider_identifier, provider_name, domain_name, filter_content) VALUES (now(), 'flickr', 'Flickr', 'https://www.flickr.com', false);"
 PGPASSWORD=deploy psql -U deploy -d openledger -h localhost -p 5433 -c "\copy image (id,created_on,updated_on,provider,source,foreign_identifier,foreign_landing_url,url,thumbnail,width,height,filesize,license,license_version,creator,creator_url,title,tags_list,last_synced_with_source,removed_from_source,meta_data,view_count,tags,watermarked,identifier) from 'sample_data.csv' with csv header"
 # Ingest and index the data
 curl -XPOST localhost:8001/task -H "Content-Type: application/json" -d '{"model": "image", "action": "INGEST_UPSTREAM"}'
