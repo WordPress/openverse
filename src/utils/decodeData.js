5dd4b41178f378249df4a503b04fd26d4fedab27
VatsalSin <vatsal.eliot@gmail.com>
2019-03-07 22:52:06 +0530

    Update

--- src/utils/decodeData.js
+++ src/utils/decodeData.js
@@ -1,8 +1,10 @@
 export default function decodeData(data) {
   if (data) {
-    const regex = /\\x([\d\w]{2})/gi;
-    const temp = data.replace(regex, (match, grp) => String.fromCharCode(parseInt(grp, 16)));
-    const res = decodeURI(temp);
+    const regexASCII = /\\x([\d\w]{2})/gi;
+    const ascii = data.replace(regexASCII, (match, grp) => String.fromCharCode(parseInt(grp, 16)));
+    const regexUni = /\\u([\d\w]{4})/gi;
+    const uni = ascii.replace(regexUni, (match, grp) => String.fromCharCode(parseInt(grp, 16)));
+    const res = decodeURI(uni);
     return res;
   }
   return '';
--- test/unit/specs/utils/decodeData.spec.js
+++ test/unit/specs/utils/decodeData.spec.js
@@ -14,9 +14,21 @@ describe('decodeData', () => {
     expect(decodeData(data)).toEqual(""); 
   });
 
-  it('decodes unicode strings', () => {
+  it('returns decoded ASCII hexacode strings', () => {
     const data = 's\\xe9'
 
     expect(decodeData(data)).toBe('s\xe9');
   });
+
+  it('returns decoded unicode strings', () => {
+    const data = 's\\u1234'
+
+    expect(decodeData(data)).toBe('s\u1234');
+  });
+
+  it('returns decoded strings consisting of both unicode and ASCII hexacode characters', () => {
+    const data = 's\\u1234\xe9'
+
+    expect(decodeData(data)).toBe('s\u1234\xe9');
+  });  
 });
