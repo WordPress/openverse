9b2328fca73422d1becad037c421f911d423dc4f
Alden Page <alden@creativecommons.org>
2019-03-13 10:19:58 -0400

    Run tests without TLS; this causes headaches without valid local certs, which I'm not going to bother with

--- cccatalog-api/test/run_test.sh
+++ cccatalog-api/test/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Local environments don't have valid certificates; suppress this warning.
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
-export INTEGRATION_TEST_URL="https://localhost:8000"
+export INTEGRATION_TEST_URL="http://localhost:8000"
 pytest -s
