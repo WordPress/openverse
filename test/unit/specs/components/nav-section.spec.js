128c0b5a30894cf62549cb199ad9eceb226ab51a
VatsalSin <vatsal.eliot@gmail.com>
2019-03-16 03:54:28 +0530

    Fix top nav search box

--- test/unit/specs/components/nav-section.spec.js
+++ test/unit/specs/components/nav-section.spec.js
@@ -1,9 +1,36 @@
 import NavSection from '@/components/NavSection';
 import render from '../../test-utils/render';
+import { SET_QUERY } from '@/store/mutation-types';
+
 
 describe('NavSection', () => {
   it('should render correct contents', () => {
     const wrapper = render(NavSection);
     expect(wrapper.find('nav').vm).toBeDefined();
+  });
+
+  it('commits a mutation when the form is submitted', () => {
+    const storeMock = {		
+      dispatch: jest.fn(),
+      commit: jest.fn(),
+      state: {
+        shareLists: {
+          length: 2
+        }
+      },
+    }
+    const opts = {
+      propsData: {
+        fixedNav: null,
+        showNavSearch: 'true',
+      },
+      mocks: {
+        $store: storeMock,
+      },
+    };
+    const wrapper = render(NavSection, opts);
+    wrapper.setData({ form: { searchTerm:'foo' } });
+    wrapper.find('.hero_search-form').trigger('submit');
+    expect(storeMock.commit).toHaveBeenCalledWith(SET_QUERY, { query: { q: 'foo' }, shouldNavigate:true});
   });
 });
