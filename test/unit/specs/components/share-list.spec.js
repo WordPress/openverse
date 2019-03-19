8550727903bc947e75c360332710db06612e7f55
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-03-20 02:29:14 +0530

    remove test for share-list

--- test/unit/specs/components/share-list.spec.js
+++
@@ -1,9 +0,0 @@
-import ShareList from '@/components/ShareList';
-import render from '../../test-utils/render';
-
-describe('ShareList', () => {
-  it('should render correct contents', () => {
-    const wrapper = render(ShareList);
-    expect(wrapper.find({ name: 'share-list' }).element).toBeDefined();
-  });
-});
