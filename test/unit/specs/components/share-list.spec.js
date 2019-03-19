ee70caead2d1cadd880b728ac2b31b5a24d87545
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-03-19 19:04:07 +0530

    modify test for ShareList

--- test/unit/specs/components/share-list.spec.js
+++ test/unit/specs/components/share-list.spec.js
@@ -5,8 +5,6 @@ describe('ShareList', () => {
 
   it('should render correct contents', () => {
     const wrapper = render(ShareList);
-   // expect(wrapper.vm).toBeDefined();
-   expect(wrapper.find({ name: 'share-list' }).element).toBeDefined();
-   expect(wrapper.find('.share-list_close-btn-ctr').element).toBeDefined();
+    expect(wrapper.find({ name: 'share-list' }).element).toBeDefined();
   });
 });
