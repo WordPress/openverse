09f710512a4b8124f9b56f18b8b9603369215f2f
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-03-22 06:47:18 +0530

    add test for SocialShareButtons

--- test/unit/specs/components/related-images.spec.js
+++ test/unit/specs/components/related-images.spec.js
@@ -1,9 +1,11 @@
 import RelatedImages from '@/components/RelatedImages';
+import SearchGrid from '@/components/SearchGrid';
 import render from '../../test-utils/render';
 
 describe('RelatedImages', () => {
   it('should render correct contents', () => {
     const wrapper = render(RelatedImages);
     expect(wrapper.find({ name : 'related-images'}).element).toBeDefined();
+    expect(wrapper.find('search-grid').exists()).toBe(false);
   });
 });
