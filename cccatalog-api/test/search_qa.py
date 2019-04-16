c992723196271234b96b03d21c09f11b10a33189
Alden Page <alden@creativecommons.org>
2019-04-16 16:31:05 -0400

    Add target search terms

--- cccatalog-api/test/search_qa.py
+++ cccatalog-api/test/search_qa.py
@@ -1,6 +1,13 @@
 import requests
+import json
 from .api_live_integration_test import API_URL
+
+"""
+Perform some basic tests to ensure that search rankings work as anticipated.
+"""
 
 
 def test_phrase_relevance():
     res = requests.get(API_URL + "/search?q=home office")
+    parsed = json.loads(res.text)
+    assert parsed[0]['id'] == 1
--- ingestion_server/ingestion_server/qa.py
+++ ingestion_server/ingestion_server/qa.py
@@ -1,6 +1,13 @@
 import uuid
 import random
+from enum import Enum
 from ingestion_server.elasticsearch_models import Image
+
+
+class TaskTypes(Enum):
+    TARGET = 1
+    LESS_RELEVANT = 2
+    NOT_RELEVANT = 3
 
 
 def create_search_qa_index():
@@ -8,12 +15,12 @@ def create_search_qa_index():
     _phrase_relevance(test_idx)
 
 
-def test_image(title, tags, creator):
+def test_image(title, tags, creator, relevance):
     _id = random.randint(0, 1000000000)
     sample_url = 'https://example.com/'
     img = Image(
         _id=_id,
-        id=_id,
+        id=relevance,
         title=title,
         identifier=str(uuid.uuid4()),
         creator=creator,
@@ -40,20 +47,29 @@ def _phrase_relevance(index):
         {'name': 'noise'},
         {'name': 'clutter'}
     ]
-    target = test_image('My home office', target_tags, 'John Fooson')
+    target = test_image(
+        'My home office', target_tags, 'John Fooson', TaskTypes.TARGET
+    )
     target.save(index=index)
 
     less_relevant1 = test_image(
         'A picture of my office',
         [{'name': 'office'}],
         'Alice Foo'
+        ,
+        TaskTypes.LESS_RELEVANT
     )
     less_relevant1.save(index=index)
 
     less_relevant2 = test_image(
         'My office in my home',
         [{'name': 'office'}, {'name': 'home'}],
-        'Gordon'
+        'Gordon',
+        TaskTypes.LESS_RELEVANT
     )
     less_relevant2.save(index=index)
 
+    not_relevant = test_image(
+        'Mastiff', [{'name': 'dog'}], 'Liam', TaskTypes.NOT_RELEVANT
+    )
+    not_relevant.save(index=index)
