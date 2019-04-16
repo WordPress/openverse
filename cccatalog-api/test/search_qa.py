1e48508b02f01bbefa088891e1a082c42be9a42d
Alden Page <alden@creativecommons.org>
2019-04-16 17:23:07 -0400

    Disable broken link filtering for QA queries; the URLs are made up.

--- cccatalog-api/test/search_qa.py
+++ cccatalog-api/test/search_qa.py
@@ -1,13 +1,19 @@
 import requests
 import json
+from enum import Enum
 from .api_live_integration_test import API_URL
 
 """
 Perform some basic tests to ensure that search rankings work as anticipated.
 """
 
+class TaskTypes(Enum):
+    TARGET = 1
+    LESS_RELEVANT = 2
+    NOT_RELEVANT = 3
+
 
 def test_phrase_relevance():
-    res = requests.get(API_URL + "/search?q=home office")
+    res = requests.get(API_URL + "/search?q=home office&filter_dead=False")
     parsed = json.loads(res.text)
-    assert parsed[0]['id'] == 1
+    assert parsed[0]['id'] == TaskTypes.Target.value
