bdc9db0b039ad900d942bd3586245e4c3902abed
Breno Ferreira <bferreira@windowslive.com>
2019-04-24 15:35:36 +0200

    remove grid from image attribution tab

--- src/components/ImageAttribution.vue
+++ src/components/ImageAttribution.vue
@@ -5,44 +5,43 @@
         Image Attribution
       </h2>
     </header>
-    <div class="grid-container fluid">
-      <div class="grid-x grid-margin-x grid-margin-y">
-        <div class="photo-attribution cell large-6">
-          <p id="attribution" class="photo_usage-attribution" ref="photoAttribution">
-            <a :href="image.foreign_landing_url">"{{ image.title }}"</a>
-            <span v-if="image.creator">
-              by
-              <a v-if="image.creator_url" :href="image.creator_url">{{ image.creator }}</a>
-              <span v-else>{{ image.creator }}</span>
-            </span>
-            is licensed under
-            <a class="photo_license" :href="ccLicenseURL">
-            {{ fullLicenseName.toUpperCase() }}
-            </a>
-            <license-icons :image="image"></license-icons>
-          </p>
-          <CopyButton id="copy-attribution-btn"
-                      el="#attribution"
-                      title="Can be used in WYSIWYG editors"
-                      @copied="onCopyAttribution">
-            Copy Attribution
-          </CopyButton>
-        </div>
-        <div class="cell large-6">
-          <span>Copy the HTML below to embed the attribution in your website</span>
-          <textarea id="attribution-html"
-                    :value="attributionHtml"
-                    cols="30" rows="10"
-                    readonly="readonly">
-          </textarea>
-          <CopyButton id="embed-attribution-btn"
-                      el="#attribution-html"
-                      title="Can be used in websites"
-                      @copied="onEmbedAttribution">
-            Copy
-          </CopyButton>
-        </div>
+    <div >
+      <div class="photo-attribution">
+        <span id="attribution" class="photo_usage-attribution" ref="photoAttribution">
+          <a :href="image.foreign_landing_url">"{{ image.title }}"</a>
+          <span v-if="image.creator">
+            by
+            <a v-if="image.creator_url" :href="image.creator_url">{{ image.creator }}</a>
+            <span v-else>{{ image.creator }}</span>
+          </span>
+          is licensed under
+          <a class="photo_license" :href="ccLicenseURL">
+          {{ fullLicenseName.toUpperCase() }}
+          </a>
+          <license-icons :image="image"></license-icons>
+        </span>
+        <CopyButton id="copy-attribution-btn"
+                    el="#attribution"
+                    title="Can be used in WYSIWYG editors"
+                    @copied="onCopyAttribution">
+          Copy
+        </CopyButton>
       </div>
+      <div class="embed-attribution">
+        <span>Copy the HTML below to embed the attribution in your website</span>
+        <textarea id="attribution-html"
+                  :value="attributionHtml"
+                  cols="30" rows="4"
+                  readonly="readonly">
+        </textarea>
+        <CopyButton id="embed-attribution-btn"
+                    el="#attribution-html"
+                    title="Can be used in websites"
+                    @copied="onEmbedAttribution">
+          Copy
+        </CopyButton>
+      </div>
+    <legal-disclaimer />
     </div>
   </section>
 </template>
@@ -50,6 +49,7 @@
 <script>
 import LicenseIcons from '@/components/LicenseIcons';
 import CopyButton from '@/components/CopyButton';
+import LegalDisclaimer from '@/components/LegalDisclaimer';
 import { COPY_ATTRIBUTION, EMBED_ATTRIBUTION } from '@/store/action-types';
 
 export default {
@@ -58,6 +58,7 @@ export default {
   components: {
     LicenseIcons,
     CopyButton,
+    LegalDisclaimer,
   },
   methods: {
     onCopyAttribution(e) {
