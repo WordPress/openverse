471ecf0c46fe5f94392ae6ba3a1152bcbfcc299f
Breno Ferreira <bferreira@windowslive.com>
2019-04-24 15:34:53 +0200

    legal disclaimer component

---
+++ src/components/LegalDisclaimer.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="legal-disclaimer">
+    CC Search aggregates data from publicly available repositories of open content.
+    CC does not host the content and is not able to determine whether the content was
+    properly placed under a CC license or whether the attribution information about the
+    content is correct. Please follow the link to the source of the content to independently
+    verify before reuse.
+  </span>
+</template>
+
+<script>
+import Tooltip from '@/components/Tooltip';
+
+export default {
+  name: 'legal-disclaimer',
+  components: {
+    Tooltip,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '../styles/photodetails.scss';
+</style>
