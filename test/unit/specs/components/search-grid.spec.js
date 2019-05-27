e2417ae9497c5e88c5c097f290b1ce8f5cb1f304
Breno Ferreira <bferreira@windowslive.com>
2019-05-27 09:58:20 -0300

    fix search grid test

--- test/unit/specs/components/search-grid.spec.js
+++ test/unit/specs/components/search-grid.spec.js
@@ -3,7 +3,18 @@ import render from '../../test-utils/render';
 
 describe('SearchGrid', () => {
   it('should render correct contents', () => {
-    const wrapper = render(SearchGrid);
+    const wrapper = render(SearchGrid, {
+      propsData: {
+        query: { q: 'foo' },
+      },
+      mocks: {
+        $store: {
+          state: {
+            imagesCount: 100,
+          },
+        },
+      },
+    });
     expect(wrapper.find('section').vm).toBeDefined();
   });
 });
