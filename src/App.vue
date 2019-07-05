ff8e1af3a2ad582ccfa9426d97dc52b9703740ac
Breno Ferreira <bferreira@windowslive.com>
2019-07-05 09:42:59 -0300

    fetch providers on server

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,16 @@ import { FETCH_IMAGE_PROVIDERS } from '@/store/action-types';
 
 export default {
   name: 'App',
+  methods: {
+    fetchProviders() {
+      return this.$store.dispatch(FETCH_IMAGE_PROVIDERS);
+    },
+  },
   beforeMount() {
-    this.$store.dispatch(FETCH_IMAGE_PROVIDERS);
+    return this.fetchProviders();
+  },
+  serverPrefetch() {
+    return this.fetchProviders();
   },
 };
 </script>
