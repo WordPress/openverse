480f010a4ae6f4c61b23d1dab54b05cd0bd20db5
geekayush <srivastavs61@gmail.com>
2019-07-26 12:23:16 +0530

    Add rel attribute to target blank

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,38 +1,43 @@
 <template>
   <div class="hero">
-    <img class="logo" src="../assets/cc-logo_large_black.png">
-    <form class="hero_search-form"
-          role="search"
-          method="post"
-          v-on:submit.prevent="onSubmit">
+    <img class="logo" src="../assets/cc-logo_large_black.png" />
+    <form class="hero_search-form" role="search" method="post" v-on:submit.prevent="onSubmit">
       <div class="search-form_ctr grid-x">
-          <div class="cell large-12">
-            <input required="required"
-                   autofocus="true"
-                   class="hero_search-input"
-                   type="search"
-                   placeholder="Search for images..."
-                   autocapitalize="none"
-                   id="searchTerm"
-                   v-model.lazy="form.searchTerm">
-          </div>
-          <div class="cell large-12">
-            <button class="hero_search-btn" title="Search"></button>
-          </div>
+        <div class="cell large-12">
+          <input
+            required="required"
+            autofocus="true"
+            class="hero_search-input"
+            type="search"
+            placeholder="Search for images..."
+            autocapitalize="none"
+            id="searchTerm"
+            v-model.lazy="form.searchTerm"
+          />
+        </div>
+        <div class="cell large-12">
+          <button class="hero_search-btn" title="Search"></button>
+        </div>
       </div>
       <home-license-filter />
     </form>
     <div class="description">
-        <p>
-          Search for free content in the public domain and under Creative Commons licenses.
-          <br />
-          Learn more about CC licenses <a href="https://creativecommons.org/share-your-work/licensing-types-examples/" target="_blank">here</a>.
-        </p>
+      <p>
+        Search for free content in the public domain and under Creative Commons licenses.
+        <br />Learn more about CC licenses
+        <a
+          href="https://creativecommons.org/share-your-work/licensing-types-examples/"
+          target="_blank"
+          rel="noopener"
+        >here</a>.
+      </p>
     </div>
     <div class="old-search-link">
       <span>
         Looking for the old CC Search portal? Go
-        <a href="https://oldsearch.creativecommons.org/">here</a>
+        <a
+          href="https://oldsearch.creativecommons.org/"
+        >here</a>
       </span>
     </div>
 
@@ -41,9 +46,7 @@
         See our Search Syntax Guide
         <a href="/search-help">
           here
-          <img class='help-icon'
-              src='../assets/help_icon.svg'
-              alt='Help' />
+          <img class="help-icon" src="../assets/help_icon.svg" alt="Help" />
         </a>
       </span>
     </div>
@@ -51,27 +54,30 @@
 </template>
 
 <script>
-import { SET_QUERY } from '@/store/mutation-types';
-import HomeLicenseFilter from './HomeLicenseFilter';
+import { SET_QUERY } from "@/store/mutation-types";
+import HomeLicenseFilter from "./HomeLicenseFilter";
 
 export default {
-  name: 'hero-section',
+  name: "hero-section",
   components: {
-    HomeLicenseFilter,
+    HomeLicenseFilter
   },
-  data: () => ({ form: { searchTerm: '' } }),
+  data: () => ({ form: { searchTerm: "" } }),
   methods: {
     onSubmit() {
-      this.$store.commit(SET_QUERY, { query: { q: this.form.searchTerm }, shouldNavigate: true });
-    },
-  },
+      this.$store.commit(SET_QUERY, {
+        query: { q: this.form.searchTerm },
+        shouldNavigate: true
+      });
+    }
+  }
 };
 </script>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-@import '../../node_modules/foundation-sites/scss/foundation';
+@import "../../node_modules/foundation-sites/scss/foundation";
 
 $hero-height: 71vh;
 
@@ -131,12 +137,12 @@ $hero-height: 71vh;
     margin: 2px;
     font-size: 24px;
     cursor: pointer;
-    transition: all .2s ease-in-out;
+    transition: all 0.2s ease-in-out;
     border-radius: 3px;
 
     &:after {
-      content: '';
-      background: url('../assets/search-icon_black.svg') center center no-repeat;
+      content: "";
+      background: url("../assets/search-icon_black.svg") center center no-repeat;
       background-size: 20px;
       opacity: 0.7;
       top: 0;
@@ -149,11 +155,13 @@ $hero-height: 71vh;
   }
 
   &:before {
-    content: '';
+    content: "";
     position: absolute;
-    background: linear-gradient(to top,
-                rgba(0, 0, 0, 0.1) 0%,
-                rgba(17, 17, 17, 0.7) 100%);
+    background: linear-gradient(
+      to top,
+      rgba(0, 0, 0, 0.1) 0%,
+      rgba(17, 17, 17, 0.7) 100%
+    );
   }
 }
 
@@ -192,7 +200,7 @@ $hero-height: 71vh;
     height: 60vh;
   }
   .search-form_ctr {
-    padding: 0 .9375rem;
+    padding: 0 0.9375rem;
   }
 
   .hero .hero_search-input {
