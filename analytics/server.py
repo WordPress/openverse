a4c8fab07b1651b2b98e5149bde7f51d06fbe2bc
Alden Page <alden@creativecommons.org>
2019-07-31 11:27:02 -0400

    Write a brief README for the analytics server

--- analytics/server.py
+++ analytics/server.py
@@ -60,7 +60,7 @@ class RedocResource:
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.content_type = 'text/html'
-        with open('redoc.html', 'r') as f:
+        with open('docs/redoc.html', 'r') as f:
             resp.body = f.read()
 
 
@@ -68,7 +68,7 @@ class OpenAPISpecResource:
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.content_type = 'text/html'
-        with open('swagger.yaml', 'r') as f:
+        with open('docs/swagger.yaml', 'r') as f:
             resp.body = f.read()
 
 
