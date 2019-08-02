b6078e9ef0b8f9b736bc7ba8a6e72b293ba92d8e
Breno Ferreira <bferreira@windowslive.com>
2019-08-02 13:58:49 -0300

    fix sixpack url

--- src/abTests/createSixpackSession.js
+++ src/abTests/createSixpackSession.js
@@ -1,13 +1,14 @@
 import sixpack from 'sixpack-client';
 import SessionId from './sessionId';
 
-const SIXPACK_URL = process.env.SIXPACK_URL;
+const SIXPACK_URL = `${process.env.API_URL}/sixpack`;
 
 const createSixpackSession = () => {
   const sessionId = SessionId();
   const session = new sixpack.Session({
     client_id: sessionId,
     base_url: SIXPACK_URL,
+    timeout: 1000,
   });
 
   return session;
