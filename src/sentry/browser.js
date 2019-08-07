6e1f80fc679ae6143a96320e42a49e03da819ec7
Breno Ferreira <bferreira@windowslive.com>
2019-08-07 18:01:29 -0300

    fix dsn env variable

--- src/sentry/browser.js
+++ src/sentry/browser.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/browser';
 
 const init = () => {
-  const options = { dsn: process.env.SSR_SENTRY_DSN };
+  const options = { dsn: process.env.BROWSER_SENTRY_DSN };
   Sentry.init(options);
 };
 
