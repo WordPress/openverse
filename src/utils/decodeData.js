75c6d117f053d35adbad602a978abcc350f7fa59
Breno Ferreira <bferreira@windowslive.com>
2019-08-12 08:47:21 -0300

    decodeData bug tests

--- src/utils/decodeData.js
+++ src/utils/decodeData.js
@@ -2,7 +2,10 @@ export default function decodeData(data) {
   if (data) {
     try {
       const regexASCII = /\\x([\d\w]{2})/gi;
-      const ascii = data.replace(regexASCII, (match, grp) => String.fromCharCode(parseInt(grp, 16)));
+      const ascii = data.replace(
+        regexASCII,
+        (match, grp) => String.fromCharCode(parseInt(grp, 16)),
+      );
       const regexUni = /\\u([\d\w]{4})/gi;
       const uni = ascii.replace(regexUni, (match, grp) => String.fromCharCode(parseInt(grp, 16)));
       const res = decodeURI(uni);
