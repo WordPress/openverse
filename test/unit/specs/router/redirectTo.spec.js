c10aede8f036d32dee62dbc872337e6bdc39a8b8
Breno Ferreira <bferreira@windowslive.com>
2019-08-13 11:31:23 -0300

    redirectTo tests

---
+++ test/unit/specs/router/redirectTo.spec.js
@@ -0,0 +1,21 @@
+import redirectTo from '@/router/redirectTo';
+
+describe('redirectTo', () => {
+  const routerMock = {
+    push: jest.fn(),
+    replace: jest.fn(),
+  };
+  const redirect = redirectTo(routerMock);
+
+  it('calls router.push if replace param is false', () => {
+    redirect('foo');
+
+    expect(routerMock.push).toHaveBeenCalledWith('foo');
+  });
+
+  it('calls router.replace if replace param is true', () => {
+    redirect('foo', true);
+
+    expect(routerMock.replace).toHaveBeenCalledWith('foo');
+  });
+});
