783b28d6727c7d768165c76c05352bcc06d04e7b
Breno Ferreira <bferreira@windowslive.com>
2019-08-15 11:22:37 -0300

    store user session id in store

--- src/abTests/createSixpackSession.js
+++ src/abTests/createSixpackSession.js
@@ -3,8 +3,8 @@ import SessionId from './sessionId';
 
 const SIXPACK_URL = `${process.env.API_URL}/sixpack`;
 
-const createSixpackSession = () => {
-  const sessionId = SessionId();
+const createSixpackSession = (existingSessionId) => {
+  const sessionId = existingSessionId || SessionId();
   const session = new sixpack.Session({
     client_id: sessionId,
     base_url: SIXPACK_URL,
