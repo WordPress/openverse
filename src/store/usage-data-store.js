ec2a47cb9b9d9145f61e58861695158875c0eaf0
Breno Ferreira <bferreira@windowslive.com>
2019-08-15 11:22:57 -0300

    fix import

--- src/store/usage-data-store.js
+++ src/store/usage-data-store.js
@@ -2,7 +2,7 @@ import {
   SEND_SEARCH_QUERY_EVENT,
   SEND_RESULT_CLICKED_EVENT,
   SEND_DETAIL_PAGE_EVENT,
-} from './action-types';
+} from './usage-data-analytics-types';
 
 const actions = UsageDataService => ({
   [SEND_SEARCH_QUERY_EVENT](context, params) {
