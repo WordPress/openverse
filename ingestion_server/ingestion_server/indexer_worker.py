90a5f8666a5d0181d866aba5850efd6f2c9f8661
Alden Page <alden@creativecommons.org>
2019-10-16 16:57:52 -0400

    Fix gunicorn logs not showing up in STDOUT. Add indexer-worker to docker-compose.

--- ingestion_server/ingestion_server/indexer_worker.py
+++ ingestion_server/ingestion_server/indexer_worker.py
@@ -3,6 +3,8 @@ Accept an HTTP request specifying a range of image IDs to reindex. After the
 data has been indexed, notify Ingestion Server and stop the instance.
 """
 import falcon
+import sys
+import logging as log
 from multiprocessing import Value
 from psycopg2.sql import SQL
 from ingestion_server.indexer import elasticsearch_connect, TableIndexer
@@ -34,19 +36,32 @@ def _execute_indexing_task(target_index, start_id, end_id):
     query = SQL('SELECT * FROM {}'
                 ' WHERE id BETWEEN {} AND {}'
                 .format('image', start_id, end_id))
+    log.info('Querying {}'.format(query))
     indexer = TableIndexer(
         elasticsearch, table, progress, finish_time
     )
+    log.info('Starting indexing task')
     indexer.replicate(table, target_index, query)
+    log.info('Finished replication')
 
 
 def _self_destruct():
     """
     Stop this instance once the task is finished.
     """
+    log.info('Shutting self down')
     pass
 
 
+root = log.getLogger()
+root.setLevel(log.DEBUG)
+handler = log.StreamHandler(sys.stdout)
+handler.setLevel(log.INFO)
+formatter = log.Formatter(
+    '%(asctime)s %(levelname)s %(filename)s:%(lineno)d - %(message)s'
+)
+handler.setFormatter(formatter)
+root.addHandler(handler)
 api = falcon.API()
 api.add_route('/indexing_job', IndexingJobResource())
 api.add_route('/healthcheck', HealthcheckResource())
