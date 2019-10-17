f5e4b3ca2660d2045a5a1a082edef6bd37fc4b1a
Alden Page <alden@creativecommons.org>
2019-10-17 11:39:21 -0400

    Create script for shipping new versions of ingestion_server and indexer_worker images simultaneously

---
+++ ingestion_server/publish_release.sh
@@ -0,0 +1,6 @@
+# Usage: ./public_release.sh [VERSION]
+docker build -t creativecommons/ingestion_server:$1 .
+docker push creativecommons/ingestion_server:$1
+
+docker build -f Dockerfile-worker -t creativecommons/indexer_worker:$1 .
+docker push creativecommons/indexer_worker:$1
