12021afdf323d2420bbbf4f43fb1e1cfc0fe3b20
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2019-11-12 15:14:00 -0400

    change method to access element to VueJS way (with $refs)

--- src/components/SearchGridFormOriginal.vue
+++ src/components/SearchGridFormOriginal.vue
@@ -131,7 +131,7 @@ export default {
       e.preventDefault();
       if (this.searchTermsModel) {
         this.$emit('onSearchFormSubmit', { query: { q: this.searchTermsModel }, shouldNavigate: true });
-        document.getElementById('searchInput').blur();
+        this.$refs.search.blur();
       }
     },
     onToggleSearchGridFilter() {
