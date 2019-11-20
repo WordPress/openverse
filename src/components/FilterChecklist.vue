7a5f83f583a4a31355ccca00f77d0f5cc5d414fc
Breno Ferreira <bferreira@windowslive.com>
2019-11-20 10:52:55 -0300

    styles improvement

--- src/components/FilterChecklist.vue
+++ src/components/FilterChecklist.vue
@@ -1,8 +1,14 @@
 <template>
   <fieldset>
-    <legend class="filters-title">{{ title }}</legend>
+    <div class="filters-title" @click.prevent="toggleFilterVisibility">
+      <span>{{ title }}</span>
 
-    <div v-for="(item, index) in options" :key="index">
+      <button class="filter-dropdown-visibility-button">
+        <img src="@/assets/arrow-bottom.svg" class="arrow-bottom" alt="toggle filters visibility" />
+      </button>
+    </div>
+
+    <div v-for="(item, index) in options" :key="index" v-if="filtersVisible">
       <input type="checkbox"
              class="filter-checkbox"
              :id="item.code"
@@ -18,21 +24,45 @@
 export default {
   name: 'filter-check-list',
   props: ['options', 'title', 'filterType'],
+  data() {
+    return { filtersVisible: false };
+  },
   methods: {
     onValueChange(e) {
       this.$emit('filterChanged', { code: e.target.id, filterType: this.$props.filterType });
+    },
+    toggleFilterVisibility(e) {
+      this.filtersVisible = !this.filtersVisible;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.filters-title {
-  font-size: 1.3em;
-  font-weight: 700;
+fieldset {
+  padding-left: 24px;
+  padding-right: 34px;
+  border-bottom: 1px solid #d8d8d8;
+  margin-top: 20px;
+  margin-bottom: 10px;
 }
 
-.filter-label {
+.filters-title {
+  font-size: 1.250em;
   font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: normal;
+  cursor: pointer;
+  margin-bottom: 16px;
+}
+
+.filter-dropdown-visibility-button {
+  float: right;
+}
+
+label {
+  color: #333333;
 }
 </style>
--- src/components/SearchGridFilter.vue
+++ src/components/SearchGridFilter.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="{ 'search-filters': true,
                  'search-filters__visible': isFilterVisible, }">
-    <form class="filter-option" role="filter">
+    <form class="filters-form" role="filter">
       <filter-check-list :options="filters.licenseTypes"
                          title="I want something that I can"
                          filterType="licenseTypes"
@@ -97,22 +97,10 @@ export default {
 .search-filters {
   background: #fafafa;
   display: none;
-  padding: 10px ;
   height: 100%;
 
   label {
-    font-size: 1em;
-    color: #35495e;
-    span {
-      margin-bottom: 1.07142857em;
-      font-size: .85em;
-      letter-spacing: 1px;
-      line-height: 1.25;
-      display: inline-block;
-      padding-top: .28571429em;
-      border-top: 5px solid #373737;
-      margin-top: -3px;
-    }
+    color: #333333;
   }
 
   &__visible {
@@ -121,23 +109,10 @@ export default {
   }
 }
 
-.filter-option {
-  margin-right: 1em;
-  padding-bottom: 0.5em;
-  padding-top: 0.5em;
-}
-
-
-.grid-x {
-  /* Small only */
-  @media screen and (max-width: 49em) {
-    display: block;
-  }
-}
-
 .search-filters_search-by,
 .clear-filters {
   margin-top: 0.4em;
+  margin-left: 24px;
 }
 
 .search-filters_clear-btn {
