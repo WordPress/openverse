995d3bb6797f60a8eb72ffdca373dfa1bc7cfa56
Breno Ferreira <bferreira@windowslive.com>
2019-11-21 21:37:39 -0300

    wip filter store tests

---
+++ test/unit/specs/store/filter-store.spec.js
@@ -0,0 +1,8 @@
+import store from '@/store/filter-store';
+import { TOGGLE_FILTER } from '@/store/action-types';
+import { SET_FILTER, SET_PROVIDERS_FILTERS, CLEAR_FILTERS } from '@/store/mutation-types';
+
+describe('Filter Store', () => {
+  describe('state', () => {
+  });
+});
