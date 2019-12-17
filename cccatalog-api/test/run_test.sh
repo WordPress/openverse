519bad6fe06c4243ab29b9bfeb09cab2e1d85495
Alden Page <alden@creativecommons.org>
2019-12-17 15:10:44 -0500

    Fix integration test path

--- cccatalog-api/test/run_test.sh
+++ cccatalog-api/test/run_test.sh
@@ -2,4 +2,4 @@
 # Local environments don't have valid certificates; suppress this warning.
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 export INTEGRATION_TEST_URL="http://localhost:8000"
-PYTHONPATH=. pytest -s --disable-pytest-warnings v1_integration_test.py
+PYTHONPATH=. pytest -s --disable-pytest-warnings test/v1_integration_test.py
