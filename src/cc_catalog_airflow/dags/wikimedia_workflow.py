3ad1c3dde5a14de80ec9241a909cf6b27b838a43
Brent Moran <brent@creativecommons.org>
2020-01-31 18:18:40 +0100

    add DAG for new Wikimedia Commons script

---
+++ src/cc_catalog_airflow/dags/wikimedia_workflow.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from provider_api_scripts import wikimedia_commons
+from util.operator_util import get_log_operator
+
+
+DAG_DEFAULT_ARGS = {
+    'owner': 'data-eng-admin',
+    'depends_on_past': False,
+    'start_date': datetime(2003, 7, 1),
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=15),
+}
+
+DAG_ID = 'wikimedia_commons_workflow'
+
+
+def get_runner_operator(dag):
+    return PythonOperator(
+        task_id='pull_wikimedia_commons_data',
+        python_callable=wikimedia_commons.main,
+        op_args=['{{ ds }}'],
+        depends_on_past=False,
+        dag=dag
+    )
+
+
+def create_dag():
+    dag = DAG(
+        dag_id=DAG_ID,
+        default_args=DAG_DEFAULT_ARGS,
+        start_date=datetime(2003, 1, 1),
+        schedule_interval='@daily',
+        catchup=True,
+    )
+
+    with dag:
+        start_task = get_log_operator(dag, DAG_ID, 'Starting')
+        run_task = get_runner_operator(dag)
+        end_task = get_log_operator(dag, DAG_ID, 'Finished')
+
+        start_task >> run_task >> end_task
+
+    return dag
+
+
+globals()[DAG_ID] = create_dag()
