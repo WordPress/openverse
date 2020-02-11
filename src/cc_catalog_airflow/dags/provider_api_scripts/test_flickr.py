bb04b7b5831c16e5667177f9de81e0be35b8a7dd
Brent Moran <brent@creativecommons.org>
2020-02-11 11:13:53 -0600

    add more tests for Flickr script

--- src/cc_catalog_airflow/dags/provider_api_scripts/test_flickr.py
+++ src/cc_catalog_airflow/dags/provider_api_scripts/test_flickr.py
@@ -1,13 +1,24 @@
+import json
 import logging
-from unittest.mock import patch, MagicMock
+import os
+from unittest.mock import patch
 
 import flickr
 
+RESOURCES = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), 'tests/resources/flickr'
+)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s:  %(message)s',
     level=logging.DEBUG,
 )
+
+
+def _get_resource_json(json_name):
+    with open(os.path.join(RESOURCES, json_name)) as f:
+        resource_json = json.load(f)
+    return resource_json
 
 
 def test_get_image_list_retries_with_none_response():
@@ -22,7 +33,7 @@ def test_get_image_list_retries_with_none_response():
 
 
 # This test will fail if default constants change.
-def test_build_query_param_dict_default(monkeypatch):
+def test_build_query_param_dict_default():
     cur_time = '12345'
     nxt_time = '12346'
     flickr_api_key = 'notset'
@@ -84,124 +95,81 @@ def test_build_query_param_dict_with_givens():
     assert actual_query_param_dict == expect_query_param_dict
 
 
+def test_process_image_data():
+    pass
+
+
 def test_get_image_url_returns_Nonetype_tuple_if_no_image():
-    data = {
-        "id": "456",
-        "owner": "456@N04",
-        "title": "233 Ordesa sept 2019",
-        "license": "3",
-        "description": {
-            "_content": "OLYMPUS DIGITAL CAMERA"
-        },
-        "dateupload": "1571326372",
-        "datetaken": "2019-09-07 16:26:44",
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-        "url_t": "https://live.staticflickr.com/456_t.jpg",
-        "height_t": 75,
-        "width_t": 100,
-    }
+    data = _get_resource_json('image_data_no_image_url.json')
     actual_tuple = flickr._get_image_url(data)
     expect_tuple = (None, None, None)
     assert expect_tuple == actual_tuple
 
 
 def test_get_image_url_returns_large_tuple_when_avail():
-    data = {
-        "id": "456",
-        "owner": "456@N04",
-        "title": "233 Ordesa sept 2019",
-        "license": "3",
-        "description": {
-            "_content": "OLYMPUS DIGITAL CAMERA"
-        },
-        "dateupload": "1571326372",
-        "datetaken": "2019-09-07 16:26:44",
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-        "url_t": "https://live.staticflickr.com/456_t.jpg",
-        "height_t": 75,
-        "width_t": 100,
-        "url_s": "https://live.staticflickr.com/456_m.jpg",
-        "height_s": 180,
-        "width_s": 240,
-        "url_m": "https://live.staticflickr.com/456.jpg",
-        "height_m": 375,
-        "width_m": 500,
-        "url_l": "https://live.staticflickr.com/456_b.jpg",
-        "height_l": 768,
-        "width_l": 1024
-    }
-    actual_tuple = flickr._get_image_url(data)
+    image_data = _get_resource_json('image_data_with_large_url_available.json')
+    actual_tuple = flickr._get_image_url(image_data)
     expect_tuple = ('https://live.staticflickr.com/456_b.jpg', 768, 1024)
     assert expect_tuple == actual_tuple
 
 
 def test_get_image_url_returns_medium_tuple_when_large_not_avail():
-    data = {
-        "id": "456",
-        "owner": "456@N04",
-        "title": "233 Ordesa sept 2019",
-        "license": "3",
-        "description": {
-            "_content": "OLYMPUS DIGITAL CAMERA"
-        },
-        "dateupload": "1571326372",
-        "datetaken": "2019-09-07 16:26:44",
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-        "url_t": "https://live.staticflickr.com/456_t.jpg",
-        "height_t": 75,
-        "width_t": 100,
-        "url_s": "https://live.staticflickr.com/456_m.jpg",
-        "height_s": 180,
-        "width_s": 240,
-        "url_m": "https://live.staticflickr.com/456.jpg",
-        "height_m": 375,
-        "width_m": 500,
-    }
+    data = _get_resource_json('image_data_with_med_url_available.json')
     actual_tuple = flickr._get_image_url(data)
     expect_tuple = ('https://live.staticflickr.com/456.jpg', 375, 500)
     assert expect_tuple == actual_tuple
 
 
 def test_get_image_url_falls_to_small_tuple():
-    data = {
-        "id": "456",
-        "owner": "456@N04",
-        "title": "233 Ordesa sept 2019",
-        "license": "3",
-        "description": {
-            "_content": "OLYMPUS DIGITAL CAMERA"
-        },
-        "dateupload": "1571326372",
-        "datetaken": "2019-09-07 16:26:44",
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-        "url_t": "https://live.staticflickr.com/456_t.jpg",
-        "height_t": 75,
-        "width_t": 100,
-        "url_s": "https://live.staticflickr.com/456_m.jpg",
-        "height_s": 180,
-        "width_s": 240,
-    }
+    data = _get_resource_json('image_data_with_small_url_available.json')
     actual_tuple = flickr._get_image_url(data)
     expect_tuple = ('https://live.staticflickr.com/456_m.jpg', 180, 240)
     assert expect_tuple == actual_tuple
 
 
-def test_create_meta_data_fills_meta_data_dict():
-    data = {
-        "title": "233 Ordesa sept 2019",
-        "description": {
-            "_content": "OLYMPUS DIGITAL CAMERA"
-        },
-        "dateupload": "1571326372",
-        "datetaken": "2019-09-07 16:26:44",
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-        "views": "9"
+def test_get_license_with_int_license_id():
+    license_info = {
+        '1': ('by-nc-sa', '2.0'),
+        '2': ('by-nc', '2.0'),
     }
+    actual_license, actual_license_version = flickr._get_license(
+        2,
+        license_info=license_info
+    )
+    expect_license, expect_license_version = 'by-nc', '2.0'
+    assert expect_license == actual_license
+    assert expect_license_version == actual_license_version
+
+
+def test_get_license_with_str_license_id():
+    license_info = {
+        '1': ('by-nc-sa', '2.0'),
+        '2': ('by-nc', '2.0'),
+    }
+    actual_license, actual_license_version = flickr._get_license(
+        '2',
+        license_info=license_info
+    )
+    expect_license, expect_license_version = 'by-nc', '2.0'
+    assert expect_license == actual_license
+    assert expect_license_version == actual_license_version
+
+
+def test_get_license_with_missing_license_id():
+    license_info = {
+        '1': ('by-nc-sa', '2.0'),
+        '2': ('by-nc', '2.0'),
+    }
+    actual_license, actual_license_version = flickr._get_license(
+        12,
+        license_info=license_info
+    )
+    assert actual_license is None
+    assert actual_license_version is None
+
+
+def test_create_meta_data_fills_meta_data_dict():
+    data = _get_resource_json('image_data_full_example.json')
     actual_dict = flickr._create_meta_data_dict(data)
     expect_dict = {
         'pub_date': '1571326372',
@@ -213,13 +181,7 @@ def test_create_meta_data_fills_meta_data_dict():
 
 
 def test_create_meta_data_fills_partial_meta_data_dict():
-    data = {
-        "title": "233 Ordesa sept 2019",
-        "dateupload": "1571326372",
-        "datetaken": "2019-09-07 16:26:44",
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-    }
+    data = _get_resource_json('image_data_partial_meta_data_info.json')
     actual_dict = flickr._create_meta_data_dict(data)
     expect_dict = {
         'pub_date': '1571326372',
@@ -229,43 +191,32 @@ def test_create_meta_data_fills_partial_meta_data_dict():
 
 
 def test_create_meta_data_makes_empty_meta_data_dict():
-    data = {
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-    }
+    data = _get_resource_json('image_data_no_meta_data_info.json')
     actual_dict = flickr._create_meta_data_dict(data)
     expect_dict = {}
     assert expect_dict == actual_dict
 
 
 def test_create_meta_data_dict_strips_html():
-    data = {
-        "title": "233 Ordesa sept 2019",
-        "description": {
-            "_content": "Warsy (Somme, France) -\n\n<a href=\"https://www.google.com/maps/@49.69839,2.64689,3a,90y,102.98h,114.44t/data=!3m6!1e1!3m4!1s6s0eTwQy1M3dTex1cH7pVQ!2e0!7i13312!8i6656\" rel=\"noreferrer nofollow\">www.google.com/maps/@49.69839,2.64689,3a,90y,102.98h,114....</a>"
-        },
-        "dateupload": "1571326372",
-        "datetaken": "2019-09-07 16:26:44",
-        "ownername": "RADIOfotoGRAFIANDO",
-        "tags": "amarilla",
-        "views": "9"
-    }
+    data = _get_resource_json('image_data_html_description.json')
     actual_dict = flickr._create_meta_data_dict(data)
-    expect_dict = {
-        'pub_date': '1571326372',
-        'date_taken': '2019-09-07 16:26:44',
-        'description': 'Warsy (Somme, France) -\n\n www.google.com/maps/@49.69839,2.64689,3a,90y,102.98h,114....',
-        'views': '9'
-    }
+    expect_dict = _get_resource_json(
+        'expect_meta_data_from_html_description.json'
+    )
+    assert expect_dict == actual_dict
+
+
+def test_create_meta_data_handles_whitespace_description():
+    data = _get_resource_json('image_data_whitespace_description.json')
+    actual_dict = flickr._create_meta_data_dict(data)
+    expect_dict = _get_resource_json(
+        'expect_meta_data_from_whitespace_description.json'
+    )
     assert expect_dict == actual_dict
 
 
 def test_create_tags_list_makes_tags_list():
-    data = {
-        "id": "456",
-        "tags": "tag1 tag2   tag3  tag3 ",
-        "width_l": 1024
-    }
+    data = _get_resource_json('image_data_varying_tags_whitespace.json')
     actual_tags_list = flickr._create_tags_list(data)
     expect_tags_list = ['tag1', 'tag2', 'tag3']
     assert len(actual_tags_list) == len(expect_tags_list)
