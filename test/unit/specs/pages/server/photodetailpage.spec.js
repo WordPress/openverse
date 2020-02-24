b1d2c0157bdb21c9e450b5ba4c9fef444ca49d8e
AyanChoudhary <darkrider251099@gmail.com>
2020-02-25 02:30:28 +0530

    feat:made test coverage script for server:photodetailpage

---
+++ test/unit/specs/pages/server/photodetailpage.spec.js
@@ -0,0 +1,20 @@
+
+import PhotoDetailPage from '@/pages/server/PhotoDetailPage';
+import render from '../../../test-utils/render';
+
+describe('PhotoDetailPage', () => {
+  const options = {
+    watermarkEnabled: true,
+    socialSharingEabled: true,
+  };
+  it('should render correct contents', () => {
+    const wrapper = render(PhotoDetailPage, options);
+
+    expect(wrapper.find('.browse-page'));
+    expect(wrapper.find({ name: 'header-section' }).vm).toBeDefined();
+    expect(wrapper.find({ name: 'photo-details' }).vm).toBeDefined();
+    expect(wrapper.find({ name: 'photo-tags' }).vm).toBeDefined();
+    expect(wrapper.find({ name: 'related-images' }).vm).toBeDefined();
+    expect(wrapper.find({ name: 'footer-section' }).vm).toBeDefined();
+  });
+});
