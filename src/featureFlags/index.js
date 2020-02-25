c521e51df09c48dc94f02b8c1e96e033f6baf08b
AyanChoudhary <darkrider251099@gmail.com>
2020-02-25 21:18:58 +0530

    feat:added a fallback for feature flags in case of no env set

--- src/featureFlags/index.js
+++ src/featureFlags/index.js
@@ -1,6 +1,17 @@
+function stringToBoolean(string) {
+  if (string) {
+    switch (string.toLowerCase().trim()) {
+      case 'true': case 'yes': case '1': return true;
+      case 'false': case 'no': case '0': case null: return false;
+      default: return Boolean(string);
+    }
+  }
+  else return false;
+}
+
 const flags = {
-  watermark: JSON.parse(process.env.WATERMARK),
-  socialSharing: JSON.parse(process.env.SOCIAL_SHARING),
+  watermark: stringToBoolean(process.env.WATERMARK),
+  socialSharing: stringToBoolean(process.env.SOCIAL_SHARING),
 };
 
 export default flags;
--- test/unit/specs/pages/server/photodetailpage.spec.js
+++ test/unit/specs/pages/server/photodetailpage.spec.js
@@ -4,8 +4,25 @@ import render from '../../../test-utils/render';
 
 describe('PhotoDetailPage', () => {
   const options = {
-    watermarkEnabled: true,
-    socialSharingEabled: true,
+    propsData: {
+      query: {
+        provider: 'foo',
+      },
+    },
+    mocks: {
+      $store: {
+        state: {
+          query: {
+            q: 'foo',
+            provider: 'foo',
+          },
+          image: {
+            id: 1,
+          },
+        },
+        dispatch: jest.fn(),
+      },
+    },
   };
   it('should render correct contents', () => {
     const wrapper = render(PhotoDetailPage, options);
@@ -14,7 +31,6 @@ describe('PhotoDetailPage', () => {
     expect(wrapper.find({ name: 'header-section' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'photo-details' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'photo-tags' }).vm).toBeDefined();
-    expect(wrapper.find({ name: 'related-images' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'footer-section' }).vm).toBeDefined();
   });
 });
