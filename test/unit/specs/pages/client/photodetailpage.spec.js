d520581c0192be70176a3c8eb76d32b12cc91dd8
AyanChoudhary <darkrider251099@gmail.com>
2020-02-25 21:22:14 +0530

    feat:added a fallback for feature flags in case of no env set

--- test/unit/specs/pages/client/photodetailpage.spec.js
+++ test/unit/specs/pages/client/photodetailpage.spec.js
@@ -3,8 +3,25 @@ import render from '../../../test-utils/render';
 
 describe('PhotoDetailPage', () => {
   const options = {
-    watermarkEnabled: true,
-    socialSharingEabled: true,
+    propsData: {
+      query: {
+        provider: 'foo',
+      },
+    },
+    mocks: {
+      $store: {
+        state: {
+          query: {
+            q: 'foo',
+            provider: 'foo',
+          },
+          image: {
+            id: 1,
+          },
+        },
+        dispatch: jest.fn(),
+      },
+    },
   };
   it('should render correct contents', () => {
     const wrapper = render(PhotoDetailPage, options);
@@ -13,7 +30,6 @@ describe('PhotoDetailPage', () => {
     expect(wrapper.find({ name: 'header-section' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'photo-details' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'photo-tags' }).vm).toBeDefined();
-    expect(wrapper.find({ name: 'related-images' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'footer-section' }).vm).toBeDefined();
   });
 });
