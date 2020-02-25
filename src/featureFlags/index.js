6a906d07e4126e2f03d9d333cb9ae02f1d6f50fe
AyanChoudhary <darkrider251099@gmail.com>
2020-02-26 01:32:56 +0530

    fix:remove tobeUndefined tests and added comment regarding flags declaration

--- src/featureFlags/index.js
+++ src/featureFlags/index.js
@@ -9,6 +9,8 @@ function stringToBoolean(string) {
   else return false;
 }
 
+// The default fallback for the flags is used for running ests regarding the PhotoDetailPage
+
 const flags = {
   watermark: stringToBoolean(process.env.WATERMARK),
   socialSharing: stringToBoolean(process.env.SOCIAL_SHARING),
