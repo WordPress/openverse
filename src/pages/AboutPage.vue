1c8967136c19f95cf6da8991107d4d50687ebc53
hemanth-hk <uppadahemanthkumar27@gmail.com>
2020-02-26 15:14:22 +0530

    updated src of the links

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -53,7 +53,7 @@
                 :key="index">
               <td>{{ imageProvider.display_name }}</td>
               <td>
-                <a :href="imageProvider.provider_url">
+                <a :href="imageProvider.source_url">
                   {{ imageProvider.source_url }}
                 </a>
               </td>
