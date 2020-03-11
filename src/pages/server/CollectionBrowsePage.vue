cc7393e856162703202c4ec25df47c77d0cf6077
Breno Ferreira <bferreira@windowslive.com>
2020-03-11 15:22:24 -0300

    refactor duplicated results page css in one file

--- src/pages/server/CollectionBrowsePage.vue
+++ src/pages/server/CollectionBrowsePage.vue
@@ -2,7 +2,7 @@
   <div class="browse-page">
     <header-section />
     <div class="search columns">
-      <div class="column is-narrow grid-sidebar" v-if="isFilterVisible">
+      <div class="column is-narrow grid-sidebar is-paddingless" v-if="isFilterVisible">
         <search-grid-filter isCollectionsPage="true"
                             :provider="provider"
                             @onSearchFilterChanged="onSearchFormSubmit"/>
@@ -41,30 +41,5 @@ export default CollectionBrowsePage;
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style lang="scss">
-  .search {
-    margin: 0;
-  }
-
-  .search-grid-ctr {
-    padding: 0;
-    background: #e9ebee;
-    min-height: 600px;
-    margin: 0;
-    transition: margin .7s ease-in-out;
-  }
-
-  .search-grid-ctr__filter-visible {
-    margin-top: 30px;
-  }
-
-  .grid-sidebar {
-    padding-top: 0;
-    background: #fafafa;
-    width: 21.875rem;
-
-    /* 48em = 768px */
-    @media (max-width: 49em) {
-      width: 100%;
-    }
-  }
+  @import "../../styles/results-page.scss";
 </style>
