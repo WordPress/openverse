e848e18fb40d6db010c4819e142d746902d595ac
Breno Ferreira <bferreira@windowslive.com>
2020-03-12 14:46:53 -0300

    wip - photo details page new styles

--- src/components/PhotoDetails.vue
+++ src/components/PhotoDetails.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="photo">
-    <div class="photo_image-ctr">
+  <div class="photo columns is-desktop is-marginless">
+    <div class="column is-two-thirds photo_image-ctr">
       <a class="photo_breadcrumb"
           :href="breadCrumbURL"
           @click.prevent="onGoBackToSearchResults"
@@ -10,63 +10,65 @@
             :src="image.url"
             :alt="image.title">
     </div>
-    <section class="tab-section">
-      <ul class="tabs" data-tabs id="example-tabs">
-        <li :class="tabClass(0, 'tabs-title')">
-          <a href="#panel0" :aria-selected="activeTab == 0" @click.prevent="setActiveTab(0)">
-            <img class='tab-icon'
-                 src='../assets/cc-by-icon_large.png'
-                 alt='Image Attribution'>
-            Attribution
-          </a>
-        </li>
-        <li :class="tabClass(1, 'tabs-title')">
-          <a href="#panel1" :aria-selected="activeTab == 1" @click.prevent="setActiveTab(1)">
-            <img class='tab-icon'
-                 src='../assets/info-icon.svg'
-                 alt='Image Info' />
-            Info
-          </a>
-        </li>
-        <li :class="tabClass(2, 'tabs-title')" v-if="watermarkEnabled">
-          <a href="#panel2" :aria-selected="activeTab == 2" @click.prevent="setActiveTab(2)">
-            <img class='tab-icon'
-                 src='../assets/download-icon.svg'
-                 alt='Image Download' />
-            Download
-          </a>
-        </li>
-        <li :class="tabClass(3, 'tabs-title')" v-if="socialSharingEnabled">
-          <a href="#panel3" :aria-selected="activeTab == 3" @click.prevent="setActiveTab(3)">
-            <img class='tab-icon'
-                 src='../assets/share-icon.svg'
-                 alt='Share Image' />
-            Share
-          </a>
-        </li>
-      </ul>
-    </section>
-    <section class="photo_info-ctr tabs-content">
-      <div :class="tabClass(0, 'tabs-panel')">
-        <image-attribution :image="image"
-                            :ccLicenseURL="ccLicenseURL"
-                            :fullLicenseName="fullLicenseName"
-                            :attributionHtml="attributionHtml()" />
-      </div>
-      <div :class="tabClass(1, 'tabs-panel')">
-        <image-info :image="image"
-                    :ccLicenseURL="ccLicenseURL"
-                    :fullLicenseName="fullLicenseName"
-                    :imageWidth="imageWidth"
-                    :imageHeight="imageHeight" />
-      </div>
-      <div :class="tabClass(2, 'tabs-panel')">
-        <watermark v-if="watermarkEnabled" :image="image" />
-      </div>
-      <div :class="tabClass(3, 'tabs-panel')">
-        <image-social-share v-if="socialSharingEnabled" :image="image" />
-      </div>
-    </section>
+    <div class="column image-info">
+      <section class="tab-section">
+        <ul class="tabs" data-tabs id="example-tabs">
+          <li :class="tabClass(0, 'tabs-title')">
+            <a href="#panel0" :aria-selected="activeTab == 0" @click.prevent="setActiveTab(0)">
+              <img class='tab-icon'
+                  src='../assets/cc-by-icon_large.png'
+                  alt='Image Attribution'>
+              Attribution
+            </a>
+          </li>
+          <li :class="tabClass(1, 'tabs-title')">
+            <a href="#panel1" :aria-selected="activeTab == 1" @click.prevent="setActiveTab(1)">
+              <img class='tab-icon'
+                  src='../assets/info-icon.svg'
+                  alt='Image Info' />
+              Info
+            </a>
+          </li>
+          <li :class="tabClass(2, 'tabs-title')" v-if="watermarkEnabled">
+            <a href="#panel2" :aria-selected="activeTab == 2" @click.prevent="setActiveTab(2)">
+              <img class='tab-icon'
+                  src='../assets/download-icon.svg'
+                  alt='Image Download' />
+              Download
+            </a>
+          </li>
+          <li :class="tabClass(3, 'tabs-title')" v-if="socialSharingEnabled">
+            <a href="#panel3" :aria-selected="activeTab == 3" @click.prevent="setActiveTab(3)">
+              <img class='tab-icon'
+                  src='../assets/share-icon.svg'
+                  alt='Share Image' />
+              Share
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section class="photo_info-ctr tabs-content">
+        <div :class="tabClass(0, 'tabs-panel')">
+          <image-attribution :image="image"
+                              :ccLicenseURL="ccLicenseURL"
+                              :fullLicenseName="fullLicenseName"
+                              :attributionHtml="attributionHtml()" />
+        </div>
+        <div :class="tabClass(1, 'tabs-panel')">
+          <image-info :image="image"
+                      :ccLicenseURL="ccLicenseURL"
+                      :fullLicenseName="fullLicenseName"
+                      :imageWidth="imageWidth"
+                      :imageHeight="imageHeight" />
+        </div>
+        <div :class="tabClass(2, 'tabs-panel')">
+          <watermark v-if="watermarkEnabled" :image="image" />
+        </div>
+        <div :class="tabClass(3, 'tabs-panel')">
+          <image-social-share v-if="socialSharingEnabled" :image="image" />
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
