2117b1761226adb28357dc7606c25fd97ebe1350
Shubham Pandey <shubhampcpandey13@gmail.com>
2020-03-13 23:51:57 +0530

    Improve rawpixel.py - rename, private non-main functions, reduce main()

--- src/cc_catalog_airflow/dags/provider_api_scripts/raw_pixel.py
+++ src/cc_catalog_airflow/dags/provider_api_scripts/raw_pixel.py
@@ -21,12 +21,10 @@ delayed_requester = DelayedRequester(DELAY)
 image_store = ImageStore(provider=PROVIDER, output_file=FILE)
 
 
-def request_content(_url, _query_params=None, _headers=None):
-    logging.info("Processing request: {}".format(_url))
+def _request_content(url, query_params=None, headers=None):
+    logging.info("Processing request: {}".format(url))
 
-    response = delayed_requester.get(
-        _url, params=_query_params, headers=_headers
-    )
+    response = delayed_requester.get(url, params=query_params, headers=headers)
 
     try:
         if response.status_code == requests.codes.ok:
@@ -34,7 +32,7 @@ def request_content(_url, _query_params=None, _headers=None):
         else:
             logging.warning(
                 "Unable to request URL: {}. Status code: {}".format(
-                    _url, response.status_code
+                    url, response.status_code
                 )
             )
             return None
@@ -45,10 +43,10 @@ def request_content(_url, _query_params=None, _headers=None):
         return None
 
 
-def get_image_list(_page=1):
+def _get_image_list(page=1):
     endpoint = "https://api.rawpixel.com/api/v1/search"
-    query_params = {"freecc0": 1, "html": 0, "page": _page}
-    request = request_content(endpoint, _query_params=query_params)
+    query_params = {"freecc0": 1, "html": 0, "page": page}
+    request = _request_content(endpoint, query_params=query_params)
 
     if request.get("results"):
         return [request.get("total"), request.get("results")]
@@ -57,13 +55,13 @@ def get_image_list(_page=1):
         return [None, None]
 
 
-def get_foreign_id_url(_image):
-    if _image.get("freecc0"):
+def _get_foreign_id_url(image):
+    if image.get("freecc0"):
         # get the image identifier
-        foreign_id = _image.get("id", "")
+        foreign_id = image.get("id", "")
 
         # get the landing page
-        foreign_url = _image.get("url")
+        foreign_url = image.get("url")
 
         if not foreign_url:
             logging.warning(
@@ -74,8 +72,8 @@ def get_foreign_id_url(_image):
         return [None, None]
 
 
-def get_image_properties(_image, foreign_url):
-    img_url = _image.get("image_opengraph")
+def _get_image_properties(image, foreign_url):
+    img_url = image.get("image_opengraph")
     if img_url:
         # extract the dimensions from the query params because
         # the dimensions in the metadata are at times
@@ -83,22 +81,22 @@ def get_image_properties(_image, foreign_url):
         query_params = urlparse(img_url)
         width = parse_qs(query_params.query).get("w", [])[0]  # width
         height = parse_qs(query_params.query).get("h", [])[0]  # height
-        thumbnail = _image.get("image_400", "")
+        thumbnail = image.get("image_400", "")
         return [img_url, width, height, thumbnail]
     else:
         logging.warning(f"Image not detected in URL: {foreign_url}")
         return [None, None, None, None]
 
 
-def get_title_owner(_image):
-    title = _image.get("image_title", "")
-    owner = _image.get("artist_names", "")
+def _get_title_owner(image):
+    title = image.get("image_title", "")
+    owner = image.get("artist_names", "")
     owner = owner.replace("(Source)", "").strip()
     return [title, owner]
 
 
-def get_tags(_image):
-    keywords = _image.get("keywords_raw")
+def _get_tags(image):
+    keywords = image.get("keywords_raw")
     if keywords:
         keyword_list = keywords.split(",")
         keyword_list = [
@@ -107,29 +105,27 @@ def get_tags(_image):
             if word.strip()
             not in ["cc0", "creative commons", "creative commons 0"]
         ]
-        tags = [
-            {"name": tag, "provider": "rawpixel"} for tag in keyword_list
-        ]
+        tags = [{"name": tag, "provider": "rawpixel"} for tag in keyword_list]
         return tags
     else:
         return {}
 
 
-def process_image_data(_image):
-    start_time = time.time()
-
+def _process_image_data(image):
     # verify the license and extract the metadata
     license = "cc0"
     version = "1.0"
 
-    foreign_id, foreign_url = get_foreign_id_url(_image)
+    foreign_id, foreign_url = _get_foreign_id_url(image)
     if not foreign_url:
         return None
-    img_url, width, height, thumbnail = get_image_properties(_image, foreign_url)
+    img_url, width, height, thumbnail = _get_image_properties(
+        image, foreign_url
+    )
     if not img_url:
         return None
-    title, owner = get_title_owner(_image)
-    tags = get_tags(_image)
+    title, owner = _get_title_owner(image)
+    tags = _get_tags(image)
 
     # TODO: How to get license_url, creator, creator_url, source?
     return image_store.add_item(
@@ -145,27 +141,20 @@ def process_image_data(_image):
     )
 
 
-def main():
-    page = 1
+def _process_pages(total, result, page):
     img_ctr = 0
     is_valid = True
-
-    logging.info("Begin: RawPixel API requests")
-
-    total, result = get_image_list(page)
-
     while (img_ctr < total) and is_valid:
         logging.info("Processing page: {}".format(page))
 
-        start_time = time.time()
         for img in result:
-            total_images = process_image_data(img)
+            total_images = _process_image_data(img)
             img_ctr = total_images if total_images else img_ctr
 
         total_images = image_store.commit()
 
         page += 1
-        total, result = get_image_list(page)
+        total, result = _get_image_list(page)
 
         if not result:
             is_valid = False
@@ -173,6 +162,18 @@ def main():
         if not total:
             total = 0
             is_valid = False
+
+    return img_ctr
+
+
+def main():
+    page = 1
+
+    logging.info("Begin: RawPixel API requests")
+
+    total, result = _get_image_list(page)
+
+    img_ctr = _process_pages(total, result, page=page)
 
     logging.info("Total images: {}".format(img_ctr))
     logging.info("Terminated!")
