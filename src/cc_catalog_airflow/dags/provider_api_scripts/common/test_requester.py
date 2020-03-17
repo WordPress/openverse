fb2d76032f9e0a36e469d10a4f5fe1ac5cb221ab
Charini Nanayakkara <charini@Charinis-Air.lan>
2020-03-17 21:49:03 +1100

    Remove non-essential parameter from get_response_json test

--- src/cc_catalog_airflow/dags/provider_api_scripts/common/test_requester.py
+++ src/cc_catalog_airflow/dags/provider_api_scripts/common/test_requester.py
@@ -44,7 +44,6 @@ def test_get_response_json_retries_with_none_response():
             assert dq.get_response_json(
                 'https://google.com/',
                 retries=2,
-                query_params={}
             )
 
     assert mock_get.call_count == 3
@@ -64,7 +63,6 @@ def test_get_response_json_retries_with_non_ok():
             assert dq.get_response_json(
                 'https://google.com/',
                 retries=2,
-                query_params={}
             )
 
     assert mock_get.call_count == 3
@@ -84,7 +82,6 @@ def test_get_response_json_retries_with_error_json():
             assert dq.get_response_json(
                 'https://google.com/',
                 retries=2,
-                query_params={}
             )
 
     assert mock_get.call_count == 3
@@ -104,7 +101,6 @@ def test_get_response_json_returns_response_json_when_all_ok():
         actual_response_json = dq.get_response_json(
             'https://google.com/',
             retries=2,
-            query_params={}
         )
 
     assert mock_get.call_count == 1
