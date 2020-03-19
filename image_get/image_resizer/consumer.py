0829c97cf77a35769a21b82be372a2b906f07857
Alden S. Page <alden@alden.page>
2020-03-19 15:19:26 -0400

    Instead of the aiokafka client, use synchronous pykafka with a wrapper. The pykafka client is more stable and actively maintained.

--- image_get/image_resizer/consumer.py
+++ image_get/image_resizer/consumer.py
@@ -1,8 +1,11 @@
 import settings
-from pykafka import KafkaClient
 import logging as log
 import asyncio
-import time
+import aiohttp
+from functools import partial
+from pykafka import KafkaClient
+from io import BytesIO
+from PIL import Image
 
 
 def kafka_connect():
@@ -10,8 +13,25 @@ def kafka_connect():
     return client
 
 
-async def poll_topic():
-    pass
+def _parse_message(message):
+    log.debug(f'Received {message}')
+    return str(message.value, 'utf-8')
+
+
+async def poll_consumer(consumer, batch_size):
+    """
+    Poll the Kafka consumer for a batch of messages and parse them.
+    :param consumer:
+    :param batch_size:
+    :return:
+    """
+    batch = []
+    for idx, message in enumerate(consumer):
+        parsed = _parse_message(message)
+        batch.append(parsed)
+        if idx >= batch_size:
+            break
+    return batch
 
 
 async def consume(kafka_topic):
@@ -19,34 +39,31 @@ async def consume(kafka_topic):
     Listen for inbound image URLs.
     :return:
     """
-
-    topic = kafka_client.topics['inbound_images']
-    consumer = topic.get_balanced_consumer(
+    consumer = kafka_topic.get_balanced_consumer(
         consumer_group='image_spiders',
         auto_commit_enable=True,
         zookeeper_connect=settings.ZOOKEEPER_HOST
     )
-
-    urls = []
+    session = aiohttp.ClientSession()
     while True:
-        while len(urls) <= settings.BATCH_SIZE:
-            for message in consumer:
-                url = str(message.value, 'utf-8')
-                urls.append(url)
-        process_images(urls)
-        urls = []
-        time.sleep(0.01)
+        messages = await poll_consumer(consumer, settings.BATCH_SIZE)
+        # Schedule resizing tasks
+        tasks = []
+        for msg in messages:
+            tasks.append(process_image(session, msg))
+        await asyncio.gather(*tasks)
 
 
-def process_images(urls):
-    tasks = []
-    for url in urls:
-        tasks.append(process_image(url))
+def thumbnail_image(img: Image):
+    img.thumbnail(size=settings.TARGET_RESOLUTION, resample=Image.NEAREST)
 
 
-async def process_image(url):
-    """ Download and resize an image. """
-    pass
+async def process_image(session, url):
+    """Get an image, resize it, and upload it to S3."""
+    loop = asyncio.get_event_loop()
+    img_resp = await session.get(url)
+    img = Image.open(BytesIO(img_resp.content))
+    await loop.run_in_executor(None, partial(thumbnail_image, img))
 
 
 if __name__ == '__main__':
