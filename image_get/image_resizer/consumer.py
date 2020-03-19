996618c92fd5cf4b3175cb6961f26e3ddf308f57
Alden S. Page <alden@alden.page>
2020-03-19 18:09:56 -0400

    Commit consumer group offset to prevent repeat processing of images

--- image_get/image_resizer/consumer.py
+++ image_get/image_resizer/consumer.py
@@ -27,22 +27,20 @@ async def poll_consumer(consumer, batch_size):
     :return:
     """
     batch = []
-    # Track how much time has passed since the last message arrived.
-    # If too much time passes, we don't need to keep waiting for a full batch.
+    # Consume messages until either batch_size has been reached or the max
+    # wait time has occurred.
     max_wait_seconds = 3
     elapsed_time = 0
     last_msg_time = dt.datetime.now()
-    count = 0
-    while count < batch_size and elapsed_time < max_wait_seconds:
+    msg_count = 0
+    while msg_count < batch_size and elapsed_time < max_wait_seconds:
         message = consumer.consume(block=False)
         if message:
             parsed = _parse_message(message)
-            log.debug(f'Received {parsed}')
             batch.append(parsed)
             last_msg_time = dt.datetime.now()
-            count += 1
+            msg_count += 1
         elapsed_time = (dt.datetime.now() - last_msg_time).total_seconds()
-    log.debug(f'Firing event batch. count, time: {count}, {elapsed_time}')
     return batch
 
 
@@ -57,18 +55,30 @@ async def consume(kafka_topic):
         zookeeper_connect=settings.ZOOKEEPER_HOST
     )
     session = aiohttp.ClientSession()
+    total = 0
     while True:
+        start = dt.datetime.now()
         messages = await poll_consumer(consumer, settings.BATCH_SIZE)
         # Schedule resizing tasks
         tasks = []
         for msg in messages:
             tasks.append(process_image(session, msg))
+        if tasks:
+            log.info('Processing image batch of size {}'.format(len(tasks)))
+            total += len(tasks)
+            total_time = (dt.datetime.now() - start).total_seconds()
+            log.info(f'Last batch took {total_time}s. Total resized so far:'
+                     f' {total}')
         await asyncio.gather(*tasks)
+        consumer.commit_offsets()
 
 
 def thumbnail_image(img: Image):
     img.thumbnail(size=settings.TARGET_RESOLUTION, resample=Image.NEAREST)
-    log.debug('Resized image')
+
+
+def save_thumbnail(img):
+    pass
 
 
 async def process_image(session, url):
@@ -78,6 +88,8 @@ async def process_image(session, url):
     buffer = BytesIO(await img_resp.read())
     img = Image.open(buffer)
     await loop.run_in_executor(None, partial(thumbnail_image, img))
+    await loop.run_in_executor(None, partial(save_thumbnail, img))
+    log.debug(f'Successfully resized image {url}')
 
 
 if __name__ == '__main__':
--- image_get/image_resizer/dummy_producer.py
+++ image_get/image_resizer/dummy_producer.py
@@ -1,4 +1,5 @@
 from pykafka import KafkaClient
+from itertools import cycle, islice
 
 urls = [
     'https://live.staticflickr.com/7454/8728178381_00be690ebc_b.jpg',
@@ -9,6 +10,6 @@ urls = [
 client = KafkaClient(hosts='kafka:9092')
 topic = client.topics['inbound_images']
 with topic.get_sync_producer() as producer:
-    for url in urls:
+    for url in islice(cycle(urls), 500):
         producer.produce(bytes(url, 'utf-8'))
 
