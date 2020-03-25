d92aeb96c6c76e82fea31f14d1118472f144fc77
Breno Ferreira <bferreira@windowslive.com>
2020-03-25 14:44:26 -0300

    hides the search rating after click

--- src/components/SearchRating.vue
+++ src/components/SearchRating.vue
@@ -1,30 +1,48 @@
 <template>
   <div class="caption">
-    <span>Are these results relevant?:</span>
-    <button class="button is-text tiny is-paddingless rating is-shadowless"
-            @click="sendSearchRatingEvent(true)">
-      Yes
-    </button>
-    •
-    <button class="button is-text tiny is-paddingless rating is-shadowless"
-            @click="sendSearchRatingEvent(false)">
-      No
-    </button>
+    <div v-if="status == 'NOT_SENT'">
+      <span>Are these results relevant?</span>
+      <button class="button is-text tiny is-paddingless rating is-shadowless"
+              @click="sendSearchRatingEvent(true)">
+        Yes
+      </button>
+      •
+      <button class="button is-text tiny is-paddingless rating is-shadowless"
+              @click="sendSearchRatingEvent(false)">
+        No
+      </button>
+    </div>
+    <div v-else-if="status == 'SENT'">
+      <span class="thank-you">Thank you for the feedback!</span>
+    </div>
   </div>
 </template>
 
 <script>
 import { SEND_SEARCH_RATING_EVENT } from '@/store/usage-data-analytics-types';
 
+const Statuses = {
+  NOT_SENT: 'NOT_SENT',
+  SENT: 'SENT',
+};
+
 export default {
   name: 'search-rating',
   props: ['searchTerm'],
+  data() {
+    return {
+      status: Statuses.NOT_SENT,
+    };
+  },
   methods: {
     sendSearchRatingEvent(isRelevant) {
       this.$store.dispatch(SEND_SEARCH_RATING_EVENT, {
         query: this.$props.searchTerm,
         relevant: isRelevant,
       });
+
+      this.status = Statuses.SENT;
+      setTimeout(() => { this.status = null; }, 1500);
     },
   },
 };
