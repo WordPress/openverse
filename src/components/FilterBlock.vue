9c8923c1a1ac0a1f1654161773eec746bddb0b39
Ayan Choudhary <darkrider251099@gmail.com>
2020-04-16 00:32:12 +0530

    fixed label rendering, refactored query parser and configured filter removal onClick

--- src/components/FilterBlock.vue
+++ src/components/FilterBlock.vue
@@ -1,11 +1,24 @@
 <template>
-    <span>
-        {{ filter.name }}
+    <span class="button tag" v-if="filterType === 'searchByCreator'">
+      <span>Creator</span>
+      <span @click="onClickCreator">&times;</span>
+    </span>
+    <span class="button tag" v-else>
+      <span>{{ filter.name }}</span>
+      <span @click="onClick">&times;</span>
     </span>
 </template>
 <script>
 export default {
   name: 'filter-block',
-  props: ['filter'],
+  props: ['filter', 'filterType'],
+  methods: {
+    onClick() {
+      this.$emit('filterChanged', { code: this.filter.code, filterType: this.$props.filterType });
+    },
+    onClickCreator() {
+      this.$emit('filterChanged', { code: this.filter.code });
+    },
+  },
 };
 </script>
--- src/components/FilterDisplay.vue
+++ src/components/FilterDisplay.vue
@@ -1,75 +1,151 @@
 <template>
     <div class="padding-horizontal-normal">
-        <span>Filter By</span>
-        <span v-for="filter in filters" :key="filter"><filter-block :filter="filter"/></span>
+        <span class="caption has-text-weight-semibold">Filter By</span>
+        <span v-for="filter in licenseFilters" :key="filter">
+          <filter-block :filter="filter"
+                        filterType="licenses"
+                        @filterChanged="onUpdateFilter" />
+        </span>
+        <span v-for="filter in licenseTypeFilters" :key="filter">
+          <filter-block :filter="filter"
+                        filterType="licenseTypes"
+                        @filterChanged="onUpdateFilter" />
+        </span>
+        <span v-for="filter in categoryFilters" :key="filter">
+          <filter-block :filter="filter"
+                        filterType="categories"
+                        @filterChanged="onUpdateFilter" />
+        </span>
+        <span v-for="filter in extensionFilters" :key="filter">
+          <filter-block :filter="filter"
+                        filterType="extensions"
+                        @filterChanged="onUpdateFilter" />
+        </span>
+        <span v-for="filter in aspectRatioFilters" :key="filter">
+          <filter-block :filter="filter"
+                        filterType="aspectRatios"
+                        @filterChanged="onUpdateFilter" />
+        </span>
+        <span v-for="filter in sizeFilters" :key="filter">
+          <filter-block :filter="filter"
+                        filterType="sizes"
+                        @filterChanged="onUpdateFilter" />
+        </span>
+        <span v-for="filter in sourceFilters" :key="filter">
+          <filter-block :filter="filter"
+                        filterType="providers"
+                        @filterChanged="onUpdateFilter" />
+        </span>
+        <span>
+          <filter-block v-if="searchByFilters"
+                        :filter="searchByFilters"
+                        filterType="searchByCreator"
+                        @filterChanged="onUpdateSearchByCreator" />
+        </span>
     </div>
 </template>
 <script>
+import { TOGGLE_FILTER } from '@/store/action-types';
 import FilterBlock from '@/components/FilterBlock';
 
 export default {
   name: 'filter-display',
-  props: ['query'],
+  props: ['query', 'isCollectionsPage', 'provider'],
   components: {
     FilterBlock,
   },
   computed: {
-    filters() {
+    licenseFilters() {
       const filterTags = [];
-      const filters = this.$store.state.filters;
-      const licenseFilters = this.query.license.split(',');
-      const licenseTypeFilters = this.query.license_type.split(',');
-      const categoryFilters = this.query.categories.split(',');
-      const extensionFilters = this.query.extension.split(',');
-      const aspectRatioFilters = this.query.aspect_ratio.split(',');
-      const sizeFilters = this.query.size.split(',');
-      const sourceFilters = this.query.source.split(',');
-      licenseFilters.forEach((filter) => {
-        const filterObj = filters.licenses.find(o => o.code === filter);
+      this.query.license.split(',').forEach((filter) => {
+        const filterObj = this.$store.state.filters.licenses.find(o => o.code === filter);
         if (filterObj) {
           filterTags.push(filterObj);
         }
       });
-      licenseTypeFilters.forEach((filter) => {
-        const filterObj = filters.licenseTypes.find(o => o.code === filter);
-        if (filterObj) {
-          filterTags.push(filterObj);
-        }
-      });
-      categoryFilters.forEach((filter) => {
-        const filterObj = filters.categories.find(o => o.code === filter);
-        if (filterObj) {
-          filterTags.push(filterObj);
-        }
-      });
-      extensionFilters.forEach((filter) => {
-        const filterObj = filters.extensions.find(o => o.code === filter);
-        if (filterObj) {
-          filterTags.push(filterObj);
-        }
-      });
-      aspectRatioFilters.forEach((filter) => {
-        const filterObj = filters.aspectRatios.find(o => o.code === filter);
-        if (filterObj) {
-          filterTags.push(filterObj);
-        }
-      });
-      sizeFilters.forEach((filter) => {
-        const filterObj = filters.sizes.find(o => o.code === filter);
-        if (filterObj) {
-          filterTags.push(filterObj);
-        }
-      });
-      sourceFilters.forEach((filter) => {
-        const filterObj = filters.providers.find(o => o.code === filter);
-        if (filterObj) {
-          filterTags.push(filterObj);
-        }
-      });
-      if (filters.searchBy.creator) {
-        filterTags.push(filters.searchBy);
-      }
       return filterTags;
+    },
+    licenseTypeFilters() {
+      const filterTags = [];
+      this.query.license_type.split(',').forEach((filter) => {
+        const filterObj = this.$store.state.filters.licenseTypes.find(o => o.code === filter);
+        if (filterObj) {
+          filterTags.push(filterObj);
+        }
+      });
+      return filterTags;
+    },
+    categoryFilters() {
+      const filterTags = [];
+      this.query.categories.split(',').forEach((filter) => {
+        const filterObj = this.$store.state.filters.categories.find(o => o.code === filter);
+        if (filterObj) {
+          filterTags.push(filterObj);
+        }
+      });
+      return filterTags;
+    },
+    extensionFilters() {
+      const filterTags = [];
+      this.query.extension.split(',').forEach((filter) => {
+        const filterObj = this.$store.state.filters.extensions.find(o => o.code === filter);
+        if (filterObj) {
+          filterTags.push(filterObj);
+        }
+      });
+      return filterTags;
+    },
+    aspectRatioFilters() {
+      const filterTags = [];
+      this.query.aspect_ratio.split(',').forEach((filter) => {
+        const filterObj = this.$store.state.filters.aspectRatios.find(o => o.code === filter);
+        if (filterObj) {
+          filterTags.push(filterObj);
+        }
+      });
+      return filterTags;
+    },
+    sizeFilters() {
+      const filterTags = [];
+      this.query.size.split(',').forEach((filter) => {
+        const filterObj = this.$store.state.filters.sizes.find(o => o.code === filter);
+        if (filterObj) {
+          filterTags.push(filterObj);
+        }
+      });
+      return filterTags;
+    },
+    sourceFilters() {
+      const filterTags = [];
+      this.query.source.split(',').forEach((filter) => {
+        const filterObj = this.$store.state.filters.providers.find(o => o.code === filter);
+        if (filterObj) {
+          filterTags.push(filterObj);
+        }
+      });
+      return filterTags;
+    },
+    searchByFilters() {
+      return this.$store.state.filters.searchBy.creator ? this.$store.state.filters.searchBy : null;
+    },
+  },
+  methods: {
+    onUpdateFilter({ code, filterType }) {
+      this.$store.dispatch(TOGGLE_FILTER, {
+        code,
+        filterType,
+        isCollectionsPage: this.$props.isCollectionsPage,
+        provider: this.$props.provider,
+        shouldNavigate: true,
+      });
+    },
+    onUpdateSearchByCreator() {
+      this.$store.dispatch(TOGGLE_FILTER, {
+        filterType: 'searchBy',
+        isCollectionsPage: this.$props.isCollectionsPage,
+        provider: this.$props.provider,
+        shouldNavigate: true,
+      });
     },
   },
 };
