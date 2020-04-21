68e4be2e3cd96b046b077418639b7ea9441bf879
Brent Moran <brent@creativecommons.org>
2020-04-21 12:14:47 +0200

    add basic tests for s3 copying method

--- src/cc_catalog_airflow/dags/util/loader/s3.py
+++ src/cc_catalog_airflow/dags/util/loader/s3.py
@@ -3,8 +3,6 @@ import os
 
 from airflow.hooks.S3_hook import S3Hook
 
-import boto3
-
 logger = logging.getLogger(__name__)
 
 DEFAULT_MEDIA_PREFIX = 'image'
@@ -23,42 +21,19 @@ def copy_file_to_s3_staging(
     logger.info(f'Creating staging object in s3_bucket:  {s3_bucket}')
     s3 = S3Hook(aws_conn_id=aws_conn_id)
     file_name = os.path.split(tsv_file_path)[1]
-    staging_prefix = _get_staging_prefix(identifier)
-    staging_key = _s3_join_path(staging_prefix, file_name)
+    staging_object_prefix = _get_staging_object_prefix(
+        identifier,
+        media_prefix,
+        staging_prefix
+    )
+    staging_key = _s3_join_path(staging_object_prefix, file_name)
     s3.load_file(tsv_file_path, staging_key, bucket_name=s3_bucket)
 
 
-def create_staging_object(
+def _get_staging_object_prefix(
         identifier,
-        tsv_file_name,
-        staging_prefix=STAGING_PREFIX,
-        s3_bucket=None,
-        access_key=None,
-        secret_key=None,
-):
-    s3_bucket = s3_bucket or os.getenv('CCCATALOG_STORAGE_BUCKET')
-    access_key = access_key or os.getenv('AWS_ACCESS_KEY')
-    secret_key = secret_key or os.getenv('AWS_SECRET_KEY')
-    logger.info(f'Creating staging object in s3_bucket:  {s3_bucket}')
-    file_name = os.path.split(tsv_file_name)[1]
-    s3_object_key = _s3_join_path(staging_prefix, identifier, file_name)
-    logger.info(f's3_object_key:  {s3_object_key}')
-    s3_object = (
-        boto3
-        .resource(
-            's3',
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key
-        )
-        .Object(s3_bucket, s3_object_key)
-    )
-    return s3_object
-
-
-def _get_staging_prefix(
-        identifier,
-        media_prefix=DEFAULT_MEDIA_PREFIX,
-        staging_prefix=STAGING_PREFIX,
+        media_prefix,
+        staging_prefix,
 ):
     return _s3_join_path(media_prefix, staging_prefix, identifier)
 
