e91097c69c7ab3a085023f2c082e1c94c81605f8
Breno Ferreira <bferreira@windowslive.com>
2020-04-23 17:14:45 -0300

    wip copyright and done components

--- src/components/ContentReportForm.vue
+++ src/components/ContentReportForm.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="card padding-normal is-clearfix">
-    <h4 class="b-header">Report this content</h4>
-
-    <form v-if="!selectedOther">
+    <copyright-notice v-if="selectedCopyright"
+                      :imageURL="imageURL"
+                      :dcmaFormUrl="dcmaFormUrl" />
+    <form v-else-if="!selectedOther">
+      <h4 class="b-header">Report this content</h4>
       <fieldset>
         <legend class="margin-bottom-small">What's the issue?</legend>
 
@@ -20,6 +22,8 @@
           <input type="radio" name="type" id="other" value="other" v-model="selectedReason">
           <label for="other" class="margin-left-small">Other</label>
         </div>
+
+        <span class="caption has-text-weight-semibold has-text-grey">For security purposes, CC collects and retains anonymized IP addresses of those who complete and submit this form.</span>
       </fieldset>
 
       <button type="button"
@@ -28,15 +32,16 @@
         Next
       </button>
     </form>
-    <form v-else>
+    <form v-else-if="selectedOther">
       <legend class="margin-bottom-small">Please describe the issue for us</legend>
-
-      <textarea class="reason" v-model="otherReasonDescription" placeholder="Issue description required" />
+      <textarea class="reason"
+                v-model="otherReasonDescription"
+                placeholder="Issue description required" />
 
       <div>
         <button class="button is-text tiny padding-top-normal is-shadowless"
                 @click="onBackClick()">
-          <img alt="back" class="back margin-right-small" src="@/assets/back_arrow.svg" />
+          <i class="icon chevron-left margin-right-small"></i> Back
         </button>
 
         <button type="button"
@@ -50,20 +55,51 @@
 </template>
 
 <script>
+import Vue from 'vue';
 import { SEND_CONTENT_REPORT } from '@/store/action-types';
+
+const DCMA_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSdZLZpYJGegL8G2FsEAHNsR1nqVx1Wxfp-oj3o0h8rqe9j8dg/viewform';
+
+const CopyrightNotice = Vue.component('copyright-notice', {
+  props: ['dcmaFormUrl', 'imageURL'],
+  template: `<div>
+    <span class="is-block">Please fill out <a href={{ this.DCMA_FORM_URL }} target="_blank" rel="noopener">this DMCA form</a> to report copyright infringement.</span>
+    <span>We recommend doing the same <a href={{ this.imageURL }} target="_blank" rel="noopener">at the source</a>.</span>
+  </div>`,
+});
+
+const DoneMessage = Vue.component('done-message', {
+  props: ['imageURL'],
+  template: `<div>
+    <span class="is-block">Thank you for reporting an issue with the results of CC Search!</span>
+    <span>We recommend doing the same <a href={{ this.imageURL }} target="_blank" rel="noopener">at the source</a>.</span>
+  </div>`,
+});
 
 export default {
   name: 'content-report-form',
-  props: ['imageId'],
+  props: ['imageId', 'imageURL'],
+  components: {
+    CopyrightNotice,
+    DoneMessage,
+  },
   data() {
     return {
       selectedReason: null,
       selectedOther: false,
+      selectedCopyright: false,
       otherReasonDescription: '',
+      dcmaFormUrl: DCMA_FORM_URL,
     };
+  },
+  computed: {
+
   },
   methods: {
     onIssueSelected() {
+      if (this.selectedReason === 'copyright') {
+        this.selectedCopyright = true;
+      }
       if (this.selectedReason === 'other') {
         this.selectedOther = true;
       }
--- src/store/report-content-store.js
+++ src/store/report-content-store.js
@@ -1,4 +1,4 @@
-import { TOGGLE_REPORT_FORM_VISIBILITY, REPORT_SENT } from './mutation-types';
+import { TOGGLE_REPORT_FORM_VISIBILITY, REPORT_SENT, REPORT_FORM_CLOSED } from './mutation-types';
 import { SEND_CONTENT_REPORT } from './action-types';
 
 const state = {
@@ -14,18 +14,16 @@ const mutations = {
   [REPORT_SENT](_state) {
     _state.isReportSent = true;
   },
+  [REPORT_FORM_CLOSED](_state) {
+    _state.isReportSent = false;
+    _state.isReportFormVisible = false;
+  },
 };
 
-const DCMA_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSdZLZpYJGegL8G2FsEAHNsR1nqVx1Wxfp-oj3o0h8rqe9j8dg/viewform';
 
 const actions = ReportService => ({
   [SEND_CONTENT_REPORT]({ commit }, params) {
-    if (params.reason === 'copyright') {
-      window.open(DCMA_FORM);
-    }
-    else {
-      ReportService.sendReport(params).then(() => commit(REPORT_SENT));
-    }
+    ReportService.sendReport(params).then(() => commit(REPORT_SENT));
   },
 });
 
