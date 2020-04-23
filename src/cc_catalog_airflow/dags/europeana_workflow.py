0cedb6ac400ca9ea455ca69583b511465af266d1
healthyCoder <allenabraham505@gmail.com>
2020-04-23 20:35:50 +0530

    Added DAG and corresponding test file New file to create DAG to execute Europeana's script Test suite to check for any import errors and the number of DAGs created  Changes to be committed: 	new file:   dags/europeana_workflow.py 	new file:   dags/test_europeana_workflow.py

---
+++ src/cc_catalog_airflow/dags/europeana_workflow.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from provider_api_scripts import europeana
+from util.operator_util import get_log_operator
+
+
+DAG_DEFAULT_ARGS = {
+    'owner': 'data-eng-admin',
+    'depends_on_past': False,
+    'start_date': datetime(1970, 1, 1),
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=15),
+}
+
+DAG_ID = 'europeana_workflow'
+
+
+def get_runner_operator(dag):
+    return PythonOperator(
+        task_id='pull_europeana_data',
+        python_callable=europeana.main,
+        op_args=['{{ ds }}'],
+        depends_on_past=False,
+        dag=dag
+    )
+
+
+def create_dag():
+    dag = DAG(
+        dag_id=DAG_ID,
+        default_args=DAG_DEFAULT_ARGS,
+        start_date=datetime(1970, 1, 1),
+        schedule_interval='@monthly',
+        catchup=False,
+    )
+
+    with dag:
+        start_task = get_log_operator(dag, DAG_ID, 'Starting')
+        run_task = get_runner_operator(dag)
+        end_task = get_log_operator(dag, DAG_ID, 'Finished')
+
+        start_task >> run_task >> end_task
+
+    return dag
+
+
+globals()[DAG_ID] = create_dag()
