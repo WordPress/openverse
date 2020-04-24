3ca6f611a684c39723ea57c138411fbb4d190602
Breno Ferreira <bferreira@windowslive.com>
2020-04-24 14:53:59 -0300

    report error component

---
+++ src/components/ContentReport/Error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span class="is-block">
+      There was an error with the report submission. Please try again.
+    </span>
+    <button class="button is-text tiny margin-top-normal is-shadowless"
+            @click="onBackClick()">
+      <span><i class="icon chevron-left margin-right-small"></i> Back</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import { BACK_FROM_REPORT_ERROR } from '@/store/mutation-types';
+
+export default {
+  name: 'report-error',
+  methods: {
+    onBackClick() {
+      this.$store.commit(BACK_FROM_REPORT_ERROR);
+    },
+  },
+};
+</script>
