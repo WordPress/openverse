a1dde28fdd318aaf22d0bd8f6da3042c8047a47e
Alden S. Page <alden@alden.page>
2020-04-24 16:10:41 -0400

    Use PENDING constant

--- cccatalog-api/cccatalog/api/models.py
+++ cccatalog-api/cccatalog/api/models.py
@@ -331,7 +331,7 @@ class ImageReport(models.Model):
     reason = models.CharField(max_length=20, choices=REPORT_CHOICES)
     description = models.TextField(max_length=500, blank=True, null=True)
     status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default='pending'
+        max_length=20, choices=STATUS_CHOICES, default=PENDING
     )
 
     class Meta:
