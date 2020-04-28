ee1e61b9ee36541200aac9f9601c672079c96b38
Brent Moran <brent@creativecommons.org>
2020-04-28 17:29:32 +0200

    enable pulling from Smithsonian API endpoint.
    Note that we're only using one unit code, ACM so far.  We'll need
    to loop through all unit codes as a next step.

--- src/cc_catalog_airflow/dags/provider_api_scripts/smithsonian.py
+++ src/cc_catalog_airflow/dags/provider_api_scripts/smithsonian.py
@@ -8,47 +8,65 @@ Output:            TSV file containing the images and the respective
 
 Notes:             None
 """
-import json
 import logging
 import os
 
 from common.storage import image
+from common import requester
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s:  %(message)s',
     level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
-DIRECTORY = os.getenv('SI_EXAMPLE_DIRECTORY')
+
+API_KEY = os.getenv('DATA_GOV_API_KEY')
+DELAY = 5.0
+LIMIT = 200  # number of rows to pull at once
+ENDPOINT = 'https://api.si.edu/openaccess/api/v1.0/search'
 PROVIDER = 'smithsonian'
 ZERO_URL = 'https://creativecommons.org/publicdomain/zero/1.0/'
+DEFAULT_PARAMS = {
+    'api_key': API_KEY,
+    'rows': LIMIT
+}
 
 image_store = image.ImageStore(provider=PROVIDER)
+delayed_requester = requester.DelayedRequester(delay=DELAY)
 
 
 def main():
-    file_list = _get_file_list()
-    logger.info(f'Processing files:\n{file_list}')
-    for file_path in file_list:
-        with open(file_path) as f:
-            response_json = json.loads(f.read())
-        search_term = file_path.split('/')[-1].split('.')[0]
-        _process_response_json(search_term, response_json)
+    _process_unit_code('ACM')
     total_images = image_store.commit()
     logger.info(f'Total images:  {total_images}')
 
 
-def _get_file_list(directory=DIRECTORY):
-    return [
-        os.path.join(directory, f)
-        for f in os.listdir(directory)
-        if f[-5:] == '.json'
-    ]
+def _process_unit_code(unit_code, endpoint=ENDPOINT, limit=LIMIT):
+    logger.info(f'Processing unit code:  {unit_code}')
+    row_offset = 0
+    total_rows = 1
+    while row_offset < total_rows:
+        query_params = _build_query_params(unit_code, row_offset)
+        response_json = delayed_requester.get_response_json(
+            endpoint,
+            retries=3,
+            query_params=query_params
+        )
+        logger.info(f'response:  {response_json.keys()}')
+        _process_response_json(response_json)
+        total_rows = response_json.get('response', {}).get('rowCount', 0)
+        row_offset += limit
 
 
-def _process_response_json(search_term, response_json):
-    logger.info(f'processing response for {search_term}')
+def _build_query_params(unit_code, row_offset, default_params=DEFAULT_PARAMS):
+    query_params = default_params.copy()
+    query_string = f'unit_code:{unit_code} AND online_media_type:Images'
+    query_params.update(q=query_string, start=row_offset)
+    return query_params
+
+
+def _process_response_json(response_json):
+    logger.info('processing response')
     rows = response_json.get('response', {}).get('rows', [])
     for row in rows:
         image_list = (
@@ -60,22 +78,25 @@ def _process_response_json(search_term, response_json):
         )
         if image_list is not None:
             title = row.get('title')
-            _process_image_list(title, image_list, search_term)
+            foreign_landing_url = row.get('content', {}).get('descriptiveNonRepeating', {}).get('guid')
+            _process_image_list(title, foreign_landing_url, image_list)
 
 
-def _process_image_list(title, image_list, search_term, license_url=ZERO_URL):
+def _process_image_list(title, foreign_landing_url, image_list, license_url=ZERO_URL):
     for image_data in image_list:
-        if image_data.get('type') == 'Images' and image_data.get('usage', {}).get('access') == 'CC0':
+        if (
+                image_data.get('type') == 'Images'
+                and image_data.get('usage', {}).get('access') == 'CC0'
+        ):
             image_store.add_item(
-                foreign_landing_url=image_data.get('guid'),
+                foreign_landing_url=foreign_landing_url,
                 image_url=image_data.get('content'),
                 thumbnail_url=image_data.get('thumbnail'),
                 license_url=license_url,
                 foreign_identifier=image_data.get('idsId'),
                 title=title,
-                meta_data=None,
-                raw_tags=[search_term],
             )
 
 
-
+if __name__ == '__main__':
+    main()
