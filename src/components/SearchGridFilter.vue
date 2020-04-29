2e02d1a9244abe997bf683d7379f5b293c33c931
abhisheknaiidu <geekyabhi12@gmail.com>
2020-04-30 00:17:20 +0530

    added css to class

--- src/components/SearchGridFilter.vue
+++ src/components/SearchGridFilter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="content-wrapper" :class="{ 'search-filters': true,
+  <div :class="{ 'search-filters': true,
                  'search-filters__visible': isFilterVisible, }">
     <form class="filters-form" role="filter">
       <filter-check-list :options="filters.licenseTypes"
@@ -124,14 +124,11 @@ export default {
   }
 
   &__visible {
+    height: 100vh;
+    overflow-y: scroll;
     border-top: 1px solid #e8e8e8;
     display: block;
   }
-}
-
-#content-wrapper {
-  height: 100vh;
-  overflow-y: scroll;
 }
 
 </style>
