ab8e59d822d889d4c5f47e04e65c4d2dd49b7d1a
abhisheknaiidu <geekyabhi12@gmail.com>
2020-04-30 00:26:36 +0530

    Fixes lint error

--- test/unit/specs/components/image-social-share.spec.js
+++ test/unit/specs/components/image-social-share.spec.js
@@ -33,10 +33,10 @@ describe('ImageSocialShare', () => {
 
   it('should invoke social share buttons with the right props', () => {
     const wrapper = render(ImageSocialShare, options);
-    const source_url = options.propsData.image.foreign_landing_url;
+    const sourceURL = options.propsData.image.foreign_landing_url;
     const url = options.propsData.image.url;
     expect(wrapper.vm.imageURL).toBe(url);
-    expect(wrapper.vm.imageSourceURL).toBe(source_url);
-    expect(wrapper.vm.shareText).toBe(encodeURI(`I found an image through CC Search @creativecommons: ${source_url}`));
+    expect(wrapper.vm.imageSourceURL).toBe(sourceURL);
+    expect(wrapper.vm.shareText).toBe(encodeURI(`I found an image through CC Search @creativecommons: ${sourceURL}`));
   });
 });
