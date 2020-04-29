2852ff67b84e815b27a98092e0a95349ac42cde9
abhisheknaiidu <geekyabhi12@gmail.com>
2020-04-30 00:04:59 +0530

    Added tests

--- test/unit/specs/components/image-social-share.spec.js
+++ test/unit/specs/components/image-social-share.spec.js
@@ -33,8 +33,10 @@ describe('ImageSocialShare', () => {
 
   it('should invoke social share buttons with the right props', () => {
     const wrapper = render(ImageSocialShare, options);
-    const url = options.propsData.image.foreign_landing_url;
+    const source_url = options.propsData.image.foreign_landing_url;
+    const url = options.propsData.image.url;
     expect(wrapper.vm.imageURL).toBe(url);
-    expect(wrapper.vm.shareText).toBe(encodeURI(`I found an image through CC Search @creativecommons: ${url}`));
+    expect(wrapper.vm.imageSourceURL).toBe(source_url);
+    expect(wrapper.vm.shareText).toBe(encodeURI(`I found an image through CC Search @creativecommons: ${source_url}`));
   });
 });
