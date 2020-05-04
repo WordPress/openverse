55e8f1b3b2ca24c1e2632dc72450d3fa8996de57
Breno Ferreira <bferreira@windowslive.com>
2020-05-04 14:33:24 -0300

    removed unnecessary code in legal disclaimer

--- src/components/ImageDetails/LegalDisclaimer.vue
+++ src/components/ImageDetails/LegalDisclaimer.vue
@@ -1,34 +1,17 @@
 <template>
-  <div v-if="sourceURL" class="legal-disclaimer">
-    <h4 class="b-header">Verify at the source</h4>
-    <div class="margin-top-normal">
-      <ul>
-        <li class="caption margin-bottom-small">
-          CC Search aggregates data from publicly available repositories of open content.
-        </li>
-        <li class="caption margin-bottom-small">
-          CC does not host the content and does not verify that the content is properly
-          CC-licensed or that the attribution information is accurate or complete.
-        </li>
-        <li class="caption margin-bottom-small">
-          Please follow the link to the source of the content to independently verify before reuse.
-        </li>
-      </ul>
-    </div>
+  <div class="margin-top-normal">
+    <p class="caption has-text-left has-text-grey-dark has-text-weight-semibold">
+      CC Search aggregates data from publicly available repositories of open content.
+      CC does not host the content and does not verify that the content is properly
+      CC-licensed or that the attribution information is accurate or complete.
+      Please follow the link to the source of the content to independently verify before reuse.
+    </p>
   </div>
 </template>
 
 <script>
-import getProviderLogo from '@/utils/getProviderLogo';
-
 export default {
   name: 'legal-disclaimer',
-  props: ['source', 'sourceProviderCode', 'sourceURL'],
-  methods: {
-    getProviderLogo(providerName) {
-      return getProviderLogo(providerName);
-    },
-  },
 };
 </script>
 
