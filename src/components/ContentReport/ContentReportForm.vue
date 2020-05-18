b23a3615bc77fabc63748f2d37ef6872d00db785
Breno Ferreira <bferreira@windowslive.com>
2020-05-18 13:51:46 -0300

    refactor content report form

--- src/components/ContentReport/ContentReportForm.vue
+++ src/components/ContentReport/ContentReportForm.vue
@@ -8,12 +8,15 @@
                       :imageURL="imageURL"
                       :providerName="providerName"
                       :dmcaFormUrl="dmcaFormUrl"
-                      @onDmcaBackClick= "onDmcaBackClick()"/>
+                      @onBackClick="onBackClick()"/>
     <done-message v-else-if="!selectedCopyright && isReportSent"
                   :imageURL="imageURL"
                   :providerName="providerName" />
     <report-error v-else-if="reportFailed"/>
-    <form v-else-if="!selectedOther">
+    <other-issue-form v-else-if="selectedOther"
+                      @onBackClick="onBackClick()"
+                      @sendContentReport="sendContentReport" />
+    <form v-else>
       <h4 class="b-header">Report this content</h4>
       <fieldset class="margin-bottom-normal">
         <legend class="margin-bottom-small">What's the issue?</legend>
@@ -46,26 +49,6 @@
         Next
       </button>
     </form>
-    <form class="other-form" v-else-if="selectedOther">
-      <h4 class="b-header">Report this content</h4>
-      <legend class="margin-bottom-small">Please describe the issue for us</legend>
-      <textarea class="reason padding-small has-text-weight-semibold"
-                v-model="otherReasonDescription"
-                placeholder="Issue description required (with at least 20 characters)" />
-      <div>
-        <button class="button other-back-button is-text tiny margin-top-normal has-text-grey"
-                @click="onBackClick()">
-          <span><i class="icon chevron-left margin-right-small"></i> Back</span>
-        </button>
-
-        <button type="button"
-                :disabled="!descriptionHasMoreThan20Chars"
-                class="button submit-other-button tiny is-info margin-top-normal is-pulled-right"
-                @click="sendContentReport()">
-          Submit
-        </button>
-      </div>
-    </form>
   </div>
 </template>
 
@@ -73,6 +56,7 @@
 import { SEND_CONTENT_REPORT } from '@/store/action-types';
 import { REPORT_FORM_CLOSED } from '@/store/mutation-types';
 import dmcaNotice from './DmcaNotice';
+import OtherIssueForm from './OtherIssueForm';
 import DoneMessage from './DoneMessage';
 import ReportError from './ReportError';
 
@@ -85,13 +69,13 @@ export default {
     DoneMessage,
     dmcaNotice,
     ReportError,
+    OtherIssueForm,
   },
   data() {
     return {
       selectedReason: null,
       selectedOther: false,
       selectedCopyright: false,
-      otherReasonDescription: '',
       dmcaFormUrl,
     };
   },
@@ -101,9 +85,6 @@ export default {
     },
     reportFailed() {
       return this.$store.state.reportFailed;
-    },
-    descriptionHasMoreThan20Chars() {
-      return this.otherReasonDescription.length >= 20;
     },
   },
   methods: {
@@ -120,15 +101,13 @@ export default {
     },
     onBackClick() {
       this.selectedOther = false;
-    },
-    onDmcaBackClick() {
       this.selectedCopyright = false;
     },
-    sendContentReport() {
+    sendContentReport(description = '') {
       this.$store.dispatch(SEND_CONTENT_REPORT, {
         identifier: this.$props.imageId,
         reason: this.selectedReason,
-        description: this.otherReasonDescription,
+        description,
       });
     },
     closeForm() {
@@ -138,11 +117,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.reason {
-  width: 100%;
-  height: 6rem;
-  font-size: 13px;
-  font-family: Source Sans Pro;
-}
-</style>
+
