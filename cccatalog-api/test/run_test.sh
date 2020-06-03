7512c56fdd54debc3d64fbac7f0080720d692671
Tanuj22 <agarwal.5@iitj.ac.in>
2020-06-03 16:04:42 +0530

    test secret key in run_test.sh and fix error with docker compose

--- cccatalog-api/test/run_test.sh
+++ cccatalog-api/test/run_test.sh
@@ -2,4 +2,4 @@
 # Local environments don't have valid certificates; suppress this warning.
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 export INTEGRATION_TEST_URL="http://localhost:8000"
-PYTHONPATH=. DJANGO_DATABASE_NAME='openledger' DJANGO_DATABASE_USER='deploy' DJANGO_DATABASE_PASSWORD='deploy' DJANGO_DATABASE_HOST='localhost' REDIS_HOST='localhost' pytest -s --disable-pytest-warnings test/v1_integration_test.py --ds=cccatalog.settings
+PYTHONPATH=. DJANGO_SECRET_KEY='ny#b__$f6ry4wy8oxre97&-68u_0lk3gw(z=d40_dxey3zw0v1' DJANGO_DATABASE_NAME='openledger' DJANGO_DATABASE_USER='deploy' DJANGO_DATABASE_PASSWORD='deploy' DJANGO_DATABASE_HOST='localhost' REDIS_HOST='localhost' pytest -s --disable-pytest-warnings test/v1_integration_test.py --ds=cccatalog.settings
