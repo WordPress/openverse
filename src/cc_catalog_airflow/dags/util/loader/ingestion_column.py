7ddcceaf3aa529c848877fd5aa4c2e5a36163b21
Brent Moran <brent@creativecommons.org>
2020-06-09 11:08:23 +0200

    add utility functions to migrate TSVs to new form

---
+++ src/cc_catalog_airflow/dags/util/loader/ingestion_column.py
@@ -0,0 +1,52 @@
+"""
+This module has a couple of temporarily-needed methods to add an
+ingestion_method column to TSV files before uploading them to S3 or
+PostgreSQL.
+"""
+import logging
+import os
+logger = logging.getLogger(__name__)
+
+
+def check_and_fix_tsv_file(tsv_file_name):
+    """
+    This function will check whether a TSV has the right number of
+    columns for the new DB schema, and attempt to add an
+    ingestion_method column if the number is one short.
+
+    It will also log a warning if the number is completely wrong.
+    """
+    logger.info(f'Checking for ingestion_method column in {tsv_file_name}')
+    old_cols_number = 17
+    new_cols_number = old_cols_number + 1
+    with open(tsv_file_name) as f:
+        test_line = f.readline()
+    line_list = [word.strip() for word in test_line.split('\t')]
+    if len(line_list) == old_cols_number:
+        _add_ingestion_method(tsv_file_name, line_list[-1])
+    elif len(line_list) == new_cols_number:
+        logger.info(
+            f'Found correct number of columns:  {new_cols_number}.'
+            '  Leaving file unchanged.'
+        )
+    else:
+        logger.warning(
+            'Wrong number of columns in file!  This cannot be fixed...'
+        )
+
+
+def _add_ingestion_method(tsv_file_name, source):
+    ingestion_method = source if source == 'commoncrawl' else 'provider_api'
+    logger.debug(f'Found source:  {source}')
+    logger.info(
+        f'Adding ingestion_method:  {ingestion_method} to {tsv_file_name}'
+    )
+    temp_tsv = tsv_file_name + '.new'
+    with open(tsv_file_name, 'r') as old_tsv, open(temp_tsv, 'w') as new_tsv:
+        old_line = old_tsv.readline().strip()
+        while old_line:
+            new_tsv.write(old_line + '\t' + ingestion_method + '\n')
+            old_line = old_tsv.readline().strip()
+
+    os.rename(tsv_file_name, tsv_file_name + '.old')
+    os.rename(temp_tsv, tsv_file_name)
