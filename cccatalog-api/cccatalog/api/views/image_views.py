b608cca194596ed283234b7a86b4164f30100a1d
Alden S. Page <alden@alden.page>
2020-06-22 14:36:53 -0400

    Use additional DETAIL_PROXY_URL for showing insecure HTTP images on detail pages

--- cccatalog-api/cccatalog/api/views/image_views.py
+++ cccatalog-api/cccatalog/api/views/image_views.py
@@ -15,7 +15,7 @@ from cccatalog.api.serializers.image_serializers import\
     InputErrorSerializer, ImageSearchQueryStringSerializer,\
     WatermarkQueryStringSerializer, ReportImageSerializer,\
     OembedSerializer
-from cccatalog.settings import THUMBNAIL_PROXY_URL
+from cccatalog.settings import DETAIL_PROXY_URL
 from cccatalog.api.utils.view_count import _get_user_ip
 from cccatalog.api.utils.watermark import watermark
 from django.http.response import HttpResponse, FileResponse
@@ -152,6 +152,14 @@ class ImageDetail(GenericAPIView, RetrieveModelMixin):
     def get(self, request, identifier, format=None, view_count=0):
         """ Get the details of a single list. """
         resp = self.retrieve(request, identifier)
+        # Proxy insecure HTTP images at full resolution.
+        if 'http://' in resp.data[search_controller.URL]:
+            original = resp.data[search_controller.URL]
+            secure = '{proxy_url}/{original}'.format(
+                proxy_url=DETAIL_PROXY_URL,
+                original=original
+            )
+            resp.data[search_controller.URL] = secure
 
         return resp
 
