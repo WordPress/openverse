625c670725b13efefa9df5020e6c3b0e2aea9c6f
kss682 <kss682@gmail.com>
2020-06-28 17:40:06 +0530

    test suite

--- src/cc_catalog_airflow/dags/provider_api_scripts/test_museum_victoria.py
+++ src/cc_catalog_airflow/dags/provider_api_scripts/test_museum_victoria.py
@@ -114,3 +114,124 @@ def test_get_batch_objects_error():
     
     assert actual_response is None
     assert mock_call.call_count == 3
+
+
+def test_get_media_info_success():
+    media = _get_resource_json("media_data_success.json")
+    actual_image_data = mv._get_media_info(media)
+    
+    expected_image_data = _get_resource_json("image_data_success.json")
+
+    assert actual_image_data == expected_image_data
+    
+
+def test_get_media_info_failure():
+    media = _get_resource_json("media_data_failure.json")
+    actual_image_data = mv._get_media_info(media)
+
+    assert len(actual_image_data) == 0
+
+
+def test_get_image_data_large():
+    image_data = _get_resource_json("large_image_data.json")
+
+    actual_image_url, actual_image_id,\
+    actual_height, actual_width = mv._get_image_data(
+        image_data
+    )
+
+    assert actual_image_url == "https://collections.museumsvictoria.com.au/content/media/45/329745-large.jpg"
+    assert actual_image_id == "45/329745"
+    assert actual_height == 2581
+    assert actual_width == 2785
+
+
+def test_get_image_data_medium():
+    image_data = _get_resource_json("medium_image_data.json")
+
+    actual_image_url, actual_image_id,\
+    actual_height, actual_width = mv._get_image_data(
+        image_data
+    )
+
+    assert actual_image_url == "https://collections.museumsvictoria.com.au/content/media/45/329745-medium.jpg"
+    assert actual_image_id == "45/329745"
+    assert actual_height == 1390
+    assert actual_width == 1500
+
+
+def test_get_image_data_small():
+    image_data = _get_resource_json("small_image_data.json")
+
+    actual_image_url, actual_image_id,\
+    actual_height, actual_width = mv._get_image_data(
+        image_data
+    )
+
+    assert actual_image_url == "https://collections.museumsvictoria.com.au/content/media/45/329745-small.jpg"
+    assert actual_image_id == "45/329745"
+    assert actual_height == 500
+    assert actual_width == 540
+
+
+def test_get_image_data_none():
+    image_data = {}
+
+    actual_image_url, actual_image_id,\
+    actual_height, actual_width = mv._get_image_data(
+        image_data
+    )
+
+    assert actual_image_url is None
+    assert actual_image_id is None
+    assert actual_height is None
+    assert actual_width is None
+
+
+def test_get_image_id():
+    image_url = "https://collections.museumsvictoria.com.au/content/media/45/329745-small.jpg"
+    actual_image_id = mv._get_image_id(image_url)
+    expected_image_id = "45/329745"
+
+    assert actual_image_id == expected_image_id
+
+
+def test_get_license_url():
+    media = _get_resource_json("cc_image_data.json")
+    actual_license_url = mv._get_license_url(media)
+    expected_license_url = "https://creativecommons.org/licenses/by/4.0"
+
+    assert actual_license_url == expected_license_url
+
+
+def test_get_license_url_failure():
+    media = _get_resource_json("media_data_failure.json")
+    actual_license_url = mv._get_license_url(media[0])
+
+    assert actual_license_url is None
+
+
+def test_get_metadata():
+    obj = _get_resource_json("batch_objects.json")
+    expected_metadata = _get_resource_json("metadata.json")
+    actual_metadata = mv._get_metadata(obj[0])
+
+    assert actual_metadata == expected_metadata
+
+
+def test_get_creator():
+    media = _get_resource_json("cc_image_data.json")
+    actual_creator = mv._get_creator(media)
+    
+    assert actual_creator == "Photographer: Deb Tout-Smith"
+
+
+def test_handle_batch_objects_success():
+    batch_objects = _get_resource_json("batch_objects.json")
+    
+    with patch.object(
+            mv.image_store,
+            'add_item') as mock_item:
+        actual_image_count = mv._handle_batch_objects(batch_objects)
+
+    assert mock_item.call_count == 1
