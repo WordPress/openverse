838c7aff288207c98bfffc6ba44e5b64692b5e8b
Zack Krida <zackkrida@pm.me>
2020-07-09 11:14:12 -0400

    Fix tests

--- test/unit/specs/pages/collectionbrowsepage.spec.js
+++ test/unit/specs/pages/collectionbrowsepage.spec.js
@@ -1,5 +1,4 @@
-import ClientCollectionBrowsePage from '@/pages/client/CollectionBrowsePage';
-import ServerCollectionBrowsePage from '@/pages/server/CollectionBrowsePage';
+import CollectionBrowsePage from '@/pages/CollectionBrowsePage';
 import render from '../../test-utils/render';
 
 describe('CollectionBrowsePage', () => {
@@ -21,21 +20,17 @@ describe('CollectionBrowsePage', () => {
       },
     },
   };
+
   it('should render correct contents', () => {
-    const wrapper = render(ClientCollectionBrowsePage);
+    const wrapper = render(CollectionBrowsePage);
     expect(wrapper.find({ name: 'header-section' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'footer-section' }).vm).toBeDefined();
   });
 
   it('should dispatch FETCH_COLLECTION_IMAGES', () => {
     const params = { foo: 'bar' };
-    const wrapper = render(ClientCollectionBrowsePage, options);
+    const wrapper = render(CollectionBrowsePage, options);
     wrapper.vm.getImages(params);
     expect(options.mocks.$store.dispatch).toHaveBeenCalledWith('FETCH_COLLECTION_IMAGES', params);
-  });
-
-  it('server side page does not render search grid', () => {
-    const wrapper = render(ServerCollectionBrowsePage, options);
-    expect(wrapper.find({ name: 'search-grid' }).vm).toBeUndefined();
   });
 });
