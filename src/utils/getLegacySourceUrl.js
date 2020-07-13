8b7cdefe4569bc74cc061e603c955a64c751916b
Zack Krida <zackkrida@pm.me>
2020-07-13 15:31:13 -0400

    restructure getLegacySourceUrl to a slightly simpler api

--- src/utils/getLegacySourceUrl.js
+++ src/utils/getLegacySourceUrl.js
@@ -1,48 +1,50 @@
 import { stringifyUrl } from 'query-string';
 
 /**
- * Accept a base url and a object of query params, and turn it into a formatted url
- * @example q('https://example.com', {cool: 'true', fun: 'yes', other: [1,2,3]}) => `https://example.com?cool=true&fun=yes&other=1,2,3`
- */
-const q = (url, queryObject) => stringifyUrl({ url, query: queryObject }, { skipNull: true, arrayFormat: 'comma' });
-
-/**
  * A mapping of each legacy source with its url builder functions for each content type.
  * Urls were based off of deta found here: https://github.com/creativecommons/cccatalog-frontend/issues/315
  */
 const legacySourceMap = {
   Europeana: {
     audio(search) {
-      return q('https://www.europeana.eu/en/search', {
-        page: 1,
-        qf: 'TYPE:"SOUND"',
-        query: `${search.query} AND RIGHTS:*creative* AND NOT RIGHTS:*nc* AND NOT RIGHTS:*nd*`,
-      });
+      return {
+        url: 'https://www.europeana.eu/en/search',
+        query: {
+          page: 1,
+          qf: 'TYPE:"SOUND"',
+          query: `${search.query} AND RIGHTS:*creative* AND NOT RIGHTS:*nc* AND NOT RIGHTS:*nd*`,
+        },
+      };
     },
     video(search) {
-      return q('https://www.europeana.eu/en/search', {
-        page: 1,
-        qf: 'TYPE:"VIDEO"',
-        query: `${search.query} AND RIGHTS:*creative* AND NOT RIGHTS:*nc* AND NOT RIGHTS:*nd*`,
-      });
+      return {
+        url: '',
+        query: {},
+      };
     },
   },
   'Wikimedia Commons': {
     audio(search) {
-      return q('https://commons.wikimedia.org/w/index.php', {
-        sort: 'relevance',
-        search: `${search.query} filetype:audio`,
-        title: 'Special:Search',
-        'advancedSearch-current': '{"fields":{"filetype":"audio"}}',
-      });
+      return {
+        url: 'https://commons.wikimedia.org/w/index.php',
+        query: {
+          sort: 'relevance',
+          search: `${search.query} filetype:audio`,
+          title: 'Special:Search',
+          'advancedSearch-current': '{"fields":{"filetype":"audio"}}',
+        },
+      };
     },
     video(search) {
-      return q('https://commons.wikimedia.org/w/index.php', {
-        sort: 'relevance',
-        search: `${search.query} filetype:video`,
-        title: 'Special:Search',
-        'advancedSearch-current': '{"fields":{"filetype":"audio"}}',
-      });
+      return {
+        url: 'https://commons.wikimedia.org/w/index.php',
+        query: {
+          sort: 'relevance',
+          search: `${search.query} filetype:video`,
+          title: 'Special:Search',
+          'advancedSearch-current': '{"fields":{"filetype":"audio"}}',
+        },
+      };
     },
   },
   Jamendo: {
@@ -59,10 +61,14 @@ const legacySourceMap = {
   },
   'Google Images': {
     image(search) {
-      return `https://www.google.com/search?${q({
-        as_rights: '(cc_publicdomain%7Ccc_attribute%7Ccc_sharealike).-(cc_noncommercial%7Ccc_nonderived)',
-        q: search.query,
-      })}`;
+      return {
+        url: 'https://www.google.com/search',
+        query: {
+          as_rights:
+            '(cc_publicdomain%7Ccc_attribute%7Ccc_sharealike).-(cc_noncommercial%7Ccc_nonderived)',
+          q: search.query,
+        },
+      };
     },
   },
   'Open Clip Art Library': {
@@ -78,15 +84,26 @@ const legacySourceMap = {
  *
  *  */
 const getLegacySourceUrl = type => (sourceName, search) => {
-  if (!search) throw new Error(`Please provide a valid query to search ${sourceName} for ${type} files.`);
+  if (!search) {
+    throw new Error(
+      `Please provide a valid query to search ${sourceName} for ${type} files.`,
+    );
+  }
 
   const source = legacySourceMap[sourceName];
-  if (!source) throw new Error(`No data avaliable for provided legacy source: ${sourceName}`);
+  if (!source) {
+    throw new Error(
+      `No data avaliable for provided legacy source: ${sourceName}`,
+    );
+  }
 
-  const getSourceTypeUrl = source[type];
-  if (!getSourceTypeUrl) throw new Error(`${sourceName} does not offer meta search for ${type}`);
+  const getSourceUrlInfo = source[type];
+  if (!getSourceUrlInfo) { throw new Error(`${sourceName} does not offer meta search for ${type}`); }
 
-  return getSourceTypeUrl(search);
+  return stringifyUrl(getSourceUrlInfo(search), {
+    skipNull: true,
+    arrayFormat: 'comma',
+  });
 };
 
 export default getLegacySourceUrl;
