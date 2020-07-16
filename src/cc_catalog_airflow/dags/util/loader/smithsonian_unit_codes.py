d6cbb9b2ed458940e044cb8f4eddad38d7728938
Charini Nanayakkara <charini@Charinis-Air.lan>
2020-07-16 22:44:46 +1000

    Change Smithsonian unit code check to store values in table

--- src/cc_catalog_airflow/dags/util/loader/smithsonian_unit_codes.py
+++ src/cc_catalog_airflow/dags/util/loader/smithsonian_unit_codes.py
@@ -7,6 +7,8 @@ import logging
 import requests
 import os
 
+from textwrap import dedent
+from airflow.hooks.postgres_hook import PostgresHook
 from util.loader import provider_details as prov
 
 logger = logging.getLogger(__name__)
@@ -21,31 +23,84 @@ PARAMS = {
 }
 SUB_PROVIDERS = prov.SMITHSONIAN_SUB_PROVIDERS
 
-
-def alert_new_unit_codes(unit_code_set,
-                         sub_prov_dict=SUB_PROVIDERS):
-  sub_provider_unit_code_set = set()
-
-  for sub_prov, unit_code_sub_set in sub_prov_dict.items():
-    sub_provider_unit_code_set = \
-      sub_provider_unit_code_set.union(unit_code_sub_set)
-
-  new_unit_codes = unit_code_set - sub_provider_unit_code_set
-
-  if len(new_unit_codes) > 0:
-    logger.info(f'The new unit codes {new_unit_codes} must be added to '
-                f'the SMITHSONIAN_SUB_PROVIDERS dictionary')
-
-  return new_unit_codes
+DB_USER_NAME = 'deploy'
+SI_UNIT_CODE_TABLE = 'smithsonian_new_unit_codes'
 
 
-def alert_unit_codes_from_api(units_endpoint=UNITS_ENDPOINT,
+def initialise_unit_code_table(postgres_conn_id, unit_code_table):
+    postgres = PostgresHook(postgres_conn_id=postgres_conn_id)
+
+    """
+    Create table to store new unit codes if it does not exist
+    """
+    postgres.run(
+      dedent(
+        f'''
+        CREATE TABLE IF NOT EXISTS public.{unit_code_table} (
+        new_unit_code character varying(80)
+        );
+        '''
+      )
+    )
+
+    postgres.run(
+      f'ALTER TABLE public.{unit_code_table} OWNER TO {DB_USER_NAME};'
+    )
+
+    """
+    Delete old unit code entries
+    """
+    postgres.run(
+      dedent(
+        f'''
+        DELETE FROM public.{unit_code_table};
+        '''
+      )
+    )
+
+
+def get_new_unit_codes(unit_code_set,
+                       sub_prov_dict=SUB_PROVIDERS):
+    sub_provider_unit_code_set = set()
+
+    for sub_prov, unit_code_sub_set in sub_prov_dict.items():
+        sub_provider_unit_code_set = \
+          sub_provider_unit_code_set.union(unit_code_sub_set)
+
+    new_unit_codes = unit_code_set - sub_provider_unit_code_set
+
+    if bool(new_unit_codes):
+        logger.info(f'The new unit codes {new_unit_codes} must be added to '
+                    f'the SMITHSONIAN_SUB_PROVIDERS dictionary')
+
+    return new_unit_codes
+
+
+def alert_unit_codes_from_api(postgres_conn_id,
+                              unit_code_table='smithsonian_new_unit_codes',
+                              units_endpoint=UNITS_ENDPOINT,
                               query_params=PARAMS):
-  response = requests.get(
-      units_endpoint,
-      params=query_params
-  )
-  unit_code_set = set(response.json().get('response', {}).get('terms', []))
+    response = requests.get(
+        units_endpoint,
+        params=query_params
+    )
+    unit_code_set = set(response.json().get('response', {}).get('terms', []))
+    new_unit_codes = get_new_unit_codes(unit_code_set)
 
-  return bool(alert_new_unit_codes(unit_code_set))
+    initialise_unit_code_table(postgres_conn_id, unit_code_table)
 
+    postgres = PostgresHook(postgres_conn_id=postgres_conn_id)
+    """
+    Populate the table with new unit codes
+    """
+    for new_unit_code in new_unit_codes:
+        postgres.run(
+          dedent(
+            f'''
+            INSERT INTO public.{unit_code_table}
+            VALUES (
+              '{new_unit_code}'
+            );
+            '''
+          )
+        )
