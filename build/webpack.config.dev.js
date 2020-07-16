681b58c0a6671af029e4a3ae017e5500ac1c90d5
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-17 02:53:55 +0530

    More changes..

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,41 +1,41 @@
-'use strict';
+'use strict'
 
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
-const helpers = require('./helpers');
-const commonConfig = require('./webpack.config.common');
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const helpers = require('./helpers')
+const commonConfig = require('./webpack.config.common')
 
 const webpackConfig = merge(commonConfig, {
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
-  output: {
-    path: helpers.root('dist'),
-    publicPath: '/',
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[id].chunk.js'
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new FriendlyErrorsPlugin()
-  ],
-  devServer: {
-    compress: true,
-    historyApiFallback: true,
-    hot: true,
-    open: false,
-    overlay: true,
-    port: 8443,
-    stats: {
-      normal: true
-    }
-  }
-});
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    output: {
+        path: helpers.root('dist'),
+        publicPath: '/',
+        filename: 'js/[name].bundle.js',
+        chunkFilename: 'js/[id].chunk.js',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new FriendlyErrorsPlugin(),
+    ],
+    devServer: {
+        compress: true,
+        historyApiFallback: true,
+        hot: true,
+        open: false,
+        overlay: true,
+        port: 8443,
+        stats: {
+            normal: true,
+        },
+    },
+})
 
-module.exports = webpackConfig;
+module.exports = webpackConfig
