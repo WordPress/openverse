fa92b401c41fcfbdabb97f92051801085c188b52
Zack Krida <zackkrida@pm.me>
2020-07-16 09:47:49 -0400

    Fix browserpage test

--- test/unit/specs/pages/browsepage.spec.js
+++ test/unit/specs/pages/browsepage.spec.js
@@ -1,12 +1,18 @@
 import BrowsePage from '@/pages/BrowsePage';
 import render from '../../test-utils/render';
+import Vuex from 'vuex';
+import { createLocalVue } from '@vue/test-utils';
 
 describe('BrowsePage', () => {
   it('should render correct contents', () => {
-    const wrapper = render(BrowsePage);
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+
+    const wrapper = render(BrowsePage, { localVue, mocks: { $route: { path: '/search' } } });
+
     expect(wrapper.find({ name: 'header-section' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'footer-section' }).vm).toBeDefined();
     expect(wrapper.find({ name: 'filter-display' }).vm).toBeDefined();
-    expect(wrapper.find({ name: 'search-grid' }).vm).toBeDefined();
+    expect(wrapper.find({ name: 'search-type-tabs' }).vm).toBeDefined();
   });
 });
