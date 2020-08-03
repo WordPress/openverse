ad38642f2c5b06f5124e74a605c46e994191c986
Zack Krida <zackkrida@pm.me>
2020-08-03 15:07:03 -0400

    Switch to single `SENTRY_DSN` .env variable

--- src/sentry/browser.js
+++ src/sentry/browser.js
@@ -1,7 +1,6 @@
 import * as Sentry from '@sentry/browser'
 import initSentry from './initSentry'
 
-const init = (Vue) =>
-  initSentry({ Sentry, dsn: process.env.BROWSER_SENTRY_DSN, Vue })
+const init = (Vue) => initSentry({ Sentry, dsn: process.env.SENTRY_DSN, Vue })
 
 export default init
--- src/sentry/server.js
+++ src/sentry/server.js
@@ -2,6 +2,6 @@ import * as Sentry from '@sentry/node'
 import initSentry from './initSentry'
 
 const init = () =>
-  initSentry({ Sentry, dsn: process.env.SSR_SENTRY_DSN, ssr: true })
+  initSentry({ Sentry, dsn: process.env.SENTRY_DSN, ssr: true })
 
 export default init
