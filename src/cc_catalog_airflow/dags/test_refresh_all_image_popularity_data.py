58e64182fbb03bcf4e71feb7cd99bcadb6310b3d
Brent Moran <brent@creativecommons.org>
2020-08-06 21:40:28 +0200

    add tests for popularity SQL DAGs

--- src/cc_catalog_airflow/dags/test_refresh_all_image_popularity_data.py
+++ src/cc_catalog_airflow/dags/test_refresh_all_image_popularity_data.py
@@ -8,7 +8,7 @@ def test_dag_loads_with_no_errors(tmpdir):
     tmp_directory = str(tmpdir)
     dag_bag = DagBag(dag_folder=tmp_directory, include_examples=False)
     dag_bag.process_file(
-        os.path.join(FILE_DIR, 'update_popularity_constants_workflow.py')
+        os.path.join(FILE_DIR, 'refresh_all_image_popularity_data.py')
     )
     assert len(dag_bag.import_errors) == 0
     assert len(dag_bag.dags) == 1
