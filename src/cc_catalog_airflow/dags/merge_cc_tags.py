9b97545f3fbb9023926661fbd79a851a2afc75e0
kss682 <kss682@gmail.com>
2020-08-07 13:27:06 +0530

    added science and met museum logic (testing)

--- src/cc_catalog_airflow/dags/merge_cc_tags.py
+++ src/cc_catalog_airflow/dags/merge_cc_tags.py
@@ -1,3 +1,21 @@
+"""
+    Museum Victoria -- 
+            API urls =     142532
+            CC urls  =     81799
+            Common   =      0
+
+
+    Science Museum -- 
+            API links =    62515
+            CC url    =    67390
+            Common    =    22228
+
+    Met Museum :
+            API links =    10574 (Did not run completely )
+            CC urls   =    522488
+            Common    =    9973
+
+"""
 import os
 import logging
 import argparse
@@ -37,9 +55,23 @@ def _modify_urls(
         sub_query = f"""
                         SPLIT_PART({_strip_url_schema(url)}, '-', 1)
                      """
+
     elif "science_museum" in provider_table:
         sub_query = f"""
-                        SPLIT_PART()
+                        RTRIM(
+                            SPLIT_PART(
+                                REVERSE(
+                            {_strip_url_schema(url)}
+                        ), '/', 1), 'medium|large')
+                     """
+
+    elif "met" in provider_table:
+        sub_query = f"""
+                      SPLIT_PART(
+                          REVERSE(
+                            {_strip_url_schema(url)}
+                          )
+                      , '/', 1)
                      """
     return sub_query
 
@@ -93,7 +125,6 @@ def _merge_tags(
         cursor.execute(
             f"""
             select {_modify_urls('b.' + COL_IMAGE, cc_table)} from {cc_table} b
-            where  b.{COL_PROVIDER} = 'museumsvictoria'
             """
         )
         for j in cursor.fetchall():
@@ -117,9 +148,9 @@ def _merge_tags(
         print(len(cc_links))
         common_links = set(api_links).intersection(set(cc_links))
         print(len(common_links))
-        print(common_links)
-        for i in common_links:
-            print(i)
+        # print(common_links)
+        # for i in common_links:
+        #     print(i)
     except Exception as e:
         logger.warning(f"Merging failed due to {e}")
         status = "Failure"
@@ -139,3 +170,17 @@ if __name__ == "__main__":
     parse.add_argument("-t", "--Table", required=True, help="Select table")
     args = parse.parse_args()
     main(cc_table=args.Table)
+
+"""
+Science Museum - 
+select RTRIM(SPLIT_PART(REVERSE(LTRIM(a.url, 'https://')), '/', 1), 'medium|large') as api_url, RTRIM(SPLIT_PART(REVERSE(LTRIM(b.url, 'https://')), '/', 1), 'medium|large') as cc_url from image as a , science_museum_2020_06_02 as b
+where
+RTRIM(SPLIT_PART(REVERSE(LTRIM(a.url, 'https://')), '/', 1), 'medium|large') = RTRIM(SPLIT_PART(REVERSE(LTRIM(b.url, 'https://')), '/', 1), 'medium|large') limit 20;
+
+
+
+MET MUSEUM :
+select distinct SPLIT_PART(REVERSE(LTRIM(a.url, 'https://')),'/', 1) as api_url from 
+image as a, met_museum_2020_06_05 as b where SPLIT_PART(REVERSE(LTRIM(a.url, 'https://')),'/', 1)=SPLIT_PART(REVERSE(LTRIM(b.url, 'https://')),'/', 1) and a.provider='met' ;
+
+"""
