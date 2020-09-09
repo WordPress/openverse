175b021f4e0fe129003b4203a01ea93e331c2d37
Rochisha <rochisha.agarwal2302@gmail.com>
2020-09-09 22:56:03 +0530

    added content

--- src/pages/SupportPage.vue
+++ src/pages/SupportPage.vue
@@ -5,8 +5,48 @@
       <div class="container is-fluid">
         <div class="margin-bottom-large">
           <h1 class="title is-2">
-            {{ $t('meta-search-page.title') }}
+            {{ $t('support.title') }}
           </h1>
+          <p class="margin-bottom-normal">{{ $t('support.inclusive') }}</p>
+          <i18n path="support.work" tag="p" class="margin-bottom-normal">
+            <template v-slot:license>
+              <a
+                aria-label="licenses"
+                href="https://creativecommons.org/licenses/"
+                >CC License Suite</a
+              >
+            </template>
+            <template v-slot:public-domain>
+              <a
+                aria-label="public domain"
+                href="https://creativecommons.org/publicdomain/"
+                >public domain tools</a
+              >
+            </template>
+            <template v-slot:photograph>
+              <a
+                aria-label="photograph"
+                href="https://www.parismuseescollections.paris.fr/fr/musee-carnavalet/oeuvres/portrait-de-l-ecrivaine-libertaire-et-feministe-caroline-remy-dite-severine"
+                >this photograph</a
+              >
+            </template>
+            <template v-slot:guide>
+              <a
+                aria-label="guide"
+                href="https://pressbooks.library.ryerson.ca/docs/"
+                >this guide</a
+              >
+            </template>
+          </i18n>
+          <h2 class="title subtitle is-3 margin-bottom-normal">
+            {{ $t('support.free') }}
+          </h2>
+          <p class="margin-bottom-normal">{{ $t('support.access') }}</p>
+          <i18n path="support.public-good" tag="p" class="margin-bottom-normal">
+            <template v-slot:public-good>
+              <strong>public good</strong>
+            </template>
+          </i18n>
         </div>
       </div>
     </main>
