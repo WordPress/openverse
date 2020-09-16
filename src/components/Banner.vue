1ce24585de019a3b8bc09f8eff598759473f1b38
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-09-17 01:24:12 +0530

    Add banner.vue file

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,30 +1,39 @@
 <template>
   <section class="section">
-    <div class="banner_two" v-bind:class="{'change':task.id}">
-      <div class="columns">
-        <section class="column section is-8">
+    <div
+      class="padding-small"
+      style="margin-bottom: 3px; background: #e6f6eb;"
+      v-bind:class="{'change':task.id}">
+    <div class="columns">
+        <div class="column section is-8">
           <p class="margin-top-normal">
             If you donate $5 today, and everyone else does the same,
             we can keep growing our database of freely reusable content. For you. For all of us.
           </p>
-        </section>
-        <section class="column section">
-          <button type="submit" class="donate button is-small" v-on:click="redirect">
+        </div>
+        <div class="column section">
+          <button type="submit" class="button is-success is-small" v-on:click="redirect">
             <i class="icon cc-letterheart-filled margin-right-small is-size-5 padding-top-smaller" />Yes, I’ll donate now
           </button>
-        </section>
-        <section class="column section">
-          <button class="label_two button is-small" v-on:click="onclick">Not today</button>
-        </section>
-        <section class="column section">
-          <button class="button_two button" v-on:click="onclick">x</button>
-        </section>
+        </div>
+        <div class="column section">
+          <button
+            class="button is-small"
+            style="border:none;background: #e6f6eb; color:#008000"
+            v-on:click="onclick"
+          >Not today</button>
+        </div>
+        <div class="column section">
+          <button
+            class="button"
+            v-on:click="onclick"
+            style=" background: #e6f6eb; margin-top: 4px;border: none;"><i class="icon cross"></i>
+          </button>
+        </div>
       </div>
     </div>
   </section>
 </template>
-
-
 <script>
 export default {
   name: 'Banner',
@@ -46,32 +55,8 @@ export default {
   },
 }
 </script>
-
-
-
 <style scoped>
 .change {
   display: none;
-}
-.banner_two {
-  background: #e6f6eb;
-  margin-bottom: 3px;
-}
-.button_two {
-  background: #e6f6eb;
-  border: none;
-  margin-top: 2px;
-}
-.donate {
-  background: #008000;
-  color: white;
-  border: none;
-  margin-top: 5px;
-}
-.label_two {
-  background: #e6f6eb;
-  color: #008000;
-  border: none;
-  margin-top: 2px;
 }
 </style>
