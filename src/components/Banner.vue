bcaec4310aec50b581ad13d71072c48cec09379d
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-09-17 02:36:21 +0530

    Add Banner.vue file

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,29 +2,27 @@
   <section class="section">
     <div
       class="padding-small"
-      style="margin-bottom: 3px; background: #e6f6eb;"
-      v-bind:class="{'change':task.id}"
-    >
+      style="background: #e6f6eb;"
+      v-bind:class="{'change':task.id}">
       <div class="columns">
-        <div class="column section is-8">
+        <div class="column is-8">
           <p class="margin-top-normal">
             If you donate $5 today, and everyone else does the same,
             we can keep growing our database of freely reusable content. For you. For all of us.
           </p>
         </div>
-        <div class="column section">
+        <div class="column">
           <button type="submit" class="button is-success is-small" v-on:click="redirect">
             <i class="icon cc-letterheart-filled margin-right-small is-size-5 padding-top-smaller" />Yes, I’ll donate now
           </button>
         </div>
-        <div class="column section">
+        <div class="column">
           <button
             class="button is-small"
-            style="border:none;background: #e6f6eb; color:#008000"
-            v-on:click="onclick"
-          >Not today</button>
+            style=" border:none;background: #e6f6eb; color:#008000"
+            v-on:click="onclick">Not today</button>
         </div>
-        <div class="column section">
+        <div class="column">
           <button class="button is-text" v-on:click="onclick">x</button>
         </div>
       </div>
@@ -36,7 +34,7 @@ export default {
   name: 'Banner',
   methods: {
     onclick() {
-      this.task.id = true
+      this.task.id = true;
     },
     redirect() {
       window.location.href =
