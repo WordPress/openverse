91793f357a2f52559e7d6ee10d74cec413707394
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-09-22 00:37:44 +0530

    Update HeaderSection.vue

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -2,18 +2,24 @@
   <header>
     <nav-section
       :showNavSearch="showNavSearch"
-      :navSearchPlaceholder="navSearchPlaceholder"/>
+      :navSearchPlaceholder="navSearchPlaceholder"
+    />
     <slot></slot>
   </header>
 </template>
+
 <script>
 import NavSection from '@/components/NavSection'
+
 export default {
   name: 'header-section',
   components: {
-    NavSection},
-  props: ['showHero', 'showNavSearch', 'isHeaderFixed', 'navSearchPlaceholder'],}
+    NavSection,
+  },
+  props: ['showHero', 'showNavSearch', 'isHeaderFixed', 'navSearchPlaceholder'],
+}
 </script>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 .header {
@@ -22,6 +28,7 @@ export default {
   width: 100%;
   max-width: 100%;
 }
+
 .header nav {
   z-index: 10;
 }
