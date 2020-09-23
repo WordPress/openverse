ff3576e03a0d805b1adeb3627653be7e2a0fe566
Tushar912 <tushar.912u@gmail.com>
2020-09-23 13:55:53 +0530

    fix style

--- cccatalog-api/cccatalog/api/serializers/image_serializers.py
+++ cccatalog-api/cccatalog/api/serializers/image_serializers.py
@@ -209,8 +209,10 @@ class ImageSearchQueryStringSerializer(serializers.Serializer):
     def validate_source(input_sources):
         allowed_sources = list(get_sources('image').keys())
         input_sources = input_sources.split(',')
-        input_sources= [input_source for input_source in input_sources if input_source in allowed_sources] 
-        input_sources= ','.join(input_sources)
+        input_sources = [input_source 
+                        for input_source in input_sources 
+                        if input_source in allowed_sources] 
+        input_sources = ','.join(input_sources)
         
         return input_sources.lower()
 
