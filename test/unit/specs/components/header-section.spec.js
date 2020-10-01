1c780ce6686f7f8590d75fb705f899613d6b340b
Zack Krida <zackkrida@pm.me>
2020-09-30 20:12:37 -1000

    Correctly mock the route path in the header-section spec

--- test/unit/specs/components/header-section.spec.js
+++ test/unit/specs/components/header-section.spec.js
@@ -3,7 +3,14 @@ import render from '../../test-utils/render'
 
 describe('HeaderSection', () => {
   it('should render correct contents', () => {
-    const wrapper = render(HeaderSection)
+    const wrapper = render(HeaderSection, {
+      mocks: {
+        $route: {
+          path: '',
+        },
+      },
+    })
+
     expect(wrapper.find('header').vm).toBeDefined()
   })
 })
