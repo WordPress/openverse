4896416a515d9e3c234c6623a659c639b626dc12
Zack Krida <zackkrida@pm.me>
2020-09-30 17:15:15 -1000

    Remove unused CollectionItem component

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 import { env } from '../../nuxt.config'
 
 // Attach nuxt config to process
 for (const [key, value] of Object.entries(env)) {
   process.env[key] = value
 }
+
+Vue.use(VueI18n)
 
 // Make sure api analytics are enabled so we can test them!
 process.env.apiAnalytics = false
