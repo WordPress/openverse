effb85fb4835cea331df826170da6542a8913228
Zack Krida <zackkrida@pm.me>
2020-10-02 12:48:27 -1000

    Catch usage data errors

--- src/store-modules/usage-data-store.js
+++ src/store-modules/usage-data-store.js
@@ -8,22 +8,42 @@ import stringToBoolean from '~/utils/stringToBoolean'
 
 const disabled = !stringToBoolean(process.env.enableInternalAnalytics)
 
+/**
+ * Catch api event failures, mainly to prevent SSR errors on test domains that cannot communicate with our API server.
+ * Alternatively, just disable internal analytics on those servers with ENABLE_INTERNAL_ANALYTICS=true in the environment.
+ */
+const handleUsageEvent = (eventName) => (promise) =>
+  promise.catch((error) =>
+    console.error({
+      message: `Failed to execute api event: ${eventName}.`,
+      error,
+    })
+  )
+
 const actions = (UsageDataService) => ({
   [SEND_SEARCH_QUERY_EVENT](context, params) {
     if (disabled) return
-    UsageDataService.sendSearchQueryEvent(params)
+    handleUsageEvent(SEND_SEARCH_QUERY_EVENT)(
+      UsageDataService.sendSearchQueryEvent(params)
+    )
   },
   [SEND_RESULT_CLICKED_EVENT](context, params) {
     if (disabled) return
-    UsageDataService.sendResultClickedEvent(params)
+    handleUsageEvent(SEND_RESULT_CLICKED_EVENT)(
+      UsageDataService.sendResultClickedEvent(params)
+    )
   },
   [SEND_SEARCH_RATING_EVENT](context, params) {
     if (disabled) return
-    UsageDataService.sendSearchRatingEvent(params)
+    handleUsageEvent(SEND_SEARCH_RATING_EVENT)(
+      UsageDataService.sendSearchRatingEvent(params)
+    )
   },
   [SEND_DETAIL_PAGE_EVENT](context, params) {
     if (disabled) return
-    UsageDataService.sendDetailPageEvent(params)
+    handleUsageEvent(SEND_DETAIL_PAGE_EVENT)(
+      UsageDataService.sendDetailPageEvent(params)
+    )
   },
 })
 
