f22c6c28607dea47f40c1cdfaee43875545894d9
Olga Bulat <obulat@gmail.com>
2021-04-11 07:50:07 +0300

    Clean up CSS

--- src/components/SearchGridForm.vue
+++ src/components/SearchGridForm.vue
@@ -6,58 +6,54 @@
     class="search-form padding-normal"
     @submit.prevent="onSubmit"
   >
-    <div class="is-flex">
-      <button
-        v-if="!isFilterVisible"
-        class="button filter-toggle"
-        type="button"
-        @click.prevent="onToggleSearchGridFilter()"
-        @keyup.enter.prevent="onToggleSearchGridFilter()"
-      >
-        {{ $t('filters.title') }}
-      </button>
-      <div class="field has-addons search-input">
-        <div class="control search-control has-icons-left margin-left-small">
-          <label for="searchInput">
-            <input
-              id="searchInput"
-              ref="search"
-              :aria-label="$t('browse-page.aria.search')"
-              required="required"
-              autofocus="true"
-              class="search-input__input input"
-              type="search"
-              :placeholder="searchBoxPlaceholder"
-              :value="searchTermsModel"
-              @input="onInput"
-              @keyup.enter="onSubmit"
-            />
-          </label>
-          <span class="icon is-left">
-            <svg
-              viewBox="0 0 30 30"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              class="icon__svg"
-            >
-              <path
-                d="M29.59 25.94l-5.842-5.842a1.405 1.405 0 00-.996-.41h-.955a12.128 12.128 0 002.578-7.5C24.375 5.455 18.92 0 12.187 0 5.455 0 0 5.455 0 12.188c0 6.732 5.455 12.187 12.188 12.187 2.83 0 5.431-.96 7.5-2.578v.955c0 .375.146.732.41.996l5.841 5.842a1.4 1.4 0 001.987 0l1.658-1.658c.55-.551.55-1.442.006-1.992zm-17.402-6.253a7.496 7.496 0 01-7.5-7.5c0-4.142 3.351-7.5 7.5-7.5 4.142 0 7.5 3.352 7.5 7.5 0 4.143-3.352 7.5-7.5 7.5z"
-                fill="currentColor"
-              />
-            </svg>
-          </span>
-        </div>
-        <div class="control">
-          <input
-            type="submit"
-            class="button is-primary"
-            :value="$t('browse-page.search-form.button')"
-            @click.prevent="onSubmit"
-            @keyup.enter.prevent="onSubmit"
+    <button
+      v-if="!isFilterVisible"
+      class="button filter-toggle"
+      type="button"
+      @click.prevent="onToggleSearchGridFilter()"
+      @keyup.enter.prevent="onToggleSearchGridFilter()"
+    >
+      {{ $t('filters.title') }}
+    </button>
+    <div
+      class="search-field field has-addons control search-control has-icons-left margin-left-small"
+    >
+      <label for="searchInput" class="search-field__label control label">
+        <input
+          id="searchInput"
+          ref="search"
+          :aria-label="$t('browse-page.aria.search')"
+          required="required"
+          autofocus="true"
+          class="search-field__input input"
+          type="search"
+          :placeholder="searchBoxPlaceholder"
+          :value="searchTermsModel"
+          @input="onInput"
+          @keyup.enter="onSubmit"
+        />
+      </label>
+      <span class="icon is-left">
+        <svg
+          viewBox="0 0 30 30"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          class="icon__svg"
+        >
+          <path
+            d="M29.59 25.94l-5.842-5.842a1.405 1.405 0 00-.996-.41h-.955a12.128 12.128 0 002.578-7.5C24.375 5.455 18.92 0 12.187 0 5.455 0 0 5.455 0 12.188c0 6.732 5.455 12.187 12.188 12.187 2.83 0 5.431-.96 7.5-2.578v.955c0 .375.146.732.41.996l5.841 5.842a1.4 1.4 0 001.987 0l1.658-1.658c.55-.551.55-1.442.006-1.992zm-17.402-6.253a7.496 7.496 0 01-7.5-7.5c0-4.142 3.351-7.5 7.5-7.5 4.142 0 7.5 3.352 7.5 7.5 0 4.143-3.352 7.5-7.5 7.5z"
+            fill="currentColor"
           />
-        </div>
-      </div>
+        </svg>
+      </span>
+      <input
+        type="submit"
+        class="control button is-primary"
+        :value="$t('browse-page.search-form.button')"
+        @click.prevent="onSubmit"
+        @keyup.enter.prevent="onSubmit"
+      />
     </div>
   </form>
   <!-- eslint-enable -->
@@ -153,50 +149,52 @@ export default {
   position: sticky;
   background-color: #f5f5f5;
   z-index: 10;
+  display: flex;
 }
 
-.search-input__input {
-  font-size: 1rem;
-  height: 2.5rem;
-  @include desktop {
-    font-size: 1.43rem;
-    height: 3.875rem;
+.has-addons {
+  .control:first-child:not(:only-child),
+  .control:first-child:not(:only-child) .input {
+    border-bottom-right-radius: 0;
+    border-top-right-radius: 0;
+    margin-right: -1px;
+  }
+  .button {
+    border-bottom-left-radius: 0;
+    border-top-left-radius: 0;
   }
 }
-.search-input {
+.search-field {
+  display: flex;
   width: 100%;
+  clear: both;
+  position: relative;
+  text-align: left;
   @include desktop {
     width: 70%;
   }
-}
-
-span.icon {
-  margin-top: auto;
-  margin-bottom: auto;
-  bottom: 0;
-  @include desktop {
-    margin-left: 0.5rem;
+  .label {
+    flex: 1;
+    margin-bottom: 0;
   }
-}
-.icon.search {
-  padding: 0.8rem;
-  @include desktop {
-    padding: 1.2rem;
-    max-height: 2.5rem;
-    max-width: 2.5rem;
-    font-size: 1.12rem;
+  .input {
+    font-size: 1rem;
+    height: 2.5rem;
+    @include desktop {
+      font-size: 1.43rem;
+      height: 3.875rem;
+    }
   }
-}
-
-.control:first-child {
-  width: 100%;
-}
-input.button {
-  font-size: 1rem;
-  padding: 0.5rem calc(1rem + 0.2rem);
-  @include desktop {
-    font-size: 1.43rem;
-    padding: calc(2rem - 0.187rem) 2.5rem;
+  .icon {
+    top: calc(50% - 1.25em);
+  }
+  .button {
+    font-size: 1rem;
+    padding: 0.5rem calc(1rem + 0.2rem);
+    @include desktop {
+      font-size: 1.43rem;
+      padding: calc(2rem - 0.187rem) 2.5rem;
+    }
   }
 }
 </style>
