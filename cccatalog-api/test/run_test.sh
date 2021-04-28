48e743b12add001e43a28b2e0b0ea3fa6aeea1b9
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-28 11:16:55 +0530

    Break long across multiple lines for readability

--- cccatalog-api/test/run_test.sh
+++ cccatalog-api/test/run_test.sh
@@ -2,7 +2,17 @@
 # Local environments don't have valid certificates; suppress this warning.
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 export INTEGRATION_TEST_URL="http://localhost:8000"
-DJANGO_SETTINGS_MODULE='cccatalog.settings' PYTHONPATH=. DJANGO_SECRET_KEY='ny#b__$f6ry4wy8oxre97&-68u_0lk3gw(z=d40_dxey3zw0v1' DJANGO_DATABASE_NAME='openledger' DJANGO_DATABASE_USER='deploy' DJANGO_DATABASE_PASSWORD='deploy' DJANGO_DATABASE_HOST='localhost' REDIS_HOST='localhost' pytest -s --disable-pytest-warnings test/v1_integration_test.py
+
+PYTHONPATH=. \
+DJANGO_SETTINGS_MODULE='cccatalog.settings' \
+DJANGO_SECRET_KEY='ny#b__$f6ry4wy8oxre97&-68u_0lk3gw(z=d40_dxey3zw0v1' \
+DJANGO_DATABASE_NAME='openledger' \
+DJANGO_DATABASE_USER='deploy' \
+DJANGO_DATABASE_PASSWORD='deploy' \
+DJANGO_DATABASE_HOST='localhost' \
+REDIS_HOST='localhost' \
+pytest -s --disable-pytest-warnings test/v1_integration_test.py
+
 succeeded=$?
 if [ $succeeded != 0 ]; then
   echo 'Tests failed. Full system logs: '
