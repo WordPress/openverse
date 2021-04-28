b913a6cc417a2694344715e54c43b40a9c1658f3
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-28 12:07:39 +0530

    Use environment variables before defaults

--- cccatalog-api/test/run_test.sh
+++ cccatalog-api/test/run_test.sh
@@ -5,12 +5,12 @@ export INTEGRATION_TEST_URL="http://localhost:8000"
 
 PYTHONPATH=. \
 DJANGO_SETTINGS_MODULE='cccatalog.settings' \
-DJANGO_SECRET_KEY='ny#b__$f6ry4wy8oxre97&-68u_0lk3gw(z=d40_dxey3zw0v1' \
-DJANGO_DATABASE_NAME='openledger' \
-DJANGO_DATABASE_USER='deploy' \
-DJANGO_DATABASE_PASSWORD='deploy' \
-DJANGO_DATABASE_HOST='localhost' \
-REDIS_HOST='localhost' \
+DJANGO_SECRET_KEY="${DJANGO_SECRET_KEY:-ny#b__$f6ry4wy8oxre97&-68u_0lk3gw(z=d40_dxey3zw0v1}" \
+DJANGO_DATABASE_NAME="${DJANGO_DATABASE_NAME:-openledger}" \
+DJANGO_DATABASE_USER="${DJANGO_DATABASE_USER:-deploy}" \
+DJANGO_DATABASE_PASSWORD="${DJANGO_DATABASE_PASSWORD:-deploy}" \
+DJANGO_DATABASE_HOST="${DJANGO_DATABASE_HOST:-localhost}" \
+REDIS_HOST="${REDIS_HOST:-localhost}" \
 pytest -s --disable-pytest-warnings test/v1_integration_test.py
 
 succeeded=$?
