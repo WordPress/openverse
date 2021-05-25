6e73dffdb689de92eff91adad44caef59e49be25
Zack Krida <zackkrida@pm.me>
2021-05-25 16:32:43 -0400

    Update the docker hub image name in the docker deployment script.

--- ingestion_server/publish_release.sh
+++ ingestion_server/publish_release.sh
@@ -1,5 +1,5 @@
 # Usage: ./public_release.sh [VERSION]
-docker build -t creativecommons/ingestion_server:$1 .
-docker build -f Dockerfile-worker -t creativecommons/indexer_worker:$1 .
-docker push creativecommons/ingestion_server:$1
-docker push creativecommons/indexer_worker:$1
+docker build -t openverse/ingestion_server:$1 .
+docker build -f Dockerfile-worker -t openverse/indexer_worker:$1 .
+docker push openverse/ingestion_server:$1
+docker push openverse/indexer_worker:$1
