201f5745103400e84d35ee16c7e0134c9cb27f5b
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-09 17:19:57 +0530

    Populate fields in the audio model

--- openverse-api/catalog/api/models/audio.py
+++ openverse-api/catalog/api/models/audio.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 from uuslug import uuslug
 
@@ -12,7 +13,62 @@ from catalog.api.models.media import (
 
 
 class Audio(AbstractMedia):
-    pass
+    set = models.ForeignKey(
+        help_text='Reference to set of which this track is a part.',
+        to=AudioSet,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    set_position = models.IntegerField(
+        blank=True,
+        null=True,
+        help_text='Ordering of the audio in the set.'
+    )
+
+    genre = models.CharField(
+        max_length=80,
+        blank=True,
+        null=True,
+        db_index=True,
+        help_text='',
+    )
+    type = models.CharField(
+        max_length=80,
+        blank=True,
+        null=True,
+        db_index=True,
+        help_text='',
+    )
+
+    duration = models.IntegerField(
+        blank=True,
+        null=True,
+        help_text='The time length of the audio file in seconds.'
+    )
+    bit_rate = models.IntegerField(
+        blank=True,
+        null=True,
+        help_text='Number in bits per second, eg. 128000.',
+    )
+    sample_rate = models.IntegerField(
+        blank=True,
+        null=True,
+        help_text='Number in hertz, eg. 44100.',
+    )
+
+    alt_files = JSONField(
+        blank=True,
+        null=True,
+        help_text='JSON describing alternative files for this audio',
+    )
+
+    @property
+    def alternative_files(self):
+        return []  # TODO: Return Python object parsed from `alt_files` field
+
+    class Meta:
+        db_table = 'audio'
 
 
 class AudioReport(AbstractMediaReport):
