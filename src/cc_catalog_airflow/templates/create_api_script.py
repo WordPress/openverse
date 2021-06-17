88a777ce80b9b34c97de9a27b7810cc79e6d010f
Olga Bulat <obulat@gmail.com>
2021-06-17 08:45:01 +0300

    Make the script output clearer
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- src/cc_catalog_airflow/templates/create_api_script.py
+++ src/cc_catalog_airflow/templates/create_api_script.py
@@ -33,10 +33,11 @@ def _get_filled_template(template_path, provider, media_type='image'):
         return script_string
 
 
-def fill_template(provider, media_type):
-    templates_path = Path(__file__).parent
+def fill_template(provider, media_type, templates_path):
+    project_path = templates_path.parent.parent.parent
     template_name = 'template_provider.py_template'
     script_template_path = templates_path / template_name
+    print(f"Creating files in {project_path}")
 
     dags_path = templates_path.parent / 'dags'
     filename = provider.replace(" ", '_').lower()
@@ -48,12 +49,12 @@ def fill_template(provider, media_type):
             script_template_path, provider, media_type
         )
         api_script.write(api_script_string)
-        print(f"Created api script: {api_script_path}")
+        print(f"API script: {api_script_path.relative_to(project_path)}")
 
     test_script_path = api_path / f"test_{filename}.py"
     with open(test_script_path, 'w+', encoding='utf8') as test_script:
         test_script.write('# TODO: Write your tests!')
-        print(f"Created api script test: {test_script_path}")
+        print(f"API script test: {test_script_path.relative_to(project_path)}")
 
     workflow_template_path = templates_path / 'workflow.py_template'
     workflow_path = dags_path / f"{filename}_workflow.py"
@@ -62,7 +63,7 @@ def fill_template(provider, media_type):
             workflow_template_path, provider
         )
         workflow_file.write(workflow_string)
-        print(f"Created workflow file: {workflow_path}")
+        print(f"Airflow workflow file: {workflow_path.relative_to(project_path)}")
 
 
 def main():
@@ -71,21 +72,21 @@ def main():
         add_help=True,
     )
     parser.add_argument(
-        '--provider',
-        help='Name of the provider to create the script for.')
+        "provider",
+        help='Create the script for this provider (eg. "Wikimedia").')
     parser.add_argument(
-        '--media',
-        help='Media type to collect data for, `audio` or `image`'
+        '-m', '--media', type=str, choices=['image', 'audio'],
+        help="Script will collect media of this type"
+             " ('audio'/'image'). Default value is 'image'"
     )
     args = parser.parse_args()
-    if args.provider:
-        provider = args.provider
-        media_type = args.media
-        if media_type not in ['audio', 'image']:
-            print(f"Media type {media_type} is not supported,"
-                  f" assuming it's image")
-            media_type = 'image'
-        fill_template(provider, media_type)
+    provider = args.provider
+    media_type = args.media
+    if media_type not in ['audio', 'image']:
+        print(f"No media type given, assuming it's `image`")
+        media_type = 'image'
+    templates_path = Path(__file__).parent
+    fill_template(provider, media_type, templates_path)
 
 
 if __name__ == "__main__":
