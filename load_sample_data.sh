685c10b28d91730c7e878572bcc6a6092c8480a2
Olga Bulat <obulat@gmail.com>
2021-06-29 17:20:35 +0300

    Unify psql \copy command format
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -29,7 +29,7 @@ docker-compose exec "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d 
 	ALTER TABLE image_view ADD COLUMN standardized_popularity double precision;
 	CREATE TABLE content_provider(provider_identifier varchar(50), provider_name varchar(250), created_on timestamp, domain_name varchar(500), filter_content boolean, notes text);
 	INSERT INTO content_provider (created_on, provider_identifier, provider_name, domain_name, filter_content) VALUES (now(), 'flickr', 'Flickr', 'https://www.flickr.com', false), (now(), 'behance', 'Behance', 'https://www.behance.net', false);
-	\copy image_view (id,created_on,updated_on,identifier,provider,source,foreign_identifier,foreign_landing_url,url,thumbnail,width,height,filesize,license,license_version,creator,creator_url,title,tags_list,last_synced_with_source,removed_from_source,meta_data,tags,watermarked,view_count,standardized_popularity) from './sample_data/sample_data.csv' with csv header
+	\copy image_view (id,created_on,updated_on,identifier,provider,source,foreign_identifier,foreign_landing_url,url,thumbnail,width,height,filesize,license,license_version,creator,creator_url,title,tags_list,last_synced_with_source,removed_from_source,meta_data,tags,watermarked,view_count,standardized_popularity) from './sample_data/sample_data.csv' with (FORMAT csv, HEADER true)
 	EOF"
 
 # This is currently added for testing purposes, we can adjust once the audio support is added
