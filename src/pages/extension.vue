dc53957876110436eceee748cc4bb546b0892d7a
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-22 15:36:30 +0530

    Create a page for the web extension and update the navigation link

---
+++ src/pages/extension.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="section">
+    <div :class="['container', isEmbedded ? '' : 'is-fluid']">
+      <div>
+        <h1 class="title is-2 margin-bottom-large">
+          <span role="img" aria-label="construction sign">🚧</span> WIP
+        </h1>
+        <div class="content">
+          <p>Work in progress...</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import iframeHeight from '~/mixins/iframeHeight'
+
+const AboutPage = {
+  name: 'about-page',
+  mixins: [iframeHeight],
+  layout({ store }) {
+    return store.state.isEmbedded
+      ? 'embedded-with-nav-search'
+      : 'with-nav-search'
+  },
+  computed: {
+    ...mapState(['isEmbedded']),
+  },
+}
+
+export default AboutPage
+</script>
+
+<style lang="scss" scoped>
+@import '~/styles/text-only-page.scss';
+</style>
