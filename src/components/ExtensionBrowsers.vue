92fb5b02fe29f0ba31fcbc4b89a3ff5b9735f84a
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-22 16:56:51 +0530

    Create the buttons for downloading extension on different browsers

---
+++ src/components/ExtensionBrowsers.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul class="buttons is-centered">
+    <li v-for="(browser, key) in browsers" :key="key">
+      <a
+        target="_blank"
+        rel="nofollow noreferrer"
+        :href="browser.extUrl"
+        class="browser-button button small margin-right-small is-opaque"
+      >
+        {{ $t(`browsers.${key}`) }}
+        <img
+          class="margin-left-small"
+          :src="browser.logo"
+          :alt="$t(`browsers.${key}`)"
+        />
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+import chromeLogo from '~/assets/browsers/chrome.svg'
+import edgeLogo from '~/assets/browsers/edge.svg'
+import firefoxLogo from '~/assets/browsers/firefox.svg'
+import operaLogo from '~/assets/browsers/opera.svg'
+
+export default {
+  name: 'ExtensionBrowsers',
+  data() {
+    return {
+      browsers: {
+        chrome: {
+          extUrl:
+            'https://chrome.google.com/webstore/detail/cc-search/agohkbfananbebiaphblgcfhcclklfnh',
+          logo: chromeLogo,
+        },
+        firefox: {
+          extUrl:
+            'https://addons.mozilla.org/en-US/firefox/addon/cc-search-extension/',
+          logo: firefoxLogo,
+        },
+        opera: {
+          extUrl: 'https://addons.opera.com/en/extensions/details/cc-search/',
+          logo: operaLogo,
+        },
+        edge: {
+          extUrl:
+            'https://microsoftedge.microsoft.com/addons/detail/cc-search/djolilnbndifmlfmcdnifdfjfbglipgc',
+          logo: edgeLogo,
+        },
+      },
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.browser-button {
+  background-color: $color-white;
+
+  img {
+    height: 1.5em;
+    width: 1.5em;
+  }
+}
+</style>
