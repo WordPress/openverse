4d30e397f5471c0eea824084744a751718fa6c3c
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-22 18:15:01 +0530

    Create the extension page without exact styling

--- src/pages/extension.vue
+++ src/pages/extension.vue
@@ -1,13 +1,63 @@
 <template>
-  <div class="section">
-    <div :class="['container', isEmbedded ? '' : 'is-fluid']">
-      <div>
-        <h1 class="title is-2 margin-bottom-large">
-          <span role="img" aria-label="construction sign">🚧</span> WIP
-        </h1>
-        <div class="content">
-          <p>Work in progress...</p>
+  <div>
+    <div class="hero-section">
+      <div :class="['container', isEmbedded ? '' : 'is-fluid']">
+        <div class="intro">
+          <h2 class="title is-2 margin-bottom-large">
+            {{ $t('extension.description.intro') }}
+          </h2>
         </div>
+        <ExtensionBrowsers />
+        <img
+          class="screenshot"
+          src="https://via.placeholder.com/1200x600"
+          alt="Extension usage"
+        />
+      </div>
+    </div>
+    <div class="section">
+      <div :class="['container', 'features', isEmbedded ? '' : 'is-fluid']">
+        <img
+          class="screenshot"
+          src="https://via.placeholder.com/520x400"
+          alt="WIP"
+        />
+        <div class="caption">
+          <h2>{{ $t('extension.features.search.heading') }}</h2>
+          <p class="margin-top-normal">
+            {{ $t('extension.features.search.content') }}
+          </p>
+        </div>
+
+        <div class="caption">
+          <h2>{{ $t('extension.features.bookmark.heading') }}</h2>
+          <p class="margin-top-normal">
+            {{ $t('extension.features.bookmark.content') }}
+          </p>
+        </div>
+        <img
+          class="screenshot"
+          src="https://via.placeholder.com/520x400"
+          alt="WIP"
+        />
+
+        <img
+          class="screenshot"
+          src="https://via.placeholder.com/520x400"
+          alt="WIP"
+        />
+        <div class="caption">
+          <h2>{{ $t('extension.features.use.heading') }}</h2>
+          <p class="margin-top-normal">
+            {{ $t('extension.features.use.content') }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div :class="['container', 'conclusion', isEmbedded ? '' : 'is-fluid']">
+        <h2>{{ $t('extension.conclusion') }}</h2>
+        <ExtensionBrowsers class="margin-top-large" />
       </div>
     </div>
   </div>
@@ -17,8 +67,11 @@
 import { mapState } from 'vuex'
 import iframeHeight from '~/mixins/iframeHeight'
 
+import ExtensionBrowsers from '~/components/ExtensionBrowsers.vue'
+
 const AboutPage = {
   name: 'about-page',
+  components: { ExtensionBrowsers },
   mixins: [iframeHeight],
   layout({ store }) {
     return store.state.isEmbedded
@@ -34,5 +87,79 @@ export default AboutPage
 </script>
 
 <style lang="scss" scoped>
-@import '~/styles/text-only-page.scss';
+.hero-section {
+  background-color: $color-wp-gray-0;
+  border-bottom: 1px solid $color-transition-gray;
+
+  $wp-max-width: 940px;
+
+  .container {
+    padding-top: 64px;
+
+    .intro {
+      text-align: center;
+      max-width: 880px;
+      margin: auto;
+    }
+
+    .screenshot {
+      display: block;
+
+      position: relative;
+      bottom: -1px;
+
+      width: 100%;
+      max-width: $wp-max-width;
+      margin: auto;
+
+      filter: drop-shadow(0 -4px 10px rgba(0, 0, 0, 0.1))
+        drop-shadow(0 -20px 33px rgba(0, 0, 0, 0.07));
+
+      border-radius: 0.5rem 0.5rem 0 0;
+    }
+  }
+}
+
+.features {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, auto);
+  gap: 3em;
+
+  img {
+    border-radius: 0.25rem;
+
+    filter: drop-shadow(0px 4px 10px rgba(0, 0, 0, 0.1))
+      drop-shadow(0px 20px 33px rgba(0, 0, 0, 0.07));
+  }
+
+  .caption {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    h2,
+    p {
+      max-width: 30rem;
+    }
+
+    &:nth-of-type(even) {
+      align-items: flex-end;
+
+      h2,
+      p {
+        text-align: right;
+      }
+    }
+  }
+}
+
+.conclusion {
+  h2 {
+    text-align: center;
+
+    max-width: 40rem;
+    margin: auto;
+  }
+}
 </style>
