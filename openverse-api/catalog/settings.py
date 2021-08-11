f9af7856e5e5cda2a200b3c3506e91ad22aac107
Dhruv Bhanushali <dhruv_b@live.com>
2021-08-12 04:25:38 +0530

    Override the Redoc theme to add extra CSS and favicon

--- openverse-api/catalog/settings.py
+++ openverse-api/catalog/settings.py
@@ -181,7 +181,9 @@ ROOT_URLCONF = 'catalog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('catalog', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
