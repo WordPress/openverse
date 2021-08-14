76a0ff115aef5391ece70baf26086928f746e7b9
Dhruv Bhanushali <dhruv_b@live.com>
2021-08-14 19:22:49 +0530

    Add the play/pause toggle button

---
+++ src/components/AudioTrack/PlayPause.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    class="bg-dark-charcoal h-16 w-16 flex items-center justify-center"
+    @click="toggle"
+  >
+    <svg
+      class="text-white h-6 w-6"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <use v-if="isPlaying" :href="`${icons.pause}#icon`" />
+      <use v-else :href="`${icons.play}#icon`" />
+    </svg>
+  </button>
+</template>
+
+<script>
+import playIcon from '~/assets/icons/play.svg'
+import pauseIcon from '~/assets/icons/pause.svg'
+
+/**
+ * Displays the control for switching between the playing and paused states of
+ * a media file.
+ */
+export default {
+  name: 'PlayPause',
+  props: {
+    /**
+     * whether the media associated with the button is currently playing
+     */
+    isPlaying: {
+      type: Boolean,
+    },
+  },
+  data: () => ({
+    icons: {
+      play: playIcon,
+      pause: pauseIcon,
+    },
+  }),
+  methods: {
+    toggle() {
+      this.$emit('toggle', !this.isPlaying)
+    },
+  },
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
 
       // Brand
       yellow: '#ffe033',
+      'dark-charcoal': '#30272e',
+      pink: '#c52b9b',
 
       // Grayscale
       black: 'black',
