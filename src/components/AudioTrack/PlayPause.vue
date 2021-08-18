4dfa1a82ddd20d3d846cc822147ed2dce2d27bc5
Zack Krida <zackkrida@pm.me>
2021-08-17 20:42:26 -0400

    Play/Pause button label

--- src/components/AudioTrack/PlayPause.vue
+++ src/components/AudioTrack/PlayPause.vue
@@ -1,12 +1,16 @@
 <template>
   <button
+    type="button"
     class="bg-dark-charcoal h-20 w-20 flex items-center justify-center disabled:opacity-70"
     @click="toggle"
   >
+    <span class="sr-only">{{ label }}</span>
     <svg
       class="text-white h-8 w-8"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
+      aria-hidden="true"
+      focusable="false"
     >
       <use v-if="isPlaying" :href="`${icons.pause}#icon`" />
       <use v-else :href="`${icons.play}#icon`" />
@@ -38,6 +42,11 @@ export default {
       pause: pauseIcon,
     },
   }),
+  computed: {
+    label() {
+      return this.$t(this.isPlaying ? 'play-pause.pause' : 'play-pause.play')
+    },
+  },
   methods: {
     toggle() {
       this.$emit('toggle', !this.isPlaying)
