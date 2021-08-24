aa3920975da101fa4fa5243c91787a1e4d599f3a
Dhruv Bhanushali <dhruv_b@live.com>
2021-08-24 12:11:12 +0530

    Migrate the component to composition API

--- src/components/AudioTrack/Waveform.vue
+++ src/components/AudioTrack/Waveform.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    ref="waveform"
+    ref="el"
     class="waveform relative bg-dark-charcoal-04 overflow-x-hidden"
     tabIndex="0"
     role="slider"
@@ -10,11 +10,12 @@
     :aria-valuemax="duration"
     :aria-valuenow="currentTime"
     :aria-valuetext="currentTimeText"
-    @keydown.arrow-left="seekJump"
-    @keydown.arrow-right="seekJump"
-    @mousemove="setSeekProgress"
+    @mousedown="handleMouseDown"
+    @mousemove="handleMouseMove"
+    @mouseup="handleMouseUp"
     @mouseleave="clearSeekProgress"
-    @click="seek"
+    @keydown.arrow-left="handleArrows"
+    @keydown.arrow-right="handleArrows"
   >
     <svg
       class="w-full h-full"
@@ -46,24 +47,19 @@
       />
     </svg>
 
-    <div
-      v-if="!isReady"
-      class="absolute inset-x-0 inset-y-0 flex items-center justify-center loading font-bold text-sm"
-    >
-      {{ $t('waveform.loading') }}
-    </div>
+    <!-- Timestamps -->
     <div
       class="current absolute top-1 font-bold text-sm bg-yellow z-10 px-1 transform -translate-x-full pointer-events-none"
       :style="{ '--current-time-left': `${progressBarWidth}px` }"
     >
-      {{ timeFmt(currentTime) }}
+      {{ timeFmt(currentTimestamp) }}
     </div>
     <div
-      v-if="seekPercentage"
+      v-if="seekFrac"
       class="seek absolute top-1 font-bold text-sm px-1 transform -translate-x-full pointer-events-none"
       :style="{ '--seek-time-left': `${seekBarWidth}px` }"
     >
-      {{ timeFmt(seekPercentage * duration) }}
+      {{ timeFmt(seekTimestamp) }}
     </div>
     <div
       v-if="showDuration"
@@ -71,10 +67,24 @@
     >
       {{ timeFmt(duration) }}
     </div>
+
+    <!-- Loading overlay -->
+    <div
+      v-if="!isReady"
+      class="absolute inset-x-0 inset-y-0 flex items-center justify-center loading font-bold text-sm"
+    >
+      {{ $t('waveform.loading') }}
+    </div>
   </div>
 </template>
 
 <script>
+import {
+  computed,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+} from '@nuxtjs/composition-api'
 import { downsampleArray, upsampleArray } from '~/utils/resampling.js'
 
 /**
@@ -123,125 +133,207 @@ export default {
       default: false,
     },
   },
-  data: () => ({
-    barWidth: 2, // px
-    barGap: 2, // px
-    timestampSpace: 0.33, // % of bar height
+  setup(props, { emit }) {
+    const el = ref(null) // template ref
 
-    /**
-     * the position of the graph that the user is hovering over to seek
-     */
-    seekPercentage: null,
+    /* Utils */
 
-    /**
-     * the length in seconds to seek jump
-     */
-    seekJumpLength: 1,
-    modifiedSeekJumpLength: 15,
-
-    waveformWidth: 100, // dummy start value
-    observer: null, // ResizeObserver
-  }),
-  computed: {
-    percentage() {
-      return this.isReady ? this.currentTime / this.duration : 0
-    },
-
-    // The waveform current time as a text string
-    currentTimeText() {
-      const time = this.timeFmt(this.currentTime)
-      return this.$t('waveform.current-time', { time })
-    },
-
-    // The seekJump length as a % of the track
-    seekJumpPercentage() {
-      return this.isReady ? this.seekJumpLength / this.duration : 0
-    },
-    modifiedSeekJumpPercentage() {
-      return this.isReady ? this.modifiedSeekJumpLength / this.duration : 0
-    },
-
-    peakCount() {
-      return Math.floor(
-        (this.waveformWidth - this.barGap) / (this.barWidth + this.barGap)
-      )
-    },
-    normalizedPeaks() {
-      if (this.peaks.length < this.peakCount) {
-        return upsampleArray(this.peaks, this.peakCount)
-      } else if (this.peaks.length > this.peakCount) {
-        return downsampleArray(this.peaks, this.peakCount)
-      }
-      return this.peaks
-    },
-
-    viewBoxHeight() {
-      return 1 + this.timestampSpace
-    },
-    viewBox() {
-      return `0 0 ${this.waveformWidth} ${this.viewBoxHeight}`
-    },
-
-    progressBarWidth() {
-      return this.waveformWidth * this.percentage
-    },
-    seekBarWidth() {
-      return this.waveformWidth * (this.seekPercentage ?? this.percentage)
-    },
-  },
-  async mounted() {
-    this.updateWaveformWidth()
-    this.observer = new ResizeObserver(this.updateWaveformWidth)
-    this.observer.observe(this.$el)
-  },
-  beforeDestroy() {
-    this.observer.disconnect()
-  },
-  methods: {
     /**
      * Format the time as hh:mm:ss, dropping the hour part if it is zero.
+     * @param {number} seconds - the number of seconds in the duration
      * @returns {string} the duration in a human-friendly format
      */
-    timeFmt(seconds) {
+    const timeFmt = (seconds) => {
       const date = new Date(0)
       date.setSeconds(seconds)
       return date.toISOString().substr(11, 8).replace(/^00:/, '')
-    },
+    }
+    /**
+     * Get the x-coordinate of the event with respect to the bounding box of the
+     * waveform.
+     * @param {MouseEvent} event - the event from which to get the position
+     * @returns {number} the x-position of the event inside the waveform
+     */
+    const getPosition = (event) => {
+      return event.clientX - el.value.getBoundingClientRect().x
+    }
+    /**
+     * Get the x-position of the event with respect to the bounding box of the
+     * waveform, as a fraction of the waveform width.
+     * @param {MouseEvent} event - the event from which to get the position
+     * @returns {number} the x-position of the event as a fraction
+     */
+    const getPositionFrac = (event) => {
+      const xPos = getPosition(event)
+      return xPos / waveformWidth.value
+    }
 
-    spaceBefore(index = this.peakCount) {
-      return index * this.barWidth + (index + 1) * this.barGap
-    },
-    spaceAbove(peak) {
-      return this.viewBoxHeight - peak
-    },
+    /* Element dimensions */
 
-    updateWaveformWidth() {
-      this.waveformWidth = this.$el.clientWidth
-    },
+    const waveformWidth = ref(0)
+    const updateWaveformWidth = () => {
+      waveformWidth.value = el.value.clientWidth
+    }
+    const observer = new ResizeObserver(updateWaveformWidth)
+    onMounted(() => {
+      observer.observe(el.value)
+      updateWaveformWidth()
+    })
+    onBeforeUnmount(() => {
+      observer.disconnect()
+    })
 
-    // Event handlers
-    getPositionPercentage(event) {
-      const x = event.clientX - this.$el.getBoundingClientRect().x
-      return x / this.waveformWidth
-    },
-    setSeekProgress(event) {
-      this.seekPercentage = this.getPositionPercentage(event)
-    },
-    clearSeekProgress() {
-      this.seekPercentage = null
-    },
-    seek(event) {
-      this.$emit('seeked', this.getPositionPercentage(event))
-    },
-    seekJump(event) {
-      this.clearSeekProgress()
+    /* Resampling */
+
+    const barWidth = 2
+    const barGap = 2
+    const peakCount = computed(() => {
+      const count = (waveformWidth.value - barGap) / (barWidth + barGap)
+      return Math.floor(count)
+    })
+    const normalizedPeaks = computed(() => {
+      const givenLength = props.peaks.length
+      const required = peakCount.value
+      if (givenLength < required) {
+        return upsampleArray(props.peaks, required)
+      } else if (givenLength > required) {
+        return downsampleArray(props.peaks, required)
+      }
+      return props.peaks
+    })
+
+    /* SVG drawing */
+
+    /**
+     * the fraction of space to reserve for the timestamps above the bars; The
+     * `viewBox` height of the waveform will be 1 + `timestampSpace`.
+     */
+    const timestampSpace = 0.33 // % of bar height
+    const viewBoxHeight = 1 + timestampSpace
+    const viewBox = computed(
+      () => `0 0 ${waveformWidth.value} ${viewBoxHeight}`
+    )
+    const spaceBefore = (index) => index * barWidth + (index + 1) * barGap
+    const spaceAbove = (peak) => viewBoxHeight - peak
+
+    /* Progress bar */
+
+    const currentFrac = computed(() =>
+      props.isReady ? props.currentTime / props.duration : 0
+    )
+    const progressBarWidth = computed(() => {
+      const frac = isDragging.value ? seekFrac.value : currentFrac.value
+      return waveformWidth.value * frac
+    })
+    const currentTimestamp = computed(() =>
+      isDragging.value ? seekTimestamp.value : props.currentTime
+    )
+
+    /* Seek bar */
+
+    const seekFrac = ref(null)
+    const seekBarWidth = computed(() => {
+      const frac = seekFrac.value ?? currentFrac.value
+      return waveformWidth.value * frac
+    })
+    const seekTimestamp = computed(() => seekFrac.value * props.duration)
+
+    /* Seeking */
+
+    const seekDelta = 1 // s
+    const modSeekDelta = 15 // s
+    /**
+     * the seek jump length as a % of the track
+     */
+    const seekDeltaFrac = computed(() => {
+      return props.isReady ? seekDelta / props.duration : 0
+    })
+    const modSeekDeltaFrac = computed(() =>
+      props.isReady ? modSeekDelta / props.duration : 0
+    )
+    const setSeekProgress = (event) => {
+      seekFrac.value = getPositionFrac(event)
+    }
+    const clearSeekProgress = () => {
+      seekFrac.value = null
+    }
+    const seek = (event) => {
+      emit('seeked', getPositionFrac(event))
+    }
+
+    /* Dragging */
+
+    const dragThreshold = 2 // px
+    let startPos = null
+    const isDragging = ref(false)
+    const handleMouseDown = (event) => {
+      isDragging.value = false
+      startPos = getPosition(event)
+      setSeekProgress(event)
+    }
+    const handleMouseMove = (event) => {
+      if (startPos) {
+        const clickPos = getPosition(event)
+        if (Math.abs(clickPos - startPos) > dragThreshold) {
+          isDragging.value = true
+        }
+      }
+      setSeekProgress(event)
+    }
+    const handleMouseUp = (event) => {
+      isDragging.value = false
+      startPos = null
+      seek(event)
+    }
+
+    /* Keyboard */
+
+    const handleArrows = (event) => {
+      clearSeekProgress()
       const { key, shiftKey } = event
-      const amount = shiftKey
-        ? this.modifiedSeekJumpPercentage
-        : this.seekJumpPercentage
-      const seekPercentage =
-        this.percentage + (key.includes('Left') ? -amount : amount)
-      this.$emit('seeked', seekPercentage)
+      const magnitude = shiftKey ? modSeekDeltaFrac.value : seekDeltaFrac.value
+      const direction = key.includes('Left') ? -1 : 1
+      const delta = magnitude * direction
+      emit('seeked', currentFrac.value + delta)
+    }
+
+    return {
+      el, // template ref
+
+      timeFmt,
+
+      barWidth,
+      normalizedPeaks,
+
+      viewBox,
+      spaceBefore,
+      spaceAbove,
+
+      progressBarWidth,
+      currentTimestamp,
+
+      seekFrac,
+      seekBarWidth,
+      seekTimestamp,
+
+      setSeekProgress,
+      clearSeekProgress,
+
+      isDragging,
+      handleMouseDown,
+      handleMouseMove,
+      handleMouseUp,
+      handleArrows,
+    }
+  },
+  computed: {
+    /**
+     * the waveform current time as a text string; This function was placed
+     * outside because `this` is not accessible inside the `setup`.
+     */
+    currentTimeText() {
+      const time = this.timeFmt(this.currentTime)
+      return this.$t('waveform.current-time', { time })
     },
   },
 }
