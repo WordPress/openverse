b828ed6496d0731bd86af916d32a5f63114d2a8b
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-01 02:57:24 +0530

    Update error serializers

--- openverse-api/catalog/api/serializers/error_serializers.py
+++ openverse-api/catalog/api/serializers/error_serializers.py
@@ -1,37 +1,38 @@
+"""
+Reference for exception handling in Django REST Framework:
+    https://www.django-rest-framework.org/api-guide/exceptions/
+"""
+
 from rest_framework import serializers
 
 
 class InputErrorSerializer(serializers.Serializer):
     """ Returned if invalid query parameters are passed. """
-    error = serializers.CharField(
-        help_text="The name of error."
-    )
-    detail = serializers.CharField(
-        help_text="The description for error."
-    )
-    fields = serializers.ListField(
-        help_text="List of query parameters that causes error."
+
+    detail = serializers.DictField(
+        help_text="Mapping of field names with errors from to that field.",
+        child=serializers.ListField(
+            child=serializers.CharField()
+        )
     )
 
 
 class NotFoundErrorSerializer(serializers.Serializer):
     """ Returned if the requested content could not be found. """
     detail = serializers.CharField(
-        help_text="The description for error"
+        help_text="The description for error."
     )
 
 
 class ForbiddenErrorSerializer(serializers.Serializer):
-    """ Returned if access to requested content is forbidden for
-    some reason."""
+    """ Returned if access to requested content is forbidden. """
     detail = serializers.CharField(
-        help_text="The description for error"
+        help_text="The description for error."
     )
 
 
 class InternalServerErrorSerializer(serializers.Serializer):
-    """ Returned if the request could not be processed by the server for an
-    unknown reason."""
+    """ Returned if the request could not be processed. """
     detail = serializers.CharField(
-        help_text="The description for error"
+        help_text="The description for error."
     )
