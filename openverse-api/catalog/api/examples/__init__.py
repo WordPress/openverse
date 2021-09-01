a0012435bd6b04b31426246b2fb277646b226fa3
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-01 16:34:27 +0530

    Define request response mappings and use them to run tests on the endpoints

--- openverse-api/catalog/api/examples/__init__.py
+++ openverse-api/catalog/api/examples/__init__.py
@@ -37,3 +37,17 @@ from catalog.api.examples.image_responses import (
     image_oembed_200_example,
     image_oembed_404_example,
 )
+
+audio_mappings = {
+    audio_search_curl: audio_search_200_example,
+    audio_stats_curl: audio_stats_200_example,
+    audio_detail_curl: audio_detail_200_example,
+    audio_complain_curl: audio_complain_201_example,
+}
+image_mappings = {
+    image_search_curl: image_search_200_example,
+    image_stats_curl: image_stats_200_example,
+    image_detail_curl: image_detail_200_example,
+    image_complain_curl: image_complain_201_example,
+    image_oembed_curl: image_oembed_200_example,
+}
