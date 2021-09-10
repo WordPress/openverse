f8d0728c1c3927b73c21286831709036e52a19d2
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-10 08:36:15 +0530

    Change `data` to `methods`

--- src/mixins/i18n-sync.js
+++ src/mixins/i18n-sync.js
@@ -2,27 +2,25 @@ import { sendWindowMessage } from '~/utils/send-message.js'
 import config from '../../nuxt.config.js'
 
 export default {
-  data() {
-    return {
-      /**
-       * Handles messages of type `localeSet` received by the `iframe`. Any
-       * other message types will by discarded.
-       *
-       * @param {string} type - the nature of the message received
-       * @param {{ locale: string, lang: string }} value - the data sent with the message
-       */
-      localeMsgHandler: ({ data: { type, value } }) => {
-        if (type !== 'localeSet') return
+  methods: {
+    /**
+     * Handles messages of type `localeSet` received by the `iframe`. Any
+     * other message types will by discarded.
+     *
+     * @param {string} type - the nature of the message received
+     * @param {{ locale: string, lang: string }} value - the data sent with the message
+     */
+    localeMsgHandler({ data: { type, value } }) {
+      if (type !== 'localeSet') return
 
-        const locale = this.$i18n.locales.find(
-          (item) => item.wpLocale === value.locale
-        )
-        if (locale) {
-          this.$i18n.setLocale(locale.code)
-          document.documentElement.lang = value.lang
-        }
-      },
-    }
+      const locale = this.$i18n.locales.find(
+        (item) => item.wpLocale === value.locale
+      )
+      if (locale) {
+        this.$i18n.setLocale(locale.code)
+        document.documentElement.lang = value.lang
+      }
+    },
   },
   mounted() {
     window.addEventListener('message', this.localeMsgHandler)
