0dbec69b94eadfcaaac16a341d5e0aba87b5585a
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-13 11:34:40 +0530

    Extend AudioTrack to be composable using layouts

---
+++ src/components/AudioTrack/layouts/Full.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="full-track w-full">
+    <div class="waveform h-30">
+      <slot name="controller" />
+    </div>
+
+    <div class="flex flex-row space-between mx-16 my-6">
+      <div class="left-content flex flex-row items-center gap-6">
+        <slot name="play-pause" />
+
+        <div class="audio-info">
+          <p class="text-3xl font-heading font-semibold">
+            {{ audio.title }}
+          </p>
+          <p class="text-base leading-snug mt-1">{{ audio.creator }}</p>
+        </div>
+      </div>
+
+      <!-- TODO: Download dropdown -->
+      <div class="right-content bg-pink h-12 w-30 rounded-sm ml-auto" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Full',
+  props: ['audio'],
+}
+</script>
