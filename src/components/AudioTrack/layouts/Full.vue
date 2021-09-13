f11dfaea802c55f650d9743ddd2e06d0b48eafb5
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-13 18:24:00 +0530

    Update the full layout to show artist and duration

--- src/components/AudioTrack/layouts/Full.vue
+++ src/components/AudioTrack/layouts/Full.vue
@@ -7,10 +7,21 @@
         <slot name="play-pause" />
 
         <div class="audio-info">
-          <h1 class="text-3xl font-heading font-semibold">
-            {{ audio.title }}
-          </h1>
-          <p class="text-base leading-snug mt-1">{{ audio.creator }}</p>
+          <i18n
+            tag="h1"
+            class="text-3xl font-heading font-semibold"
+            path="audio-track.title"
+          >
+            <template #title>{{ audio.title }}</template>
+            <template #creator>
+              <a class="text-pink hover:text-pink" :href="audio.creator_url">{{
+                audio.creator
+              }}</a>
+            </template>
+          </i18n>
+          <p class="text-base leading-snug mt-1">
+            {{ timeFmt(audio.duration) }}
+          </p>
         </div>
       </div>
 
@@ -24,6 +35,22 @@
 export default {
   name: 'Full',
   props: ['audio'],
+  setup() {
+    /**
+     * Format the time as hh:mm:ss, dropping the hour part if it is zero.
+     * @param {number} ms - the number of milliseconds in the duration
+     * @returns {string} the duration in a human-friendly format
+     */
+    const timeFmt = (ms) => {
+      const date = new Date(0)
+      date.setSeconds(ms / 1e3)
+      return date.toISOString().substr(11, 8).replace(/^00:/, '')
+    }
+
+    return {
+      timeFmt,
+    }
+  },
 }
 </script>
 
