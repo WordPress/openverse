25837a28ad754d1907b157e08ca25c2db0d55b2f
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-14 13:39:59 +0530

    Create the `NoticeBar` component

---
+++ src/components/NoticeBar/NoticeBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="font-heading font-semibold text-base text-center leading-tight bg-yellow text-dark-blue py-6"
+  >
+    <!-- @slot Content goes here -->
+    <slot />
+  </div>
+</template>
+
+<script>
+/**
+ * Displays notices is a yellow strip. It on
+ */
+export default {
+  name: 'NoticeBar',
+}
+</script>
