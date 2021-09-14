d2bb8cad129b7bbb76d40443868e9d0f088a379d
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-14 14:00:12 +0530

    Create the CC Search → Openverse migration notice

---
+++ src/components/MigrationNotice.vue
@@ -0,0 +1,24 @@
+<template>
+  <NoticeBar>
+    {{ $t('migration-notice.intro') }}
+    <i18n tag="p" path="migration-notice.more">
+      <template #read-more>
+        <a
+          class="text-dark-blue hover:text-dark-blue underline"
+          href="https://wordpress.org/news/2021/05/welcome-to-openverse/"
+          target="_blank"
+          >{{ $t('migration-notice.read') }}</a
+        >
+      </template>
+    </i18n>
+  </NoticeBar>
+</template>
+
+<script>
+import NoticeBar from '~/components/NoticeBar/NoticeBar.vue'
+
+export default {
+  name: 'MigrationNotice',
+  components: { NoticeBar },
+}
+</script>
