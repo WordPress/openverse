9721e911f7482489077b2c33f7f3de77f82060e3
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-09-14 08:57:37 -0700

    Add DownloadButton

---
+++ src/components/DownloadButton.vue
@@ -0,0 +1,111 @@
+<template>
+  <DropdownButton>
+    <template #default="{ buttonProps }">
+      <a
+        v-bind="buttonProps"
+        class="whitespace-nowrap"
+        :href="selectedFormat.download_url"
+        :download="downloadName"
+      >
+        <span>{{ $t('download-button.download') }}</span>
+        <span class="ml-4 selected-format">
+          {{ selectedFormat.extension_name }}
+        </span>
+        <span class="ml-1 font-thin">{{
+          getFormatSize(selectedFormat.filesize)
+        }}</span>
+      </a>
+    </template>
+
+    <template
+      #items="{
+        activeItemClass,
+        itemClass,
+        itemA11yProps,
+        toggleOpen,
+        onItemKeydown,
+      }"
+    >
+      <ul>
+        <li v-for="format in formats" :key="format.name">
+          <button
+            class="flex justify-between w-full"
+            :class="[
+              itemClass,
+              selectedFormat.extension_name === format.extension_name
+                ? activeItemClass
+                : '',
+            ]"
+            type="button"
+            v-bind="itemA11yProps"
+            @click="
+              setSelectedFormat(format)
+              toggleOpen()
+            "
+            @keydown="onItemKeydown"
+          >
+            <span class="font-bold mr-2">{{ format.extension_name }}</span>
+            <span>{{ getFormatSize(format.filesize) }}</span>
+          </button>
+        </li>
+      </ul>
+    </template>
+  </DropdownButton>
+</template>
+
+<script>
+import filesize from 'filesize'
+
+const LS_DOWNLOAD_FORMAT_EXTENSION_KEY = 'openverse:download-format-extension'
+
+export default {
+  name: 'DownloadButton',
+  props: {
+    formats: {
+      type: Array,
+      required: true,
+      validator: (formats) => {
+        const properties = ['extension_name', 'download_url', 'filesize']
+        return formats.every((format) => properties.every((p) => p in format))
+      },
+    },
+    fileName: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    const savedFormatExtension =
+      localStorage.getItem(LS_DOWNLOAD_FORMAT_EXTENSION_KEY) ?? null
+    let format = this.formats[0]
+    if (savedFormatExtension) {
+      const found = this.formats.find(
+        (format) => format.extension_name === savedFormatExtension
+      )
+      if (found) {
+        format = found
+      }
+    }
+
+    return { selectedFormat: format }
+  },
+  computed: {
+    downloadName() {
+      return `${this.fileName} - ${this.selectedFormat.extension_name}`
+    },
+  },
+  methods: {
+    getFormatSize(size) {
+      return filesize(size, { locale: this.$i18n.locale })
+    },
+    setSelectedFormat(format) {
+      localStorage.setItem(
+        LS_DOWNLOAD_FORMAT_EXTENSION_KEY,
+        format.extension_name
+      )
+      this.selectedFormat = format
+    },
+    onClick() {},
+  },
+}
+</script>
---
+++ src/components/meta/DownloadButton.stories.js
@@ -0,0 +1,36 @@
+import DownloadButton from '~/components/DownloadButton'
+
+export default {
+  title: 'Components/DownloadButton',
+  component: DownloadButton,
+}
+
+export const Default = () => ({
+  template: `<DownloadButton :file-name="fileName" :formats="formats" />`,
+  components: { DownloadButton },
+  data: () => ({
+    fileName: '01 - Paul Werner - Let Me Hear You I - Let Me Hear You I',
+    formats: [
+      {
+        extension_name: 'MP3 98kbs',
+        download_url: 'https://mp3d.jamendo.com/download/track/1532771/mp31/',
+        filesize: 676352,
+      },
+      {
+        extension_name: 'MP3 VBR',
+        download_url: 'https://mp3d.jamendo.com/download/track/1532771/mp32/',
+        filesize: 1362432,
+      },
+      {
+        extension_name: 'FLAC',
+        download_url: 'https://mp3d.jamendo.com/download/track/1532771/flac/',
+        filesize: 6333717,
+      },
+      {
+        extension_name: 'OGG',
+        download_url: 'https://mp3d.jamendo.com/download/track/1532771/ogg/',
+        filesize: 1362432,
+      },
+    ],
+  }),
+})
