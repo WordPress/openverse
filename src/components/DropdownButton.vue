140ce9222be3b879e57cf7db03842d62c0d057f4
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-09-23 10:30:13 -0700

    Fix safari button appearance

--- src/components/DropdownButton.vue
+++ src/components/DropdownButton.vue
@@ -146,7 +146,7 @@ export default DropdownButton
 
 <style lang="css" scoped>
 .dropdown-button {
-  @apply flex items-center justify-center bg-pink text-white font-bold p-2 px-4 transition-shadow duration-100 ease-linear disabled:opacity-70 focus:outline-none focus-visible:ring focus-visible:ring-offset-2 focus-visible:ring-pink hover:bg-dark-pink no-underline;
+  @apply flex items-center justify-center bg-pink text-white font-bold p-2 px-4 transition-shadow duration-100 ease-linear disabled:opacity-70 focus:outline-none focus-visible:ring focus-visible:ring-offset-2 focus-visible:ring-pink hover:bg-dark-pink no-underline appearance-none;
 }
 
 .dropdown-button-active {
