033dd84b02ebefda97d792bb2f1fac058eb71f58
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-10-15 15:39:41 -0700

    Fix import typo for POT generation (#326)

--- src/locales/scripts/json-to-pot.js
+++ src/locales/scripts/json-to-pot.js
@@ -1,6 +1,6 @@
 const json = require('../en.json')
 const fs = require('fs')
-const { createPotFile } = require('json-pot-helpers')
+const { createPotFile } = require('./json-pot-helpers')
 
 try {
   const fileName = process.cwd() + '/src/locales/po-files/openverse.pot'
