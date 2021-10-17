60fd02a24694d7cbad5edc23e33586b618b4d0f8
Ulan <ulanr92@gmail.com>
2021-10-16 19:10:59 -0500

    updated requested changes from Dhruv Bhanushali

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
     node: true,
-    commonjs: true,
-    es2021: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
