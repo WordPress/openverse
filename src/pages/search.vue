4171a0c3f3cf4a4b2d46f0df48f7a8b06a0efe1e
Olga Bulat <obulat@gmail.com>
2021-10-29 17:54:34 +0300

    Fix filters not being set on SSR (#386)

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -96,11 +96,13 @@ const BrowsePage = {
       setSearchTypeFromUrl: SET_SEARCH_TYPE_FROM_URL,
       updateSearchType: UPDATE_SEARCH_TYPE,
     }),
+    ...mapActions(FILTER, {
+      setFiltersFromUrl: SET_FILTERS_FROM_URL,
+    }),
     ...mapMutations(SEARCH, {
       setQuery: SET_QUERY,
     }),
     ...mapMutations(FILTER, {
-      setFiltersFromUrl: SET_FILTERS_FROM_URL,
       setFilterVisibility: SET_FILTER_IS_VISIBLE,
     }),
     getMediaItems(params, mediaType) {
