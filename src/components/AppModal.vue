c58a2ae35a678caaba23ebe50e7d512fa0a895f2
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-12-17 09:10:20 -0800

    Fix filters modal position in iframe (#537)

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -1,22 +1,16 @@
 <template>
   <!-- eslint-disable-next-line vuejs-accessibility/click-events-have-key-events -->
-  <div ref="modal" class="overlay app-modal" @click.self="$emit('close')">
+  <div
+    ref="modal"
+    class="overlay app-modal fixed w-full flex justify-center z-50 bg-dark-charcoal bg-opacity-75"
+    @click.self="$emit('close')"
+  >
     <FocusTrap :active="true">
-      <div class="modal relative" aria-modal="true" role="dialog">
-        <header v-if="title" class="modal-header pt-8 ps-8 pe-4 pb-2">
-          <slot name="header">
-            <h3>{{ title }}</h3>
-          </slot>
-          <button
-            type="button"
-            class="close-button text-gray text-lgr lg:text-base"
-            :aria-label="$t('browse-page.aria.close')"
-            @click="$emit('close')"
-            @keypress.enter="$emit('close')"
-          >
-            <i class="icon cross" />
-          </button>
-        </header>
+      <div
+        class="rounded mt-10 mb-80 min-h-min bg-white"
+        aria-modal="true"
+        role="dialog"
+      >
         <slot default />
       </div>
     </FocusTrap>
@@ -54,54 +48,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-.modal {
-  position: relative;
-  margin: 0px auto;
-  max-width: 85vw;
-  max-height: 85vh;
-  overflow-x: hidden;
-  overflow-y: auto;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px 3px;
-  background-color: #fff;
-}
-
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  z-index: 600;
-  background: #00000094;
-}
-
-.modal-header {
-  display: flex;
-  justify-content: space-around;
-  align-items: flex-start;
-  width: 100%;
-}
-
-.close-button {
-  appearance: none;
-  border: none;
-  height: auto;
-  margin: -20px -20px -20px auto;
-  padding: 20px;
-  background-color: transparent;
-  line-height: 1;
-  cursor: pointer;
-  .icon {
-    height: auto;
-  }
-  &:hover {
-    color: rgb(120, 120, 120);
-  }
-}
-</style>
