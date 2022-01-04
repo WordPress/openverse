7e79a5a684a91448c6ceeb7d596ba5e1b5409391
Prabhav Pandey <prabhavp380@gmail.com>
2022-01-04 21:41:58 +0530

    Deleted all instances of publish_release.sh (#449)

--- ingestion_server/publish_release.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env sh
-
-# Usage: ./public_release.sh [VERSION]
-docker build -t openverse/ingestion_server:$1 .
-docker build -f Dockerfile-worker -t openverse/indexer_worker:$1 .
-docker push openverse/ingestion_server:$1
-docker push openverse/indexer_worker:$1
