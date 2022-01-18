aeddca5d1e9a33366d910d15c403a57bc5cf1715
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-01-18 11:36:37 -0500

    Stop logging vuex stuff (#625)

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,5 @@
-import { createLogger } from 'vuex'
 import { FETCH_MEDIA_PROVIDERS } from '~/constants/action-types'
 import { PROVIDER } from '~/constants/store-modules'
-
-export const plugins = [createLogger()]
 
 export const actions = {
   async nuxtServerInit({ dispatch }) {
