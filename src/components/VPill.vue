e0003eea63bbc8539ca060d980e61ac22f125925
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-01-19 07:56:14 -0500

    Add VPill component (#622)

---
+++ src/components/VPill.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="bg-yellow uppercase w-min px-2 py-1 text-sm font-semibold rounded-sm"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'VPill',
+})
+</script>
