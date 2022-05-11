8f8eb8496ee5810803346bda9d0a77117b1cffc4
J Overalls <42696142+foroveralls@users.noreply.github.com>
2022-05-11 12:16:06 +0200

    Add 3D model type to constants (#690)

--- ingestion_server/ingestion_server/constants/media_types.py
+++ ingestion_server/ingestion_server/constants/media_types.py
@@ -4,5 +4,6 @@ Also see `api/constants/media_types.py`
 
 AUDIO_TYPE = "audio"
 IMAGE_TYPE = "image"
+MODEL_3D_TYPE = "model_3d"
 
-MEDIA_TYPES = [AUDIO_TYPE, IMAGE_TYPE]
+MEDIA_TYPES = [AUDIO_TYPE, IMAGE_TYPE, MODEL_3D_TYPE]
