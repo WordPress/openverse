43fd8e2f93becf98fef2091dcd932840a2d9683f
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-06-29 23:55:20 +1000

    Move PO file into special branch (#1535)
    * Add workflow to update POT file in translations branch
    
    * Remove POT file from main repository and ignore it
    
    * Always write POT file
    
    * Remove pre-commit pot file generation
    
    * Fix ENOENT issue due to nonexistent directory
    
    * Stop checking PO generation in CI

--- src/locales/scripts/json-to-pot.js
+++ src/locales/scripts/json-to-pot.js
@@ -1,31 +1,14 @@
 const fs = require('fs')
+const path = require('path')
 
 const json = require('../en.json')
 
 const { createPotFile } = require('./json-pot-helpers')
 
-const matchPotCreationDate = /^"POT-Creation-Date: .*\\n"$/gm
-
 try {
-  const fileName = process.cwd() + '/src/locales/po-files/openverse.pot'
-  const existingPotFile = String(fs.readFileSync(fileName)).replace(
-    matchPotCreationDate,
-    ''
-  )
-  const potFile = createPotFile(json)
+  const fileName = path.resolve(process.cwd(), 'openverse.pot')
 
-  if (existingPotFile === potFile.replace(matchPotCreationDate, '')) {
-    console.log('No change detected in pot file, skipping write')
-    process.exit(0)
-  }
-
-  fs.writeFileSync(fileName, createPotFile(json))
-  if (process.env.CI) {
-    console.error(
-      'Detected uncommitted POT file changes. Please run `pnpm i18n:generate-pot` and commit the updated POT file.'
-    )
-    process.exit(1)
-  }
+  fs.writeFileSync(fileName, createPotFile(json), { flag: 'w' })
   console.log(`Successfully wrote pot file to ${fileName}`)
 } catch (err) {
   console.error(err)
