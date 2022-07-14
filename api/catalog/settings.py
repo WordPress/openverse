2d109d9b4ddf4e12b34b4d85fdc7ae8c47fb9378
Raghuvar arora <raghuvararora@gmail.com>
2022-07-14 07:43:21 +0530

    increased token expiry to 12 hours (#788)
    * increased token expiry to 12 hours
    
    * made token expiry time configurable

--- api/catalog/settings.py
+++ api/catalog/settings.py
@@ -123,7 +123,10 @@ OAUTH2_PROVIDER = {
     "SCOPES": {
         "read": "Read scope",
         "write": "Write scope",
-    }
+    },
+    "ACCESS_TOKEN_EXPIRE_SECONDS": config(
+        "ACCESS_TOKEN_EXPIRE_SECONDS", default=3600 * 12, cast=int
+    ),
 }
 
 OAUTH2_PROVIDER_APPLICATION_MODEL = "api.ThrottledApplication"
