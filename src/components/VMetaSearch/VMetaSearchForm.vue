c2a5833491da0ee3383b9a0676a1b7eaba2c7c42
John L. Budd <88851966+Jbudd512@users.noreply.github.com>
2022-08-19 06:16:34 -0500

    Replace Search result page `h` element with a `p` element (#1647)

--- src/components/VMetaSearch/VMetaSearchForm.vue
+++ src/components/VMetaSearch/VMetaSearchForm.vue
@@ -2,50 +2,52 @@
   <section
     :key="type"
     ref="sectionRef"
-    class="meta-search mt-12 p-6 text-center"
+    class="meta-search p-6 text-center"
     data-testid="meta-search-form"
     @keydown.tab.exact="handleTab"
   >
-    <header class="mb-10">
-      <i18n
-        v-if="!hasNoResults"
-        :path="
-          isSupported
-            ? 'meta-search.form.supported-title'
-            : 'meta-search.form.unsupported-title'
-        "
-        tag="h4"
-        class="mb-2 text-4xl"
-      >
-        <template #openverse>Openverse</template>
-        <template #type>{{ $t(`meta-search.form.types.${type}`) }}</template>
-      </i18n>
-      <i18n
-        v-else
-        path="meta-search.form.no-results-title"
-        tag="h4"
-        class="mb-2 text-4xl"
-      >
-        <template #type>{{ $t(`meta-search.form.types.${type}`) }}</template>
-        <template #query>{{ query.q }}</template>
-      </i18n>
-      <i18n path="meta-search.form.caption" tag="p">
-        <template #type>{{
-          $t(`meta-search.form.types-plural.${type}`)
-        }}</template>
-        <template #break>
-          <br />
-        </template>
-      </i18n>
-    </header>
+    <i18n
+      v-if="!hasNoResults"
+      :path="
+        isSupported
+          ? 'meta-search.form.supported-title'
+          : 'meta-search.form.unsupported-title'
+      "
+      tag="p"
+      class="text-base font-normal leading-[130%]"
+    >
+      <template #openverse>Openverse</template>
+      <template #type>{{ $t(`meta-search.form.types.${type}`) }}</template>
+    </i18n>
+    <i18n
+      v-else
+      path="meta-search.form.no-results-title"
+      tag="p"
+      class="text-base font-normal leading-[130%]"
+    >
+      <template #type>{{ $t(`meta-search.form.types.${type}`) }}</template>
+      <template #query>{{ query.q }}</template>
+    </i18n>
+    <i18n
+      class="text-base font-normal leading-[130%]"
+      path="meta-search.form.caption"
+      tag="p"
+    >
+      <template #type>{{
+        $t(`meta-search.form.types-plural.${type}`)
+      }}</template>
+      <template #break>
+        <br />
+      </template>
+    </i18n>
 
     <VMetaSourceList
-      class="mt-6 mb-10 md:justify-center"
+      class="my-8 md:justify-center"
       :type="type"
       :query="query"
     />
 
-    <p class="my-0 mx-auto max-w-3xl text-sm font-semibold">
+    <p class="my-0 mx-auto max-w-3xl text-base font-normal leading-[130%]">
       {{ $t('meta-search.caption', { openverse: 'Openverse' }) }}
     </p>
   </section>
