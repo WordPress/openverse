70c66f93c4599c4fda6b67b20081b9603fe0e446
Staci Cooper <63313398+stacimc@users.noreply.github.com>
2022-08-31 13:31:48 -0700

    Initialize iNaturalist with dagrun conf (#707)
    * Pass args through to super()
    
    * Update openverse_catalog/dags/providers/provider_api_scripts/inaturalist.py
    
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>
    
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>

--- openverse_catalog/dags/providers/provider_api_scripts/inaturalist.py
+++ openverse_catalog/dags/providers/provider_api_scripts/inaturalist.py
@@ -46,8 +46,8 @@ class INaturalistDataIngester(ProviderDataIngester):
 
     providers = {"image": prov.INATURALIST_DEFAULT_PROVIDER}
 
-    def __init__(self, *kwargs):
-        super(INaturalistDataIngester, self).__init__()
+    def __init__(self, *args):
+        super().__init__(*args)
         self.pg = PostgresHook(POSTGRES_CONN_ID)
 
         # adjustments to buffer limits. TO DO: try to integrate this with the dev
