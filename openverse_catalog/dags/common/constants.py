d678dc7add4ebf428f9402d9300b342ff9d175d3
Madison Swain-Bowden <bowdenm@spu.edu>
2022-10-10 13:29:50 -0700

    Disable email on failure by default (#788)

--- openverse_catalog/dags/common/constants.py
+++ openverse_catalog/dags/common/constants.py
@@ -17,6 +17,9 @@ DAG_DEFAULT_ARGS = {
     "owner": "data-eng-admin",
     "depends_on_past": False,
     "start_date": datetime(2019, 1, 15),
+    # Failure emails are on by default but the Slack failure callback will be used in
+    # place of them if the DAG default args are used.
+    "email_on_failure": False,
     "email_on_retry": False,
     "email": [CONTACT_EMAIL],
     "retries": 2,
