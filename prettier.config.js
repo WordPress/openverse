43fa1cbf7dd112c58623781e553d5d9aa69c9d46
Dhruv Bhanushali <dhruv_b@live.com>
2022-11-23 12:05:28 +0400

    Add a disclaimer on top of synced files

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,7 @@
+// This file is being synced from WordPress/openverse. Any changes made to it
+// here will be overwritten. Please make any necessary edits to these files:
+// - https://github.com/WordPress/openverse/blob/main/prettier.config.js.jinja
+
 module.exports = {
   trailingComma: 'es5',
   tabWidth: 2,
