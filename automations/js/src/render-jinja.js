083f196cf10cc24f95e43b767b47304cf748dc68
Dhruv Bhanushali <dhruv_b@live.com>
2022-11-29 20:21:07 +0400

    Sync all pre-commit configs from this repo (#334)

---
+++ automations/js/src/render-jinja.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
+const nunjucks = require('nunjucks')
+
+const rootDir = path.join(__dirname, '..', '..', '..')
+nunjucks.configure(rootDir, {
+  trimBlocks: true,
+  lstripBlocks: true,
+  autoescape: true,
+})
+
+function render(inFileName, outFile, context = {}) {
+  const inFilePath = path.join(rootDir, inFileName)
+  const outFilePath = path.join(rootDir, outFile)
+  fs.writeFileSync(
+    outFilePath,
+    nunjucks.renderString(fs.readFileSync(inFilePath).toString(), context)
+  )
+}
+
+render(process.argv[2], process.argv[3], JSON.parse(process.argv[4]))
