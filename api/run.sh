e60997f63b792c441d1c900ee8afc627a72a16ea
Madison Swain-Bowden <bowdenm@spu.edu>
2022-12-19 17:13:39 -0800

    Add Django database migrations to Dockerfile entrypoint (#1049)

--- api/run.sh
+++ api/run.sh
@@ -8,4 +8,10 @@ while [ "$(curl -s -o /dev/null -w '%{http_code}' "$es_url")" != "200" ]; do
 done
 echo "Elasticsearch connection established!"
 
+# Run any database migrations
+if [ "$DJANGO_MIGRATE_DB_ON_STARTUP" != "False" ]; then
+  echo "Running database migrations..."
+  python manage.py migrate
+fi
+
 exec "$@"
