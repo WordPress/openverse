f9b2a94b3f72a94ad902ed24f4f009abdf04396f
Dhruv Bhanushali <dhruv_b@live.com>
2022-12-23 10:39:28 +0400

    Authenticate with `GITHUB_TOKEN` when setting up pre-commit (#2065)

--- bin/install-pre-commit.sh
+++ bin/install-pre-commit.sh
@@ -4,8 +4,16 @@ set -e
 
 if [ -z "$SKIP_PRE_COMMIT" ] && [ ! -f ./pre-commit.pyz ]
 then
+  echo "downloading pre-commit"
+  curl \
+    ${GITHUB_TOKEN:+ --header "Authorization: Bearer ${GITHUB_TOKEN}"} \
+    https://api.github.com/repos/pre-commit/pre-commit/releases/latest \
+  | grep -o 'https://.*\.pyz' -m 1 \
+  | xargs \
+  | xargs curl -fsJo pre-commit.pyz -L
+
   echo "installing pre-commit"
-  pnpm pc:download && pnpm pc:install
+  pnpm pc:install
 else
   echo "skipping pre-commit installation"
 fi
