6bc3db23ebfbfa5aad52bab9a93a2eb130fea7b8
Openverse (Bot) <101814513+openverse-bot@users.noreply.github.com>
2022-12-31 05:42:10 +1100

    🔄 Synced file(s) with WordPress/openverse (#2054)
    Co-authored-by: openverse-bot <null>

--- prettier.config.js
+++ prettier.config.js
@@ -11,7 +11,7 @@ module.exports = {
   proseWrap: "always",
   overrides: [
     {
-      files: "*.yml",
+      files: ["*.yml", "*.yaml"],
       options: {
         proseWrap: "preserve",
       },
