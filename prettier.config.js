97a45b142c629f2830b0ae53325ea8e3db0c04d1
Openverse (Bot) <101814513+openverse-bot@users.noreply.github.com>
2023-01-10 04:28:13 +1100

    🔄 Synced file(s) with WordPress/openverse (#2081)
    Co-authored-by: openverse-bot <null>

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 // This file is being synced from WordPress/openverse. Any changes made to it
 // here will be overwritten. Please make any necessary edits to these files:
 // - https://github.com/WordPress/openverse/blob/main/prettier.config.js.jinja
-// - https://github.com/WordPress/openverse/blob/main/prettier.config.frontend.js.jinja
+// - https://github.com/WordPress/openverse/blob/main/templates/prettier.config.frontend.js.jinja
 
 module.exports = {
   trailingComma: "es5",
