840b56e7bf2b59c8c897d44bfc4baa7bed32b9c2
Dhruv Bhanushali <dhruv_b@live.com>
2023-02-25 08:04:15 +0400

    Prevent injection in weekly report script (#773)

---
+++ automations/js/src/html.mjs
@@ -0,0 +1,17 @@
+/**
+ * Replace characters in the string that have special meanings in HTML with
+ * their HTML character entities to prevent injection.
+ *
+ * @param text {string} - the string to sanitise
+ * @returns {string} the clean string that can be interpolated into HTML markup
+ */
+export const escapeHtml = (text) => {
+  const charHtmlMap = {
+    "'": '&apos;',
+    '"': '&quot;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '&': '&amp;',
+  }
+  return text.replace(/[&"'<>]/g, (c) => charHtmlMap[c])
+}
