f89358733ddb12939777271af95b120b8ee3f1a1
Openverse (Bot) <101814513+openverse-bot@users.noreply.github.com>
2023-02-28 09:01:11 +1100

    🔄 synced file(s) with WordPress/openverse (#1002)
    * 🔄 synced local '.github/workflows/label_new_pr.yml' with remote '.github/workflows/label_new_pr.yml'
    
    * 🔄 synced local '.github/release_drafter.yml' with remote '.github/release_drafter.yml'
    
    * 🔄 synced local 'prettier.config.js' with remote 'templates/prettier.config.js.jinja'
    
    * 🔄 synced local '.pre-commit-config.yaml' with remote 'templates/.pre-commit-config.yaml.jinja'
    
    * 🔄 created local '.github/PULL_REQUEST_TEMPLATE/pull_request_template.md' from remote 'templates/pull_request_template.catalog.md.jinja'
    
    * Remove previous PR template
    
    ---------
    
    Co-authored-by: openverse-bot <null>
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 // This file is being synced from WordPress/openverse. Any changes made to it
 // here will be overwritten. Please make any necessary edits to these files:
-// - https://github.com/WordPress/openverse/blob/main/prettier.config.js.jinja
+// - https://github.com/WordPress/openverse/blob/main/templates/prettier.config.js.jinja
 
 module.exports = {
   trailingComma: "es5",
