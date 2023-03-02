0855001e684e8a51d7e0c558cfdea0a65670b1c4
anton202 <a.kluge202@gmail.com>
2023-03-02 14:07:21 +0200

    lint

--- frontend/test/unit/specs/components/VToggleSwitch/v-toggle-switch.spec.js
+++ frontend/test/unit/specs/components/VToggleSwitch/v-toggle-switch.spec.js
@@ -3,37 +3,37 @@ import { mount } from "@vue/test-utils"
 import VToggleSwitch from "~/components/VToggleSwitch/VToggleSwitch.vue"
 
 test("should render a toggle swtich with a string lable", async () => {
-    const wrapper = mount(VToggleSwitch, {
-        slots: {
-            default: "<span>Toggle Switch</span>",
-        },
-    })
+  const wrapper = mount(VToggleSwitch, {
+    slots: {
+      default: "<span>Toggle Switch</span>",
+    },
+  })
 
-    const toggleSwitch = wrapper.find('input[type="checkbox"]')
-    const knob = wrapper.find(".knob")
-    const layer = wrapper.find(".layer")
+  const toggleSwitch = wrapper.find('input[type="checkbox"]')
+  const knob = wrapper.find(".knob")
+  const layer = wrapper.find(".layer")
 
-    expect(wrapper.find("span").text()).toBe("Toggle Switch")
-    expect(toggleSwitch.exists()).toBe(true)
-    expect(knob.exists()).toBe(true)
-    expect(layer.exists()).toBe(true)
+  expect(wrapper.find("span").text()).toBe("Toggle Switch")
+  expect(toggleSwitch.exists()).toBe(true)
+  expect(knob.exists()).toBe(true)
+  expect(layer.exists()).toBe(true)
 })
 
 test("should render a checked toggle switch if `checked` is true", () => {
-    const wrapper = mount(VToggleSwitch, {
-        propsData: {
-            checked: true,
-        },
-    })
+  const wrapper = mount(VToggleSwitch, {
+    propsData: {
+      checked: true,
+    },
+  })
 
-    const toggleSwitch = wrapper.find("input:checked")
+  const toggleSwitch = wrapper.find("input:checked")
 
-    expect(toggleSwitch.element.checked).toBe(true)
+  expect(toggleSwitch.element.checked).toBe(true)
 })
 
 test("should emit change event on change", () => {
-    const wrapper = mount(VToggleSwitch)
+  const wrapper = mount(VToggleSwitch)
 
-    wrapper.vm.onChange()
-    expect(wrapper.emitted().change).toBeTruthy()
+  wrapper.vm.onChange()
+  expect(wrapper.emitted().change).toBeTruthy()
 })
