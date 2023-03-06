9e6628b4bbb8d201b3fdf91e72d1e2bbf4454a34
Dhruv Bhanushali <dhruv_b@live.com>
2023-03-06 04:58:32 +0400

    fixup! Create composable for firing events

--- frontend/src/composables/use-analytics.ts
+++ frontend/src/composables/use-analytics.ts
@@ -5,29 +5,57 @@ import type { Events, EventName } from "~/types/analytics"
 
 import type { Context } from "@nuxt/types"
 
-export const useAnalytics = (context?: Context) => {
-  const { $plausible, $ua, i18n } = context ?? useContext()
+type ContextReturn = ReturnType<typeof useContext>
+
+/**
+ * Get Plausible props that work identically on the server-side and the
+ * client-side. This excludes props that need `window`.
+ *
+ * @param $ua - the User-Agent information from `express-useragent`
+ * @param i18n - the Nuxt i18n instance from `@nuxtjs/i18n`
+ */
+export const getIsomorphicProps = ({ $ua, i18n }: Context | ContextReturn) => ({
+  timestamp: new Date().toISOString(),
+  language: i18n.locale,
+  ...($ua
+    ? {
+        ua: $ua.source,
+        os: $ua.os,
+        platform: $ua.platform,
+        browser: $ua.browser,
+        version: $ua.version,
+      }
+    : {}),
+})
+
+/**
+ * Get Plausible props that work only on the client-side. This only includes
+ * props that need `window`.
+ */
+export const getWindowProps = () => ({
+  origin: window.location.origin,
+  pathname: window.location.pathname,
+  referrer: window.document.referrer,
+  width: window.innerWidth,
+  height: window.innerHeight,
+})
+
+/**
+ * The `ctx` parameter must be supplied if using this composable outside the
+ * bounds of the composition API.
+ *
+ * @param ctx - the Nuxt context
+ */
+export const useAnalytics = (ctx?: Context) => {
+  const context = ctx ?? useContext()
+  const { $plausible } = context
 
   /**
    * fields that should always be sent from custom events
    */
   const mandatoryProps = computed(() => ({
-    timestamp: new Date().toISOString(),
-    origin: window.location.origin,
-    pathname: window.location.pathname,
-    referrer: document.referrer,
-    language: i18n.locale,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    ...($ua
-      ? {
-          ua: $ua.source,
-          os: $ua.os,
-          platform: $ua.platform,
-          browser: $ua.browser,
-          version: $ua.version,
-        }
-      : {}),
+    ...getIsomorphicProps(context),
+    ...getWindowProps(),
   }))
 
   /**
