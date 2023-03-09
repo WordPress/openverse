d0b441fa54ac4a0486f7cededd4e3352069cc750
Dhruv Bhanushali <dhruv_b@live.com>
2023-03-09 16:12:50 +0400

    Simplify analytics composable

--- frontend/src/composables/use-analytics.ts
+++ frontend/src/composables/use-analytics.ts
@@ -1,53 +1,48 @@
+import { computed } from "vue"
 import { useContext } from "@nuxtjs/composition-api"
 
 import type { Events, EventName } from "~/types/analytics"
 
-import type { Context } from "@nuxt/types"
-
-type ContextReturn = ReturnType<typeof useContext>
-
-/**
- * Get Plausible props that work identically on the server-side and the
- * client-side. This excludes props that need `window`.
- *
- * @param $ua - the User-Agent information from `express-useragent`
- * @param i18n - the Nuxt i18n instance from `@nuxtjs/i18n`
- */
-export const getIsomorphicProps = ({ $ua, i18n }: Context | ContextReturn) => ({
-  timestamp: new Date().toISOString(),
-  language: i18n.locale,
-  ...($ua
-    ? {
-        ua: $ua.source,
-        os: $ua.os,
-        platform: $ua.platform,
-        browser: $ua.browser,
-        version: $ua.version,
-      }
-    : {}),
-})
-
-/**
- * Get Plausible props that work only on the client-side. This only includes
- * props that need `window`.
- */
-export const getWindowProps = () => ({
-  origin: window.location.origin,
-  pathname: window.location.pathname,
-  referrer: window.document.referrer,
-  width: window.innerWidth,
-  height: window.innerHeight,
-})
-
 /**
  * The `ctx` parameter must be supplied if using this composable outside the
  * bounds of the composition API.
- *
- * @param ctx - the Nuxt context
  */
 export const useAnalytics = () => {
-  const context = useContext()
-  const { $plausible } = context
+  const { $plausible, $ua, i18n } = useContext()
+
+  /**
+   * the Plausible props that work identically on the server-side and the
+   * client-side; This excludes props that need `window`.
+   */
+  const isomorphicProps = computed(() => ({
+    timestamp: new Date().toISOString(),
+    language: i18n.locale,
+    ...($ua
+      ? {
+          ua: $ua.source,
+          os: $ua.os,
+          platform: $ua.platform,
+          browser: $ua.browser,
+          version: $ua.version,
+        }
+      : {}),
+  }))
+
+  /**
+   * the Plausible props that work only on the client-side; This only includes
+   * props that need `window`.
+   */
+  const windowProps = computed(() =>
+    window
+      ? {
+          origin: window.location.origin,
+          pathname: window.location.pathname,
+          referrer: window.document.referrer,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        }
+      : {}
+  )
 
   /**
    * Send a custom event to Plausible. Mandatory props are automatically merged
@@ -62,8 +57,8 @@ export const useAnalytics = () => {
   ) => {
     $plausible.trackEvent(name, {
       props: {
-        ...getIsomorphicProps(context),
-        ...getWindowProps(),
+        ...isomorphicProps.value,
+        ...windowProps.value,
         ...payload, // can override mandatory props
       },
     })
