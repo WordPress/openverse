e13457fb7fa9c16ad3ab2232ef43640f72804024
Dhruv Bhanushali <dhruv_b@live.com>
2023-03-09 16:00:53 +0400

    Remove function for sending events using Axios

--- frontend/src/composables/use-analytics.ts
+++ frontend/src/composables/use-analytics.ts
@@ -1,15 +1,8 @@
-import axios from "axios"
-import { isRef } from "vue"
 import { useContext } from "@nuxtjs/composition-api"
-
-import { log } from "~/utils/console"
 
 import type { Events, EventName } from "~/types/analytics"
 
 import type { Context } from "@nuxt/types"
-
-const OPENVERSE_UA =
-  "Openverse/0.1 (https://wordpress.org/openverse; openverse@wordpress.org)"
 
 type ContextReturn = ReturnType<typeof useContext>
 
@@ -47,34 +40,14 @@ export const getWindowProps = () => ({
 })
 
 /**
- * Get Plausible props present in `window` through alternate channels. This uses
- * Nuxt context fields that are only present on the server-side.
- *
- * @param req - the request received by the Nuxt SSR server
- * @param route - the Vue router route being rendered from `vue-router`
- * @param $cookies - the cookies object from `cookie-universal-nuxt`
- */
-export const getPseudoWindowProps = ({
-  req,
-  route,
-  $cookies,
-}: Context | ContextReturn) => ({
-  origin: req.headers.host ?? "", // `Host` header includes port.
-  pathname: isRef(route) ? route.value.fullPath : route.fullPath,
-  referrer: req.headers.referer ?? "",
-  width: $cookies.get("uiDeviceWidth") ?? -1,
-  height: $cookies.get("uiDeviceHeight") ?? -1,
-})
-
-/**
  * The `ctx` parameter must be supplied if using this composable outside the
  * bounds of the composition API.
  *
  * @param ctx - the Nuxt context
  */
-export const useAnalytics = (ctx?: Context) => {
-  const context = ctx ?? useContext()
-  const { $cookies, $plausible, route, req } = context
+export const useAnalytics = () => {
+  const context = useContext()
+  const { $plausible } = context
 
   /**
    * Send a custom event to Plausible. Mandatory props are automatically merged
@@ -96,67 +69,7 @@ export const useAnalytics = (ctx?: Context) => {
     })
   }
 
-  /**
-   * Send a custom event to Plausible. Mandatory props are automatically merged
-   * with the event-specific props. This function is meant to be used on the
-   * server side because it uses their API instead of using the integration.
-   *
-   * @param name - the name of the event being recorded
-   * @param payload - the additional information to record about the event
-   */
-  const sendCustomEventApi = async <T extends EventName>(
-    name: T,
-    payload: Events[T]
-  ) => {
-    const origin = req.headers.host ?? "" // `Host` header includes port.
-    const pathname = isRef(route) ? route.value.fullPath : route.fullPath
-    const protocol =
-      origin.includes("localhost") || origin.match(/(\d{1,3}\.){3}\d{1,3}/)
-        ? "http:"
-        : "https:"
-    const url = `${protocol}//${origin}${pathname}`
-
-    const referrer = req.headers.referer ?? ""
-
-    const width = $cookies.get("uiDeviceWidth") ?? -1
-
-    const xFFHeader = req.headers["x-forwarded-for"]
-    const xForwardedFor = Array.isArray(xFFHeader) // If multiple `X-Forwarded-For`...
-      ? xFFHeader[0] // ...pick first.
-      : xFFHeader // If single `X-Forwarded-For`...
-      ? xFFHeader // ...use it.
-      : req.socket.remoteAddress // If remote address known...
-      ? req.socket.remoteAddress // ...use it.
-      : ""
-
-    const res = await axios.post(
-      "/api/event",
-      {
-        domain: process.env.PLAUSIBLE_DOMAIN ?? "localhost",
-        name, // Event name
-        url,
-        referrer,
-        width,
-        props: {
-          ...getIsomorphicProps(context),
-          ...getPseudoWindowProps(context),
-          ...payload,
-        },
-      },
-      {
-        baseURL: process.env.PLAUSIBLE_API_HOST ?? "http://localhost:50288",
-        headers: {
-          "User-Agent": req.headers["user-agent"] ?? OPENVERSE_UA,
-          Referrer: referrer,
-          "X-Forwarded-For": xForwardedFor,
-        },
-      }
-    )
-    log(`Sent ${name}; Received ${res.status}: ${res.data}`)
-  }
-
   return {
     sendCustomEvent,
-    sendCustomEventApi,
   }
 }
