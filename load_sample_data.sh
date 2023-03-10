15111c26bbca2095e2547e1f9678dc786008d9ea
Dhruv Bhanushali <dhruv_b@live.com>
2023-03-10 17:48:29 +0400

    Remove redundant steps from loading script

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -24,7 +24,7 @@ docker-compose exec -T "$WEB_SERVICE_NAME" /bin/bash -c "python3 manage.py shell
 	EOF"
 
 # Load content providers
-docker-compose exec -T "$DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d openledger <<-EOF
+docker-compose exec -T "$DB_SERVICE_NAME" /bin/bash -c "psql <<-EOF
 	DELETE FROM content_provider;
 	INSERT INTO content_provider
 		(created_on, provider_identifier, provider_name, domain_name, filter_content, media_type)
@@ -36,63 +36,27 @@ docker-compose exec -T "$DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d openle
 		(now(), 'wikimedia_audio', 'Wikimedia', 'https://commons.wikimedia.org', false, 'audio');
 	EOF"
 
-docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d openledger <<-EOF
+docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql <<-EOF
 	DROP TABLE IF EXISTS content_provider CASCADE;
 	EOF"
-docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "PGPASSWORD=deploy pg_dump -t content_provider -U deploy -d openledger -h db | psql -U deploy -d openledger"
+docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "PGPASSWORD=deploy pg_dump -t content_provider -h db | psql"
 
 # Load sample data for images
-docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "PGPASSWORD=deploy pg_dump -s -t image -U deploy -d openledger -h db | psql -U deploy -d openledger"
-docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d openledger <<-EOF
-	ALTER TABLE image
-		RENAME TO image_view;
-	ALTER TABLE image_view
-		ADD COLUMN standardized_popularity double precision,
-		ADD COLUMN ingestion_type          varchar(1000);
-
-	\copy image_view \
-			(identifier, created_on, updated_on, ingestion_type, provider, source, foreign_identifier, foreign_landing_url, url, thumbnail, width, height, filesize, license, license_version, creator, creator_url, title, meta_data, tags, watermarked, last_synced_with_source, removed_from_source, filetype, category, standardized_popularity) \
+docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql <<-EOF
+	\copy image \
+			(identifier, created_on, updated_on, ingestion_type, provider, source, foreign_identifier, foreign_landing_url, url, thumbnail, width, height, filesize, license, license_version, creator, creator_url, title, meta_data, tags, watermarked, last_synced_with_source, removed_from_source, filetype, category) \
 		from './sample_data/sample_images.csv' \
 		with (FORMAT csv, HEADER true);
+	REFRESH MATERIALIZED VIEW image_view;
 	EOF"
 
 # Load sample data for audio
-docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "PGPASSWORD=deploy pg_dump -s -t audio -U deploy -d openledger -h db | head -n -14 | psql -U deploy -d openledger"
-docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d openledger <<-EOF
-	ALTER TABLE audio
-		RENAME TO audio_view;
-	ALTER TABLE audio_view
-		ADD COLUMN standardized_popularity double precision,
-		ADD COLUMN ingestion_type          varchar(1000),
-		ADD COLUMN audio_set               jsonb;
-
-	\copy audio_view \
-			(identifier, created_on, updated_on, ingestion_type, provider, source, foreign_identifier, foreign_landing_url, url, thumbnail, filetype, duration, bit_rate, sample_rate, category, genres, audio_set, audio_set_position, alt_files, filesize, license, license_version, creator, creator_url, title, meta_data, tags, watermarked, last_synced_with_source, removed_from_source, standardized_popularity) \
+docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql <<-EOF
+	\copy audio \
+			(identifier, created_on, updated_on, ingestion_type, provider, source, foreign_identifier, foreign_landing_url, url, thumbnail, filetype, duration, bit_rate, sample_rate, category, genres, audio_set, set_position, alt_files, filesize, license, license_version, creator, creator_url, title, meta_data, tags, watermarked, last_synced_with_source, removed_from_source) \
 		from './sample_data/sample_audio.csv' \
 		with (FORMAT csv, HEADER true);
-	EOF"
-
-# Make one-off modifications to the upstream DB for ingesting audiosets
-docker-compose exec -T "$UPSTREAM_DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d openledger <<-EOF
-	UPDATE audio_view
-		SET audio_set_foreign_identifier = audio_set->>'foreign_identifier';
-
-	DROP VIEW IF EXISTS audioset_view;
-	CREATE VIEW audioset_view
-	AS
-		SELECT DISTINCT
-            (audio_set ->> 'foreign_identifier')  :: varchar(1000) as foreign_identifier,
-            (audio_set ->> 'title')               :: varchar(2000) as title,
-            (audio_set ->> 'foreign_landing_url') :: varchar(1000) as foreign_landing_url,
-            (audio_set ->> 'creator')             :: varchar(2000) as creator,
-            (audio_set ->> 'creator_url')         :: varchar(2000) as creator_url,
-            (audio_set ->> 'url')                 :: varchar(1000) as url,
-            (audio_set ->> 'filesize')            :: integer       as filesize,
-            (audio_set ->> 'filetype')            :: varchar(80)   as filetype,
-            (audio_set ->> 'thumbnail')           :: varchar(1000) as thumbnail,
-			provider
-		FROM audio_view
-		WHERE audio_set IS NOT NULL;
+	REFRESH MATERIALIZED VIEW audio_view;
 	EOF"
 
 # Load search quality assurance data.
