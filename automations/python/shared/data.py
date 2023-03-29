b2d56e7d962d4f36d4de1bb16c5270260c3b1501
Tomvth <50104959+Tomvth@users.noreply.github.com>
2023-03-29 15:57:29 +0200

    Added docstring for encoding parameter

--- automations/python/shared/data.py
+++ automations/python/shared/data.py
@@ -19,6 +19,7 @@ def get_data(file_name: str, encoding: str = None) -> dict:
     Calls to the function with the same file name will be cached for performance.
 
     :param file_name: the name of the YAML file to read
+    :param encoding: the encoding of the YAML file to be read
     :return: the contents of the YAML file as a Python object
     """
 
