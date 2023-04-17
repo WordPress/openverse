8a3cf76feaec3b0b0c4129607880b8bb1eb81aec
Madison Swain-Bowden <bowdenm@spu.edu>
2023-04-17 12:35:42 -0700

    Update set matrix images to match new CI

--- automations/python/workflows/set_matrix_images.py
+++ automations/python/workflows/set_matrix_images.py
@@ -16,6 +16,17 @@ if "frontend" in changes:
         {"image": "frontend", "context": "frontend", "target": "app"}
     )
     publish_matrix["image"].append("frontend")
+if any(item in changes for item in ["api", "ingestion_server", "catalog"]):
+    build_matrix["image"].append("upstream_db")
+    build_matrix["include"].append(
+        {"image": "upstream_db", "context": "docker/upstream_db", "target": "db"}
+    )
+if "catalog" in changes:
+    build_matrix["image"].append("catalog")
+    build_matrix["include"].append(
+        {"image": "catalog", "context": "catalog", "target": "cat"}
+    )
+    publish_matrix["image"].append("catalog")
 if "api" in changes or "ingestion_server" in changes:
     # Always build the ingestion server and API images for either changeset
     build_matrix["image"] += ["api", "ingestion_server"]
@@ -32,12 +43,12 @@ if "api" in changes or "ingestion_server" in changes:
     if "ingestion_server" in changes:
         publish_matrix["image"].append("ingestion_server")
 
-DO_BUILD = "true" if len(build_matrix["image"]) else "false"
-DO_PUBLISH = "true" if len(publish_matrix["image"]) else "false"
+do_build = "true" if len(build_matrix["image"]) else "false"
+do_publish = "true" if len(publish_matrix["image"]) else "false"
 
-with open(os.environ.get("GITHUB_OUTPUT"), "a", encoding="utf-8") as gh_out:
+with open(os.environ.get("GITHUB_OUTPUT"), "a") as gh_out:
     for dest in [sys.stdout, gh_out]:
-        print(f"do_build={DO_BUILD}", file=dest)
+        print(f"do_build={do_build}", file=dest)
         print(f"build_matrix={json.dumps(build_matrix)}", file=dest)
-        print(f"do_publish={DO_PUBLISH}", file=dest)
+        print(f"do_publish={do_publish}", file=dest)
         print(f"publish_matrix={json.dumps(publish_matrix)}", file=dest)
