fdc846a7810d9a01218d93b36fd0ecdbe6ddc48c
Dhruv Bhanushali <dhruv_b@live.com>
2023-04-23 13:33:49 +0400

    Document OAuth2 endpoints like media endpoints

--- api/catalog/api/serializers/oauth2_serializers.py
+++ api/catalog/api/serializers/oauth2_serializers.py
@@ -6,9 +6,50 @@ from catalog.api.models import OAuth2Registration
 
 
 class OAuth2RegistrationSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        help_text=(
+            "A unique human-readable name for your application or project "
+            "requiring access to the Openverse API."
+        ),
+        required=True,
+        min_length=1,
+        max_length=150,
+    )
+    description = serializers.CharField(
+        help_text=(
+            "A description of what you are trying to achieve with your project "
+            "using the API. Please provide as much detail as possible!"
+        ),
+        min_length=1,
+        max_length=10000,
+    )
+    email = serializers.EmailField(
+        help_text=(
+            "A valid email that we can reach you at if we have any questions "
+            "about your use case or data consumption."
+        ),
+        min_length=1,
+        max_length=254,
+    )
+
     class Meta:
         model = OAuth2Registration
         fields = ("name", "description", "email")
+
+
+class OAuth2ApplicationSerializer(serializers.Serializer):
+    client_id = serializers.CharField(
+        help_text="The unique, public identifier of your application.",
+    )
+    client_secret = serializers.CharField(
+        help_text="The secret key used to authenticate your application.",
+    )
+    name = serializers.CharField(
+        help_text="The name of your application or project.",
+    )
+    msg = serializers.CharField(
+        help_text="Some additional information about the application.",
+    )
 
 
 class OAuth2RegistrationSuccessful(serializers.ModelSerializer):
@@ -30,14 +71,13 @@ class OAuth2RegistrationSuccessful(serializers.ModelSerializer):
         fields = ("name", "client_id", "client_secret")
 
 
-class OAuth2KeyInfo(serializers.Serializer):
+class OAuth2KeyInfoSerializer(serializers.Serializer):
     requests_this_minute = serializers.IntegerField(
-        help_text="The number of requests your key has performed in the last "
-        "minute.",
+        help_text="The number of requests your key has performed in the last minute.",
         allow_null=True,
     )
     requests_today = serializers.IntegerField(
-        help_text="The number of requests your key has performed in the last " "day.",
+        help_text="The number of requests your key has performed in the last day.",
         allow_null=True,
     )
     rate_limit_model = serializers.CharField(
@@ -45,4 +85,7 @@ class OAuth2KeyInfo(serializers.Serializer):
         "'standard' or 'enhanced'; enhanced users enjoy higher rate "
         "limits than their standard key counterparts. Contact "
         "Openverse if you need a higher rate limit."
+    )
+    verified = serializers.BooleanField(
+        help_text="Whether the application has verified the submitted email address."
     )
--- api/catalog/api/views/oauth2_views.py
+++ api/catalog/api/views/oauth2_views.py
@@ -1,6 +1,7 @@
 import logging as log
 import secrets
 import smtplib
+from textwrap import dedent
 
 from django.conf import settings
 from django.core.cache import cache
@@ -9,115 +10,41 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
+from drf_spectacular.utils import extend_schema
 from oauth2_provider.generators import generate_client_secret
 
-from catalog.api.docs.media_docs import refer_sample
+from catalog.api.docs.oauth2_docs import key_info, register
 from catalog.api.models import OAuth2Verification, ThrottledApplication
-from catalog.api.serializers.error_serializers import (
-    ForbiddenErrorSerializer,
-    InternalServerErrorSerializer,
-)
 from catalog.api.serializers.oauth2_serializers import (
-    OAuth2KeyInfo,
+    OAuth2KeyInfoSerializer,
     OAuth2RegistrationSerializer,
-    OAuth2RegistrationSuccessful,
 )
 from catalog.api.utils.oauth2_helper import get_token_info
 from catalog.api.utils.throttle import OnePerSecond, TenPerDay
-from catalog.custom_auto_schema import CustomAutoSchema
-from catalog.example_responses import (
-    key_info_200_example,
-    key_info_403_example,
-    key_info_500_example,
-    register_api_oauth2_201_example,
-)
 
 
+@extend_schema(tags=["auth"])
 class Register(APIView):
-    swagger_schema = CustomAutoSchema
-    register_api_oauth2_description = f"""
-register_api_oauth2 is an API endpoint to register access to the API via OAuth2.
-
-Upon registering, you will receive a `client_id` and `client_secret`, which you can then
-use to authenticate using the standard OAuth2 Client Credentials flow. See the Register
-and Authenticate section for instructions on registering access to the API via OAuth2.
-
-> **WARNING:** You must keep `client_secret` confidential, as anybody with your
-> `client_secret` can impersonate your application.
-
-Authenticated users have higher rate limits than anonymous users. Additionally, by
-identifying yourself, you can request Openverse to adjust your personal rate limit
-depending on your organization's needs.
-
-{refer_sample}"""
-
     throttle_classes = (TenPerDay,)
-    register_api_oauth2_response = {
-        "201": openapi.Response(
-            description="OK",
-            examples=register_api_oauth2_201_example,
-            schema=OAuth2RegistrationSuccessful,
-        )
-    }
 
-    register_api_oauth2_bash = """
-# Register for a key
-curl \\
-  -X POST \\
-  -H "Content-Type: application/json" \\
-  -d '{"name": "My amazing project", "description": "To access Openverse API", "email": "user@example.com"}' \\
-  https://api.openverse.engineering/v1/auth_tokens/register/
-"""  # noqa: E501
-
-    register_api_oauth2_request = openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        required=["name", "description", "email"],
-        properties={
-            "name": openapi.Schema(
-                title="Name",
-                type=openapi.TYPE_STRING,
-                min_length=1,
-                max_length=150,
-                description="A unique human-readable name for your application "
-                "or project requiring access to the Openverse API.",
-            ),
-            "description": openapi.Schema(
-                title="Description",
-                type=openapi.TYPE_STRING,
-                min_length=1,
-                max_length=10000,
-                description="A description of what you are trying to achieve "
-                "with your project using the API. Please provide "
-                "as much detail as possible!",
-            ),
-            "email": openapi.Schema(
-                title="Email",
-                type=openapi.TYPE_STRING,
-                min_length=1,
-                max_length=254,
-                format=openapi.FORMAT_EMAIL,
-                description="A valid email that we can reach you at if we "
-                "have any questions about your use case or "
-                "data consumption.",
-            ),
-        },
-        example={
-            "name": "My amazing project",
-            "description": "To access Openverse API",
-            "email": "user@example.com",
-        },
-    )
-
-    @swagger_auto_schema(
-        operation_id="register_api_oauth2",
-        operation_description=register_api_oauth2_description,
-        request_body=register_api_oauth2_request,
-        responses=register_api_oauth2_response,
-        code_examples=[{"lang": "Bash", "source": register_api_oauth2_bash}],
-    )
+    @register
     def post(self, request, format=None):
+        """
+        Register an application to access to API via OAuth2.
+
+        Upon registering, you will receive a `client_id` and `client_secret`,
+        which you can then use to authenticate using the standard OAuth2 Client
+        Credentials flow. See the Register and Authenticate section for
+        instructions on registering access to the API via OAuth2.
+
+        > ⚠️ **WARNING:** You must keep `client_secret` confidential, as anybody
+        > with your `client_secret` can impersonate your application.
+
+        Authenticated users have higher rate limits than anonymous users.
+        Additionally, by identifying yourself, you can request Openverse to
+        adjust your personal rate limit depending on your organization's needs.
+        """
+
         # Store the registration information the developer gave us.
         serialized = OAuth2RegistrationSerializer(data=request.data)
         if not serialized.is_valid():
@@ -146,13 +73,15 @@ curl \\
         verification.save()
         token = verification.code
         link = request.build_absolute_uri(reverse("verify-email", [token]))
-        verification_msg = f"""
-To verify your Openverse API credentials, click on the following link:
+        verification_msg = dedent(
+            f"""
+            To verify your Openverse API credentials, click on the following link:
 
-{link}
+            {link}
 
-If you believe you received this message in error, please disregard it.
+            If you believe you received this message in error, please disregard it.
         """
+        )
         try:
             send_mail(
                 subject="Verify your API credentials",
@@ -204,49 +133,23 @@ class VerifyEmail(APIView):
             )
 
 
+@extend_schema(tags=["auth"])
 class CheckRates(APIView):
-    swagger_schema = CustomAutoSchema
-    key_info_description = f"""
-key_info is an API endpoint to get information about your API key.
-
-You can use this endpoint to get information about your API key such as
-`requests_this_minute`, `requests_today`, and `rate_limit_model`.
-
-> **NOTE:** If you get a 403 Forbidden response, it means your access token has expired.
-
-{refer_sample}"""
-
     throttle_classes = (OnePerSecond,)
 
-    key_info_response = {
-        "200": openapi.Response(
-            description="OK", examples=key_info_200_example, schema=OAuth2KeyInfo
-        ),
-        "403": openapi.Response(
-            description="Forbidden",
-            examples=key_info_403_example,
-            schema=ForbiddenErrorSerializer,
-        ),
-        "500": openapi.Response(
-            description="Internal Server Error",
-            examples=key_info_500_example,
-            schema=InternalServerErrorSerializer,
-        ),
-    }
-
-    key_info_bash = """
-curl \\
-  -H "Authorization: Bearer DLBYIcfnKfolaXKcmMC8RIDCavc2hW" \\
-  http://api.openverse.engineering/v1/rate_limit/
-"""
-
-    @swagger_auto_schema(
-        operation_id="key_info",
-        operation_description=key_info_description,
-        responses=key_info_response,
-        code_examples=[{"lang": "Bash", "source": key_info_bash}],
-    )
+    @key_info
     def get(self, request, format=None):
+        """
+        Get information about your API key.
+
+        You can use this endpoint to get information about your API key such as
+        `requests_this_minute`, `requests_today`, and `rate_limit_model`.
+
+        > ℹ️ **NOTE:** If you get a 403 Forbidden response, it means your access
+        > token has expired.
+        """
+
+        # TODO: Replace 403 responses with DRF `authentication_classes`.
         if not request.auth:
             return Response(status=403, data="Forbidden")
 
@@ -278,6 +181,7 @@ curl \\
                 scope="exempt_oauth2_client_credentials_burst", client_id=client_id
             )
         else:
+            # TODO: Replace 500 response with exception.
             return Response(status=500, data="Unknown API key rate limit type")
 
         sustained_requests_list = cache.get(sustained_throttle_key)
@@ -287,10 +191,12 @@ curl \\
         burst_requests_list = cache.get(burst_throttle_key)
         burst_requests = len(burst_requests_list) if burst_requests_list else None
 
-        response_data = {
-            "requests_this_minute": burst_requests,
-            "requests_today": sustained_requests,
-            "rate_limit_model": throttle_type,
-            "verified": verified,
-        }
-        return Response(status=200, data=response_data)
+        response_data = OAuth2KeyInfoSerializer(
+            {
+                "requests_this_minute": burst_requests,
+                "requests_today": sustained_requests,
+                "rate_limit_model": throttle_type,
+                "verified": verified,
+            }
+        )
+        return Response(status=200, data=response_data.data)
