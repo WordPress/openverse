41379624245063322581c8474606a5a310f16b1e
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2023-04-24 12:07:46 +1000

    Fix f-string

--- automations/python/workflows/write_changelog.py
+++ automations/python/workflows/write_changelog.py
@@ -8,7 +8,7 @@ release_body = os.environ["RELEASE_BODY"]
 
 file_path = Path(__file__).parents[3] / f"documentation/changelogs/{app}/{tag_date}.md"
 file_path.write_text(
-    """
+    f"""
     # {tag_date}
 
     {release_body}
