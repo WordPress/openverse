cfc9e98ecfdbf1f369343441309925a6692c71dc
Openverse (Bot) <101814513+openverse-bot@users.noreply.github.com>
2023-04-27 00:59:02 -0400

    Publish changelog for catalog-2023.04.27.02.43.13 (#1914)
    * Publish changelog for catalog-2023.04.27.02.43.13
    
    * Fix changelog format
    
    * Modify changelog generation script to prevent any indentation (hopefully)
    
    ---------
    
    Co-authored-by: stacimc <stacimc@users.noreply.github.com>
    Co-authored-by: sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>

--- automations/python/workflows/write_changelog.py
+++ automations/python/workflows/write_changelog.py
@@ -7,13 +7,11 @@ app = os.environ["APP"]
 tag_date = os.environ["DATE"]
 release_body = os.environ["RELEASE_BODY"]
 
-file_path = Path(__file__).parents[3] / f"documentation/changelogs/{app}/{tag_date}.md"
-file_path.write_text(
-    dedent(
-        f"""
-        # {tag_date}
+changelog = f"""
+# {tag_date}
 
-        {release_body}
-        """
-    )
-)
+{release_body}
+"""
+
+file_path = Path(__file__).parents[3] / f"documentation/changelogs/{app}/{tag_date}.md"
+file_path.write_text(dedent(changelog))
