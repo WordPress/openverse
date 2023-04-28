13b57d51cff352404ae96a00ed5e8ce1e9241c01
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2023-04-28 13:48:57 +1000

    Fall back to aria-label if title not present

--- frontend/test/playwright/e2e/all-results-keyboard.spec.ts
+++ frontend/test/playwright/e2e/all-results-keyboard.spec.ts
@@ -7,12 +7,12 @@ import { keycodes } from "~/constants/key-codes"
 const walkToNextOfType = async (type: "image" | "audio", page: Page) => {
   const isActiveElementOfType = () => {
     return page.evaluate(
-      ([contextType]) =>
-        new RegExp(`^/${contextType}: \\w+?/`, "i").test(
-          // Use the link title to ensure we are only using the context
-          // that a real user would have (e.g., via a screen-reader)
-          (document.activeElement as HTMLAnchorElement | null)?.title ?? ""
-        ),
+      ([contextType]) => {
+        const regex = new RegExp(`^${contextType}:`, "i")
+        const element = document.activeElement as HTMLAnchorElement | null
+        const toTest = element?.title || element?.ariaLabel || ""
+        return regex.test(toTest)
+      },
       [type]
     )
   }
@@ -45,7 +45,7 @@ test.describe.configure({ mode: "parallel" })
 
 test.describe("all results grid keyboard accessibility test", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("/search?q=birds")
+    await page.goto("/search/?q=birds")
   })
 
   test("should open image results as links", async ({ page }) => {
@@ -59,7 +59,7 @@ test.describe("all results grid keyboard accessibility test", () => {
     )
   })
 
-  test("should open audio results as links", async ({ page }) => {
+  test.only("should open audio results as links", async ({ page }) => {
     await walkToType("audio", page)
     await page.keyboard.press("Enter")
     await page.waitForURL(
