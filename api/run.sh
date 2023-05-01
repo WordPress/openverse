2dafb6c477771ee629e233c8c6a5f0ba8dc49239
Michael <msolorio.github@gmail.com>
2023-05-01 09:37:46 -0700

    Add sh fmt pre commit hook (#1128)
    * Add sh fmt pre commit hook
    
    * Lint shell scripts
    
    ---------
    
    Co-authored-by: Dhruv Bhanushali <dhruv_b@live.com>

--- api/run.sh
+++ api/run.sh
@@ -4,7 +4,7 @@ set -e
 
 es_url="http://${ELASTICSEARCH_URL:-es}:${ELASTICSEARCH_PORT:-9200}/_cluster/health"
 while [ "$(curl -s -o /dev/null -w '%{http_code}' "$es_url")" != "200" ]; do
-  echo "Waiting for Elasticsearch connection..." && sleep 5;
+  echo "Waiting for Elasticsearch connection..." && sleep 5
 done
 echo "Elasticsearch connection established!"
 
