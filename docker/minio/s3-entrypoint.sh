ab7f5e2b16938e1f0d937b5a01d182a6b443c58c
Arun Chib <arunchib25@gmail.com>
2023-05-04 11:07:02 +0530

    Storybook configuration is split between Nuxt config and Storybook directory #475

--- docker/minio/s3-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for b in $${BUCKETS_TO_CREATE//,/ }; do
-    echo "Making bucket $$b" && mkdir -p /data/$$b;
-    done &&
-    exec $$0 "$$@"
--- frontend/test/storybook/playwright.config.ts
+++ frontend/test/storybook/playwright.config.ts
@@ -16,6 +16,37 @@ const config: PlaywrightTestConfig = {
     url: "http://localhost:54000/iframe.html?id=introduction-openverse-ui--page",
     reuseExistingServer: !process.env.CI || process.env.PWDEBUG === "1",
   },
+  storybook: {
+    port: 6006, // standard port for Storybook
+    stories: ["~/**/*.stories.@(mdx|js)"],
+    addons: [
+      {
+        name: "@storybook/addon-essentials",
+        options: {
+          backgrounds: true,
+          viewport: true,
+          toolbars: true,
+        },
+      },
+    ],
+    parameters: {
+      backgrounds: {
+        default: "White",
+        values: [
+          { name: "White", value: "#ffffff" },
+          { name: "Dark charcoal", value: "#30272e" },
+        ],
+      },
+      options: {
+        storySort: {
+          order: ["Introduction", ["Openverse UI"], "Meta"],
+        },
+      },
+      viewport: {
+        viewports: VIEWPORTS,
+      },
+    },
+  },
   use: {
     baseURL: "http://localhost:54000",
     trace: "retain-on-failure",
