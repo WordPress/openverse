861a0cb0a4fe350bf4fdc092d7131b907e94d873
Dhruv Bhanushali <dhruv_b@live.com>
2023-05-05 18:38:46 +0400

    Lint entrypoints

--- docker/minio/s3_entrypoint.sh
+++ docker/minio/s3_entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -euxo pipefail
+
 for b in ${BUCKETS_TO_CREATE//,/ }; do
-    echo "Making bucket $b" && mkdir -p /data/$b;
-    done &&
-    exec $0 "$@"
+  echo "Making bucket $b" && mkdir -p /data/"$b"
+done &&
+  exec $0 "$@"
