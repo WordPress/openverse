b8502fd774754b072a56652a69243002d75f3e27
Madison Swain-Bowden <bowdenm@spu.edu>
2023-05-12 12:18:55 -0700

    Add check staging utility function

--- catalog/dags/database/staging_database_restore/utils.py
+++ catalog/dags/database/staging_database_restore/utils.py
@@ -2,7 +2,7 @@ import functools
 
 from airflow.providers.amazon.aws.hooks.rds import RdsHook
 
-from database.staging_database_restore.constants import AWS_RDS_CONN_ID
+from database.staging_database_restore import constants
 
 
 def setup_rds_hook(func: callable) -> callable:
@@ -13,7 +13,29 @@ def setup_rds_hook(func: callable) -> callable:
 
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
-        rds_hook = RdsHook(aws_conn_id=AWS_RDS_CONN_ID)
+        rds_hook = RdsHook(aws_conn_id=constants.AWS_RDS_CONN_ID)
         return func(*args, **kwargs, rds_hook=rds_hook)
+
+    return wrapped
+
+
+def ensure_staging(func: callable) -> callable:
+    """
+    Ensure that the staging database is the one being used for a target function.
+    This requires that the target function has a db_instance parameter in its keyword
+    arguments.
+    """
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        if "db_identifier" not in kwargs:
+            raise ValueError("The target function must have a db_identifier parameter")
+        db_identifier = kwargs["db_identifier"]
+        if db_identifier != constants.STAGING_IDENTIFIER:
+            raise ValueError(
+                f"The target function must be called with the staging database "
+                f"identifier ({constants.STAGING_IDENTIFIER}), not {db_identifier}"
+            )
+        return func(*args, **kwargs)
 
     return wrapped
