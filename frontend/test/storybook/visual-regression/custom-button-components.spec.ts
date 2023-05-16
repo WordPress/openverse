932e34960c9b8e82f2c27b0cbefce68f18277833
Olga Bulat <obulat@gmail.com>
2023-05-16 14:07:23 +0300

    Add storybook visual regression tests

---
+++ frontend/test/storybook/visual-regression/custom-button-components.spec.ts
@@ -0,0 +1,39 @@
+import { test } from "@playwright/test"
+
+import { makeGotoWithArgs } from "~~/test/storybook/utils/args"
+import breakpoints from "~~/test/playwright/utils/breakpoints"
+
+const wrapperLocator = "#wrapper"
+test.describe("VLoadMore button", () => {
+  const gotoWithArgs = makeGotoWithArgs(
+    "components-custombuttoncomponents--v-load-more"
+  )
+  breakpoints.describeMobileAndDesktop(({ expectSnapshot }) => {
+    test("resting", async ({ page }) => {
+      await gotoWithArgs(page)
+      await expectSnapshot("v-load-more-resting", page.locator(wrapperLocator))
+    })
+
+    test("hovered", async ({ page }) => {
+      await gotoWithArgs(page)
+      await page.getByRole("button").hover()
+      await expectSnapshot("v-load-more-hovered", page.locator(wrapperLocator))
+    })
+
+    test("focused", async ({ page }) => {
+      await gotoWithArgs(page)
+      await page.getByRole("button").focus()
+      await expectSnapshot("v-load-more-focused", page.locator(wrapperLocator))
+    })
+
+    test("focused hovered", async ({ page }) => {
+      await gotoWithArgs(page)
+      await page.getByRole("button").focus()
+      await page.getByRole("button").hover()
+      await expectSnapshot(
+        "v-load-more-focused-hovered",
+        page.locator(wrapperLocator)
+      )
+    })
+  })
+})
