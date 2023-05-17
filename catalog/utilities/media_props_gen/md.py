049b2c6adf58addaa3fe8b0041d2880ad2ecec67
Olga Bulat <obulat@gmail.com>
2023-05-17 17:11:48 +0300

    Add a script to generate the media_properties.md
    Signed-off-by: Olga Bulat <obulat@gmail.com>

---
+++ catalog/utilities/media_props_gen/md.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+
+class Md:
+    @staticmethod
+    def heading(level: int, text: str) -> str:
+        """Add a heading to a markdown string."""
+        return f"{'#' * level} {text}\n"
+
+    @staticmethod
+    def line(text: str) -> str:
+        """Add a line to a markdown string."""
+        return f"{text}\n"
+
+    @staticmethod
+    def parse(file_name: Path) -> dict[str, dict[str, str]]:
+        """
+        Parse the markdown documentation file and return a dictionary with the
+        field name as key and the description as value.
+        """
+        with open(file_name) as f:
+            contents = [line for line in f.readlines() if line.strip()]
+        current_field = ""
+        properties = {}
+        prop = ""
+        value = {}
+        for i, line in enumerate(contents):
+            if line.startswith("# "):
+                if current_field and value:
+                    properties[current_field] = value
+                current_field = line.replace("# ", "").strip()
+                value = {}
+                continue
+            elif line.startswith("## "):
+                prop = line.replace("## ", "").strip()
+                value[prop] = ""
+                continue
+            else:
+                value[prop] += line
+
+        return properties
