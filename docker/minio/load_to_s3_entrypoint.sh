1d1696078a55e0ea40d0b799301e0883eef403c2
Arun Chib <arunchib25@gmail.com>
2023-05-20 14:58:41 +0530

    1. Fixing Containers Failed to run due to entrypoint files not found

--- docker/minio/load_to_s3_entrypoint.sh
+++ docker/minio/load_to_s3_entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This does *not* allow for testing permissions issues that may come up in real AWS.
+# And, if you remove files from /tests/s3-data, you will need to use `just down -v`
+# and `just up` or `just recreate` to see the minio bucket without those files.
 set -euxo pipefail
 
 /usr/bin/mc config host add s3 http://s3:5000 "${AWS_ACCESS_KEY}" "${AWS_SECRET_KEY}"
--- docker/minio/s3_entrypoint.sh
+++ docker/minio/s3_entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Create empty buckets on every container startup
+# Note: $0 is included in the exec because "/bin/bash -c" swallows the first
+# argument, so it must be re-added at the beginning of the exec call
 set -euxo pipefail
 
 for b in ${BUCKETS_TO_CREATE//,/ }; do
