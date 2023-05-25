07cae14d8f214380a5629e5cd0c5e97c1cf383dc
Zack Krida <zackkrida@pm.me>
2023-05-25 17:18:53 -0400

    Fix url path matching in "should allow toggling audio playback via play/pause click" test

--- frontend/test/playwright/e2e/all-results-keyboard.spec.ts
+++ frontend/test/playwright/e2e/all-results-keyboard.spec.ts
@@ -83,8 +83,13 @@ test.describe("all results grid keyboard accessibility test", () => {
     const focusedResult = await locateFocusedResult(page)
     const playButton = await audio.getInactive(focusedResult)
     await playButton.click()
+
+    // Get the path for comparison purposes
+    const url = new URL(page.url())
+    const path = url.pathname + url.search
+
     // should not navigate
-    expect(page.url()).toMatch(/\/search\?q=birds$/)
+    expect(path).toMatch(/\/search\/?\?q=birds$/)
 
     const pauseButton = await audio.getActive(focusedResult)
     pauseButton.click()
