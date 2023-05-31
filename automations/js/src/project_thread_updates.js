c9ae247df66c23abc86679234f67cb02deec6cb6
Zack Krida <zackkrida@pm.me>
2023-05-31 15:30:09 -0400

    Use corret statuses and GraphQL

--- automations/js/src/project_thread_updates.js
+++ automations/js/src/project_thread_updates.js
@@ -1,29 +1,73 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
 
+const githubMeta = {
+  projectBoardID: 70,
+  projectStatusColumnName: 'status',
+  repoOwner: 'wordpress',
+  repo: 'openverse',
+}
+
+const allowedStatuses = ['In Kickoff', 'In RFC', 'In Progress', 'Shipped']
+
+const GET_PROJECT_CARDS = `
+  query {
+    repository(name:"${githubMeta.repo}", owner:"${githubMeta.repoOwner}") {
+      projectV2(number: ${githubMeta.projectBoardID}) {
+        items(last: 100) {
+          nodes {
+            id
+            fieldValueByName(name: "${githubMeta.projectStatusColumnName}") {
+              ...on ProjectV2ItemFieldSingleSelectValue {
+                name
+              }
+            }
+            content {
+              __typename
+              ...on Issue {
+                id
+                url
+                assignees(first:10) {
+                  nodes {
+                    login
+                  }
+                }
+                author {
+                  login
+                }
+                comments(last:100) {
+                  nodes {
+                    createdAt
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 async function run() {
   try {
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
-    const context = github.context
     const isDryRun = core.getBooleanInput('dry_run') ?? false
 
-    // Fetch all issues with the label "🧭 project: thread" and not labeled with "blocked"
-    const { data: issues } = await octokit.rest.issues.listForRepo({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      labels: '🧭 project: thread,-blocked',
-      state: 'open',
-    })
+    // Fetch project cards with their associated issue data
+    const { data: result } = await octokit.graphql(GET_PROJECT_CARDS)
 
     const currentDate = new Date()
 
-    for (const issue of issues) {
-      const { data: comments } = await octokit.rest.issues.listComments({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: issue.number,
-      })
+    for (const node of result.repository.projectV2.items.nodes) {
+      const issue = node.content
+      if (issue.__typename !== 'Issue') continue
 
+      // Check the status of the card
+      const status = node.fieldValueByName.name
+      if (!allowedStatuses.includes(status)) continue
+
+      const comments = issue.comments.nodes
       const fourteenDaysAgo = new Date(
         currentDate.setDate(currentDate.getDate() - 14)
       )
@@ -31,26 +75,29 @@ async function run() {
       if (
         // Check if the issue has been commented on in the last 14 days
         !comments.some(
-          (comment) => new Date(comment.created_at) > fourteenDaysAgo
+          (comment) => new Date(comment.createdAt) > fourteenDaysAgo
         )
       ) {
-        // If not, leave a comment on the issue
-        const recipient = issue.assignee
-          ? issue.assignee.login
-          : issue.user.login
+        // If not, leave a reminder comment on the issue
+        const recipient = issue.assignees.nodes[0]
+          ? issue.assignees.nodes[0].login
+          : issue.author.login
         const body = `Hi @${recipient}, this project has not received an update comment in 14 days. Please leave an update comment as soon as you can. See the [documentation on project updates](https://docs.openverse.org/projects/planning.html#providing-project-updates) for more information.`
 
         if (isDryRun) {
-          console.info(
-            `Would have commented on issue #${issue.number}: ${body}`
-          )
+          console.info(`Would have commented on issue ${issue.url}: ${body}`)
         } else {
-          await octokit.rest.issues.createComment({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            issue_number: issue.number,
-            body,
-          })
+          // Extract the owner, repo, and issue number from the issue URL
+          // const [, , , owner, repo, , issue_number] = issue.url.split('/')
+
+          console.info('Not a dry run!')
+          console.info(`Would have commented on issue ${issue.url}: ${body}`)
+          // await octokit.rest.issues.createComment({
+          //   owner,
+          //   repo,
+          //   issue_number,
+          //   body,
+          // })
         }
       }
     }
