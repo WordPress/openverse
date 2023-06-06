5a3a6c29a0eb2b19085a965bfaaf6329e4e60e57
Olga Bulat <obulat@gmail.com>
2023-06-06 10:33:29 +0300

    Fix the query

--- frontend/src/middleware/search.ts
+++ frontend/src/middleware/search.ts
@@ -4,15 +4,21 @@ import type { Middleware } from "@nuxt/types"
 
 export const searchMiddleware: Middleware = ({ redirect, route, $pinia }) => {
   const {
-    query: { q, searchBy },
+    query: { q },
   } = route
   /**
-   * This anonymous middleware redirects any search without a query to the homepage.
-   * This is meant to block direct access to /search and all sub-routes, with
-   * an exception for the 'creator' filter. The creator filter doesn't send
-   * the search query to the API.
+   * This middleware redirects any search without a query to the homepage.
+   * This is meant to block direct access to /search and all sub-routes.
+   *
+   * The `q` parameter is required for searches on the frontend.
+   * The API also allows for calls without the `q` parameter when searching within
+   * `creator` field, but the frontend query still uses `q` for it.
+   * The `?q=cat&searchBy=creator` frontend query is converted to
+   * `?creator=cat&searchBy=creator` API request query in
+   * `prepare-search-query-params`.
+   * Note that the search by creator is not displayed in the UI.
    */
-  if (!q && !searchBy) return redirect("/")
+  if (!q) return redirect("/")
   /**
    * We need to make sure that query `q` exists before checking if it matches
    * the store searchTerm.
