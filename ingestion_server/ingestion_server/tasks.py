797261b08b0ab76c11380b12785a85a69de9aa64
Madison Swain-Bowden <bowdenm@spu.edu>
2023-06-09 02:06:44 -0700

    Remove noop QA/test data loading steps on ingestion server (#2353)

--- ingestion_server/ingestion_server/tasks.py
+++ ingestion_server/ingestion_server/tasks.py
@@ -50,10 +50,6 @@ class TaskTypes(Enum):
     DELETE_INDEX = auto()
     """delete the given index after it has been superseded by a new one"""
 
-    LOAD_TEST_DATA = auto()
-    """create indices in ES for QA tests; this is not intended to run in production but
-    can be run without negative consequences"""
-
     CREATE_AND_POPULATE_FILTERED_INDEX = auto()
     """create a filtered index based on existing alias index"""
 
