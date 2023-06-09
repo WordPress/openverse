bced52c3f469d6f6b889182dbb453b25bba930c2
Olga Bulat <obulat@gmail.com>
2023-06-09 18:51:59 +0300

    Add SELECT_SEARCH_RESULT event

--- frontend/src/components/VAudioDetails/VRelatedAudio.vue
+++ frontend/src/components/VAudioDetails/VRelatedAudio.vue
@@ -10,7 +10,12 @@
       class="-mx-2 mb-12 flex flex-col gap-4 md:-mx-4"
     >
       <li v-for="audio in media" :key="audio.id">
-        <VAudioTrack :audio="audio" layout="row" :size="audioTrackSize" />
+        <VAudioTrack
+          :audio="audio"
+          layout="row"
+          :size="audioTrackSize"
+          @mousedown="sendSelectSearchResultEvent(audio)"
+        />
       </li>
     </ol>
     <LoadingIcon
@@ -27,6 +32,11 @@
 import { computed, defineComponent, PropType } from "vue"
 
 import { useUiStore } from "~/stores/ui"
+
+import { useSearchStore } from "~/stores/search"
+import { useRelatedMediaStore } from "~/stores/media/related-media"
+import { useAnalytics } from "~/composables/use-analytics"
+import { AUDIO } from "~/constants/media"
 
 import type { FetchState } from "~/models/fetch-state"
 import type { AudioDetail } from "~/models/media"
@@ -49,12 +59,24 @@ export default defineComponent({
   },
   setup() {
     const uiStore = useUiStore()
+    const relatedMediaStore = useRelatedMediaStore()
 
     const audioTrackSize = computed(() => {
       return uiStore.isBreakpoint("md") ? "l" : "s"
     })
 
-    return { audioTrackSize }
+    const { sendCustomEvent } = useAnalytics()
+    const sendSelectSearchResultEvent = (audio: AudioDetail) => {
+      sendCustomEvent("SELECT_SEARCH_RESULT", {
+        id: audio.id,
+        relatedTo: relatedMediaStore.mainMediaId,
+        mediaType: AUDIO,
+        provider: audio.provider,
+        query: useSearchStore().searchTerm,
+      })
+    }
+
+    return { audioTrackSize, sendSelectSearchResultEvent }
   },
 })
 </script>
