2bdd645cac3aa94b6f651c7fa06645a510fcb6a0
Arun Chib <40028485+arun-chib@users.noreply.github.com>
2023-06-09 10:18:37 +0530

    Added Comments.

--- docker/minio/s3_entrypoint.sh
+++ docker/minio/s3_entrypoint.sh
@@ -1,3 +1,6 @@
+# Create empty buckets on every container startup
+# Note: $0 is included in the exec because "/bin/bash -c" swallows the first
+# argument, so it must be re-added at the beginning of the exec call
 #!/bin/bash
 set -euxo pipefail
 for b in ${BUCKETS_TO_CREATE//,/ }; do
