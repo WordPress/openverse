14ed651a99c0e1976609de51dd65fda4ad7fe528
Zack Krida <zackkrida@pm.me>
2023-06-15 09:46:44 -0400

    Introduce a random API change to see if the images build in CI

--- api/api/apps.py
+++ api/api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
-    name = "api"
+    name = "api-OBVIOUSLY_DONT_MERGE_THIS_PR"
     default_auto_field = "django.db.models.AutoField"
