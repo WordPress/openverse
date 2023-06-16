03f32a46ad848f69acd3c8d86c0555ccb1a60e04
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2023-06-16 12:05:45 +1000

    Prevent unnecessary post process results recursion

--- api/test/unit/controllers/test_search_controller.py
+++ api/test/unit/controllers/test_search_controller.py
@@ -1,9 +1,11 @@
 import random
+import re
 from collections.abc import Callable
 from enum import Enum, auto
 from unittest import mock
 from uuid import uuid4
 
+import pook
 import pytest
 from django_redis import get_redis_connection
 from elasticsearch_dsl import Search
@@ -539,3 +541,379 @@ def test_resolves_index(
     )
 
     search_class.assert_called_once_with(index=searched_index)
+
+
+@mock.patch(
+    "api.controllers.search_controller._post_process_results",
+    wraps=search_controller._post_process_results,
+)
+@pook.on
+def test_no_post_process_results_recursion(
+    wrapped_post_process_results, image_media_type_config, settings
+):
+    mock_result_url = "https://example.com/openverse-image-result-url"
+    mock_es_response = {
+        "took": 3,
+        "timed_out": False,
+        "_shards": {"total": 18, "successful": 18, "skipped": 0, "failed": 0},
+        "hits": {
+            "total": {"value": 45, "relation": "eq"},
+            "max_score": 11.0007305,
+            "hits": [
+                {
+                    "_index": "image-init",
+                    "_type": "_doc",
+                    "_id": "3345",
+                    "_score": 7.607353,
+                    "_source": {
+                        "thumbnail": None,
+                        "aspect_ratio": "wide",
+                        "extension": "jpg",
+                        "size": "large",
+                        "authority_boost": 85,
+                        "max_boost": 85,
+                        "min_boost": 1,
+                        "id": i + 1,
+                        "identifier": str(uuid4()),
+                        "title": "Bird Nature Photo",
+                        "foreign_landing_url": "https://example.com/photo/LYTN21EBYO",
+                        "creator": "Nature's Beauty",
+                        "creator_url": "https://example.com/author/121424",
+                        "url": mock_result_url,
+                        "license": "cc0",
+                        "license_version": "1.0",
+                        "license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
+                        "provider": "example",
+                        "source": "example",
+                        "category": "photograph",
+                        "created_on": "2022-02-26T08:48:33+00:00",
+                        "tags": [{"name": "bird"}],
+                        "mature": False,
+                    },
+                }
+                for i in range(5)
+            ],
+        },
+    }
+
+    es_host = settings.ES.transport.kwargs["host"]
+    es_port = settings.ES.transport.kwargs["port"]
+
+    # `origin_index` enforced by passing `exact_index=True` below.
+    es_endpoint = (
+        f"http://{es_host}:{es_port}/{image_media_type_config.origin_index}/_search"
+    )
+
+    mock_search = pook.post(es_endpoint, reply=200, response_json=mock_es_response)
+
+    # Ensure dead link filtering does not remove any results
+    pook.head(
+        mock_result_url,
+        reply=200,
+    ).times(len(mock_es_response["hits"]["hits"]))
+
+    serializer = image_media_type_config.search_request_serializer(
+        # This query string does not matter, ultimately, as pook is mocking
+        # the ES response regardless of the input
+        data={"q": "bird perched"}
+    )
+    serializer.is_valid()
+    results, _, _, _ = search_controller.search(
+        search_params=serializer,
+        ip=0,
+        origin_index=image_media_type_config.origin_index,
+        exact_index=True,
+        page=3,
+        page_size=20,
+        filter_dead=True,
+    )
+
+    assert {r["_source"]["identifier"] for r in mock_es_response["hits"]["hits"]} == {
+        r.identifier for r in results
+    }
+
+    assert wrapped_post_process_results.call_count == 1
+    assert mock_search.total_matches == 1
+
+
+@pytest.mark.parametrize(
+    # both scenarios force `post_process_results`
+    # to recurse to fill the page due to the dead link
+    # configuration present in the test body
+    "page, page_size, mock_total_hits",
+    # Note the following
+    # - DEAD_LINK_RATIO causes all query sizes to start at double the page size
+    # - The test function is configured so that each request only returns 2 live
+    #   results
+    # - We clear the redis cache between each test, meaning there is no query-based
+    #   dead link mask. This forces `from` to 0 for each case.
+    # - Recursion should only continue while results still exist that could fulfill
+    #   the requested page size. Once the available hits are exhaused, the function
+    #   should stop recursing. This is why exceeding  or matching the available
+    #   hits is significant.
+    (
+        # First request: from: 0, size: 10
+        # Second request: from: 0, size: 15, exceeds max results
+        pytest.param(1, 5, 12, id="first_page"),
+        # First request: from: 0, size: 22
+        # Second request: from 0, size: 33, exceeds max results
+        pytest.param(3, 4, 32, id="last_page"),
+        # First request: from: 0, size: 22
+        # Second request: from 0, size: 33, matches max results
+        pytest.param(3, 4, 33, id="last_page_with_exact_max_results"),
+    ),
+)
+@mock.patch(
+    "api.controllers.search_controller._post_process_results",
+    wraps=search_controller._post_process_results,
+)
+@pook.on
+def test_post_process_results_recurses_as_needed(
+    wrapped_post_process_results,
+    image_media_type_config,
+    settings,
+    page,
+    page_size,
+    mock_total_hits,
+    # request the redis mock to auto-clean Redis between each test run
+    # otherwise the dead link query mask causes test details to leak
+    # between each run
+    redis,
+):
+    mock_live_result_url = "https://example.com/openverse-image-result-url"
+    mock_dead_result_url = f"{mock_live_result_url}-dead"
+
+    mock_es_response_1 = {
+        "took": 3,
+        "timed_out": False,
+        "_shards": {"total": 18, "successful": 18, "skipped": 0, "failed": 0},
+        "hits": {
+            "total": {"value": mock_total_hits, "relation": "eq"},
+            "max_score": 11.0007305,
+            "hits": [
+                {
+                    "_index": "image-init",
+                    "_type": "_doc",
+                    "_id": "3345",
+                    "_score": 7.607353,
+                    "_source": {
+                        "thumbnail": None,
+                        "aspect_ratio": "wide",
+                        "extension": "jpg",
+                        "size": "large",
+                        "authority_boost": 85,
+                        "max_boost": 85,
+                        "min_boost": 1,
+                        "id": i + 1,
+                        "identifier": str(uuid4()),
+                        "title": "Bird Nature Photo",
+                        "foreign_landing_url": "https://example.com/photo/LYTN21EBYO",
+                        "creator": "Nature's Beauty",
+                        "creator_url": "https://example.com/author/121424",
+                        "url": mock_live_result_url,
+                        "license": "cc0",
+                        "license_version": "1.0",
+                        "license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
+                        "provider": "example",
+                        "source": "example",
+                        "category": "photograph",
+                        "created_on": "2022-02-26T08:48:33+00:00",
+                        "tags": [{"name": "bird"}],
+                        "mature": False,
+                    },
+                }
+                for i in range(2)
+            ]
+            + [
+                {
+                    "_index": "image-init",
+                    "_type": "_doc",
+                    "_id": "3345",
+                    "_score": 7.607353,
+                    "_source": {
+                        "thumbnail": None,
+                        "aspect_ratio": "wide",
+                        "extension": "jpg",
+                        "size": "large",
+                        "authority_boost": 85,
+                        "max_boost": 85,
+                        "min_boost": 1,
+                        "id": i + 1,
+                        "identifier": str(uuid4()),
+                        "title": "Bird Nature Photo",
+                        "foreign_landing_url": "https://example.com/photo/LYTN21EBYO",
+                        "creator": "Nature's Beauty",
+                        "creator_url": "https://example.com/author/121424",
+                        "url": mock_dead_result_url,
+                        "license": "cc0",
+                        "license_version": "1.0",
+                        "license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
+                        "provider": "example",
+                        "source": "example",
+                        "category": "photograph",
+                        "created_on": "2022-02-26T08:48:33+00:00",
+                        "tags": [{"name": "bird"}],
+                        "mature": False,
+                    },
+                }
+                for i in range(8)
+            ],
+        },
+    }
+
+    mock_es_response_2 = {
+        "took": 3,
+        "timed_out": False,
+        "_shards": {"total": 18, "successful": 18, "skipped": 0, "failed": 0},
+        "hits": {
+            "total": {"value": mock_total_hits, "relation": "eq"},
+            "max_score": 11.0007305,
+            "hits": mock_es_response_1["hits"]["hits"]
+            + [
+                {
+                    "_index": "image-init",
+                    "_type": "_doc",
+                    "_id": "3345",
+                    "_score": 7.607353,
+                    "_source": {
+                        "thumbnail": None,
+                        "aspect_ratio": "wide",
+                        "extension": "jpg",
+                        "size": "large",
+                        "authority_boost": 85,
+                        "max_boost": 85,
+                        "min_boost": 1,
+                        "id": i + 1,
+                        "identifier": str(uuid4()),
+                        "title": "Bird Nature Photo",
+                        "foreign_landing_url": "https://example.com/photo/LYTN21EBYO",
+                        "creator": "Nature's Beauty",
+                        "creator_url": "https://example.com/author/121424",
+                        "url": mock_live_result_url,
+                        "license": "cc0",
+                        "license_version": "1.0",
+                        "license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
+                        "provider": "example",
+                        "source": "example",
+                        "category": "photograph",
+                        "created_on": "2022-02-26T08:48:33+00:00",
+                        "tags": [{"name": "bird"}],
+                        "mature": False,
+                    },
+                }
+                # Two more live results, then fill the rest of the page with dead ones
+                for i in range(2)
+            ]
+            + [
+                {
+                    "_index": "image-init",
+                    "_type": "_doc",
+                    "_id": "3345",
+                    "_score": 7.607353,
+                    "_source": {
+                        "thumbnail": None,
+                        "aspect_ratio": "wide",
+                        "extension": "jpg",
+                        "size": "large",
+                        "authority_boost": 85,
+                        "max_boost": 85,
+                        "min_boost": 1,
+                        "id": i + 1,
+                        "identifier": str(uuid4()),
+                        "title": "Bird Nature Photo",
+                        "foreign_landing_url": "https://example.com/photo/LYTN21EBYO",
+                        "creator": "Nature's Beauty",
+                        "creator_url": "https://example.com/author/121424",
+                        "url": mock_dead_result_url,
+                        "license": "cc0",
+                        "license_version": "1.0",
+                        "license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
+                        "provider": "example",
+                        "source": "example",
+                        "category": "photograph",
+                        "created_on": "2022-02-26T08:48:33+00:00",
+                        "tags": [{"name": "bird"}],
+                        "mature": False,
+                    },
+                }
+                for i in range(
+                    mock_total_hits - len(mock_es_response_1["hits"]["hits"]) - 2
+                )
+            ],
+        },
+    }
+
+    es_host = settings.ES.transport.kwargs["host"]
+    es_port = settings.ES.transport.kwargs["port"]
+
+    # `origin_index` enforced by passing `exact_index=True` below.
+    es_endpoint = (
+        f"http://{es_host}:{es_port}/{image_media_type_config.origin_index}/_search"
+    )
+
+    # `from` is always 0 if there is no query mask
+    # see `_paginate_with_dead_link_mask` branch 1
+    # Testing this with a query mask would introduce far more complexity
+    # with no significant benefit
+    re.compile('from":0')
+
+    mock_first_es_request = (
+        pook.post(es_endpoint)
+        # The dead link ratio causes the initial query size to double
+        .body(re.compile(f'size":{(page_size * page) * 2}'))
+        .body(re.compile('from":0'))
+        .reply(200)
+        .json(mock_es_response_1)
+        .mock
+    )
+
+    mock_second_es_request = (
+        pook.post(es_endpoint)
+        # Size is clamped to the total number of available hits
+        .body(re.compile(f'size":{mock_total_hits}'))
+        .body(re.compile('from":0'))
+        .reply(200)
+        .json(mock_es_response_2)
+        .mock
+    )
+
+    live_results = [
+        r
+        for r in mock_es_response_2["hits"]["hits"]
+        if r["_source"]["url"] == mock_live_result_url
+    ]
+
+    pook.head(
+        mock_live_result_url,
+        reply=200,
+    )
+
+    pook.head(
+        mock_dead_result_url,
+        reply=400,
+    )
+
+    serializer = image_media_type_config.search_request_serializer(
+        # This query string does not matter, ultimately, as pook is mocking
+        # the ES response regardless of the input
+        data={"q": "bird perched"}
+    )
+    serializer.is_valid()
+    results, _, _, _ = search_controller.search(
+        search_params=serializer,
+        ip=0,
+        origin_index=image_media_type_config.origin_index,
+        exact_index=True,
+        page=page,
+        page_size=page_size,
+        filter_dead=True,
+    )
+
+    assert mock_first_es_request.total_matches == 1
+    assert mock_second_es_request.total_matches == 1
+
+    assert {r["_source"]["identifier"] for r in live_results} == {
+        r.identifier for r in results
+    }
+
+    assert wrapped_post_process_results.call_count == 2
