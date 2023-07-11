1a74036fd5f33acfe9d8afcdefff7a67989ff257
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-07-11 19:59:00 +0700

    Update frontend/src/components/VErrorSection/VErrorImage.vue
    Co-authored-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/components/VErrorSection/VErrorImage.vue
+++ frontend/src/components/VErrorSection/VErrorImage.vue
@@ -51,7 +51,7 @@ export default defineComponent({
         const errorImage: ErrorImage = {
           ...image,
           originalTitle: image.title,
-          src: require(`/error_images/${image.file}.jpg`),
+          src: `/error_images/${image.file}.jpg`,
           alt: `errorImages.${image.id}`,
           license: image.license as License,
           license_version: image.license_version as LicenseVersion,
