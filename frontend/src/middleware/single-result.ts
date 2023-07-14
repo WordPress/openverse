a888b230bc28a2fb1e914e4e9d0c49a23f9efc9e
Olga Bulat <obulat@gmail.com>
2023-07-14 07:30:15 +0300

    Update frontend/src/middleware/single-result.ts
    Co-authored-by: sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>

--- frontend/src/middleware/single-result.ts
+++ frontend/src/middleware/single-result.ts
@@ -14,20 +14,26 @@ export const singleResultMiddleware: Middleware = async ({
 }) => {
   const mediaType = route.fullPath.includes("/image/") ? IMAGE : AUDIO
   const singleResultStore = useSingleResultStore($pinia)
+
   if (process.server) {
     const media = await singleResultStore.fetch(mediaType, route.params.id)
     await useRelatedMediaStore($pinia).fetchMedia(mediaType, route.params.id)
+
     if (!media) {
       error(singleResultStore.fetchState.fetchingError ?? {})
     }
   } else {
+    // Client-side rendering
     singleResultStore.setMediaById(mediaType, route.params.id)
+
     if (from && from.path.includes("/search/")) {
       const searchStore = useSearchStore($pinia)
       searchStore.setBackToSearchPath(from.fullPath)
+
       const searchTerm = Array.isArray(route.query.q)
         ? route.query.q[0]
         : route.query.q
+
       if (searchTerm) {
         searchStore.setSearchTerm(searchTerm)
       }
