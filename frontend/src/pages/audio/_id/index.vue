8debbb9ee6b9d7081192706d9350f25f185dc3d6
nevvada <nlemaster47@gmail.com>
2023-07-21 10:25:44 -0700

    update padding around back to results button

--- frontend/src/pages/audio/_id/index.vue
+++ frontend/src/pages/audio/_id/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main :id="skipToContentTargetId" tabindex="-1">
-    <div v-if="backToSearchPath" class="w-full px-2 py-2 md:px-6">
+    <div v-if="backToSearchPath" class="w-full px-4 pb-4 md:px-8">
       <VBackToSearchResultsLink
         :id="$route.params.id"
         :href="backToSearchPath"
