3eed898797387dd881fd0cfbc2f13b870747069e
Zack Krida <zackkrida@pm.me>
2023-07-24 16:51:50 -0400

    Safety wall unit tests, story, and visual regression tests

--- frontend/test/storybook/visual-regression/v-safety-wall.spec.ts
+++ frontend/test/storybook/visual-regression/v-safety-wall.spec.ts
@@ -2,15 +2,28 @@ import { test } from "@playwright/test"
 
 import breakpoints from "~~/test/playwright/utils/breakpoints"
 
+// Only used to isolate part of the Storybook frame;
+// this shouldn't be used to reference this component in
+// other E2E tests. Use a label or other visual property.
+const safetyWallLocator = "#safety-wall"
+
 test.describe("VSafetyWall", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/iframe.html?id=components-vsafetywall--default-story")
   })
 
-  // breakpoints.describeEvery(({ expectSnapshot }) => {
-  breakpoints.describeEvery(({}) => {
-    test.fixme("todo", () => {
-      // wow
+  breakpoints.describeEvery(({ expectSnapshot }) => {
+    test("Renders the wall correctly for sensitive media", async ({ page }) => {
+      await expectSnapshot(
+        `v-safetywall-default`,
+        page.locator(safetyWallLocator)
+      )
+    })
+    test("Renders the wall correctly for sensitive media", async ({ page }) => {
+      await expectSnapshot(
+        `v-safetywall-default`,
+        page.locator(safetyWallLocator)
+      )
     })
   })
 })
--- frontend/test/unit/specs/components/VSafetyWall/v-safety-wall.spec.js
+++ frontend/test/unit/specs/components/VSafetyWall/v-safety-wall.spec.js
@@ -1,5 +1,50 @@
-// import VSafetyWall from "~/components/VSafetyWall/VSafetyWall.vue"
+import { fireEvent } from "@testing-library/vue"
 
-describe("VSafetyWall", () => {
-  // todo
+import { render } from "~~/test/unit/test-utils/render"
+
+import { useSearchStore } from "~/stores/search"
+
+import VSafetyWall from "~/components/VSafetyWall/VSafetyWall.vue"
+
+jest.mock("~/stores/search", () => ({
+  useSearchStore: jest.fn(),
+}))
+
+describe("VSafetyWall.vue", () => {
+  let mockStore
+  let options = {}
+
+  beforeEach(() => {
+    mockStore = {
+      backToSearchPath: "/somepath",
+    }
+    useSearchStore.mockReturnValue(mockStore)
+
+    options = {
+      props: {
+        media: {
+          sensitivity: [
+            "sensitive_text",
+            "provider_supplied_sensitive",
+            "user_reported_sensitive",
+          ],
+        },
+      },
+    }
+  })
+
+  it("emits reveal event when showMedia method is called", async () => {
+    const { getByText } = render(VSafetyWall, options)
+    const showButton = getByText("Show content")
+
+    await fireEvent.click(showButton)
+
+    expect(useSearchStore).toHaveBeenCalled()
+  })
+
+  it("backToSearchPath gets the value from the store", () => {
+    render(VSafetyWall, options)
+
+    expect(useSearchStore).toHaveBeenCalled()
+  })
 })
