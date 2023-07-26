d82a56bf3cedc20d2d1eb7377c32f2e5d290c373
Zack Krida <zackkrida@pm.me>
2023-07-26 12:14:55 -0400

    Discard changes to frontend/src/components/VBackToSearchResultsLink.vue

--- frontend/src/components/VBackToSearchResultsLink.vue
+++ frontend/src/components/VBackToSearchResultsLink.vue
@@ -5,7 +5,7 @@
     :href="href"
     has-icon-start
     variant="transparent-gray"
-    size="small"
+    size="medium"
     class="label-bold inline-flex"
     v-bind="$attrs"
     @mousedown="handleClick"
