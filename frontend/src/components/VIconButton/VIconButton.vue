969f75b1ea505859f1d0da88f679377d11e9efd6
Olga Bulat <obulat@gmail.com>
2023-07-27 12:08:43 +0300

    Create a new `VIconButton` (#2697)
    * Add a new VIconButton component
    * Add `icon-only` size to VButton
    * Use icon-only for sizes
    * Remove `filled-transparent` button variant

--- frontend/src/components/VIconButton/VIconButton.vue
+++ frontend/src/components/VIconButton/VIconButton.vue
@@ -1,100 +1,69 @@
 <template>
   <VButton
-    v-bind="buttonProps"
     :aria-label="label"
-    size="disabled"
-    class="icon-button flex flex-shrink-0 flex-grow-0 items-center justify-center"
-    :class="[buttonSizeClasses, { 'border-1.5': !borderless }]"
-    :type="type"
+    :size="size"
+    :variant="variant"
+    class="icon-button"
+    icon-only
     v-on="$listeners"
   >
-    <slot name="default" :icon-size="iconSize" />
+    <slot name="default" :icon-size="6" />
     <VIcon
       v-if="iconProps"
       class="pointer-events-none"
-      :size="iconSize"
+      :size="6"
       v-bind="iconProps"
     />
   </VButton>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from "vue"
+import { defineComponent, PropType } from "vue"
 
-import type { ButtonType } from "~/types/button"
+import type { ButtonSize, ButtonVariant } from "~/types/button"
 
 import VIcon, { type IconProps } from "~/components/VIcon/VIcon.vue"
-import VButton, { type ButtonProps } from "~/components/VButton.vue"
+import VButton from "~/components/VButton.vue"
 
 import type { TranslateResult } from "vue-i18n"
 
-const SIZE_MAP = Object.freeze({
-  tiny: { icon: 6, button: "w-6 h-6" },
-  close: { icon: 6, button: "w-8 h-8" },
-  small: { icon: 6, button: "w-10 h-10" },
-  medium: { icon: 6, button: "w-12 h-12" },
-  large: { icon: 8, button: "w-14 h-14" },
-  "extra-large": { icon: 12, button: "w-20 h-20" },
-} as const)
-type Size = keyof typeof SIZE_MAP
-
+/**
+ * The icon-only version of VButton component. In some cases, VButton is replaced
+ * with VIconButton in small breakpoints.
+ */
 export default defineComponent({
   name: "VIconButton",
   components: { VIcon, VButton },
   props: {
     /**
-     * the size of the button; The size affects both the size of the button
-     * itself and the icon inside it.
+     * The size of the button, matches the sizes of VButton component.
      */
     size: {
-      type: String as PropType<Size>,
-      default: "medium",
-      validator: (val: string) => Object.keys(SIZE_MAP).includes(val),
+      type: String as PropType<Exclude<ButtonSize, "disabled">>,
+      required: true,
     },
     /**
-     * props to pass down to the `VIcon` component nested inside the button; See
-     * documentation on `VIcon`.
+     * The visual variant of the button, matches the variants of VButton component.
+     */
+    variant: {
+      type: String as PropType<ButtonVariant>,
+      required: true,
+    },
+    /**
+     * Props to pass down to the `VIcon` component nested inside the button.
+     * See documentation on `VIcon`.
      */
     iconProps: {
       type: Object as PropType<IconProps>,
       required: false,
     },
     /**
-     * props to pass down to the `VButton` component nested inside the button;
-     * See documentation on `VButton`.
-     */
-    buttonProps: {
-      type: Object as PropType<Omit<ButtonProps, "size">>,
-      default: () => ({ variant: "transparent-tx" }),
-    },
-    /**
-     * the label for the button; This is used for accessibility purposes.
+     * The label used for accessibility purposes.
      */
     label: {
       type: [String, Object] as PropType<string | TranslateResult>,
       required: true,
     },
-    /**
-     * whether the button should have a border or not.
-     * The `focus-` custom TW classes don't always work well with border.
-     */
-    borderless: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  setup(props, { attrs }) {
-    const type = (attrs["type"] ?? "button") as ButtonType
-
-    const buttonSizeClasses = computed(() => SIZE_MAP[props.size].button)
-    const iconSize = computed(() => SIZE_MAP[props.size].icon)
-
-    return {
-      type,
-
-      buttonSizeClasses,
-      iconSize,
-    }
   },
 })
 </script>
