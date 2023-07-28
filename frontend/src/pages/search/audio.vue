2b034f857c651069944b035e2b11c5c6135fcb39
Olga Bulat <obulat@gmail.com>
2023-07-28 14:25:41 +0300

    Extract reusable VAudioCollection.vue (#2695)
    * Extract reusable VAudioCollection.vue
    
    Co-authored-by: Zack Krida <zackkrida@pm.me>
    
    * Move analytics test to e2e
    
    * Move the gap setting to the `ol` level
    
    ---------
    
    Co-authored-by: Zack Krida <zackkrida@pm.me>

--- frontend/src/pages/search/audio.vue
+++ frontend/src/pages/search/audio.vue
@@ -1,102 +1,50 @@
 <template>
-  <!-- Negative margin compensates for the `p-4` padding in row layout. -->
-  <section class="-mx-2 md:-mx-4">
-    <VSnackbar size="large" :is-visible="snackbar.isVisible.value">
-      <i18n path="audioResults.snackbar.text" tag="p">
-        <template
-          v-for="keyboardKey in ['spacebar', 'left', 'right']"
-          #[keyboardKey]
-        >
-          <kbd :key="keyboardKey" class="font-sans">{{
-            $t(`audioResults.snackbar.${keyboardKey}`)
-          }}</kbd>
-        </template>
-      </i18n>
-    </VSnackbar>
-    <VGridSkeleton
-      v-if="results.length === 0 && !fetchState.isFinished"
-      is-for-tab="audio"
-    />
-    <ol :aria-label="$t('browsePage.aria.results', { query: searchTerm })">
-      <li v-for="audio in results" :key="audio.id">
-        <VAudioTrack
-          class="mb-2 md:mb-1"
-          :audio="audio"
-          :size="audioTrackSize"
-          layout="row"
-          :search-term="searchTerm"
-          @interacted="handleInteraction"
-          @mousedown="snackbar.handleMouseDown"
-          @focus="snackbar.show"
-        />
-      </li>
-    </ol>
-    <VLoadMore />
-  </section>
+  <VAudioCollection
+    :results="results"
+    :is-related="false"
+    :fetch-state="fetchState"
+    :collection-label="collectionLabel"
+  />
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, inject } from "vue"
+import { computed, defineComponent, PropType } from "vue"
 
 import { useSearchStore } from "~/stores/search"
-import { useUiStore } from "~/stores/ui"
+import { useI18n } from "~/composables/use-i18n"
+import type { AudioDetail } from "~/types/media"
+import type { FetchState } from "~/types/fetch-state"
 
-import { useAnalytics } from "~/composables/use-analytics"
-import { useAudioSnackbar } from "~/composables/use-audio-snackbar"
+import VAudioCollection from "~/components/VSearchResultsGrid/VAudioCollection.vue"
 
-import { IsSidebarVisibleKey } from "~/types/provides"
-import type { AudioInteractionData } from "~/types/analytics"
-
-import VSnackbar from "~/components/VSnackbar.vue"
-import VAudioTrack from "~/components/VAudioTrack/VAudioTrack.vue"
-import VLoadMore from "~/components/VLoadMore.vue"
-import VGridSkeleton from "~/components/VSkeleton/VGridSkeleton.vue"
-
-import { propTypes } from "./search-page.types"
+import type { NuxtError } from "@nuxt/types"
 
 export default defineComponent({
   name: "AudioSearch",
   components: {
-    VSnackbar,
-    VAudioTrack,
-    VGridSkeleton,
-    VLoadMore,
+    VAudioCollection,
   },
-  props: propTypes,
-  setup(props) {
-    const searchStore = useSearchStore()
+  props: {
+    results: {
+      type: Array as PropType<AudioDetail[]>,
+      required: true,
+    },
+    fetchState: {
+      type: Object as PropType<FetchState<NuxtError> | FetchState>,
+      required: true,
+    },
+  },
+  setup() {
+    const i18n = useI18n()
 
-    const uiStore = useUiStore()
+    const collectionLabel = computed(() => {
+      const query = useSearchStore().searchTerm
 
-    const { sendCustomEvent } = useAnalytics()
-
-    const searchTerm = computed(() => searchStore.searchTerm)
-    const results = computed(() => props.resultItems.audio)
-
-    const isDesktopLayout = computed(() => uiStore.isDesktopLayout)
-    const filterVisibleRef = inject(IsSidebarVisibleKey)
-
-    const audioTrackSize = computed(() =>
-      !isDesktopLayout.value ? "s" : filterVisibleRef.value ? "l" : "m"
-    )
-
-    const snackbar = useAudioSnackbar()
-
-    const handleInteraction = (data: AudioInteractionData) => {
-      snackbar.hide()
-      sendCustomEvent("AUDIO_INTERACTION", {
-        ...data,
-        component: "AudioSearch",
-      })
-    }
+      return i18n.t("browsePage.aria.results", { query }).toString()
+    })
 
     return {
-      searchTerm,
-      results,
-      audioTrackSize,
-
-      handleInteraction,
-      snackbar,
+      collectionLabel,
     }
   },
 })
