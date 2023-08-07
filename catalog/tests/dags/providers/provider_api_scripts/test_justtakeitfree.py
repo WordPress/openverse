7d44c53d6b1564d40381c50f7b45eeab01f25878
Olga Bulat <obulat@gmail.com>
2023-08-07 19:59:22 +0300

    Add justtakeitfree as an image provider

---
+++ catalog/tests/dags/providers/provider_api_scripts/test_justtakeitfree.py
@@ -0,0 +1,72 @@
+"""
+TODO: Add additional tests for any methods you added in your subclass.
+Try to test edge cases (missing keys, different data types returned, Nones, etc).
+You may also need to update the given test names to be more specific.
+
+Run your tests locally with `just test -k justtakeitfree`
+"""
+
+import json
+from pathlib import Path
+from unittest.mock import patch
+
+from common.licenses import get_license_info
+from providers.provider_api_scripts.justtakeitfree import JusttakeitfreeDataIngester
+
+
+RESOURCES = Path(__file__).parent / "resources/justtakeitfree"
+
+# Set up test class
+jtif = JusttakeitfreeDataIngester()
+
+
+class FileSizeHeadResponse:
+    def __init__(self, size):
+        self.headers = {"Content-Length": size}
+
+
+def test_get_next_query_params_default_response():
+    actual_result = jtif.get_next_query_params(None)
+    actual_result.pop("key", None)
+    expected_result = {
+        "page": 1,
+    }
+    assert actual_result == expected_result
+
+
+def test_get_next_query_params_updates_parameters():
+    previous_query_params = {
+        "page": 1,
+    }
+    actual_result = jtif.get_next_query_params(previous_query_params)
+    actual_result.pop("key", None)
+
+    expected_result = {
+        "page": 2,
+    }
+    assert actual_result == expected_result
+
+
+def test_get_record_data():
+    with open(RESOURCES / "single_item.json") as f:
+        resource_json = json.load(f)
+
+    with patch.object(jtif.delayed_requester, "head") as head_patch:
+        # Returns None when 404
+        head_patch.return_value = FileSizeHeadResponse(100)
+        actual_data = jtif.get_record_data(resource_json)
+
+    expected_data = {
+        "foreign_landing_url": "https://justtakeitfree.com/photo/2/",
+        "url": "https://justtakeitfree.com/photos/2.jpg",
+        "foreign_identifier": "2",
+        "creator": "Justtakeitfree Free Photos",
+        "creator_url": "https://justtakeitfree.com",
+        "license_info": get_license_info(
+            "https://creativecommons.org/licenses/by/4.0/"
+        ),
+        "raw_tags": ["Baturyn fortress", "Baturyn citadel", "cossack fortress"],
+        "filesize": 100,
+    }
+
+    assert actual_data == expected_data
