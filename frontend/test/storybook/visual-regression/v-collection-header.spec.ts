e6ff3169d73bc5df3dadbe6e79a964aad4f6a6ad
Olga Bulat <obulat@gmail.com>
2023-08-22 10:47:21 +0300

    Update storybook test

--- frontend/test/storybook/visual-regression/v-collection-header.spec.ts
+++ frontend/test/storybook/visual-regression/v-collection-header.spec.ts
@@ -5,22 +5,15 @@ import breakpoints from "~~/test/playwright/utils/breakpoints"
 test.describe("VCollectionHeader", () => {
   breakpoints.describeMobileAndDesktop(({ expectSnapshot }) => {
     for (const languageDirection of ["ltr", "rtl"]) {
-      for (const collection of [
-        "tag",
-        "creator",
-        "source",
-        "source-with-long-name",
-      ]) {
-        test(`${collection}-${languageDirection}`, async ({ page }) => {
-          await page.goto(
-            `/iframe.html?id=components-vcollectionheader--default-story&args=id:${collection}`
-          )
-          await expectSnapshot(
-            `VCollectionHeader=${collection}-${languageDirection}`,
-            page.locator(".wrapper")
-          )
-        })
-      }
+      test(`All headers ${languageDirection}`, async ({ page }) => {
+        await page.goto(
+          `/iframe.html?id=components-vcollectionheader--all-collections`
+        )
+        await expectSnapshot(
+          `VCollectionHeaders-${languageDirection}`,
+          page.locator(".wrapper")
+        )
+      })
     }
   })
 })
