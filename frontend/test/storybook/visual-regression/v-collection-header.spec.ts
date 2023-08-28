1c1d9f58b7fa81ab9c07f6bc6e0e00d5df16a5f5
Olga Bulat <obulat@gmail.com>
2023-08-28 10:39:08 +0300

    Add `VCollectionHeader` component (#2734)
    * Add `VCollectionHeader` component
    
    * Update the analytics events
    
    * Update storybook test
    
    * Update snapshots
    
    * Improve storybook snapshots
    
    * Add requested style changes
    
    * Update snapshots
    
    * Update creator results label in the story
    
    * Update snapshots
    
    * Prevent flakiness in single result tests
    
    * Update frontend/src/types/analytics.ts
    
    * Update frontend/src/components/VCollectionHeader/VCollectionHeader.vue

---
+++ frontend/test/storybook/visual-regression/v-collection-header.spec.ts
@@ -0,0 +1,19 @@
+import { test } from "@playwright/test"
+
+import breakpoints from "~~/test/playwright/utils/breakpoints"
+
+test.describe("VCollectionHeader", () => {
+  breakpoints.describeEvery(({ expectSnapshot }) => {
+    for (const languageDirection of ["ltr", "rtl"]) {
+      test(`All headers ${languageDirection}`, async ({ page }) => {
+        await page.goto(
+          `/iframe.html?id=components-vcollectionheader--all-collections`
+        )
+        await expectSnapshot(
+          `VCollectionHeaders-${languageDirection}`,
+          page.locator(".wrapper")
+        )
+      })
+    }
+  })
+})
