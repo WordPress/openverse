5f7796a028860809e53cad88644dca69400f8c81
Ashir Amin <ashiramin123@gmail.com>
2023-09-02 20:04:53 -0500

    added value for STATIC_ROOT for local dev setup

--- api/conf/settings/static.py
+++ api/conf/settings/static.py
@@ -5,6 +5,6 @@ from decouple import config
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Where to collect static files in production/development deployments
-STATIC_ROOT = config("STATIC_ROOT", default="/var/api_static_content/static")
+STATIC_ROOT = config("STATIC_ROOT", default="/static")
 
 STATIC_URL = "static/"
