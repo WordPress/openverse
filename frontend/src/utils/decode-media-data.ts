d0cb976937830559d1fba23115e4ecaa559ee5d8
Dhruv Bhanushali <hi@dhruvkb.dev>
2023-09-07 10:43:33 +0400

    Handle the fact that `Array.sort` works in-place

--- frontend/src/utils/decode-media-data.ts
+++ frontend/src/utils/decode-media-data.ts
@@ -108,7 +108,7 @@ export const decodeMediaData = <T extends Media>(
     featureFlagStore.isOn("fetch_sensitive")
       ? getFakeSensitivities(media.id)
       : media[SENSITIVITY_RESPONSE_PARAM] ?? []
-  const sortedSensitivity = sensitivity.sort()
+  sensitivity.sort()
   const isSensitive = sensitivity.length > 0
 
   return {
@@ -117,7 +117,7 @@ export const decodeMediaData = <T extends Media>(
     frontendMediaType: mediaType,
     creator: decodeString(media.creator),
     tags: media.tags ? parseTags(media.tags) : ([] as Tag[]),
-    sensitivity: sortedSensitivity,
+    sensitivity,
     isSensitive,
   } as T
 }
