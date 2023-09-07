652069c9cfd8c904de2c7fc47e29168b3e9a8862
Olga Bulat <obulat@gmail.com>
2023-09-07 06:57:01 +0300

    Add test

--- frontend/test/playwright/e2e/seo.spec.ts
+++ frontend/test/playwright/e2e/seo.spec.ts
@@ -1,13 +1,13 @@
 import { expect, test } from "@playwright/test"
 
 test.describe.configure({ mode: "parallel" })
+const DESCRIPTION =
+  "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing."
 
 const pages = {
   home: {
     url: "/",
     title: "Openly Licensed Images, Audio and More | Openverse",
-    description:
-      "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing.",
     ogImage: "/openverse-default.jpg",
     ogTitle: "Openverse",
     robots: "all",
@@ -15,8 +15,6 @@ const pages = {
   allSearch: {
     url: "/search/?q=birds",
     title: "birds | Openverse",
-    description:
-      "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing.",
     ogImage: "/openverse-default.jpg",
     ogTitle: "Openverse",
     robots: "all",
@@ -24,8 +22,6 @@ const pages = {
   imageSearch: {
     url: "/search/image?q=birds",
     title: "birds | Openverse",
-    description:
-      "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing.",
     ogImage: "/openverse-default.jpg",
     ogTitle: "Openverse",
     robots: "all",
@@ -33,8 +29,6 @@ const pages = {
   audioSearch: {
     url: "/search/audio?q=birds",
     title: "birds | Openverse",
-    description:
-      "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing.",
     ogImage: "/openverse-default.jpg",
     ogTitle: "Openverse",
     robots: "all",
@@ -42,8 +36,6 @@ const pages = {
   imageDetail: {
     url: "/image/da5cb478-c093-4d62-b721-cda18797e3fb",
     title: "bird | Openverse",
-    description:
-      "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing.",
     ogImage:
       "https://api.openverse.engineering/v1/images/da5cb478-c093-4d62-b721-cda18797e3fb/thumb/",
     ogTitle: "bird",
@@ -52,8 +44,6 @@ const pages = {
   audioDetail: {
     url: "/audio/2e38ac1e-830c-4e9c-b13d-2c9a1ad53f95",
     title: "Hello Black & White Bird | Openverse",
-    description:
-      "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing.",
     ogImage:
       "https://api.openverse.engineering/v1/audio/2e38ac1e-830c-4e9c-b13d-2c9a1ad53f95/thumb/",
     ogTitle: "Hello Black & White Bird",
@@ -62,23 +52,18 @@ const pages = {
   about: {
     url: "/about",
     title: "About Openverse | Openverse",
-    description:
-      "Search over 700 million free and openly licensed images, photos, audio, and other media types for reuse and remixing.",
     ogImage: "/openverse-default.jpg",
     ogTitle: "Openverse",
     robots: "all",
   },
 }
-test.describe("page metadata", () => {
+test.describe("Page metadata", () => {
   for (const openversePage of Object.values(pages)) {
     test(`${openversePage.url}`, async ({ page }) => {
       await page.goto(openversePage.url)
       await expect(page).toHaveTitle(openversePage.title)
       const metaDescription = page.locator('meta[name="description"]')
-      await expect(metaDescription).toHaveAttribute(
-        "content",
-        openversePage.description
-      )
+      await expect(metaDescription).toHaveAttribute("content", DESCRIPTION)
 
       const metaRobots = page.locator('meta[name="robots"]')
       await expect(metaRobots).toHaveAttribute("content", openversePage.robots)
@@ -96,17 +81,4 @@ test.describe("page metadata", () => {
       )
     })
   }
-
-  test("sensitive image does not show og title", async ({ page }) => {
-    await page.route("**/da5cb478-c093-4d62-b721-cda18797e3fb", (route) => {
-      console.log("route", route)
-    })
-    await page.goto("/search/image?q=birds")
-    await page.locator(".image-grid>li").first().click()
-
-    await expect(page.locator("meta[name='og:title']")).toHaveAttribute(
-      "content",
-      /sensitive/
-    )
-  })
 })
