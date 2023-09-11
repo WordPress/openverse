c686cd48431f34cb3c129835ad6e4dc457d088bc
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2023-09-11 11:30:11 +1000

    Map content report parameters to API expectations

--- frontend/src/data/report-service.ts
+++ frontend/src/data/report-service.ts
@@ -1,5 +1,5 @@
 import type { MediaType } from "~/constants/media"
-import type { ReportReason } from "~/constants/content-report"
+import { ReportReason, SENSITIVE } from "~/constants/content-report"
 import { createApiService, getResourceSlug } from "~/data/api-service"
 
 interface ReportParams {
@@ -9,13 +9,36 @@ interface ReportParams {
   description?: string
 }
 
+interface LegacyAPIParams extends Omit<ReportParams, "reason"> {
+  reason: Exclude<ReportReason, "sensitive"> | "mature"
+}
+
+/**
+ * Map report parameters to the legacy parameters temporarily expected
+ * by the Openverse API.
+ *
+ * Specifically, the Openverse API expects a "mature" reason instead
+ * of a "sensitive" reason, so this function maps `reason` from `sensitive`
+ * to `mature`. Once the Openverse API handles `sensitive` reason, this
+ * function can be removed.
+ *
+ * The API work chain starts with this issue:
+ * https://github.com/WordPress/openverse/issues/2626
+ * @param params Content report modal parameters
+ * @returns Parameters mapped to match Openverse API expectations
+ */
+const mapToLegacy = (params: ReportParams): LegacyAPIParams => ({
+  ...params,
+  reason: params.reason === SENSITIVE ? "mature" : params.reason,
+})
+
 const apiService = createApiService()
 const ReportService = {
   sendReport(params: ReportParams) {
     const mediaType = params.mediaType
     return apiService.post(
       `/${getResourceSlug(mediaType)}${params.identifier}/report`,
-      params
+      mapToLegacy(params)
     )
   },
 }
