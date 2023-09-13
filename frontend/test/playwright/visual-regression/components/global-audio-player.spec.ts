2326ca5221f7bfd150d0a23aa00fa244137a6747
Dhruv Bhanushali <dhruv_b@live.com>
2023-09-13 11:30:20 +0400

    Wait for network idle before snapshot

--- frontend/test/playwright/visual-regression/components/global-audio-player.spec.ts
+++ frontend/test/playwright/visual-regression/components/global-audio-player.spec.ts
@@ -29,6 +29,9 @@ for (const dir of languageDirections) {
         .click()
       // To make the tests consistent, set the played area to the same position
       await page.mouse.click(170, 650)
+      // Allow audio to buffer to the seeked position
+      // eslint-disable-next-line playwright/no-networkidle
+      await page.waitForLoadState("networkidle")
       await expectSnapshot(`global-audio-player-on-search-${dir}.png`, page)
     })
   })
