25112325e86a5e6698d505c1e4be14b61629ad83
Olga Bulat <obulat@gmail.com>
2023-09-20 10:12:01 +0300

    Re-add gevent monkey-patching (#3047)
    * Re-add gevent monkey-patching
    
    * Add greenlet
    
    * Update the lock file

--- api/conf/wsgi.py
+++ api/conf/wsgi.py
@@ -7,6 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+from gevent import monkey
+
+
+monkey.patch_all()
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
--- api/manage.py
+++ api/manage.py
@@ -3,6 +3,11 @@
 import os
 import sys
 
+from gevent import monkey
+
+
+monkey.patch_all()
+
 
 def main():
     """Run administrative tasks."""
