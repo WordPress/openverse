afbd4077250e8be136cd963be056828a699065cf
ngken0995 <ngken0995@gmail.com>
2023-10-04 15:40:09 -0400

    create VTag component

---
+++ frontend/src/components/VTag/VTag.vue
@@ -0,0 +1,30 @@
+<template>
+  <VButton
+    as="VLink"
+    size="small"
+    variant="filled-gray"
+    class="label-bold"
+    :href="href"
+    >{{ title }}</VButton
+  >
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api"
+
+import VButton from "~/components/VButton.vue"
+
+export default defineComponent({
+  name: "VTag",
+  components: { VButton },
+  props: {
+    title: { type: String },
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped></style>
