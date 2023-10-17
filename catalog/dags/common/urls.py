d3aa9c591303ce4b3856a46246ea5eb591dcee96
ngken0995 <ngken0995@gmail.com>
2023-10-17 13:32:38 -0400

    move if condition after logging

--- catalog/dags/common/urls.py
+++ catalog/dags/common/urls.py
@@ -37,14 +37,15 @@ def validate_url_string(url_string, strip_slash: bool = True):
     strip_slash: Flag (bool) decides whether to strip slashes in URL or not,
     Default value is `True`
     """
-    if url_string and " " in url_string:
-        raise SpaceInUrlError("space is present in URL")
 
     logger.debug(f"Validating_url {url_string}")
     if not type(url_string) == str or not url_string:
         return
-    else:
-        upgraded_url = _add_best_scheme(url_string, strip_slash)
+
+    if url_string and " " in url_string:
+        raise SpaceInUrlError(f"space is present in URL: {url_string}")
+
+    upgraded_url = _add_best_scheme(url_string, strip_slash)
 
     parse_result = urlparse(upgraded_url)
     tld = tldextract.extract(upgraded_url)
