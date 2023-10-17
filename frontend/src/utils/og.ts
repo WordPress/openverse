667d153e7ead5a023ae5b85108f095cf654a6cd3
RougeCode007 <abioyefeyifoluwa@gmail.com>
2023-10-17 14:29:05 +0100

    fix test

--- frontend/src/utils/og.ts
+++ frontend/src/utils/og.ts
@@ -1,5 +1,5 @@
 import type { MetaInfo } from "vue-meta"
-import type { MetaPropertyName } from "vue-meta/types/vue-meta"
+import type { MetaPropertyName, MetaPropertyProperty } from "vue-meta/types/vue-meta"
 
 export const createDetailPageMeta = ({
   title,
@@ -18,7 +18,7 @@ export const createDetailPageMeta = ({
       name: "robots",
       content: "noindex",
     },
-  ] as (MetaPropertyName)[]
+  ] as (MetaPropertyName | MetaPropertyProperty)[]
   if (title) {
     meta.push({
       hid: "og:title",
