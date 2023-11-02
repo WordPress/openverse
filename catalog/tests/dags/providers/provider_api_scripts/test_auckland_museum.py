9ed60456f547e16b7c6c327ac213ff0b02d56702
ngken0995 <ngken0995@gmail.com>
2023-11-02 16:16:27 -0400

    revise test

--- catalog/tests/dags/providers/provider_api_scripts/test_auckland_museum.py
+++ catalog/tests/dags/providers/provider_api_scripts/test_auckland_museum.py
@@ -1,7 +1,4 @@
 from pathlib import Path
-from unittest.mock import patch
-
-import pytest
 
 from catalog.tests.dags.providers.provider_api_scripts.resources.json_load import (
     make_resource_json_func,
@@ -16,43 +13,18 @@ CC_BY_4_0 = get_license_info("https://creativecommons.org/licenses/by/4.0/")
 ingester = AucklandMuseumDataIngester()
 _get_resource_json = make_resource_json_func("aucklandmuseum")
 
-AUDIO_FILE_SIZE = 2484439
-
-
-@pytest.fixture
-def file_size_patch():
-    with patch.object(ingester, "_get_file_info") as get_file_info_mock:
-        get_file_info_mock.return_value = AUDIO_FILE_SIZE
-        yield
-
 
 def test_get_next_query_params_default_response():
     actual_param = ingester.get_next_query_params(None)
     expected_param = {
         "q": "_exists_:primaryRepresentation+copyright:CC",
-        "size": "100",
-        "from": ingester.from_start,
+        "size": "2000",
+        "from": ingester.batch_start,
     }
     assert actual_param == expected_param
 
 
-def test_get_next_query_params_updates_parameters():
-    previous_query_params = {
-        "q": "_exists_:primaryRepresentation+copyright:CC",
-        "size": "100",
-        "from": ingester.from_start,
-    }
-    actual_result = ingester.get_next_query_params(previous_query_params)
-
-    expected_result = {
-        "q": "_exists_:primaryRepresentation+copyright:CC",
-        "size": "100",
-        "from": ingester.from_start + 100,
-    }
-    assert actual_result == expected_result
-
-
-def test_get_record_data(file_size_patch):
+def test_get_record_data():
     # High level test for `get_record_data`. One way to test this is to create a
     # `tests/resources/AucklandMuseum/single_item.json` file containing a sample json
     # representation of a record from the API under test, call `get_record_data` with
@@ -76,7 +48,6 @@ def test_get_record_data(file_size_patch):
         "url": "http://api.aucklandmuseum.com/id/media/v/214749",
         "license_info": CC_BY_4_0,
         "thumbnail_url": "http://api.aucklandmuseum.com/id/media/v/214749?rendering=thumbnail.jpg",
-        "filesize": AUDIO_FILE_SIZE,
         "creator": "R. O. Gardner",
         "title": "Cypholophus macrocephalus mollis (Blume) Wedd. var. mollis (Wedd.) Wedd.",
         "meta_data": meta_data,
