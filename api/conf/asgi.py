c2666df75cb91bb829d2209304e7ee6b00559b9d
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2023-11-15 11:30:03 +1100

    Add Sentry ASGI middleware
    Hopefully this stops the memory leak https://github.com/getsentry/sentry-python/issues/419

--- api/conf/asgi.py
+++ api/conf/asgi.py
@@ -4,6 +4,8 @@ import django
 from django.conf import settings
 from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
 from conf.asgi_handler import OpenverseASGIHandler
 
 
@@ -15,8 +17,21 @@ def get_asgi_application():
     return OpenverseASGIHandler()
 
 
-application = get_asgi_application()
+# Give the base application a unique name so that it's easily referenced
+# by other modules without needing to juggle through any of the various
+# middlewares.
+# For example, `ASGIStaticFileHandler` saves the application as
+# `self.application`. However, `SentryAsgiMiddleware` saves it as `self.app`.
+# If we relied on just referencing the pseudo-standard `application` exported
+# in this module, we'd have to choose how to resolve the base application
+# with the lifecycle handlers depending on what middleware was applied.
+# Instead, we can just take advantage of the fact that these middleware
+# all treat the passed-in application object as a singleton, so we're safe
+# to directly reference `OPENVERSE_APPLICATION`, knowing that any middleware
+# applied to it will be referencing the same application object in the end.
+OPENVERSE_APPLICATION = get_asgi_application()
 
+application = SentryAsgiMiddleware(OPENVERSE_APPLICATION)
 
 if settings.ENVIRONMENT == "local":
     static_files_application = ASGIStaticFilesHandler(application)
