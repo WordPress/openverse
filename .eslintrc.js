9c201a1992cbb2c89911e114cfaa095cc9e1abae
bhavik001 <bhmistry@myseneca.ca>
2023-11-22 01:39:19 -0500

    Update .eslintrc.jd file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,10 @@ module.exports = {
   root: true,
   extends: ["plugin:@openverse/project"],
   plugins: ["@openverse"],
-}
+  rules: {
+    // Add or override rules here
+    curly: ['error', 'all'],
+    'default-case': 'error',
+    eqeqeq: 'error',
+  },
+};
