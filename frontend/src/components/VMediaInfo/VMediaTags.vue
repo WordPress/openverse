de203f19c5480b5f55ea01050679b3032ffb0796
Olga Bulat <obulat@gmail.com>
2023-11-27 09:48:16 +0300

    Add source, creator and tag links to the single result page media info (#3338)
    * Make chevron icons RTL-friendly
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add VByLine and VMediaInfo to SRP
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update snapshots
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add items-center to audio full layout
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Fix tag spacing
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update scroll button spacings
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update scroll button spacings
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update snapshots
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update fading
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Remove line-clamping for h1 and fix CTA placement
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Improve scrolling
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update snapshots
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Make chevron icons RTL-friendly
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add VByLine and VMediaInfo to SRP
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update snapshots
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add items-center to audio full layout
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Fix tag spacing
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update scroll button spacings
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update scroll button spacings
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update snapshots
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update fading
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Remove line-clamping for h1 and fix CTA placement
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Improve scrolling
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update snapshots
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Remove unused variable
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add more spacing to scroll buttons
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * More fading
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Fix scrolling edge-cases
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Stop using the ui store for button variant
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add requested changes
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Remove scroll snap and update last 2 scrolls
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    ---------
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/components/VMediaInfo/VMediaTags.vue
+++ frontend/src/components/VMediaInfo/VMediaTags.vue
@@ -1,9 +1,9 @@
 <template>
-  <ul v-if="tags.length && additionalSearchViews" class="flex flex-wrap gap-2">
+  <ul v-if="tags.length && additionalSearchViews" class="flex flex-wrap gap-3">
     <VTag
       v-for="(tag, index) in tags"
       :key="index"
-      href="/"
+      :href="localizedTagPath(tag)"
       :title="tag.name"
     />
   </ul>
@@ -15,12 +15,12 @@
 </template>
 <script lang="ts">
 import { computed, defineComponent, PropType } from "vue"
+import { useContext } from "@nuxtjs/composition-api"
 
 import type { Tag } from "~/types/media"
 import { useFeatureFlagStore } from "~/stores/feature-flag"
 
 import VMediaTag from "~/components/VMediaTag/VMediaTag.vue"
-
 import VTag from "~/components/VTag/VTag.vue"
 
 export default defineComponent({
@@ -33,13 +33,18 @@ export default defineComponent({
     },
   },
   setup() {
+    const { app } = useContext()
     const featureFlagStore = useFeatureFlagStore()
 
     const additionalSearchViews = computed(() =>
       featureFlagStore.isOn("additional_search_views")
     )
 
-    return { additionalSearchViews }
+    const localizedTagPath = (tag: Tag) => {
+      return app.localePath({ path: `tag/${tag.name}` })
+    }
+
+    return { additionalSearchViews, localizedTagPath }
   },
 })
 </script>
