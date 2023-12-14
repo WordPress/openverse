e7ecf6c8e2de85fdca9389fbbfbc60b42fb1330f
Dhruv Bhanushali <dhruv_b@live.com>
2023-12-14 12:22:51 +0400

    Dedent code and increase logging

--- automations/js/src/project_automation/issues.mjs
+++ automations/js/src/project_automation/issues.mjs
@@ -4,22 +4,25 @@ import { getBoard } from '../utils/projects.mjs'
  * Set the "Priority" custom field based on the issue's labels. Also move
  * the card for critical issues directly to the "📅 To Do" column.
  *
- * @param issue {import('@octokit/rest')}
- * @param board {import('../utils/projects.mjs').Project}
- * @param card {import('../utils/projects.mjs').Card}
- * @param core {import('@actions/core')} for logging
+ * @param core {import('@actions/core')} GitHub Actions toolkit, for logging
+ * @param issue {Issue} the issue for which to set the "Priority" custom field
+ * @param backlogBoard {Project} the project board for issues
+ * @param issueCard {Card} the card for the issue to sync
  */
-async function syncPriority(issue, board, card, core) {
-  core.debug(`Syncing priority for issue "${issue.number}".`)
+async function syncPriority(core, issue, backlogBoard, issueCard) {
+  core.info(`Syncing priority for issue "${issue.number}".`)
+
   const priority = issue.labels.find((label) =>
     label.name.includes('priority')
   )?.name
+  core.debug(`Priority: ${priority}`)
 
   if (priority) {
-    await board.setCustomChoiceField(card.id, 'Priority', priority, core)
+    await backlogBoard.setCustomChoiceField(issueCard.id, 'Priority', priority)
   }
   if (priority === '🟥 priority: critical') {
-    await board.moveCard(card.id, board.columns.ToDo)
+    core.info('Moving critical issue to "📅 To Do" column.')
+    await backlogBoard.moveCard(issueCard.id, backlogBoard.columns.ToDo)
   }
 }
 
@@ -31,69 +34,75 @@ async function syncPriority(issue, board, card, core) {
  * @param context {import('@actions/github').context} info about the current event
  */
 export const main = async (octokit, core, context) => {
-  const { EVENT_ACTION: eventAction } = process.env
+  core.info('Starting script `issues.mjs`.')
 
-  core.debug(`Event action received: ${eventAction}`)
+  const { EVENT_ACTION: eventAction } = process.env
+  core.debug(`Event action: ${eventAction}`)
 
   const issue = context.payload.issue
+  core.debug(`Issue node ID: ${issue.node_id}`)
+
   const label = context.payload.label
   core.info('Issue details:', issue)
 
   if (issue.labels.some((label) => label.name === '🧭 project: thread')) {
     core.warning('Issue is a project thread. Exiting.')
-    process.exit(0)
+    return
   }
 
-  await core.group('Processing Issue or PR', async () => {
-    core.debug('Getting instance for the project')
-    const backlogBoard = await getBoard(octokit, core, 'Backlog')
+  const backlogBoard = await getBoard(octokit, core, 'Backlog')
 
-    core.debug('Adding the issue or PR to the project')
-    const card = await backlogBoard.addCard(issue.node_id)
+  // Create new, or get the existing, card for the current issue.
+  const card = await backlogBoard.addCard(issue.node_id)
+  core.debug(`Issue card ID: ${card.id}`)
 
-    switch (eventAction) {
-      case 'opened':
-      case 'reopened': {
-        if (issue.labels.some((label) => label.name === '⛔ status: blocked')) {
-          await backlogBoard.moveCard(card.id, backlogBoard.columns.Blocked)
-        } else {
-          await backlogBoard.moveCard(card.id, backlogBoard.columns.Backlog)
-        }
-        await syncPriority(issue, backlogBoard, card, core)
-        break
+  switch (eventAction) {
+    case 'opened':
+    case 'reopened': {
+      if (issue.labels.some((label) => label.name === '⛔ status: blocked')) {
+        core.info('Issue was opened, labelled as blocked.')
+        await backlogBoard.moveCard(card.id, backlogBoard.columns.Blocked)
+      } else {
+        await backlogBoard.moveCard(card.id, backlogBoard.columns.Backlog)
       }
-
-      case 'closed': {
-        if (issue.state_reason === 'completed') {
-          await backlogBoard.moveCard(card.id, backlogBoard.columns.Done)
-        } else {
-          await backlogBoard.moveCard(card.id, backlogBoard.columns.Discarded)
-        }
-        break
-      }
-
-      case 'assigned': {
-        if (card.status === backlogBoard.columns.Backlog) {
-          await backlogBoard.moveCard(card.id, backlogBoard.columns.ToDo)
-        }
-        break
-      }
-
-      case 'labeled': {
-        if (label.name === '⛔ status: blocked') {
-          await backlogBoard.moveCard(card.id, backlogBoard.columns.Blocked)
-        }
-        await syncPriority(issue, backlogBoard, card, core)
-        break
-      }
-
-      case 'unlabeled': {
-        if (label.name === '⛔ status: blocked') {
-          await backlogBoard.moveCard(card.id, backlogBoard.columns.Backlog)
-        }
-        await syncPriority(issue, backlogBoard, card, core)
-        break
-      }
+      await syncPriority(core, issue, backlogBoard, card)
+      break
     }
-  })
+
+    case 'closed': {
+      if (issue.state_reason === 'completed') {
+        core.info('Issue was closed as completed.')
+        await backlogBoard.moveCard(card.id, backlogBoard.columns.Done)
+      } else {
+        core.info('Issue was closed as discarded.')
+        await backlogBoard.moveCard(card.id, backlogBoard.columns.Discarded)
+      }
+      break
+    }
+
+    case 'assigned': {
+      if (card.status === backlogBoard.columns.Backlog) {
+        await backlogBoard.moveCard(card.id, backlogBoard.columns.ToDo)
+      }
+      break
+    }
+
+    case 'labeled': {
+      if (label.name === '⛔ status: blocked') {
+        core.info('Issue was labeled as blocked.')
+        await backlogBoard.moveCard(card.id, backlogBoard.columns.Blocked)
+      }
+      await syncPriority(core, issue, backlogBoard, card)
+      break
+    }
+
+    case 'unlabeled': {
+      if (label.name === '⛔ status: blocked') {
+        core.info('Issue was unlabeled as blocked.')
+        await backlogBoard.moveCard(card.id, backlogBoard.columns.Backlog)
+      }
+      await syncPriority(core, issue, backlogBoard, card)
+      break
+    }
+  }
 }
