cb2579d12502a1b69225ea5171df8a3d1fec1a4c
Olga Bulat <obulat@gmail.com>
2023-12-19 15:02:50 +0300

    Replace _id with [id] dynamic pages
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/pages/audio/source/[...source].vue
+++ frontend/src/pages/audio/source/[...source].vue
@@ -1,5 +1,5 @@
 <template>
-  <VCollectionPage media-type="image" />
+  <VCollectionPage media-type="audio" />
 </template>
 
 <script lang="ts">
@@ -10,28 +10,29 @@ import { useFetch } from "@nuxtjs/composition-api"
 import { useMediaStore } from "~/stores/media"
 import { useSearchStore } from "~/stores/search"
 import { parseCollectionPath } from "~/utils/parse-collection-path"
-import { IMAGE } from "~/constants/media"
+import { AUDIO } from "~/constants/media"
 import { collectionMiddleware } from "~/middleware/collection"
 
 import VCollectionPage from "~/components/VCollectionPage.vue"
 
 export default defineNuxtComponent({
-  name: "VImageSourcePage",
+  name: "VAudioSourcePage",
   components: { VCollectionPage },
   layout: "content-layout",
   middleware: collectionMiddleware,
   setup() {
     const route = useRoute()
-    const collectionParams = parseCollectionPath(route.params.pathMatch)
+    const collectionParams = parseCollectionPath(route.params.source)
     if (!collectionParams) {
       throw new Error("Invalid collection path")
     }
-    useSearchStore().setCollectionState(collectionParams, IMAGE)
+    useSearchStore().setCollectionState(collectionParams, AUDIO)
     const mediaStore = useMediaStore()
 
     useFetch(async () => {
       await mediaStore.fetchMedia()
     })
+
     return {}
   },
 })
--- frontend/src/pages/image/source/[...source].vue
+++ frontend/src/pages/image/source/[...source].vue
@@ -1,5 +1,5 @@
 <template>
-  <VCollectionPage media-type="audio" />
+  <VCollectionPage media-type="image" />
 </template>
 
 <script lang="ts">
@@ -10,29 +10,28 @@ import { useFetch } from "@nuxtjs/composition-api"
 import { useMediaStore } from "~/stores/media"
 import { useSearchStore } from "~/stores/search"
 import { parseCollectionPath } from "~/utils/parse-collection-path"
-import { AUDIO } from "~/constants/media"
+import { IMAGE } from "~/constants/media"
 import { collectionMiddleware } from "~/middleware/collection"
 
 import VCollectionPage from "~/components/VCollectionPage.vue"
 
 export default defineNuxtComponent({
-  name: "VAudioSourcePage",
+  name: "VImageSourcePage",
   components: { VCollectionPage },
   layout: "content-layout",
   middleware: collectionMiddleware,
   setup() {
     const route = useRoute()
-    const collectionParams = parseCollectionPath(route.params.pathMatch)
+    const collectionParams = parseCollectionPath(route.params.source)
     if (!collectionParams) {
       throw new Error("Invalid collection path")
     }
-    useSearchStore().setCollectionState(collectionParams, AUDIO)
+    useSearchStore().setCollectionState(collectionParams, IMAGE)
     const mediaStore = useMediaStore()
 
     useFetch(async () => {
       await mediaStore.fetchMedia()
     })
-
     return {}
   },
 })
