7331fd6850a2805836f4786f97aad916a22f74ff
gmottajr <jrmottagerson@gmail.com>
2023-12-21 04:50:44 +0000

    cleaning commit

--- codeowners-validator.sh
+++ codeowners-validator.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v "$(pwd):/repo" -e REPOSITORY_PATH=/repo -e OWNERCHECKER_REPOSITORY=/repo gmottajr/codeowners-validator:latest
+docker run --rm -v "$(pwd):/repo" -e REPOSITORY_PATH=/repo -e OWNERCHECKER_REPOSITORY=/repo mszostok/codeowners-validator
