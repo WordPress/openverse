62eea03525e72406a0db9c0be67ee8ecf2629af3
Olga Bulat <obulat@gmail.com>
2023-12-22 08:51:39 +0300

    Cookies types
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/utils/cookies.ts
+++ frontend/src/utils/cookies.ts
@@ -1,8 +1,7 @@
 import { isProd } from "~/utils/node-env"
+import { useCookie } from "#imports"
 
-import type { CookieSerializeOptions } from "cookie"
-
-export const cookieOptions: CookieSerializeOptions = {
+export const cookieOptions: Parameters<typeof useCookie>[1] = {
   path: "/",
   sameSite: "strict",
   maxAge: 60 * 60 * 24 * 60, // 60 days; Makes the cookie persistent.
