69ac02216741f705e1e6f350a6731f5ff966f3a9
Olga Bulat <obulat@gmail.com>
2024-01-03 09:46:23 +0300

    Rename pages with dynamic parameters
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/pages/image/source/[...source].vue
+++ frontend/src/pages/image/source/[...source].vue
@@ -1,5 +1,5 @@
 <template>
-  <VCollectionPage media-type="audio" />
+  <VCollectionPage media-type="image" />
 </template>
 
 <script lang="ts">
@@ -8,29 +8,28 @@ import { defineComponent, useFetch, useRoute } from "@nuxtjs/composition-api"
 import { useMediaStore } from "~/stores/media"
 import { useSearchStore } from "~/stores/search"
 import { parseCollectionPath } from "~/utils/parse-collection-path"
-import { AUDIO } from "~/constants/media"
+import { IMAGE } from "~/constants/media"
 import { collectionMiddleware } from "~/middleware/collection"
 
 import VCollectionPage from "~/components/VCollectionPage.vue"
 
 export default defineComponent({
-  name: "VAudioSourcePage",
+  name: "VImageSourcePage",
   components: { VCollectionPage },
   layout: "content-layout",
   middleware: collectionMiddleware,
   setup() {
     const route = useRoute()
-    const collectionParams = parseCollectionPath(route.value.params.pathMatch)
+    const collectionParams = parseCollectionPath(route.value.params.source)
     if (!collectionParams) {
       throw new Error("Invalid collection path")
     }
-    useSearchStore().setCollectionState(collectionParams, AUDIO)
+    useSearchStore().setCollectionState(collectionParams, IMAGE)
     const mediaStore = useMediaStore()
 
     useFetch(async () => {
       await mediaStore.fetchMedia()
     })
-
     return {}
   },
 })
