4e4ee3730d0fb110f3dbfcc2736e8b8868d147b7
Olga Bulat <obulat@gmail.com>
2024-01-03 18:13:43 +0300

    Fix isServer and isClient types
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/utils/node-env.ts
+++ frontend/src/utils/node-env.ts
@@ -1,5 +1,5 @@
 export const isDev: boolean = process.env.NODE_ENV === "development"
 export const isTest: boolean = process.env.NODE_ENV === "test"
 export const isProd: boolean = process.env.NODE_ENV === "production"
-export const isServer: boolean = process.server
-export const isClient: boolean = process.client
+export const isServer: boolean = process.server ?? false
+export const isClient: boolean = process.client ?? false
