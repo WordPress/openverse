76838f387bff5e654cbf385e6dc9e0e367bb64a6
Staci Cooper <staci.cooper@automattic.com>
2024-01-03 16:05:35 -0800

    Add merge index tests

---
+++ catalog/tests/dags/es/create_new_es_index/test_create_new_es_index.py
@@ -0,0 +1,87 @@
+from es.create_new_es_index.create_new_es_index import merge_index_configurations
+
+
+def test_merge_index_configurations():
+    """Test merge_index_configurations with realistic data."""
+    current_index_config = {
+        "aliases": {"audio": {}},
+        "mappings": {
+            "dynamic": "false",
+            "properties": {"authority_boost": {"type": "rank_feature"}},
+        },
+        "settings": {
+            "index": {
+                "routing": {
+                    "allocation": {"include": {"_tier_preference": "data_content"}}
+                },
+                "refresh_interval": "-1",
+                "number_of_shards": "1",
+                "provided_name": "audio-init",
+                "creation_date": "1704314839358",
+                "analysis": {
+                    "filter": {
+                        "stem_overrides": {
+                            "type": "stemmer_override",
+                            "rules": ["animals => animal", "animal => animal"],
+                        },
+                    },
+                    "analyzer": {"tokenizer": "standard"},
+                },
+                "number_of_replicas": "1",
+                "uuid": "fHs6A-7oSpOmutQLgWA0sw",
+                "version": {"created": "8080299"},
+            }
+        },
+    }
+
+    # New index config contains changes to some keys, including nested keys.
+    new_index_config = {
+        "settings": {
+            "index": {
+                "routing": {"allocation": {"include": {"_tier_preference": "foo"}}},
+                "refresh_interval": "9999",
+                "number_of_shards": "9999",
+                "analysis": {
+                    "filter": {
+                        "stem_overrides": {
+                            "type": "stemmer_override",
+                            "rules": [
+                                "foo => bar",
+                            ],
+                        },
+                    }
+                },
+            }
+        }
+    }
+
+    # Aliases and excluded settings dropped, changes merged.
+    expected_merged_config = {
+        "mappings": {
+            "dynamic": "false",
+            "properties": {"authority_boost": {"type": "rank_feature"}},
+        },
+        "settings": {
+            "index": {
+                "routing": {"allocation": {"include": {"_tier_preference": "foo"}}},
+                "refresh_interval": "9999",
+                "number_of_shards": "9999",
+                "analysis": {
+                    "filter": {
+                        "stem_overrides": {
+                            "type": "stemmer_override",
+                            "rules": [
+                                "foo => bar",
+                            ],
+                        },
+                    },
+                    "analyzer": {"tokenizer": "standard"},
+                },
+                "number_of_replicas": "1",
+            }
+        },
+    }
+    actual_merged_config = merge_index_configurations.function(
+        new_index_config, current_index_config
+    )
+    assert actual_merged_config == expected_merged_config
