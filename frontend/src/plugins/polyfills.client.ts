4f49265e1aafdf0c04e8f4f9c32c0efae7da24a7
Olga Bulat <obulat@gmail.com>
2024-01-05 06:49:51 +0300

    Add export to pluging
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/plugins/polyfills.client.ts
+++ frontend/src/plugins/polyfills.client.ts
@@ -3,3 +3,6 @@
 // Purpose: Sentry issue for issue with Chrome < 69 and our useStorage composable:
 // https:openverse.sentry.io/share/issue/2b2635c85f1c4cecbe7560df5d4892b5/
 import "core-js/actual/array/flat-map"
+import { defineNuxtPlugin } from "#imports"
+
+export default defineNuxtPlugin(() => {})
