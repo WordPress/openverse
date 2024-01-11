4bc02fd08680c2d21890f5b8f1dc4e1ca9d682f3
Olga Bulat <obulat@gmail.com>
2024-01-11 14:20:42 +0300

    Add photos redirect
    Signed-off-by: Olga Bulat <obulat@gmail.com>

---
+++ frontend/src/pages/photos/[id].vue
@@ -0,0 +1,19 @@
+<template><div></div></template>
+<script lang="ts">
+import { defineComponent, definePageMeta, navigateTo } from "#imports"
+
+export default defineComponent({
+  name: "PhotosRedirect",
+  setup() {
+    definePageMeta({
+      middleware: (to) => {
+        const localePrefix = to.path.startsWith("/photos/")
+          ? ""
+          : `/${to.path.split("/")[1]}`
+        const redirectUrl = `${localePrefix}/image/${to.params.id}`
+        return navigateTo(redirectUrl, { redirectCode: 301 })
+      },
+    })
+  },
+})
+</script>
