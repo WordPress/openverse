c16ecf4210af7a9986b54a94f470a2884ff864ef
Olga Bulat <obulat@gmail.com>
2024-01-19 17:22:19 +0300

    Handle case when there are no providers
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/server.prepare.ts
+++ frontend/server.prepare.ts
@@ -6,8 +6,8 @@ import { sortProviders } from "./src/utils/provider"
 export default defineNuxtPrepareHandler(async () => {
   const apiUrl =
     process.env.NUXT_PUBLIC_API_URL || "https://api.openverse.engineering/"
-  const audioProviders = await $fetch(`${apiUrl}v1/audio/stats/`)
-  const imageProviders = await $fetch(`${apiUrl}v1/images/stats/`)
+  const audioProviders = (await $fetch(`${apiUrl}v1/audio/stats/`)) ?? []
+  const imageProviders = (await $fetch(`${apiUrl}v1/images/stats/`)) ?? []
 
   return {
     ok: true,
--- frontend/src/utils/provider.ts
+++ frontend/src/utils/provider.ts
@@ -5,7 +5,10 @@ import type { MediaProvider } from "~/types/media-provider"
  * @param data - initial unordered list of providers
  */
 export const sortProviders = (data: MediaProvider[]): MediaProvider[] => {
-  return [...data].sort((sourceObjectA, sourceObjectB) => {
+  if (!data.length || !Array.isArray(data)) {
+    return []
+  }
+  return data.sort((sourceObjectA, sourceObjectB) => {
     const nameA = sourceObjectA.source_name.toUpperCase()
     const nameB = sourceObjectB.source_name.toUpperCase()
     return nameA.localeCompare(nameB)
