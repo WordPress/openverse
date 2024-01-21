c6bff94ac98837b8f34c7167fd5446eb2cc23c3d
ngken0995 <ngken0995@gmail.com>
2024-01-20 22:55:32 -0500

    testing pickle

--- api/test/test_auth.py
+++ api/test/test_auth.py
@@ -1,15 +1,12 @@
 import time
 import uuid
-from test.constants import API_URL
 
-from django.urls import reverse
 from django.utils.http import urlencode
 
 import pytest
-from oauth2_provider.models import AccessToken
 
-from api.models import OAuth2Verification, ThrottledApplication
 
+import pickle
 
 cache_availability_params = pytest.mark.parametrize(
     "is_cache_reachable, cache_name",
@@ -77,138 +74,23 @@ def test_auth_token_exchange(client, test_auth_tokens_registration):
 
 
 @pytest.mark.django_db
-def test_auth_token_exchange_unsupported_method(client):
-    res = client.get(
-        "/v1/auth_tokens/token/",
-        verify=False,
-    )
-    assert res.status_code == 405
-    assert res.json()["detail"] == 'Method "GET" not allowed.'
-
-
-def _integration_verify_most_recent_token(client):
-    verify = OAuth2Verification.objects.last()
-    code = verify.code
-    path = reverse("verify-email", args=[code])
-    return client.get(path)
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    "rate_limit_model",
-    [x[0] for x in ThrottledApplication.RATE_LIMIT_MODELS],
-)
-@cache_availability_params
-@pytest.mark.skipif(
-    API_URL != "http://localhost:8000",
-    reason=(
-        "This test needs to cheat by looking in the database,"
-        " so it needs to skip in non-local environments where"
-        " that isn't possible."
-    ),
-)
-def test_auth_email_verification(
-    request,
-    client,
-    is_cache_reachable,
-    cache_name,
-    rate_limit_model,
-    test_auth_token_exchange,
-):
-    res = _integration_verify_most_recent_token(client)
-    assert res.status_code == 200
-    test_auth_rate_limit_reporting(
-        request,
-        client,
-        is_cache_reachable,
-        cache_name,
-        rate_limit_model,
-        test_auth_token_exchange,
-        verified=True,
-    )
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    "rate_limit_model",
-    [x[0] for x in ThrottledApplication.RATE_LIMIT_MODELS],
-)
-@cache_availability_params
-def test_auth_rate_limit_reporting(
-    request,
-    client,
-    is_cache_reachable,
-    cache_name,
-    rate_limit_model,
-    test_auth_token_exchange,
-    verified=False,
-):
-    request.getfixturevalue(cache_name)
-
-    # We're anonymous still, so we need to wait a second before exchanging
-    # the token.
-    time.sleep(1)
-    token = test_auth_token_exchange["access_token"]
-    application = AccessToken.objects.get(token=token).application
-    application.rate_limit_model = rate_limit_model
-    application.save()
-    res = client.get("/v1/rate_limit/", HTTP_AUTHORIZATION=f"Bearer {token}")
-    res_data = res.json()
-    if is_cache_reachable:
-        assert res.status_code == 200
-    else:
-        assert res.status_code == 424
-        assert res_data["requests_this_minute"] is None
-        assert res_data["requests_today"] is None
-
-    if verified:
-        assert res_data["rate_limit_model"] == rate_limit_model
-        assert res_data["verified"] is True
-    else:
-        assert res_data["rate_limit_model"] == rate_limit_model
-        assert res_data["verified"] is False
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    "verified",
-    (True, False),
-)
-def test_auth_response_headers(
-    client, verified, test_auth_tokens_registration, test_auth_token_exchange
-):
-    if verified:
-        _integration_verify_most_recent_token(client)
-
-    token = test_auth_token_exchange["access_token"]
-
-    res = client.get("/v1/images/", HTTP_AUTHORIZATION=f"Bearer {token}")
-
-    assert (
-        res.headers["x-ov-client-application-name"]
-        == test_auth_tokens_registration["name"]
-    )
-    assert res.headers["x-ov-client-application-verified"] == str(verified)
-
-
-def test_unauthed_response_headers(client):
-    res = client.get("/v1/images")
-
-    assert "x-ov-client-application-name" not in res.headers
-    assert "x-ov-client-application-verified" not in res.headers
-
-
-@pytest.mark.django_db
 @pytest.mark.parametrize(
     "sort_dir, exp_indexed_on",
     [
         ("desc", "2022-12-31"),
-        ("asc", "2022-01-01"),
+        # ("asc", "2022-01-01"),
     ],
 )
 def test_sorting_authed(
     client, monkeypatch, test_auth_token_exchange, sort_dir, exp_indexed_on
 ):
+    with open("test.pickle", "rb") as inp:
+        tech_companies = pickle.load(inp)
+
+    monkeypatch.setattr(
+        "api.api.views.media_views.MediaViewSet.get_db_results", tech_companies
+    )
+
     time.sleep(1)
     token = test_auth_token_exchange["access_token"]
     query_params = {"unstable__sort_by": "indexed_on", "unstable__sort_dir": sort_dir}
@@ -218,53 +100,3 @@ def test_sorting_authed(
     res_data = res.json()
     indexed_on = res_data["results"][0]["indexed_on"][:10]  # ``indexed_on`` is ISO.
     assert indexed_on == exp_indexed_on
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    "authority_boost, exp_source",
-    [
-        ("1.0", "stocksnap"),
-        ("0.0", "flickr"),  # Authority boost is disabled
-    ],
-)
-def test_authority_authed(
-    client, monkeypatch, test_auth_token_exchange, authority_boost, exp_source
-):
-    monkeypatch.setattr("api.views.media_views.MediaViewSet.get_db_results", False)
-    time.sleep(1)
-    token = test_auth_token_exchange["access_token"]
-    query_params = {
-        "q": "cat",
-        "unstable__authority": "true",
-        "unstable__authority_boost": authority_boost,
-    }
-    res = client.get("/v1/images/", query_params, HTTP_AUTHORIZATION=f"Bearer {token}")
-    assert res.status_code == 200
-
-    res_data = res.json()
-    source = res_data["results"][0]["source"]
-    assert source == exp_source
-
-
-@pytest.mark.django_db
-def test_page_size_limit_unauthed(client):
-    query_params = {"page_size": 20}
-    res = client.get("/v1/images/", query_params)
-    assert res.status_code == 200
-    query_params["page_size"] = 21
-    res = client.get("/v1/images/", query_params)
-    assert res.status_code == 401
-
-
-@pytest.mark.django_db
-def test_page_size_limit_authed(client, test_auth_token_exchange):
-    time.sleep(1)
-    token = test_auth_token_exchange["access_token"]
-    query_params = {"page_size": 21}
-    res = client.get("/v1/images/", query_params, HTTP_AUTHORIZATION=f"Bearer {token}")
-    assert res.status_code == 200
-
-    query_params = {"page_size": 500}
-    res = client.get("/v1/images/", query_params, HTTP_AUTHORIZATION=f"Bearer {token}")
-    assert res.status_code == 200
