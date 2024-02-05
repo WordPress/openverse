e2649b2e0e21c7570eff514c5dff45a0bbe3d29f
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2024-02-05 17:04:51 +1100

    Add shim for tests

--- frontend/test/unit/setup-after-env.js
+++ frontend/test/unit/setup-after-env.js
@@ -5,6 +5,8 @@ import failOnConsole from "jest-fail-on-console"
 
 import { i18n } from "~~/test/unit/test-utils/i18n"
 
+import { normalizeFetchingError } from "~/plugins/errors"
+
 failOnConsole()
 
 Vue.prototype.$nuxt = {
@@ -15,5 +17,6 @@ Vue.prototype.$nuxt = {
     },
     // i18n returned by `useI18n` composable (`useContext().i18n`)
     i18n,
+    $processFetchingError: jest.fn(normalizeFetchingError),
   },
 }
