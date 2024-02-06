fb6b602c3f7e9bc24f38bde9c756a3e880278790
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2024-02-06 12:29:50 +1100

    Add healthcheck dag tests

---
+++ catalog/tests/dags/elasticsearch_cluster/test_healthcheck_dag.py
@@ -0,0 +1,99 @@
+import pytest
+
+from elasticsearch_cluster.healthcheck_dag import compose_notification
+
+
+_TEST_ENV = "testing_environment"
+
+
+def _make_response_body(**kwargs):
+    # Default values based on real response from Openverse staging cluster
+    # Only the `cluster_name` is changed to reflect the environment
+    return {
+        "cluster_name": "testcluster",
+        "status": "green",
+        "timed_out": False,
+        "number_of_nodes": 6,
+        "number_of_data_nodes": 3,
+        "active_primary_shards": 51,
+        "active_shards": 103,
+        "relocating_shards": 0,
+        "initializing_shards": 0,
+        "unassigned_shards": 0,
+        "delayed_unassigned_shards": 0,
+        "number_of_pending_tasks": 0,
+        "number_of_in_flight_fetch": 0,
+        "task_max_waiting_in_queue_millis": 0,
+        "active_shards_percent_as_number": 100,
+    } | kwargs
+
+
+def _missing_node_keys(master_nodes: int, data_nodes: int):
+    total_nodes = master_nodes + data_nodes
+    return (
+        f"Elasticsearch {_TEST_ENV} cluster node count is **{total_nodes}**",
+        "Expected 6 total nodes.",
+        f"Master nodes: **{master_nodes}** of expected 3",
+        f"Data nodes: **{data_nodes}** of expected 3",
+    )
+
+
+@pytest.mark.parametrize(
+    ("expected_message_type", "message_keys", "cluster_health_response"),
+    (
+        pytest.param(
+            "alert",
+            (f"Elasticsearch {_TEST_ENV} cluster status is **red**",),
+            _make_response_body(status="red"),
+            id="red-status",
+        ),
+        pytest.param(
+            "alert",
+            _missing_node_keys(master_nodes=3, data_nodes=2),
+            _make_response_body(
+                status="yellow",
+                number_of_nodes=5,
+                number_of_data_nodes=2,
+            ),
+            id="missing-data-node",
+        ),
+        pytest.param(
+            "alert",
+            _missing_node_keys(master_nodes=2, data_nodes=3),
+            _make_response_body(
+                status="yellow",
+                number_of_nodes=5,
+                number_of_data_nodes=3,
+            ),
+            id="missing-master-node",
+        ),
+        pytest.param(
+            "alert",
+            _missing_node_keys(master_nodes=1, data_nodes=2),
+            _make_response_body(
+                status="yellow",
+                number_of_nodes=3,
+                number_of_data_nodes=2,
+            ),
+            id="missing-some-of-both-node-types",
+        ),
+        pytest.param(
+            "notification",
+            (f"Elasticsearch {_TEST_ENV} cluster health is **yellow**.",),
+            _make_response_body(
+                status="yellow",
+            ),
+            id="yellow-status-all-nodes-present",
+        ),
+    ),
+)
+def test_compose_notification(
+    expected_message_type, message_keys, cluster_health_response
+):
+    message_type, message = compose_notification.function(
+        _TEST_ENV, cluster_health_response
+    )
+
+    assert message_type == expected_message_type
+    for message_key in message_keys:
+        assert message_key in message
