b60807b213a39070040f1ff5719fde5cab05c789
Staci Cooper <staci.cooper@automattic.com>
2024-02-06 14:43:05 -0800

    Add new dag

---
+++ catalog/dags/elasticsearch_cluster/create_proportional_by_source_staging_index/create_proportional_by_source_staging_index.py
@@ -0,0 +1,141 @@
+import logging
+import math
+
+from airflow.decorators import task, task_group
+from airflow.providers.http.operators.http import HttpOperator
+from requests import Response
+
+from common.constants import (
+    API_HTTP_CONN_ID,
+    MEDIA_RESOURCE_SLUGS,
+)
+from common.elasticsearch import remove_excluded_index_settings
+
+
+logger = logging.getLogger(__name__)
+
+
+@task
+def get_source_index(source_index: str, media_type: str):
+    """
+    Get the desired source index. If a source_index was passed in
+    params, we use that; else we default to the filtered media index.
+    """
+    return source_index or f"{media_type}-filtered"
+
+
+@task
+def get_destination_index_name(
+    media_type: str, current_datetime_str: str, percentage_of_prod: int
+):
+    """Get the desired name for the destination index."""
+    percentage_str = round(percentage_of_prod * 100)
+
+    return (
+        f"{media_type}-{percentage_str}-percent-of-providers"
+        f"-{current_datetime_str.lower()}"
+    )
+
+
+@task
+def get_destination_alias(media_type: str):
+    return f"{media_type}-subset-by-source"
+
+
+@task
+def get_destination_index_config(source_config: dict, destination_index_name: str):
+    """
+    Build the index configuration for the destination index, based on the
+    source index configuration.
+    """
+    destination_config = remove_excluded_index_settings(source_config)
+
+    # Apply the desired index name
+    destination_config["index"] = destination_index_name
+    return destination_config
+
+
+@task_group(group_id="get_production_source_counts")
+def get_production_source_counts(media_type: str) -> HttpOperator:
+    """
+    Get the count of records per source for the given media type in the
+    production media index.
+
+    Currently, the counts are taken from the main media index. In the
+    future, this should be updated to actually query production elasticsearch
+    for the counts from the `source_index` (which may be different, for
+    for example the filtered media index).
+    """
+
+    def response_filter_source_counts(response: Response) -> dict[str, int]:
+        """
+        Handle the response for the `get_production_source_counts` task.
+
+        Extracts a dictionary mapping source names to their record count.
+        """
+        data = response.json()
+        return {item["source_name"]: item["media_count"] for item in data}
+
+    @task
+    def get_media_stats_endpoint(media_type):
+        """
+        Get the stats endpoint for the given media type, from which to get source
+        information. It is necessary to split this out into a separate task
+        because the `media_type` Param can only be accessed by template in the
+        HttpOperator, but we need to look up the appropriate media resource
+        slug which may be different (e.g. "images" rather than "image")
+        """
+        return f"{MEDIA_RESOURCE_SLUGS.get(media_type)}/stats"
+
+    endpoint = get_media_stats_endpoint(media_type)
+
+    source_counts = HttpOperator(
+        task_id="get_source_counts",
+        http_conn_id=API_HTTP_CONN_ID,
+        endpoint=endpoint,
+        method="GET",
+        response_check=lambda response: response.status_code == 200,
+        response_filter=response_filter_source_counts,
+    )
+
+    # Return the output from the task group so that it can be used in
+    # subsequent tasks
+    return source_counts.output
+
+
+@task
+def get_proportional_source_count_kwargs(
+    production_source_counts: dict[str, int], percentage_of_prod: int
+):
+    """
+    Return a list of kwargs for each mapped task to reindex the
+    documents for each source individually.
+
+    For each task we will have:
+    * `max_docs`: The count of records for this source needed in the new
+                  index in order for the source to make up the same
+                  proportion of the new index as it does in the
+                  production unfiltered media index.
+    * `query`:    An elasticsearch query that will be used to restrict
+                  the reindexing task to records from this source.
+    """
+    # Given the record counts for each source in production, determine
+    # what proportion of the production total each source represents
+    production_total = sum(production_source_counts.values())
+    production_source_proportions = {
+        source: production_count / production_total
+        for source, production_count in production_source_counts.items()
+    }
+
+    # The total number of records in the proportional subset index
+    subset_total = math.floor(production_total * percentage_of_prod)
+
+    # Return a list of kwargs that will be passed to the mapped reindex
+    # tasks, for reindexing each source into the new index.
+    return [
+        {
+            "max_docs": round(subset_total * source_proportion),
+            "query": {"term": {"source.keyword": source}},
+        }
+        for source, source_proportion in production_source_proportions.items()
+    ]
---
+++ catalog/dags/elasticsearch_cluster/create_proportional_by_source_staging_index/create_proportional_by_source_staging_index_dag.py
@@ -0,0 +1,189 @@
+"""
+# Create Proportional By Source Staging Index DAG
+
+This DAG is used to create a new staging Elasticsearch index that is a subset
+of a production source index, such that the proportions of records by source is
+equal to the proportions of records by source in the provider API.
+
+Note that proportions match the proportions by source in the unfiltered media
+index, not necessarily the source index which may differ. The size of the
+new index is also calculated based on a percentage of the unfiltered media index,
+rather than the source index.
+
+
+Required Dagrun Configuration parameters:
+
+* media_type:         The media type for which to create a new index.
+* percentage_of_prod: A float indicating the proportion of items to take from each
+                      source from the total amount existing in the production
+                      source index
+
+Optional params:
+
+* source_index: An existing production Elasticsearch index to use as the basis for
+                the new index. If not provided, the index aliased to
+                `<media_type>-filtered` will be used.
+
+## When this DAG runs
+
+This DAG is on a `None` schedule and is run manually.
+
+## Race conditions
+
+Because this DAG runs on the staging ingestion server and staging elasticsearch
+cluster, it does _not_ interfere with the `data_refresh` or
+`create_filtered_index` DAGs.
+
+However, as the DAG operates on the staging API database it will exit
+immediately if any of the following DAGs are running:
+* `staging_database_restore`
+* `recreate_full_staging_index`
+* `create_new_staging_es_index`
+"""
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag
+from airflow.models import Variable
+from airflow.models.param import Param
+
+from common import elasticsearch as es
+from common import slack
+from common.constants import (
+    AUDIO,
+    DAG_DEFAULT_ARGS,
+    MEDIA_TYPES,
+    STAGING,
+)
+from common.sensors.utils import prevent_concurrency_with_dags
+from database.staging_database_restore.constants import (
+    DAG_ID as STAGING_DB_RESTORE_DAG_ID,
+)
+from elasticsearch_cluster.create_new_es_index.create_new_es_index_types import (
+    CREATE_NEW_INDEX_CONFIGS,
+)
+from elasticsearch_cluster.create_proportional_by_source_staging_index import (
+    create_proportional_by_source_staging_index as create_index,
+)
+from elasticsearch_cluster.recreate_staging_index.recreate_full_staging_index import (
+    DAG_ID as RECREATE_STAGING_INDEX_DAG_ID,
+)
+
+
+DAG_ID = "create_proportional_by_source_staging_index"
+
+
+@dag(
+    dag_id=DAG_ID,
+    default_args=DAG_DEFAULT_ARGS,
+    schedule=None,
+    start_date=datetime(2024, 1, 31),
+    tags=["database", "elasticsearch"],
+    max_active_runs=1,
+    catchup=False,
+    doc_md=__doc__,
+    params={
+        "media_type": Param(
+            default=AUDIO,
+            enum=MEDIA_TYPES,
+            description="The media type for which to create the index.",
+        ),
+        "percentage_of_prod": Param(
+            default=0.5,
+            type="number",
+            minimum=0,
+            maximum=1,
+            description=(
+                "The proportion of items to take of each provider from"
+                " the total amount existing in the source index."
+            ),
+        ),
+        "source_index": Param(
+            default=None,
+            type=["string", "null"],
+            description=(
+                "Optionally, the existing production Elasticsearch index"
+                " to use as the basis for the new index. If not provided,"
+                " the index aliased to `<media_type>-filtered` will be used."
+            ),
+        ),
+    },
+    render_template_as_native_obj=True,
+)
+def create_proportional_by_source_staging_index():
+    # Fail early if any conflicting DAGs are running
+    prevent_concurrency = prevent_concurrency_with_dags(
+        external_dag_ids=[
+            STAGING_DB_RESTORE_DAG_ID,
+            RECREATE_STAGING_INDEX_DAG_ID,
+            CREATE_NEW_INDEX_CONFIGS[STAGING].dag_id,
+        ]
+    )
+
+    es_host = es.get_es_host(environment=STAGING)
+
+    source_index_name = create_index.get_source_index(
+        source_index="{{ params.source_index }}",
+        media_type="{{ params.media_type }}",
+    )
+
+    source_config = es.get_index_configuration(
+        source_index=source_index_name, es_host=es_host
+    )
+
+    destination_index_name = create_index.get_destination_index_name(
+        media_type="{{ params.media_type }}",
+        current_datetime_str="{{ ts_nodash }}",
+        percentage_of_prod="{{ params.percentage_of_prod }}",
+    )
+
+    destination_alias = create_index.get_destination_alias(
+        media_type="{{ params.media_type }}"
+    )
+
+    destination_index_config = create_index.get_destination_index_config(
+        source_config=source_config, destination_index_name=destination_index_name
+    )
+
+    new_index = es.create_index(index_config=destination_index_config, es_host=es_host)
+
+    production_source_counts = create_index.get_production_source_counts(
+        media_type="{{ params.media_type }}",
+    )
+
+    desired_source_counts = create_index.get_proportional_source_count_kwargs.override(
+        task_id="get_desired_source_counts"
+    )(
+        production_source_counts=production_source_counts,
+        percentage_of_prod="{{ params.percentage_of_prod }}",
+    )
+
+    reindex = es.trigger_and_wait_for_reindex.partial(
+        destination_index=destination_index_name,
+        source_index=source_index_name,
+        timeout=timedelta(hours=12),
+        requests_per_second=Variable.get(
+            "ES_INDEX_THROTTLING_RATE", 20_000, deserialize_json=True
+        ),
+        es_host=es_host,
+    ).expand_kwargs(desired_source_counts)
+
+    point_alias = es.point_alias(
+        index_name=destination_index_name, alias=destination_alias, es_host=es_host
+    )
+
+    notify_completion = slack.notify_slack(
+        text=f"Reindexing complete for {destination_index_name}.",
+        dag_id=DAG_ID,
+        username="Proportional by Source Staging Index Creation",
+        icon_emoji=":elasticsearch:",
+    )
+
+    # Setup additional dependencies
+    prevent_concurrency >> es_host
+    es_host >> [source_index_name, destination_index_name, destination_alias]
+    production_source_counts >> desired_source_counts
+    new_index >> production_source_counts
+    reindex >> point_alias >> notify_completion
+
+
+create_proportional_by_source_staging_index()
