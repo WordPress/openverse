0c92ab5834634525d44660dc1e1f6e1af8d04c7e
Olga Bulat <obulat@gmail.com>
2024-02-13 07:32:37 +0300

    Add links to additional views to sources table (#3749)
    * Add links to additional views to sources table
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add an e2e test
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Use Flickr to reuse the existing tape
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Fix test locator
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    ---------
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>

---
+++ frontend/test/playwright/e2e/sources.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "@playwright/test"
+
+import { getH1 } from "~~/test/playwright/utils/components"
+import { preparePageForTests } from "~~/test/playwright/utils/navigation"
+
+test.describe.configure({ mode: "parallel" })
+
+test("sources table has links to source pages", async ({ page }) => {
+  await preparePageForTests(page, "xl", {
+    features: { additional_search_views: "on" },
+  })
+  await page.goto("/sources")
+  await page
+    .getByRole("cell", { name: "Flickr", exact: true })
+    .getByRole("link")
+    .click()
+  await page.waitForURL("/image/source/flickr")
+
+  await expect(getH1(page, "Flickr")).toBeVisible()
+})
