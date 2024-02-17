ce6f7b7a1509aebf602e01255ee01f54dab86b3c
Staci Cooper <staci.cooper@automattic.com>
2024-02-16 16:30:09 -0800

    Use the connection URI instead of host

--- catalog/dags/elasticsearch_cluster/shared.py
+++ catalog/dags/elasticsearch_cluster/shared.py
@@ -8,4 +8,4 @@ from common.constants import Environment
 @task
 def get_es_host(environment: Environment) -> XComArg:
     conn = Connection.get_connection_from_secrets(f"elasticsearch_http_{environment}")
-    return conn.host
+    return conn.get_uri()
