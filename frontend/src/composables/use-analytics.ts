c743833222253f860302f6bda941a463df5279c6
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2024-02-28 09:49:27 +1100

    Add deprecation and explanation notice to composable
    Co-authored-by: Zack Krida <6351754+zackkrida@users.noreply.github.com>

--- frontend/src/composables/use-analytics.ts
+++ frontend/src/composables/use-analytics.ts
@@ -1,5 +1,11 @@
 import { useContext } from "@nuxtjs/composition-api"
 
+/**
+ * This wrapper around the plugin, retained to reduce code churn.
+ * @see Refer to frontend/src/plugins/analytics.ts for plugin implementation
+ *
+ * @deprecated For new code, use `$sendCustomEvent` from Nuxt context
+ */
 export const useAnalytics = () => {
   const { $sendCustomEvent } = useContext()
 
