1e184bca2920f67907bb745cabffce465b094c27
Madison Swain-Bowden <bowdenm@spu.edu>
2024-03-13 16:27:31 -0700

    Add string name for model, and create prefs on user creation

--- api/api/models/moderation.py
+++ api/api/models/moderation.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserPreferences(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     preferences = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f"{self.user.username}'s preferences"
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserPreferences.objects.create(user=instance)
+    instance.userpreferences.save()
