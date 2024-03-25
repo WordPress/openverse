4807c7967de7b7add7357723fd06fc61735a4c7d
zackdotcat <git@zack.cat>
2024-03-25 12:26:32 -0400

    Remove unused colors

--- frontend/src/components/TableSortIcon.vue
+++ frontend/src/components/TableSortIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col" :class="[active ? 'text-dark-blue' : 'text-gray']">
+  <div class="flex flex-col" :class="[active ? 'text-black' : 'text-gray']">
     <svg
       width="12"
       height="6"
