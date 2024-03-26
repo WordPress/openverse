a6cb9079463575df9370b1ec194ddad8cc88b362
zackdotcat <git@zack.cat>
2024-03-26 12:31:28 -0400

    Address flake in v-language-select.spec.ts

--- frontend/test/storybook/visual-regression/v-language-select.spec.ts
+++ frontend/test/storybook/visual-regression/v-language-select.spec.ts
@@ -3,7 +3,10 @@ import { Page, test, expect } from "@playwright/test"
 import { makeGotoWithArgs } from "~~/test/storybook/utils/args"
 
 const expectSnapshot = async (name: string, page: Page) => {
-  expect(await page.screenshot()).toMatchSnapshot({ name: `${name}.png` })
+  expect(await page.screenshot()).toMatchSnapshot({
+    name: `${name}.png`,
+    maxDiffPixelRatio: 0.01, // for flake and variability in the component width
+  })
 }
 
 test.describe("VLanguageSelect", () => {
