e5fce55df1deba3bae88275d9379009366af27fb
Olga Bulat <obulat@gmail.com>
2024-04-03 12:45:31 +0300

    Update frontend/src/utils/sentry-config.ts

--- frontend/src/utils/sentry-config.ts
+++ frontend/src/utils/sentry-config.ts
@@ -7,7 +7,7 @@ import type { ModuleOptions } from "@nuxtjs/sentry"
  */
 export const sentryConfig: ModuleOptions = {
   dsn: process.env.SENTRY_DSN,
-  logMockCalls: true,
+  logMockCalls: false,
   lazy: true,
   clientConfig: {
     // Only allow errors that come from openverse.org or a subdomain
