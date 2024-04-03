5655284d245a1ece275371b183951e303bc4833b
Olga Bulat <obulat@gmail.com>
2024-04-03 06:49:37 +0300

    Fix types
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/composables/use-collection-meta.ts
+++ frontend/src/composables/use-collection-meta.ts
@@ -1,11 +1,9 @@
 import { computed } from "vue"
 
-import { TranslateResult } from "vue-i18n"
-
-import type { SupportedMediaType } from "~/constants/media"
-import type { CollectionParams } from "~/types/search"
 import { useI18n } from "~/composables/use-i18n"
 import { useProviderStore } from "~/stores/provider"
+import type { SupportedMediaType } from "~/constants/media"
+import type { CollectionParams } from "~/types/search"
 
 import type { ComputedRef } from "vue"
 
@@ -20,13 +18,11 @@ export const useCollectionMeta = ({
 }) => {
   const pageTitle = computed(() => {
     // The default page title. It should be overwritten by the specific collection type.
-    let title: string | TranslateResult =
-      "Openly Licensed Images, Audio and More"
+    const title = "Openly Licensed Images, Audio and More"
 
     switch (collectionParams.value?.collection) {
       case "creator": {
-        title = collectionParams.value.creator
-        break
+        return `${collectionParams.value.creator} | Openverse`
       }
 
       case "source": {
@@ -34,17 +30,15 @@ export const useCollectionMeta = ({
           collectionParams.value.source,
           mediaType
         )
-        title = i18n.t(`collection.pageTitle.source.${mediaType}`, {
+        return `${i18n.t(`collection.pageTitle.source.${mediaType}`, {
           source: sourceName,
-        })
-        break
+        })} | Openverse`
       }
 
       case "tag": {
-        title = i18n.t(`collection.pageTitle.tag.${mediaType}`, {
+        return `${i18n.t(`collection.pageTitle.tag.${mediaType}`, {
           tag: collectionParams.value.tag,
-        })
-        break
+        })} | Openverse`
       }
     }
 
