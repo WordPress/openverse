d25fa3afa7b490fd69c55e0b7bb9232898833533
Olga Bulat <obulat@gmail.com>
2024-04-09 06:49:50 +0300

    Turn on additional search views frontend (#4043)
    * Switch the additional_search_views on
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update tests
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    ---------
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/test/unit/test-utils/i18n.js
+++ frontend/test/unit/test-utils/i18n.js
@@ -2,10 +2,12 @@ import VueI18n from "vue-i18n"
 
 const messages = require("~/locales/en.json")
 
-export const i18n = new VueI18n({
+const i18n = new VueI18n({
   locale: "en",
   fallbackLocale: "en",
   messages: {
     en: messages,
   },
 })
+i18n.localeProperties = { code: "en", dir: "ltr" }
+export { i18n }
