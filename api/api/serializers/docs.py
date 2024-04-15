06592367ce4c00346fbc0ccd39c33aa6fc8430b8
Olga Bulat <obulat@gmail.com>
2024-04-15 12:11:14 +0300

    Show the unstable params and improve collection docs (#4085)
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- api/api/serializers/docs.py
+++ api/api/serializers/docs.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 
-from api.constants.parameters import TAG
+from api.constants.parameters import COLLECTION, TAG
+
+
+UNSTABLE_WARNING = """
+\n\n_Caution: Parameters prefixed with `unstable__` are experimental and
+may change or be removed without notice in future updates. Use them
+with caution as they are not covered by our API versioning policy._\n\n
+"""
 
 
 CREATOR_COLLECTIONS_DISABLED = """
@@ -10,20 +17,20 @@ word `john`. If the value contains space, items that contain any of
 the words in the value will match. To search for several values,
 join them with a comma."""
 
-CREATOR = """
+CREATOR = f"""
 _When `q` parameter is present, `creator` parameter is ignored._
 
 **Creator collection**
-When used with `collection=creator&source=sourceName`, returns the collection of media
+When used with `{COLLECTION}=creator&source=sourceName`, returns the collection of media
 by the specified creator. Notice that a single creator's media items
 can be found on several sources, but this collection only returns the
 items from the specified source.
 This is why for this collection, both the creator and the source
 parameters are required, and matched exactly. For a fuzzy creator search,
-use the default search without the `collection` parameter.
+use the default search without the `{COLLECTION}` parameter.
 
 **Creator search**
-When used without the `collection` parameter, will search in the creator field only.
+When used without the `{COLLECTION}` parameter, will search in the creator field only.
 The search is fuzzy, so `creator=john` will match any value that includes the
 word `john`. If the value contains space, items that contain any of
 the words in the value will match. To search for several values,
@@ -34,9 +41,10 @@ CREATOR_HELP_TEXT = (
     CREATOR if settings.SHOW_COLLECTION_DOCS else CREATOR_COLLECTIONS_DISABLED
 )
 COLLECTION_HELP_TEXT = f"""
+{UNSTABLE_WARNING}
 The kind of media collection to return.
 
-Should be used with `{TAG}`, `source` or `creator`+`source`"""
+Must be used with `{TAG}`, `source` or `creator`+`source`"""
 
 EXCLUDED_SOURCE_HELP_TEXT = """
 A comma separated list of data sources to exclude from the search.
@@ -48,7 +56,7 @@ A comma separated list of data sources; valid values are
 
 SOURCE = """
 For default search, a comma separated list of data sources.
-When the `collection` parameter is used, this parameter only accepts a single source.
+When the `{collection_param}` parameter is used, this parameter only accepts a single source.
 
 Valid values are `source_name`s from the stats endpoint: {origin}/v1/{media_path}/stats/.
 """
@@ -57,8 +65,9 @@ SOURCE_HELP_TEXT = (
     SOURCE if settings.SHOW_COLLECTION_DOCS else SOURCE_HELP_TEXT_COLLECTIONS_DISABLED
 )
 
-TAG_HELP_TEXT = """
-_Must be used with `collection=tag`_
+TAG_HELP_TEXT = f"""
+{UNSTABLE_WARNING}
+_Must be used with `{COLLECTION}=tag`_
 
 Get the collection of media with a specific tag. Returns the collection of media
 that has the specified tag, matching exactly and entirely.
