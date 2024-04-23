c2f5d956d1d1beb83953088c235074f2686b9e4e
sarayourfriend <git@sarayourfriend.pictures>
2024-04-24 08:34:55 +1000

    Delete the archive directory and references to it

--- archive/openledger_sql/popularity/08_create_audio_standardized_popularity_func.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION standardized_audio_popularity(provider text, meta_data jsonb)
-RETURNS FLOAT AS $$
-  SELECT ($2->>metric)::FLOAT / (($2->>metric)::FLOAT + constant)
-  FROM audio_popularity_constants WHERE provider=$1;
-$$
-LANGUAGE SQL
-STABLE
-RETURNS NULL ON NULL INPUT;
