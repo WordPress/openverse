cde870a4a01fb5e92efefc06865874c522762557
zackdotcat <git@zack.cat>
2024-04-24 07:29:34 -0400

    Fix PR

--- frontend/src/server-middleware/robots.js
+++ frontend/src/server-middleware/robots.js
@@ -1,44 +1,23 @@
 const { LOCAL, PRODUCTION } = require("../constants/deploy-env")
 
-const AI_ROBOTS_CONTENT = `
-# Block known AI crawlers
-User-agent: GPTBot
-Disallow: /
+const deniedUserAgents = [
+  "GPTBot",
+  "CCBot",
+  "ChatGPT-User",
+  "Google-Extended",
+  "anthropic-ai",
+  "Omgilibot",
+  "Omgili",
+  "FacebookBot",
+  "Diffbot",
+  "Bytespider",
+  "ImagesiftBot",
+  "cohere-ai",
+]
 
-User-agent: CCBot
-Disallow: /
-
-User-agent: ChatGPT-User
-Disallow: /
-
-User-agent: Google-Extended
-Disallow: /
-
-User-agent: anthropic-ai
-Disallow: /
-
-User-agent: Omgilibot
-Disallow: /
-
-User-agent: Omgili
-Disallow: /
-
-User-agent: FacebookBot
-Disallow: /
-
-User-agent: Diffbot
-Disallow: /
-
-User-agent: Bytespider
-Disallow: /
-
-User-agent: ImagesiftBot
-Disallow: /
-
-User-agent: cohere-ai
-Disallow: /
-
-`
+const aiDisallowRules = deniedUserAgents
+  .map((ua) => `User-agent: ${ua}\nDisallow: /\n`)
+  .join("\n")
 
 /**
  * Send the correct robots.txt information per-environment.
@@ -47,8 +26,8 @@ export default function robots(_, res) {
   const deployEnv = process.env.DEPLOYMENT_ENV ?? LOCAL
 
   const contents =
-    deployEnv === PRODUCTION
-      ? `# Block search result pages
+    deployEnv !== PRODUCTION
+      ? `# Block search result pages and single result pages
 User-agent: *
 Crawl-delay: 10
 Disallow: /search/audio/
@@ -57,8 +36,7 @@ Disallow: /search/
 Disallow: /image/
 Disallow: /audio/
 
-crawl-delay:
-${AI_ROBOTS_CONTENT}
+${aiDisallowRules}
       `
       : `# Block everyone from the staging site
 User-agent: *
