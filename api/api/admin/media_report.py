777b8488e36f4992bc21bc504ac3e362f148a4c3
Olga Bulat <obulat@gmail.com>
2024-04-25 14:35:50 +0300

    Move form html to the template
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- api/api/admin/media_report.py
+++ api/api/admin/media_report.py
@@ -16,6 +16,7 @@ def create_link(url: str, text: str):
 
 
 class MediaReportAdmin(admin.ModelAdmin):
+    change_form_template = "admin/api/media_report/change_form.html"
     list_display = ("id", "reason", "is_pending", "description", "created_at", "url")
     list_filter = (
         ("decision", admin.EmptyFieldListFilter),  # ~status, i.e. pending or moderated
@@ -51,48 +52,6 @@ class MediaReportAdmin(admin.ModelAdmin):
             return None
         return True
 
-    @admin.display(description="Title and creator")
-    def title(self, obj):
-        title = obj.media_obj.title or "Unnamed media"
-
-        if not (obj.media_obj.creator or obj.media_obj.creator_url):
-            return title
-
-        if obj.media_obj.creator and obj.media_obj.creator_url:
-            return mark_safe(
-                f"{title} by {create_link(obj.media_obj.creator_url, obj.media_obj.creator)}"
-            )
-        else:
-            creator = obj.media_obj.creator or obj.media_obj.creator_url
-            creator_string = f" by {creator}" if creator else ""
-            return f"{title}{creator_string}"
-
-    def media_display(self, obj):
-        pass
-
-    @admin.display(description="Media description")
-    def media_description(self, obj):
-        return obj.media_obj.meta_data.get("description")
-
-    @admin.display(description="Tags")
-    def tags(self, obj):
-        """Display tag names grouped by provider."""
-        if not obj.media_obj.tags:
-            return ""
-        tags_by_provider = {}
-        for tag in obj.media_obj.tags:
-            tags_by_provider.setdefault(tag["provider"], []).append(tag["name"])
-        return mark_safe(
-            "".join(
-                f"<p><strong>{provider}</strong>: {', '.join(names)}</p>"
-                for provider, names in tags_by_provider.items()
-            )
-        )
-
-    @admin.display(description="From")
-    def from_field(self, obj):
-        return f"provider: {obj.media_obj.provider}, source: {obj.media_obj.source}"
-
     @admin.display(description="URLs")
     def landing_urls(self, obj):
         if not self.media_type or not obj:
@@ -105,36 +64,18 @@ class MediaReportAdmin(admin.ModelAdmin):
             f'{create_link(openverse_url, "openverse.org")}'
         )
 
-    @admin.display(description="Other reports")
-    def other_reports(self, obj):
-        """
-        Display a table of other reports for the same media object.
-        Cannot use the ``TabularInline`` class because it requires a Parent -> Child relationship.
-        """
+    def get_other_reports(self, obj):
         if not self.media_type or not obj:
-            return ""
+            return []
+
         reports = (
             self.model.objects.filter(media_obj__identifier=obj.media_obj.identifier)
             .exclude(id=obj.id)
             .order_by("created_at")
         )
-        logging.info(
-            f"Found {reports.count()} other reports for {obj.media_obj.identifier}"
-        )
         for report in reports:
-            logging.info(
-                f"Report {report.id} created at {report.created_at} for {report.media_obj.identifier}"
-            )
-        if not reports:
-            return ""
-        result = "<table><thead><tr><th>Date</th><th>Report reason</th><th>Status</th><th>Report link</th></tr></thead><tbody>"
-        for report in reports:
-            report_link_html = f'<a href="{reverse("admin:api_imagereport_change", args=[report.id])}">{report.id}</a>'
-            created_at = report.created_at.strftime("%Y-%m-%d %H:%M:%S")
-            report_row = f"<tr><td>{created_at}</td><td>{report.reason}</td><td>{report.status}</td><td>{report_link_html}</td></tr>"
-            result += report_row
-        result += "</tbody></table>"
-        return mark_safe(result)
+            report.href = reverse("admin:api_imagereport_change", args=[report.id])
+        return reports
 
     def get_exclude(self, request, obj=None):
         # ``identifier`` cannot be edited on an existing report.
@@ -164,22 +105,65 @@ class MediaReportAdmin(admin.ModelAdmin):
                     ],
                 },
             ),
-            (
-                "Media details",
-                {
-                    "fields": [
-                        "media_display",
-                        "title",
-                        "tags",
-                        "media_description",
-                        "from_field",
-                        "landing_urls",
-                        "media_obj_id",
-                    ]
-                },
-            ),
-            ("Other reports", {"fields": ["other_reports"]}),
         ]
+
+    def _get_media_obj_data(self, obj):
+        additional_data = {
+            "other_reports": self.get_other_reports(obj),
+            "identifier": obj.media_obj.identifier,
+            "foreign_landing_url": obj.media_obj.foreign_landing_url,
+            "description": obj.media_obj.meta_data.get("description", ""),
+            "title": obj.media_obj.title,
+            "provider": obj.media_obj.provider,
+            "source": obj.media_obj.source,
+            "creator": obj.media_obj.creator or obj.media_obj.creator_url,
+            "creator_url": obj.media_obj.creator_url,
+            "url": obj.media_obj.url,
+            "tags": None,
+            "reverse_media_url": reverse(
+                f"admin:api_{self.media_type}_change", args=[obj.media_obj.identifier]
+            ),
+        }
+
+        if obj.media_obj.tags:
+            tags_by_provider = {}
+            for tag in obj.media_obj.tags:
+                tags_by_provider.setdefault(tag["provider"], []).append(tag["name"])
+            additional_data["tags"] = tags_by_provider
+        if self.media_type == "image":
+            additional_data["thumb_url"] = (
+                f"https://api.openverse.engineering{obj.media_url()}thumb/"
+            )
+        return additional_data
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["media_type"] = self.media_type
+
+        obj = self.get_object(request, object_id)
+        if obj and obj.media_obj:
+            additional_data = self._get_media_obj_data(obj)
+            extra_context = {**extra_context, **additional_data}
+
+        return super().change_view(
+            request,
+            object_id,
+            form_url,
+            extra_context=extra_context,
+        )
+
+    def render_change_form(
+        self, request, context, add=False, change=False, form_url="", obj=None
+    ):
+        context.update(
+            {
+                "add": add,
+                "change": change,
+            }
+        )
+        return super().render_change_form(
+            request, context, add=add, change=change, form_url=form_url, obj=obj
+        )
 
     def get_readonly_fields(self, request, obj=None):
         if obj is None:
@@ -189,14 +173,7 @@ class MediaReportAdmin(admin.ModelAdmin):
             "reason",
             "description",
             "has_sensitive_text",
-            "media_display",
-            "title",
-            "tags",
-            "media_description",
-            "from_field",
-            "landing_urls",
             "media_obj_id",
-            "other_reports",
         ]
         # ``status`` cannot be changed on a finalised report.
         if obj.status != PENDING:
@@ -205,36 +182,11 @@ class MediaReportAdmin(admin.ModelAdmin):
 
 
 class ImageReportAdmin(MediaReportAdmin):
-    change_form_template = "admin/api/imagereport/change_form.html"
     media_type = "image"
-
-    @admin.display(description="Image")
-    def media_display(self, obj):
-        """
-        Display a blurred image with a clickable overlay.
-        Use the image thumbnail if available, else replace with the direct image url.
-        """
-        if obj.media_obj.url:
-            thumb_url = f"https://api.openverse.engineering{obj.media_url()}thumb/"
-            return mark_safe(
-                f'<div class="container"><img src="{thumb_url}" alt="Media Image" class="blur-image" height="300" '
-                f'onclick="toggleBlur(this)" onerror="replace(this, \'{obj.media_obj.url}\')" style="cursor: pointer;" />'
-                f"<p> Show content </p></div>"
-            )
-        return "No Image Available"
 
 
 class AudioReportAdmin(MediaReportAdmin):
     media_type = "audio"
-
-    @admin.display(description="Audio")
-    def media_display(self, obj):
-        if obj.media_obj.url:
-            return mark_safe(
-                f'<audio controls><source src="{obj.media_obj.url}" type="audio/mpeg">'
-                f"Your browser does not support the audio element.</audio>"
-            )
-        return "No Audio Available"
 
 
 class MediaSubreportAdmin(admin.ModelAdmin):
