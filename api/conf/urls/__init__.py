25b9137d02fad26d7eea9b48ac7df6b24dd9ad11
Krystle Salazar <krystle.salazar@automattic.com>
2024-04-26 02:38:58 -0400

    Remove obsolete field from docker-compose.yml (#4200)
    * Remove version from docker-compose.yml
    
    * Remove unused imports

--- api/conf/urls/__init__.py
+++ api/conf/urls/__init__.py
@@ -5,8 +5,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/topics/http/urls/
 """
 
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView, TemplateView
