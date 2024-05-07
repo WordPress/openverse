749eece753f4f056fac1617fd334fc6f286e4340
Olga Bulat <obulat@gmail.com>
2024-05-07 18:09:55 +0300

    Revert header changes
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/components/VHeader/VHeaderInternal.vue
+++ frontend/src/components/VHeader/VHeaderInternal.vue
@@ -52,7 +52,7 @@
           @open="openPageMenu"
         >
           <template #top-bar>
-            <header class="flex h-20 w-full justify-between px-2 py-4">
+            <div class="flex h-20 w-full justify-between px-2 py-4">
               <VHomeLink variant="light" />
               <VIconButton
                 variant="transparent-tx"
@@ -62,7 +62,7 @@
                 :label="$t('modal.closePagesMenu')"
                 @click="closePageMenu"
               />
-            </header>
+            </div>
           </template>
           <template #default>
             <nav>
