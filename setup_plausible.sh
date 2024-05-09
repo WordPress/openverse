0ed6673a6721e196dc978dbf2f834606b7358fd8
Princewill Onyenanu <princekelvin91@gmail.com>
2024-05-09 18:18:42 +0100

    replace sample secret key for plausible with a more obvioulsy safe value (#4283)

--- setup_plausible.sh
+++ setup_plausible.sh
@@ -8,18 +8,21 @@ docker compose exec -T "$PLAUSIBLE_SERVICE_NAME" \
   /app/bin/plausible rpc \
   'Plausible.Auth.User.new(%{name: "Deploy", email: "deploy@example.com", password: "deploy", password_confirmation: "deploy"}) |> Plausible.Repo.insert'
 
-# Create an API key with '{sites:provision:*}' scope
-# API key: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa (that's 64 times 'a')
-# Key hash: SHA256(`SECRET_KEY_BASE` + API key)
+# load necessary environment variables into shell session
+# Note: API_KEY with '{sites:provision:*}' scope and SECRET_KEY_BASE
+# are both gotten from env file.
+# shellcheck disable=SC1091
+source docker/plausible/env.docker
+
 docker compose exec -T "$PLAUSIBLE_DB_SERVICE_NAME" /bin/bash -c "psql -U deploy -d plausible <<-EOF
 	INSERT INTO api_keys
 	  (id, user_id, name, key_prefix, key_hash, inserted_at, updated_at, scopes, hourly_request_limit)
 	VALUES
-	  (1, 1, 'Development', 'aaaaaa', '332015ffc9f0e1f475c0fadb1e1a14d2eb09774249f71961f5a2c477efc0a0fc', now(), now(), '{sites:provision:*}', 1000)
+	  (1, 1, 'Development', 'aaaaaa', encode(digest('$SECRET_KEY_BASE$API_KEY', 'sha256'), 'hex'), now(), now(), '{sites:provision:*}', 1000)
   ON CONFLICT (id) DO NOTHING
 	EOF"
 
-authorization_header="Authorization: Bearer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+authorization_header="Authorization: Bearer $API_KEY"
 local_plausible="http://localhost:50288"
 
 # Create site using API key
