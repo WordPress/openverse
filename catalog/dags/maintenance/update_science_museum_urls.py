d24adae972b4c1fea994a64ee3874a6dea07f8da
Staci Cooper <staci.cooper@automattic.com>
2024-05-09 12:11:17 -0700

    Commit ids to update to temp table and drop later

--- catalog/dags/maintenance/update_science_museum_urls.py
+++ catalog/dags/maintenance/update_science_museum_urls.py
@@ -32,6 +32,7 @@ logger = logging.getLogger(__name__)
 
 DAG_ID = "update_science_museum_urls"
 INVALID_IDS_TABLE = "science_museum_invalid_ids"
+IDS_TO_UPDATE_TABLE = "temp_science_museum_rows_to_validate"
 
 UPDATE_URLS_QUERY = dedent(
     """
@@ -44,6 +45,22 @@ CREATE_TABLE_QUERY = dedent(
     f"""
     CREATE TABLE IF NOT EXISTS {INVALID_IDS_TABLE}
     (identifier uuid);
+    """
+)
+CREATE_TEMP_TABLE_QUERY = dedent(
+    f"""
+    CREATE TABLE {IDS_TO_UPDATE_TABLE} AS
+    SELECT ROW_NUMBER() OVER() row_id, identifier, url
+    FROM image
+    WHERE provider='sciencemuseum';
+    """
+)
+DROP_TEMP_TABLE_QUERY = f"DROP TABLE {IDS_TO_UPDATE_TABLE};"
+GET_BATCH_QUERY = dedent(
+    """
+    SELECT identifier, url
+    FROM {table_name}
+    WHERE row_id > {start} AND row_id <= {end};
     """
 )
 
@@ -65,24 +82,31 @@ def run_sql(
 
 @task
 def get_batches(
+    total_count: int,
     batch_size: int,
+):
+    return [
+        {"start": i, "end": i + batch_size} for i in range(0, total_count, batch_size)
+    ]
+
+
+@task(max_active_tis_per_dagrun=3)
+def process_batch(
+    start: int,
+    end: int,
     postgres_conn_id: str = POSTGRES_CONN_ID,
     task: AbstractOperator = None,
 ):
+    invalid_ids = []
+
     postgres = PostgresHook(
         postgres_conn_id=postgres_conn_id,
         default_statement_timeout=PostgresHook.get_execution_timeout(task),
     )
     records = postgres.get_records(
-        "SELECT identifier, url FROM image where provider='sciencemuseum';"
+        GET_BATCH_QUERY.format(table_name=IDS_TO_UPDATE_TABLE, start=start, end=end)
     )
 
-    return [records[i : i + batch_size] for i in range(0, len(records), batch_size)]
-
-
-@task(max_active_tis_per_dagrun=3)
-def process_batch(records):
-    invalid_ids = []
     for identifier, url in records:
         # Failed to reach URL
         if rewrite_redirected_url(url) is None:
@@ -147,19 +171,33 @@ def update_science_museum_urls():
         sql=CREATE_TABLE_QUERY
     )
 
+    # Temp table used to store the information that needs to be
+    # validated.
+    create_temp_table = run_sql.override(task_id="create_temp_table")(
+        sql=CREATE_TEMP_TABLE_QUERY, handler=RETURN_ROW_COUNT
+    )
+
     # Split records into batches
-    batches = get_batches(batch_size="{{ params.batch_size }}")
+    batches = get_batches(
+        total_count=create_temp_table, batch_size="{{ params.batch_size }}"
+    )
 
     # Verify each record's url
-    process = process_batch.expand(records=batches)
+    process = process_batch.expand_kwargs(batches)
 
     # Record the identifiers of records with invalid urls
     record = record_invalid_ids(process)
+
+    # Drop the temp table.
+    drop_temp_table = run_sql.override(task_id="drop_temp_table")(
+        sql=DROP_TEMP_TABLE_QUERY
+    )
 
     # Report the number of invalid records to Slack
     notify_slack(record)
 
     update >> create_table >> process
+    record >> drop_temp_table
 
 
 update_science_museum_urls()
