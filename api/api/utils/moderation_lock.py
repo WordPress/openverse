d97dbc50085c05874456996c1d03b9cde3b9a791
Dhruv Bhanushali <dhruv_b@live.com>
2024-05-23 17:02:06 +0400

    Handle failure to connect to Redis

--- api/api/utils/moderation_lock.py
+++ api/api/utils/moderation_lock.py
@@ -1,8 +1,9 @@
 import time
 
+import django_redis
 import structlog
-from django_redis import get_redis_connection
 from redis import Redis
+from redis.exceptions import ConnectionError
 
 
 REPORT_LOCK_PREFIX = "soft_lock_report"
@@ -10,25 +11,31 @@ MODERATOR_LOCK_PREFIX = "soft_lock_moderator"
 PREFIXES = [REPORT_LOCK_PREFIX, MODERATOR_LOCK_PREFIX]
 
 logger = structlog.get_logger(__name__)
-redis: Redis = get_redis_connection("default")
 
 
 class LockManager:
     def __init__(self, media_type):
         self.media_type = media_type
+        redis: Redis = django_redis.get_redis_connection("default")
+        try:
+            redis.ping()
+            self.redis = redis
+        except ConnectionError:
+            logger.error("Redis connection failed")
+            self.redis = None
 
     def prune(self):
         """Delete all expired locks."""
 
         def _prune(pattern: str):
             now = int(time.time())
-            pipeline = redis.pipeline()
-            for key in redis.keys(pattern):
-                for value, score in redis.zrange(key, 0, -1, withscores=True):
+            pipe = self.redis.pipeline()
+            for key in self.redis.keys(pattern):
+                for value, score in self.redis.zrange(key, 0, -1, withscores=True):
                     if score <= now:
                         logger.info("Deleting expired lock", key=key, value=value)
-                        pipeline.zrem(key, value)
-            pipeline.execute()
+                        pipe.zrem(key, value)
+            pipe.execute()
 
         for prefix in PREFIXES:
             _prune(f"{prefix}:*")
@@ -41,10 +48,13 @@ class LockManager:
         :param object_id: the ID of the report being viewed
         """
 
+        if not self.redis:
+            return
+
         object = f"{self.media_type}:{object_id}"
         expiration = int(time.time()) + 5 * 60  # 5 minutes from now
 
-        pipe = redis.pipeline()
+        pipe = self.redis.pipeline()
         logger.info("Adding lock", object=object, user=username, expiration=expiration)
         pipe.zadd(f"{REPORT_LOCK_PREFIX}:{object}", {username: expiration})
         pipe.zadd(f"{MODERATOR_LOCK_PREFIX}:{username}", {object: expiration})
@@ -58,9 +68,12 @@ class LockManager:
         :param object_id: the ID of the report being viewed
         """
 
+        if not self.redis:
+            return
+
         object = f"{self.media_type}:{object_id}"
 
-        pipe = redis.pipeline()
+        pipe = self.redis.pipeline()
         logger.info("Removing lock", object=object, user=username)
         pipe.zrem(f"{REPORT_LOCK_PREFIX}:{object}", username)
         pipe.zrem(f"{MODERATOR_LOCK_PREFIX}:{username}", object)
@@ -74,12 +87,15 @@ class LockManager:
         :return: the list of moderators on a particular item
         """
 
+        if not self.redis:
+            return set()
+
         self.prune()
 
         object = f"{self.media_type}:{object_id}"
         mods = set(
             item.decode("utf-8")
-            for item in redis.zrange(
+            for item in self.redis.zrange(
                 f"{REPORT_LOCK_PREFIX}:{object}",
                 start=0,
                 end=-1,
