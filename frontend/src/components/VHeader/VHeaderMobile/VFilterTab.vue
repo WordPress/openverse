3265d2a1585b63e7e99d4006df64e054e8b96290
Madison Swain-Bowden <bowdenm@spu.edu>
2024-05-27 16:55:37 -0700

    Add aria-label to VFilterTab

--- frontend/src/components/VHeader/VHeaderMobile/VFilterTab.vue
+++ frontend/src/components/VHeader/VHeaderMobile/VFilterTab.vue
@@ -1,14 +1,18 @@
 <template>
-  <VTab id="filters" size="medium" class="gap-x-2">
+  <VTab id="filters" size="medium" class="gap-x-2" :aria-label="ariaLabel">
     <VFilterIconOrCounter :applied-filter-count="appliedFilterCount" />
     <h2 class="label-regular">{{ $t("filters.title") }}</h2>
   </VTab>
 </template>
 <script lang="ts">
+import { computed, defineComponent } from "vue"
+
+import { useI18n } from "~/composables/use-i18n"
+
 import VFilterIconOrCounter from "~/components/VHeader/VFilterIconOrCounter.vue"
 import VTab from "~/components/VTabs/VTab.vue"
 
-export default {
+export default defineComponent({
   name: "VFilterTab",
   components: { VFilterIconOrCounter, VTab },
   props: {
@@ -17,5 +21,12 @@ export default {
       default: 0,
     },
   },
-}
+  setup(props) {
+    const i18n = useI18n()
+    const ariaLabel = computed(() =>
+      i18n.tc("header.filterButton.withCount", props.appliedFilterCount)
+    )
+    return { ariaLabel }
+  },
+})
 </script>
