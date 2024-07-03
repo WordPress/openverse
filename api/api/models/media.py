d599fd7603fb01038ebda8fb06d3c6f09ca8fd28
Krystle Salazar <krystle.salazar@automattic.com>
2024-07-03 17:48:57 -0400

    Drop `status` column from media reports (#4590)
    Co-authored-by: sarayourfriend <git@sarayourfriend.pictures>

--- api/api/models/media.py
+++ api/api/models/media.py
@@ -16,11 +16,6 @@ from api.models.base import OpenLedgerModel
 from api.models.mixins import ForeignIdentifierMixin, IdentifierMixin, MediaMixin
 
 
-PENDING = "pending_review"
-MATURE_FILTERED = "mature_filtered"
-DEINDEXED = "deindexed"
-NO_ACTION = "no_action"
-
 MATURE = "mature"
 DMCA = "dmca"
 OTHER = "other"
@@ -191,13 +186,6 @@ class AbstractMediaReport(models.Model):
 
     REPORT_CHOICES = [(MATURE, MATURE), (DMCA, DMCA), (OTHER, OTHER)]
 
-    STATUS_CHOICES = [
-        (PENDING, PENDING),
-        (MATURE_FILTERED, MATURE_FILTERED),
-        (DEINDEXED, DEINDEXED),
-        (NO_ACTION, NO_ACTION),
-    ]
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     media_obj = models.ForeignKey(
@@ -226,12 +214,6 @@ class AbstractMediaReport(models.Model):
         null=True,
         help_text="The explanation on why media is being reported.",
     )
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
-    """
-    All statuses except ``PENDING`` are deprecated. Instead refer to the
-    property ``is_pending``.
-    """
-
     decision = models.ForeignKey(
         to="AbstractMediaDecision",
         on_delete=models.SET_NULL,
