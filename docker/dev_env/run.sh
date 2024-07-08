38709674a0e022daad7f28ebb479ea058b7aa9d6
sarayourfriend <git@sarayourfriend.pictures>
2024-07-09 09:48:07 +1000

    Make `ov` base image updates overall more convenient and take immediate effect (#4560)
    * Keep pipx outside opt so its packages are updatable
    
    * Make base image swappy
    
    * Go back to using a big base image, but make it better to swap out
    
    Also make run.sh a lot faster for most runs
    
    * Fix permissions issues for pdm

--- docker/dev_env/run.sh
+++ docker/dev_env/run.sh
@@ -3,9 +3,10 @@
 set -e
 
 container_name="openverse-dev_env"
+image_name="$container_name"
 volume_name="openverse_dev-env"
 
-if ! docker volume inspect openverse-dev_env &>/dev/null; then
+if ! docker volume inspect "$container_name" &>/dev/null; then
   docker volume create "$volume_name" 1>/dev/null
 fi
 
@@ -13,6 +14,12 @@ shared_args=(
   -i
   --env "OPENVERSE_PROJECT=$OPENVERSE_PROJECT"
   --env "TERM=xterm-256color"
+  # Avoid overwriting `.venv`'s from the host
+  --env "PDM_VENV_IN_PROJECT=False"
+  # Install global pipx packages separate from user packages
+  # Prevents permissions issue with packages installed as root
+  # when running packages.sh
+  --env "PIPX_GLOBAL_HOME=/pipx"
   --workdir "$(pwd)"
 )
 
@@ -72,11 +79,20 @@ darwin*)
 
 esac
 
+# Ensure cache directory uses, `ov clean` destroys it
+mkdir -p "$DEV_ENV"/.cache
+
 if command -v pnpm &>/dev/null; then
-  host_pnpm_store="$(pnpm store path)"
+  if [ ! -f "$DEV_ENV"/.cache/pnpm-store-path ]; then
+    # This command can be slow, so cache the response
+    # Should be safe, it's rare to change this, ov clean removes it otherwise
+    pnpm store path >"$DEV_ENV"/.cache/pnpm-store-path
+  fi
+
+  host_pnpm_store=$(cat "$DEV_ENV"/.cache/pnpm-store-path)
 
   # Share the pnpm cache with the container, if it's available locally
-  if [ "$host_pnpm_store" != "" ]; then
+  if [ -n "$host_pnpm_store" ]; then
     pnpm_home="$(dirname "$host_pnpm_store")"
     shared_args+=(--env PNPM_HOME="$pnpm_home")
     run_args+=(-v "$pnpm_home:$pnpm_home:rw,z")
@@ -84,20 +100,44 @@ if command -v pnpm &>/dev/null; then
 fi
 
 if command -v pdm &>/dev/null; then
+  if [ ! -f "$DEV_ENV"/.cache/pdm-cache-path ]; then
+    # These commands can be slow, so cache the response
+    # Should be safe, it's rare to change this
+    if [ "$(pdm config --quiet install.cache)" == "True" ]; then
+      pdm config --quiet cache_dir >"$DEV_ENV"/.cache/pdm-cache-path
+    else
+      echo "" >"$DEV_ENV"/.cache/pdm-cache-path
+    fi
+  fi
+
+  host_pdm_cache=$(cat "$DEV_ENV"/.cache/pdm-cache-path)
+
   # Share the PDM cache with the container, if it's available locally
   # --quiet so PDM doesn't repeatedly fill the console with update messages
   # if they're enabled
-  if [ "$(pdm config --quiet install.cache)" == "True" ]; then
-    host_pdm_cache="$(pdm config --quiet cache_dir)"
+  if [ -n "$host_pdm_cache" ]; then
     shared_args+=(--env "PDM_CACHE_DIR=$host_pdm_cache")
     run_args+=(-v "$host_pdm_cache:$host_pdm_cache:rw,z")
   fi
 fi
 
 existing_container_id=$(docker ps -a --filter name="$container_name" -q)
+existing_container_image_id=$(docker ps -a --format="{{ .Image }}" -f name="$container_name")
+
+run_container() {
+  docker run "${shared_args[@]}" "${run_args[@]}" "$image_name":latest 1>/dev/null
+}
 
 if [ -z "$existing_container_id" ]; then
-  docker run "${shared_args[@]}" "${run_args[@]}" openverse-dev_env:latest
+  run_container
+elif [ "$existing_container_image_id" != "$image_name":latest ]; then
+  # If the existing container is running the latest image, it will be referred to as the ":latest" tag
+  # Otherwise, $existing_container_image_id will be the actual image ID hash, in which case we aren't running the latest available image
+  # Redirect output to stderr so that it isn't hidden but still won't affect piping or otherwise capturing stdout when running `ov`.
+  printf "Detected a new version of the base image. Stopping the existing container and starting a new one with the latest base image.\n" >/dev/stderr
+  docker stop "$existing_container_id" 1>/dev/stderr
+  docker rm "$existing_container_id" 1>/dev/stderr
+  run_container
 else
   # Do not need to bother checking if the container is already running, docker start
   # is a noop in that case with no adverse effects
