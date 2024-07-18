6f3665eff3ee02ca17c41517abebd5c2a57e91ce
Olga Bulat <obulat@gmail.com>
2024-07-18 12:27:59 +0300

    Remove unused PACKAGE_MANAGER arg
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/bin/playwright.sh
+++ frontend/bin/playwright.sh
@@ -1,16 +1,10 @@
 #! /usr/bin/env sh
 set -e
 
-package_manager() {
-  grep packageManager ../package.json | awk '{print $2}' | sed 's/[",]//g'
-}
-
 version() {
   pnpm ls --depth=0 | grep -e playwright | awk '{print $2}'
 }
 
-PACKAGE_MANAGER=$(package_manager)
-export PACKAGE_MANAGER
 export USER_ID="${USER_ID:-$(id -u)}"
 export PLAYWRIGHT_ARGS="$*"
 PLAYWRIGHT_VERSION=$(version)
@@ -19,7 +13,7 @@ export TEST_COMMAND="${TEST_COMMAND:-test:playwright:local}"
 
 pnpm i18n:get-translations --en-only
 
-echo Running Playwright v"$PLAYWRIGHT_VERSION" as "$USER_ID" with Playwright arguments "$PLAYWRIGHT_ARGS" under package manager "$PACKAGE_MANAGER"
+echo Running Playwright v"$PLAYWRIGHT_VERSION" as "$USER_ID" with Playwright arguments "$PLAYWRIGHT_ARGS"
 
 docker compose -f docker-compose.playwright.yml up --build --force-recreate --exit-code-from playwright --remove-orphans
 
