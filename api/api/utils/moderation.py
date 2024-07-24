f8ba3e4b30881ab9334ca295dff1bdf997221681
Dhruv Bhanushali <dhruv_b@live.com>
2024-07-25 01:11:52 +0400

    Sprinkle logging

--- api/api/utils/moderation.py
+++ api/api/utils/moderation.py
@@ -1,5 +1,7 @@
 from typing import Literal
 
+import structlog
+
 from api.constants.moderation import DecisionAction
 from api.models.audio import (
     Audio,
@@ -16,6 +18,9 @@ from api.models.image import (
     SensitiveImage,
 )
 from api.models.media import AbstractDeletedMedia, AbstractMedia, AbstractSensitiveMedia
+
+
+logger = structlog.get_logger(__name__)
 
 
 def perform_moderation(
@@ -68,38 +73,65 @@ def perform_moderation(
     else:
         identifiers = list(mod_objects.values_list("identifier", flat=True))
 
+    logger.info(
+        "Performing bulk moderation action.",
+        action=action,
+        model=mod_objects.model._meta.label,
+        identifiers=identifiers,
+    )
+
     match action:
         case DecisionAction.MARKED_SENSITIVE:
-            SensitiveMedia.objects.bulk_create(
+            created = SensitiveMedia.objects.bulk_create(
                 [SensitiveMedia(media_obj_id=identifier) for identifier in identifiers]
             )
+            logger.debug(f"Created sensitive-{media_type} items.", count=len(created))
             SensitiveMedia.bulk_perform_action(True, mod_objects)
 
         case DecisionAction.DEINDEXED_COPYRIGHT | DecisionAction.DEINDEXED_SENSITIVE:
-            DeletedMedia.objects.bulk_create(
+            created = DeletedMedia.objects.bulk_create(
                 [DeletedMedia(media_obj_id=identifier) for identifier in identifiers]
             )
+            logger.debug(f"Created deleted-{media_type} items.", count=len(created))
             DeletedMedia.bulk_perform_action(mod_objects)
 
         case DecisionAction.REVERSED_MARK_SENSITIVE:
             media_items = Media.objects.filter(identifier__in=identifiers)
             SensitiveMedia.bulk_perform_action(False, media_items)
-            mod_objects.delete()
+            logger.debug(
+                f"Unmarked {media_type} items as sensitive.",
+                identifier_count=len(identifiers),
+                media_item_count=len(media_items),
+            )
+
+            count, _ = mod_objects.delete()
+            logger.debug(f"Deleted sensitive-{media_type} items.", count=count)
 
         case DecisionAction.REVERSED_DEINDEX:
             # There is no bulk action for reversed-deindex. The media
             # item will eventually be reindexed through data refresh.
-            mod_objects.delete()
+            count, _ = mod_objects.delete()
+            logger.debug(f"Deleted deleted-{media_type} items.", count=count)
 
     media_decision = MediaDecision.objects.create(
         action=action,
         moderator=request.user,
         notes=request.POST.get("notes"),
     )
-    MediaDecisionThrough.objects.bulk_create(
+    logger.debug(
+        "Decision created",
+        decision=media_decision.id,
+        action=media_decision.action,
+        notes=media_decision.notes,
+        moderator=media_decision.moderator.get_username(),
+    )
+
+    created = MediaDecisionThrough.objects.bulk_create(
         [
             MediaDecisionThrough(decision_id=media_decision.id, media_obj_id=identifier)
             for identifier in identifiers
         ]
     )
+    logger.debug(f"Created {media_type}-decision-through items.", count=len(created))
+
     return media_decision
