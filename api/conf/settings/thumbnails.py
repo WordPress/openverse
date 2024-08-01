6fb7cdd5f35c02f6cef5d3101520e4d24917e9df
sarayourfriend <git@sarayourfriend.pictures>
2024-08-01 14:26:37 +0100

    Decrease timeout for and add timing to thumb extension request

--- api/conf/settings/thumbnails.py
+++ api/conf/settings/thumbnails.py
@@ -32,3 +32,8 @@ THUMBNAIL_FAILURE_CACHE_TOLERANCE = config(
 
 # Timeout when requesting the thumbnail from the upstream image proxy
 THUMBNAIL_UPSTREAM_TIMEOUT = config("THUMBNAIL_UPSTREAM_TIMEOUT", default=4, cast=int)
+
+# Timeout when trying to determine the filetype based on a HEAD request to the upstream image provider
+THUMBNAIL_EXTENSION_REQUEST_TIMEOUT = config(
+    "THUMBNAIL_EXTENSION_REQUEST_TIMEOUT", default=4, cast=int
+)
