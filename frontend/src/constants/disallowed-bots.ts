949331b01bec9ea0658b53d10bd100d90748caac
zackdotcat <git@zack.cat>
2024-08-02 12:15:23 -0400

    Inline nuxt config keys, add top-level comment, extract bots list to constant

---
+++ frontend/src/constants/disallowed-bots.ts
@@ -0,0 +1,17 @@
+/**
+ * Bot user agents blocked in our robots.txt file.
+ */
+export const disallowedBots = [
+  "GPTBot",
+  "CCBot",
+  "ChatGPT-User",
+  "Google-Extended",
+  "anthropic-ai",
+  "Omgilibot",
+  "Omgili",
+  "FacebookBot",
+  "Diffbot",
+  "Bytespider",
+  "ImagesiftBot",
+  "cohere-ai",
+]
