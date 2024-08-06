a149656921c7611b77e5249398a032248c1c0a96
Olga Bulat <obulat@gmail.com>
2024-08-06 20:06:46 +0300

    Try fixing nitro build
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/app.config.ts
+++ frontend/src/app.config.ts
@@ -1,4 +1,4 @@
-import { defineAppConfig } from "#app/nuxt"
+import { defineAppConfig } from "#imports"
 
 export default defineAppConfig({
   semanticVersion: import.meta.env.SEMANTIC_VERSION as string,
