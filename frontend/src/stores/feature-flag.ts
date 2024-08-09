b14d042765afe65f09107ac969c055ad9d89e19f
Olga Bulat <obulat@gmail.com>
2024-08-09 07:03:31 +0300

    Fix feature flag initialization
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/src/stores/feature-flag.ts
+++ frontend/src/stores/feature-flag.ts
@@ -1,4 +1,4 @@
-import { useCookie, useRuntimeConfig } from "#imports"
+import { tryUseNuxtApp, useCookie } from "#imports"
 
 import { defineStore } from "pinia"
 import { useStorage } from "@vueuse/core"
@@ -131,8 +131,20 @@ const FEATURE_FLAG = "feature_flag"
 
 export const useFeatureFlagStore = defineStore(FEATURE_FLAG, {
   state: () => {
-    const deploymentEnv = useRuntimeConfig().public.deploymentEnv as DeployEnv
-    return initializeFlagState(deploymentEnv)
+    const nuxtApp = tryUseNuxtApp()
+    if (nuxtApp) {
+      return initializeFlagState(
+        nuxtApp.$config.public.deploymentEnv as DeployEnv
+      )
+    }
+    if (!process.env.NUXT_PUBLIC_DEPLOYMENT_ENV) {
+      throw new Error(
+        "Cannot initialize feature flags store. Deployment environment not set in the Nuxt app context and the env variable is unavailable."
+      )
+    }
+    return initializeFlagState(
+      process.env.NUXT_PUBLIC_DEPLOYMENT_ENV as DeployEnv
+    )
   },
   getters: {
     /**
