92c32d74e3196c2ef2b3f07270ec2d4d1b6c3bbe
sarayourfriend <git@sarayourfriend.pictures>
2024-08-09 16:45:58 +0100

    Remove unused and confusing development deployment env

--- frontend/src/constants/deploy-env.ts
+++ frontend/src/constants/deploy-env.ts
@@ -1,10 +1,9 @@
 export const LOCAL = "local"
-export const DEVELOPMENT = "development"
 export const STAGING = "staging"
 export const PRODUCTION = "production"
 
 // The order of the environments is important. They should be arranged in
 // increasing order of code-readiness, from local to production.
-export const DEPLOY_ENVS = [LOCAL, DEVELOPMENT, STAGING, PRODUCTION] as const
+export const DEPLOY_ENVS = [LOCAL, STAGING, PRODUCTION] as const
 
 export type DeployEnv = (typeof DEPLOY_ENVS)[number]
