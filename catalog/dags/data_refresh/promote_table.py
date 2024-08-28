1f6b69dd4206d68ba3ce8687fa9c84487d5e3ebe
Staci Cooper <staci.cooper@automattic.com>
2024-08-27 19:00:05 -0700

    Don't dynamically map separate tasks for creating each index
    This isn't possible because dynamically mapping tasks within a dynamically mapped taskgroup is not supported in our version of Airflow.

--- catalog/dags/data_refresh/promote_table.py
+++ catalog/dags/data_refresh/promote_table.py
@@ -5,6 +5,7 @@ from dataclasses import asdict
 from typing import NamedTuple
 
 from airflow.decorators import task, task_group
+from airflow.utils.trigger_rule import TriggerRule
 
 from common.constants import POSTGRES_API_CONN_IDS, Environment
 from common.sql import fetch_all, run_sql
@@ -20,8 +21,9 @@ class TableIndex(NamedTuple):
     index_def: str
 
 
-@task
-def transform_index_def(existing_index_def: str, temp_table_name: str):
+def _transform_index_def(
+    existing_index_def: str, temp_table_name: str, table_name: str
+) -> str:
     """
     Given a CREATE INDEX statement for an index on the existing media table, return a
     transformed statement that can be run to apply an identical index to the temp table.
@@ -29,15 +31,22 @@ def transform_index_def(existing_index_def: str, temp_table_name: str):
     is_pk = "(id)" in existing_index_def
     tokens = existing_index_def.split(" ")
 
-    # The index name always follows the word 'INDEX' in the def,
-    # e.g. `CREATE [UNIQUE] INDEX {index_name}`
-    index_name_idx = tokens.index("INDEX") + 1
-    old_index_name = tokens[index_name_idx]
     # Avoid a naming collision with the existing index by prepending
     # `temp_import` to the index name. Later when the temp table is promoted,
     # and the current media table is deleted, the indices will be restored
     # to their original names.
-    temp_index_name = f"temp_import_{old_index_name}"
+    # The index name always follows the word 'INDEX' in the def,
+    # e.g. `CREATE [UNIQUE] INDEX {index_name}`
+    index_name_idx = tokens.index("INDEX") + 1
+    old_index_name = tokens[index_name_idx]
+    # For most indices, we just prefix the index name with `temp_import_`. The pk
+    # index is automatically created at an earlier step and has a different
+    # naming convention.
+    temp_index_name = (
+        f"temp_import_{old_index_name}"
+        if not is_pk
+        else f"temp_import_{table_name}_pkey"
+    )
     tokens[index_name_idx] = temp_index_name
 
     # Update the table name to the temp table. The table name always follows
@@ -46,51 +55,72 @@ def transform_index_def(existing_index_def: str, temp_table_name: str):
     schema_name, table_name = tokens[table_name_idx].split(".")
     tokens[table_name_idx] = f"{schema_name}.{temp_table_name}"
 
-    return TableIndex(
-        old_index_name,
-        temp_index_name,
-        # The primary key index already exists, so we do not need to create a new one.
-        " ".join(tokens) if not is_pk else "",
-    )
+    return TableIndex(old_index_name, temp_index_name, " ".join(tokens))
+
+
+@task
+def transform_index_defs(
+    existing_index_defs: list[str], temp_table_name: str, table_name: str
+):
+    """
+    Given a CREATE INDEX statement for an index on the existing media table, return a
+    transformed statement that can be run to apply an identical index to the temp table.
+    """
+    return [
+        _transform_index_def(index_def, temp_table_name, table_name)
+        for index_def in existing_index_defs
+    ]
+
+
+@task
+def create_table_indices(postgres_conn_id: str, index_configs: list[TableIndex]):
+    for index_config in index_configs:
+        if "(id)" in index_config.index_def:
+            # Skip the primary key index, as this already exists
+            logger.info(f"Skipping adding {index_config.index_name} index.")
+            continue
+
+        # Create the index
+        run_sql.function(
+            postgres_conn_id=postgres_conn_id, sql_template=index_config.index_def
+        )
 
 
 @task_group(group_id="regenerate_table_indices")
 def generate_table_indices_for_table(
-    table_name: str, temp_table_name: str, postgres_conn_id: str
+    downstream_table_name: str, temp_table_name: str, postgres_conn_id: str
 ):
     """
     Apply all indices on the main media table to the temp table. Indices will be given
     names prefixed with `temp_import_` in this step to avoid collisions, and renamed
     later when the table is promoted.
     """
-    # for each table in table_mappings, generate indices
-    # Get the existing index defs from the current media table. This returns a dictionary mapping
-    # index names to their existing CREATE statements
-    existing_index_defs = run_sql.override(task_id="get_existing_index_defs")(
+    # Get the CREATE statements for the indices applied to the live (old) table
+    existing_index_defs = run_sql.override(
+        task_id="get_existing_index_defs", trigger_rule=TriggerRule.NONE_FAILED
+    )(
         postgres_conn_id=postgres_conn_id,
         sql_template="SELECT indexdef FROM pg_indexes WHERE tablename='{table_name}'",
         handler=fetch_all,
-        table_name=table_name,
+        table_name=downstream_table_name,
     )
 
-    # Transform the CREATE statements so they can be used to apply to the temp table
-    new_index_defs = transform_index_def.partial(
-        temp_table_name=temp_table_name
-    ).expand(existing_index_def=existing_index_defs)
-
-    # Create the new indices on the temp table
-    new_table_indices = run_sql.partial(
-        postgres_conn_id=postgres_conn_id,
-    ).expand(
-        sql_template=[
-            new_index.index_def
-            for new_index in new_index_defs
-            if new_index.index_def is not None
-        ]
+    # Transform the CREATE statements so they can be used to apply identical indices
+    # to the temp table
+    new_index_configs = transform_index_defs(
+        temp_table_name=temp_table_name,
+        table_name=downstream_table_name,
+        existing_index_defs=existing_index_defs,
     )
 
-    # Return the names of the newly created indices
-    return new_table_indices
+    # Actually create the new indices on the temp table
+    create_table_indices(
+        postgres_conn_id=postgres_conn_id, index_configs=new_index_configs
+    )
+
+    # Return the information for the newly created indices, so that they can later
+    # be renamed to match the live index names
+    return new_index_configs
 
 
 @task_group
