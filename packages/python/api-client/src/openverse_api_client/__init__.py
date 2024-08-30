f2d5c031b6061e327656c2abf3d67fe71c6fceb4
sarayourfriend <git@sarayourfriend.pictures>
2024-08-30 12:03:02 +1000

    Remove Python api-client package

--- packages/python/api-client/src/openverse_api_client/__init__.py
+++
@@ -1,7 +0,0 @@
-from openverse_api_client._generated.async_client import (
-    AsyncOpenverseClient as AsyncOpenverseClient,
-)
-from openverse_api_client._generated.models import *  # noqa: F403
-from openverse_api_client._generated.sync_client import (
-    OpenverseClient as OpenverseClient,
-)
