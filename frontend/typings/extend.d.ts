668b43a3d5b3ae645ca4377ded09674d6ec8e6d6
Olga Bulat <obulat@gmail.com>
2024-09-05 21:14:24 +0300

    Update Nuxt to v3.13.1 (#4869)
    * Update Pinia
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update Nuxt to v3.13.1
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Add typing fix
    
    https://nuxt.com/blog/v3-13#vue-typescript-changes
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Update Vue
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    * Revert Pinia updates
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>
    
    ---------
    
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- frontend/typings/extend.d.ts
+++ frontend/typings/extend.d.ts
@@ -5,3 +5,18 @@ declare module "h3" {
     $sentry?: Sentry
   }
 }
+
+// Fix until the libraries are updated to correctly augment `vue`.
+// See https://nuxt.com/blog/v3-13#vue-typescript-changes
+
+import type {
+  ComponentCustomOptions as _ComponentCustomOptions,
+  ComponentCustomProperties as _ComponentCustomProperties,
+} from "vue"
+
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties extends _ComponentCustomProperties {}
+  interface ComponentCustomOptions extends _ComponentCustomOptions {}
+}
+
+export {}
