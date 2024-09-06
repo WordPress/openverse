4ecdf470397c5381c456e4735fbc5b102bfe9104
Olga Bulat <obulat@gmail.com>
2024-09-06 16:07:59 +0300

    Update the error string

--- frontend/test/storybook/functional/smoke-test.spec.ts
+++ frontend/test/storybook/functional/smoke-test.spec.ts
@@ -22,7 +22,8 @@ const ignoredProblems = [
   /Failed to load resource: net::ERR_CONNECTION_REFUSED/,
   /Sentry DSN wasn't provided/,
   // Errors from the Storybook nuxt module
-  /\[nuxt] error caught during app initialization TypeError: Cannot read properties of undefined \(reading 'cdnURL'\)/,
+  // TypeError or Va
+  /\[nuxt] error caught during app initialization \w*: Cannot read properties of undefined \(reading 'cdnURL'\)/,
   /Failed to load resource: the server responded with a status of 404 \(Not Found\)/,
   /\[Vue Router warn]: Record with path "\/iframe\.html" is either missing a "component\(s\)" or "children" property/,
 ]
