fe4159074e5589a862e9b8e0f736bcf11caea283
Aditya Raj <araj0259@gmail.com>
2024-09-23 23:57:07 +0530

    Added Test to acoomodate match_pharses

--- api/test/unit/controllers/test_search_controller_search_query.py
+++ api/test/unit/controllers/test_search_controller_search_query.py
@@ -185,11 +185,11 @@ def test_create_search_query_q_search_with_filters(
         ],
         "should": [
             {
-                "simple_query_string": {
-                    "boost": 10000,
-                    "fields": ["title"],
-                    "query": "cat",
-                    "flags": DEFAULT_SQS_FLAGS,
+                "match_phrase": {
+                    "title": {
+                        "query": "cat",
+                        "boost": 10000,
+                    }
                 }
             },
             {"rank_feature": {"boost": 10000, "field": "standardized_popularity"}},
