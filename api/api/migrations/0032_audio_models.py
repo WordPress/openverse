b7104c76d5790bff37b1771d7956c93b6a89705f
Staci Cooper <staci.cooper@automattic.com>
2024-10-09 14:03:28 -0700

    Revert "Ran with ruff"
    This reverts commit 0c644b4729cbbf3c37fbfeaf89d8c890c83c3293.

--- api/api/migrations/0032_audio_models.py
+++ api/api/migrations/0032_audio_models.py
@@ -2,8 +2,8 @@
 
 import django.contrib.postgres.fields
 import django.contrib.postgres.fields.jsonb
-import django.db.models.deletion
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
