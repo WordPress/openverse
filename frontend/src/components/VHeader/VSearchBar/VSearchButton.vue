dc2d11c3cafb37f7abbcc9ca36b5100fc0a89760
Olga Bulat <obulat@gmail.com>
2024-10-17 12:11:35 +0300

    Use inner ring for search button focus ring

--- frontend/src/components/VHeader/VSearchBar/VSearchButton.vue
+++ frontend/src/components/VHeader/VSearchBar/VSearchButton.vue
@@ -22,9 +22,9 @@ const { doneHydrating } = useHydrating()
     :aria-label="$t('search.search')"
     size="disabled"
     :disabled="!doneHydrating"
-    variant="plain"
+    variant="plain--avoid"
     :class="[
-      'h-full flex-shrink-0 rounded-s-none border-s-0 p-0.5px ps-1.5px focus-slim-filled hover:text-over-dark focus-visible:border-s group-focus-within:border-tx group-focus-within:bg-primary group-focus-within:text-over-dark group-focus-within:hover:bg-primary-hover group-hover:border-tx group-hover:bg-primary group-hover:text-over-dark',
+      'h-full flex-shrink-0 rounded-s-none border-1.5 p-0.5px hover:text-over-dark focus-visible:rounded-sm focus-visible:border-default focus-visible:ring focus-visible:ring-inset focus-visible:ring-[--color-bg] group-focus-within:border-tx group-focus-within:bg-primary group-focus-within:text-over-dark group-focus-within:hover:bg-primary-hover group-hover:border-tx group-hover:bg-primary group-hover:text-over-dark dark:focus-visible:ring-[--color-bg-overlay]',
       route === 'search' ? 'w-12' : 'w-14 sm:w-16',
       {
         'border-tx bg-secondary hover:bg-primary': route === 'search',
