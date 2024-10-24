649d72bf9b9779d418706ca1963c88ee84dfc3a9
Olga Bulat <obulat@gmail.com>
2024-10-24 10:11:48 +0300

    Simplify gitignore handling
    Signed-off-by: Olga Bulat <obulat@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 import { fileURLToPath } from "node:url"
-
 import path from "node:path"
+import fs from "node:fs"
 
 // eslint-disable-next-line import/no-unresolved
 import { config as defineConfig } from "typescript-eslint"
-import { includeIgnoreFile } from "@eslint/compat"
+import { convertIgnorePatternToMinimatch } from "@eslint/compat"
 import openverse from "@openverse/eslint-plugin"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -15,6 +15,36 @@ const gitignoreFiles = [
   path.resolve(__dirname, "automations", "js", ".gitignore"),
   path.resolve(__dirname, "packages", "js", "api-client", ".gitignore"),
 ]
+
+/**
+ * Vendored in from `@eslint/compat` to support multiple ignore files.
+ * Reads an ignore file and returns a list of ignore patterns.
+ * @param {string[]} ignoreFilePaths The absolute path to the ignore file.
+ * @returns {string[]} An array of ignore patterns.
+ * @throws {Error} If the ignore file path is not an absolute path.
+ */
+function mapIgnoreFiles(ignoreFilePaths) {
+  for (const ignoreFilePath of ignoreFilePaths) {
+    if (!path.isAbsolute(ignoreFilePath)) {
+      throw new Error("The ignore file location must be an absolute path.")
+    }
+  }
+
+  /** @type {string[]} */
+  const ignores = []
+  for (const ignoreFilePath of ignoreFilePaths) {
+    const ignoreFile = fs.readFileSync(ignoreFilePath, "utf8")
+    const lines = ignoreFile.split(/\r?\n/u)
+    ignores.push(
+      ...lines
+        .map((line) => line.trim())
+        .filter((line) => line && !line.startsWith("#"))
+        .map(convertIgnorePatternToMinimatch)
+    )
+  }
+
+  return ignores
+}
 
 // List of files from old .eslintignore
 const eslintIgnores = [
@@ -32,20 +62,11 @@ const eslintIgnores = [
   "packages/**/dist/**",
 ]
 
-const ignoreConfigs = gitignoreFiles
-  .map((gitignoreFile) => includeIgnoreFile(gitignoreFile))
-  .reduce(
-    (acc, gitignoreFile) => {
-      return {
-        ...acc,
-        ignores: (acc.ignores ?? []).concat(gitignoreFile.ignores ?? []),
-      }
-    },
-    { name: "openverse:ignore-files", ignores: eslintIgnores }
-  )
-
 export default defineConfig(
-  ignoreConfigs,
+  {
+    name: "openverse:ignore-files",
+    ignores: [...mapIgnoreFiles(gitignoreFiles), ...eslintIgnores],
+  },
   { files: [".pnpmfile.cjs"] },
   ...openverse.default.configs.project
 )
