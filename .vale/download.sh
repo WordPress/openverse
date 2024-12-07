5ff1c825d9e299af04d5030109dd77e8f1c35556
Dhruv Bhanushali <dhruv_b@live.com>
2024-12-07 11:04:10 +0400

    Prevent path conflicts between linting inside and outside `ov`

--- .vale/download.sh
+++ .vale/download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+BIN_NAME="$1"
 VERSION="3.7.1"
 
 if [ "$(uname)" = "Darwin" ]; then
@@ -19,4 +20,5 @@ URL="https://github.com/errata-ai/vale/releases/download/v${VERSION}/vale_${VERS
 echo "Downloading ${URL}"
 curl --output vale.tar.gz --location "${URL}"
 tar -xvzf "vale.tar.gz" vale
+mv vale "${BIN_NAME}"
 rm "vale.tar.gz"
