63218157d0bcdf4b2314ab1072797f9bd444ea9f
Dhruv Bhanushali <dhruv_b@live.com>
2024-12-07 11:39:41 +0400

    Streamline download script

--- .vale/download.sh
+++ .vale/download.sh
@@ -2,18 +2,8 @@
 
 BIN_NAME="$1"
 VERSION="3.7.1"
-
-if [ "$(uname)" = "Darwin" ]; then
-  OS="macOS"
-else
-  OS="Linux"
-fi
-
-if [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "arm64" ]; then
-  ARCH="arm64"
-else
-  ARCH="64-bit"
-fi
+OS=$([ "$(uname)" = "Darwin" ] && echo "macOS" || echo "Linux")
+ARCH=$([ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "arm64" ] && echo "arm64" || echo "64-bit")
 
 URL="https://github.com/errata-ai/vale/releases/download/v${VERSION}/vale_${VERSION}_${OS}_${ARCH}.tar.gz"
 
