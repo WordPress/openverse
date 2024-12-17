3c21810f72c31fb2dedd080ea5975b2127ded6bc
Olga Bulat <obulat@gmail.com>
2024-12-18 00:38:15 +0300

    Remove logging

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -5,13 +5,6 @@ import dotenv from "dotenv"
 // @see the section on server setup: https://nuxt.com/modules/sentry
 dotenv.config()
 
-console.log(
-  "Will initialize Sentry with dsn",
-  process.env.NUXT_PUBLIC_SENTRY_DSN
-)
-console.log("environment", process.env.NUXT_PUBLIC_SENTRY_ENVIRONMENT)
-console.log("release", process.env.SEMANTIC_VERSION)
-
 Sentry.init({
   dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NUXT_PUBLIC_SENTRY_ENVIRONMENT,
