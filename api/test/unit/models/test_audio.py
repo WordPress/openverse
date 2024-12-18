35b39e5658c07d04ba4d67c02c1d8e0e79276d53
Dhruv Bhanushali <dhruv_b@live.com>
2024-12-18 21:31:14 +0400

    Update tests

--- api/test/unit/models/test_audio.py
+++ api/test/unit/models/test_audio.py
@@ -41,28 +41,3 @@ def test_audio_waveform_caches(generate_peaks_mock, audio_fixture):
     audio_fixture.delete()
 
     assert AudioAddOn.objects.count() == 1
-
-
-@pytest.mark.django_db
-@mock.patch("api.models.audio.AudioAddOn.objects.get")
-def test_audio_waveform_sent_when_present(get_mock, audio_fixture):
-    # When ``AudioAddOn.waveform_peaks`` exists, waveform is filled
-    peaks = [0, 0.25, 0.5, 0.25, 0.1]
-    get_mock.return_value = mock.Mock(waveform_peaks=peaks)
-    assert audio_fixture.get_waveform() == peaks
-
-
-@pytest.mark.django_db
-@mock.patch("api.models.audio.AudioAddOn.objects.get")
-def test_audio_waveform_blank_when_absent(get_mock, audio_fixture):
-    # When ``AudioAddOn`` does not exist, waveform is blank
-    get_mock.side_effect = AudioAddOn.DoesNotExist()
-    assert audio_fixture.get_waveform() == []
-
-
-@pytest.mark.django_db
-@mock.patch("api.models.audio.AudioAddOn.objects.get")
-def test_audio_waveform_blank_when_none(get_mock, audio_fixture):
-    # When ``AudioAddOn.waveform_peaks`` is None, waveform is blank
-    get_mock.return_value = mock.Mock(waveform_peaks=None)
-    assert audio_fixture.get_waveform() == []
