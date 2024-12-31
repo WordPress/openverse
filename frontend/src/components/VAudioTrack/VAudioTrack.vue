467af21d4627daeaa83b75ec0d391b0fb62edfd9
Anuj <anuj01062004@gmail.com>
2024-12-31 20:53:55 +0530

    fix: only show audio message when track ID matches active

--- frontend/src/components/VAudioTrack/VAudioTrack.vue
+++ frontend/src/components/VAudioTrack/VAudioTrack.vue
@@ -296,9 +296,9 @@ watch(
 
 /* Timekeeping */
 
-const message = computed(() =>
-  activeMediaStore.message
-    ? t(`audioTrack.messages.${activeMediaStore.message}`)
+const message = computed(() => 
+  activeMediaStore.id === props.audio.id && activeMediaStore.message 
+    ? i18n.t(`audioTrack.messages.${activeMediaStore.message}`).toString() 
     : ""
 )
 
