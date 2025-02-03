fbbf7e1870109a124238d749db6410a2ad79a44e
Nirav sherasiya <sherasiyanirav@gmail.com>
2025-02-03 14:08:59 +0530

    Update parameters value

--- catalog/dags/providers/provider_reingestion_workflows.py
+++ catalog/dags/providers/provider_reingestion_workflows.py
@@ -74,8 +74,8 @@ PROVIDER_REINGESTION_WORKFLOWS = [
         dagrun_timeout=timedelta(days=7),
         daily_list_length=6,
         one_month_list_length=9,
-        three_month_list_length=18,
-        six_month_list_length=30,
+        three_month_list_length=38,
+        six_month_list_length=20,
     ),
     ProviderReingestionWorkflow(
         # 64 total reingestion days
